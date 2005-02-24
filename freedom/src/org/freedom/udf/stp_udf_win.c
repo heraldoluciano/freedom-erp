32b7e5000433e80df05b33b49dedc6a9ef559e20
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-02-24 18:35:45 +0000

    Func�o para gravar log no Firebird.

--- freedom/src/org/freedom/udf/stp_udf_win.c
+++ freedom/src/org/freedom/udf/stp_udf_win.c
@@ -1,14 +1,20 @@
 #include <unixpass.h>
 #include <stdio.h>
 char *ib_password(char *val);
-int printLog(char *val);
+char *printLog(char *val);
 char *ib_password(char *val) {
 	val = (char *)mycrypt(val,"9z");
 	val = (char *)(mycrypt(val+2,"9z")+2);
 	return (char *)val;	
 }
 
-int printLog(char *val) {
-   printf(val);
-   return 0;
+char *printLog(char *val) {
+	FILE *myfile;
+	char* fname = "fb_freedom.log";
+	myfile = fopen (fname, "w");
+	if (!myfile) 
+	  printf("File %s not open\n", fname);
+    fprintf(myfile, val);
+	fclose(myfile);
+	return (char *)val;
 }
