548d951010507e60c4cdec370696b69cdb66f35d
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-03-04 18:17:10 +0000

    Ajustes de padroniza��o.

--- trunk/freedom/lib/exec/freedompcp.sh
+++ trunk/freedom/lib/exec/freedompcp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /opt/freedom
 java -classpath jars/freedompcp.jar:jars/jcommon-0.8.7.jar:jars/jfreechart-0.9.12.jar:jars/itext-1.02b.jar:jars/firebirdsql-full.jar:lib/nachocalendar-0.21.jar -DARQINI=freedom.ini  projetos.freedompcp.FreedomPCP
+
