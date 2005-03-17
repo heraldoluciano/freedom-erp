0d3390c18f05a5189d3c683aae1d44b0f2b88f0f
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-03-17 20:05:33 +0000

    *** empty log message ***

--- freedom/src/org/freedom/modulos/std/FCredCli.java
+++ freedom/src/org/freedom/modulos/std/FCredCli.java
@@ -23,7 +23,10 @@
 package org.freedom.modulos.std;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.util.Date;
 
+import org.freedom.acao.CarregaEvent;
+import org.freedom.acao.CarregaListener;
 import org.freedom.componentes.GuardaCampo;
 import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
@@ -31,7 +34,7 @@ import org.freedom.componentes.ListaCampos;
 import org.freedom.componentes.Painel;
 import org.freedom.telas.FTabDados;
 
-public class FCredCli extends FTabDados implements ActionListener {
+public class FCredCli extends FTabDados	implements ActionListener, CarregaListener {
   private Painel pinGeral = new Painel(330, 200);
   private Painel pinFicha = new Painel(330, 200);
   private JTextFieldPad txtCodCli = new JTextFieldPad(JTextFieldPad.TP_INTEGER,5,0);
@@ -41,7 +44,7 @@ public class FCredCli extends FTabDados implements ActionListener {
   private JTextFieldFK txtDescTipoCli = new JTextFieldFK(JTextFieldPad.TP_STRING,50,0);
   private JTextFieldPad txtCodTpCred = new JTextFieldPad(JTextFieldPad.TP_INTEGER,5,0);
   private JTextFieldFK txtDescTpCred = new JTextFieldFK(JTextFieldPad.TP_STRING,50,0);
-  private JTextFieldFK txtVlrTpCred = new JTextFieldFK(JTextFieldPad.TP_DECIMAL,15,3);
+  private JTextFieldFK txtVlrTpCred = new JTextFieldFK(JTextFieldPad.TP_DECIMAL,15,2);
   private JTextFieldPad txtDtIniTr = new JTextFieldPad(JTextFieldPad.TP_DATE,10,0);
   private JTextFieldPad txtDtVencto = new JTextFieldPad(JTextFieldPad.TP_DATE,10,0);
   private JTextFieldPad txtRazCli = new JTextFieldPad(JTextFieldPad.TP_STRING, 50, 0);
@@ -61,6 +64,10 @@ public class FCredCli extends FTabDados implements ActionListener {
   private JTextFieldPad txtRamalCli = new JTextFieldPad(JTextFieldPad.TP_STRING, 8, 0);
   private JTextFieldPad txtFaxCli = new JTextFieldPad(JTextFieldPad.TP_STRING, 8, 0);
   private JTextFieldPad txtCelCli = new JTextFieldPad(JTextFieldPad.TP_STRING, 8, 0);
+  private JTextFieldPad txtNatCli = new JTextFieldPad(JTextFieldPad.TP_STRING, 30, 0);
+  private JTextFieldPad txtUFNatCli = new JTextFieldPad(JTextFieldPad.TP_STRING, 2, 0);
+  private JTextFieldPad txtTempoResCli = new JTextFieldPad(JTextFieldPad.TP_STRING, 20, 0);
+  
   
   private ListaCampos lcTipoCred = new ListaCampos(this,"TR");
   private ListaCampos lcTipoCli = new ListaCampos(this,"TI");
@@ -68,6 +75,8 @@ public class FCredCli extends FTabDados implements ActionListener {
     setTitulo("Ficha cadastral/Crédito por cliente");
     setAtribos(50, 10, 600, 520);
 
+
+    
     lcTipoCli.add(new GuardaCampo( txtCodTipoCli, "CodTipoCli", "Cód.tp.cli.", ListaCampos.DB_PK, true));
     lcTipoCli.add(new GuardaCampo( txtDescTipoCli, "DescTipoCli", "Descrição do tipo de cliente", ListaCampos.DB_SI, false));
     lcTipoCli.montaSql(false, "TIPOCLI", "VD");    
@@ -84,49 +93,61 @@ public class FCredCli extends FTabDados implements ActionListener {
     txtCodTpCred.setTabelaExterna(lcTipoCred);
 
 	setPainel(pinGeral);
+	
+	lcCampos.addCarregaListener(this);
+	
 	adicTab("Crédito", pinGeral);
-    adicCampo(txtCodCli, 7, 60, 80, 20,"CodCli","Cód.cli.", ListaCampos.DB_PK, true);
-    adicCampo(txtRazCli, 90, 60, 387, 20,"RazCli","Razão social do cliente", ListaCampos.DB_SI, false);
-    adicCampo(txtDataCli, 480, 60, 95, 20,"DataCli","Cadastro", ListaCampos.DB_SI, false);
+    adicCampo(txtCodCli, 7, 20, 70, 20,"CodCli","Cód.cli.", ListaCampos.DB_PK, true);
+    adicCampo(txtRazCli, 80, 20, 257, 20,"RazCli","Razão social do cliente", ListaCampos.DB_SI, false);
 	adicCampoInvisivel(txtCodTipoCli, "CodTipoCli","Cód.tp.cli", ListaCampos.DB_FK, txtDescTipoCli,false);
+    adicDescFK(txtDescTipoCli, 340, 20, 130, 20, "DescTipoCli", "Desc. tipo de cliente");
+    adicCampo(txtDataCli, 473, 20, 95, 20,"DataCli","Cadastro", ListaCampos.DB_SI, false);
 
-  	adicCampo(txtEndCli, 7, 100, 307, 20, "EndCli", "Endereço", ListaCampos.DB_SI, false);
-  	adicCampo(txtNumCli, 400, 100, 77, 20, "NumCli", "Num.", ListaCampos.DB_SI, false);
-  	adicCampo(txtComplCli, 480, 100, 70, 20, "ComplCli", "Compl.", ListaCampos.DB_SI, false);
-  	adicCampo(txtBairCli, 7, 140, 180, 20, "BairCli", "Bairro", ListaCampos.DB_SI, false);
-  	adicCampo(txtCidCli, 190, 140, 177, 20, "CidCli", "Cidade", ListaCampos.DB_SI, false);
-  	adicCampo(txtCepCli, 370, 140, 77, 20, "CepCli", "Cep", ListaCampos.DB_SI, false);
-  	adicCampo(txtUFCli, 450, 140, 50, 20, "UFCli", "UF", ListaCampos.DB_SI, false);
+    adicCampo(txtCodTpCred, 7, 60, 70, 20,"CodTpCred","Cód.tp.cred", ListaCampos.DB_FK, txtDescTpCred, true);
+    adicDescFK(txtDescTpCred, 80, 60, 212, 20, "DescTpCred", "Descrição do crédito");
+    adicDescFK(txtVlrTpCred, 295, 60, 107, 20, "VlrTpCred", "Valor");
+    
+    adicCampo(txtDtIniTr, 405 , 60, 80, 20,"DtIniTr","Dt.ab.créd.", ListaCampos.DB_SI, true);
+    adicCampo(txtDtVencto, 488, 60, 80, 20,"DtVenctoTr","Vencimento", ListaCampos.DB_SI,true);
+
+	
+  	adicCampo(txtEndCli, 7, 100, 330, 20, "EndCli", "Endereço", ListaCampos.DB_SI, false);
+  	adicCampo(txtNumCli, 340, 100, 77, 20, "NumCli", "Num.", ListaCampos.DB_SI, false);
+  	adicCampo(txtComplCli, 420, 100, 149, 20, "ComplCli", "Compl.", ListaCampos.DB_SI, false);
+
+  	adicCampo(txtBairCli, 7, 140, 210, 20, "BairCli", "Bairro", ListaCampos.DB_SI, false);
+  	adicCampo(txtCidCli, 220, 140, 210, 20, "CidCli", "Cidade", ListaCampos.DB_SI, false);
+  	adicCampo(txtCepCli, 433, 140, 80, 20, "CepCli", "Cep", ListaCampos.DB_SI, false);
+  	adicCampo(txtUFCli, 516, 140, 52, 20, "UFCli", "UF", ListaCampos.DB_SI, false);
+  	
   	adicCampo(txtFoneCli, 7, 180, 100, 20, "FoneCli", "Telefone", ListaCampos.DB_SI, false);
-  	adicCampo(txtRamalCli, 110, 180, 44, 20, "FaxCli", "Fax", ListaCampos.DB_SI, false); 	
+  	adicCampo(txtRamalCli, 110, 180, 44, 20, "RamalCli", "Ramal", ListaCampos.DB_SI, false); 	
   	adicCampo(txtFaxCli, 157, 180, 77, 20, "FaxCli", "Fax", ListaCampos.DB_SI, false);
-  	adicCampo(txtCelCli, 7, 220, 100, 20, "CelCli", "Celular",ListaCampos.DB_SI, false);
-	
-	
-	adicDescFK(txtDescTipoCli, 7, 300, 250, 20, "DescTipoCli", "Descrição do tipo de cliente");
-    adicCampo(txtDtIniTr, 450, 20, 90, 20,"DtIniTr","Dt.ab.créd.", ListaCampos.DB_SI, false);
-
-	
-    adicCampo(txtCodTpCred, 7, 20, 80, 20,"CodTpCred","Cód.tp.cred", ListaCampos.DB_FK, txtDescTpCred, true);
-    adicDescFK(txtDescTpCred, 90, 20, 157, 20, "DescTpCred", "Descrição do crédito");
-    adicDescFK(txtVlrTpCred, 250, 20, 87, 20, "VlrTpCred", "Valor");
-	adicCampo(txtDtVencto, 340, 20, 100, 20,"DtVenctoTr","Vencimento", ListaCampos.DB_SI, txtDescTpCred, true);
-	
-	
-	
+  	adicCampo(txtCelCli, 237, 180, 100, 20, "CelCli", "Celular",ListaCampos.DB_SI, false);
+		
+  	adicCampo(txtNatCli, 7, 220, 200, 20, "NatCli", "Naturalidade",ListaCampos.DB_SI, false);
+  	adicCampo(txtUFNatCli, 210, 220, 52, 20, "UfNatCli", "Uf Natur.",ListaCampos.DB_SI, false);
+  	adicCampo(txtTempoResCli, 265, 220, 160, 20, "TempoResCli", "Tempo de residência.",ListaCampos.DB_SI, false);
 	
 	setListaCampos( true, "CLIENTE", "VD");
 	lcCampos.setPodeIns(false);
 	lcCampos.setPodeExc(false);
     lcCampos.setQueryInsert(false);
-    
-    
+        
 	setPainel(pinFicha);
 	adicTab("Ficha cadastral", pinFicha);
-    
-    
-    
+      
   }
+  
+  public void afterCarrega(CarregaEvent cevt) {
+  
+  	if (txtDtIniTr.getVlrString().equals("")){
+  		txtDtIniTr.setVlrDate(new Date());
+  	}
+  
+  }
+  public void beforeCarrega(CarregaEvent cevt) {}
+  
   public void execShow(Connection cn) {
     lcTipoCli.setConexao(cn);
     lcTipoCred.setConexao(cn);
