c1e19414b5cab7d7ed39b4319a74e286cbf73bb6
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-03-22 13:51:04 +0000

    Altera��es em setConex�o e execShow para corrigir focus inicial das janelas.

--- trunk/freedom/src/org/freedom/modulos/cfg/DLRGrauInst.java
+++ trunk/freedom/src/org/freedom/modulos/cfg/DLRGrauInst.java
@@ -27,14 +27,16 @@ import java.util.Vector;
 import javax.swing.JLabel;
 
 import org.freedom.componentes.JRadioGroup;
-import org.freedom.telas.FDialogo;
+import org.freedom.telas.Aplicativo;
+import org.freedom.telas.FFDialogo;
 
-public class DLRGrauInst extends FDialogo {
+public class DLRGrauInst extends FFDialogo {
   private JRadioGroup rgOrdem = null;
   private JLabel lbOrdem = new JLabel("Ordenar por:");
   private Vector vLabs = new Vector();
   private Vector vVals = new Vector();
   public DLRGrauInst() {
+  	super(Aplicativo.framePrinc);
 	setTitulo("Ordem do Relatório");
 	setAtribos(300,120);
 	vLabs.addElement("Código");
