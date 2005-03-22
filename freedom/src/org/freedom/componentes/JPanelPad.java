e1dbc5e944276b7338cf064f634add05177acd51
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-03-22 20:57:55 +0000

    Altera��es relativas ao foco das janelas.

---
+++ freedom/src/org/freedom/componentes/JPanelPad.java
@@ -0,0 +1,54 @@
+/*
+ * Created on 22/03/2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.freedom.componentes;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+/**
+ * @author robson
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class JPanelPad extends JPanel {
+
+	/**
+	 * 
+	 */
+	public JPanelPad() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public JPanelPad(boolean arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public JPanelPad(LayoutManager arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public JPanelPad(LayoutManager arg0, boolean arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
