0ffe13c29bcfae889b2158b2df04c605b3952086
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-03-23 21:31:57 +0000

    Altera��es relativas ao foco das janelas.

--- trunk/freedom/src/org/freedom/componentes/JTabbedPanePad.java
+++ trunk/freedom/src/org/freedom/componentes/JTabbedPanePad.java
@@ -18,11 +18,9 @@ import javax.swing.event.ChangeListener;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
-public class JTabbedPanePad extends JTabbedPane implements ChangeListener {
+public class JTabbedPanePad extends JTabbedPane implements ChangeListener  {
 	// Flag que indica para o component procurar o primeiro campo para foco.
 	private boolean initFirstFocus = true;
-    private Component firstFocus = null;
-	
 	/**
 	 * 
 	 */
@@ -48,11 +46,6 @@ public class JTabbedPanePad extends JTabbedPane implements ChangeListener {
 		setChangedListener();
 	}
 	
-	public JTabbedPanePad(Component firstFocus) {
-		this();
-		setFirstFocus(firstFocus);
-	}
-
 	private void setChangedListener() {
 		addChangeListener(this);
 	}
@@ -66,80 +59,16 @@ public class JTabbedPanePad extends JTabbedPane implements ChangeListener {
 	}
 	
 	public void stateChanged(ChangeEvent e) {
-		//super.stateChanged(e);
-		//if (e.)
-		//firstFocus();
-		//System.out.println("Teste do change listener do TTabbedPanePad");
+		firstFocus(getSelectedComponent());
 	}
 	
-	public synchronized void firstFocus() {
-	  	if ( (firstFocus!=null) && (firstFocus.hasFocus()) && (initFirstFocus) )
-	  		firstFocus.requestFocus();
-/*		Component c = null;
-		if (initFirstFocus) {
-			c = getComponentFocus(null);
-			if (c!=null)
-				c.requestFocus();
-		} */
+	public void firstFocus(Component c) {
+	  	if ( (initFirstFocus) && (c!=null) ) {
+	  		if (c instanceof JPanelPad) {
+	  		//	System.out.println("Entrou no first focus do tabbed");
+	  			( (JPanelPad) c).firstFocus();
+	  		}
+		}
 	}
 
-    public synchronized void setFirstFocus(Component firstFocus) {
-	  	this.firstFocus = firstFocus;
-	}
-	
-/*	public synchronized Component getComponentFocus(Component cf) {
-		Component c = null;
-		//String nome = "";
-		if (cf==null) {
-			for (int i=0; i<getComponentCount(); i++) {
-				cf = getComponent(i);
-				if (cf!=null) {
-					//nome = cf.getName();
-					//System.out.println(nome);
-					if (cf instanceof Container) {
-						c = getContainerFocus( (Container) cf);
-						if (c!=null) {
-						//	nome = c.getName();
-							//System.out.println(nome);
-							break;
-						}
-					}
-					else {
-						if (cf.hasFocus()) {
-							c = cf;
-							break;
-						}
-					}
-				}
-				c = null;
-			}
-		}
-		else {
-			if (cf instanceof Container) {
-				c = getContainerFocus((Container) cf);
-			}
-			else if (cf.hasFocus())
-				c = cf;
-		}
-		return c;
-	} */
-/*	private synchronized Component getContainerFocus(Container cf) {
-		Component c = null;
-		if (cf != null) {
-			for (int i=0; i<cf.getComponentCount(); i++) {
-				c = cf.getComponent(i);
-				if (c!=null) {
-					if (c instanceof Container) {
-						c = getContainerFocus((Container) c);
-						break;
-					}
-					else if (c.hasFocus())
-						break;
-					else
-						c = null;
-				}
-			}
-		}
-		return c;
-	}*/
 }
