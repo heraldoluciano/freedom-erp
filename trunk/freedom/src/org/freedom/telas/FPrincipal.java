0c9b6444306430047d81e65322c8016deb3e85fd
arlm <arlm@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-04-04 19:31:10 +0000

    Corre��es TemTela

--- trunk/freedom/src/org/freedom/telas/FPrincipal.java
+++ trunk/freedom/src/org/freedom/telas/FPrincipal.java
@@ -1,24 +1,31 @@
 /**
  * @version 14/11/2000 <BR>
  * @author Setpoint Informática Ltda./Fernando Oliveira da Silva <BR>
- *
+ * 
  * Projeto: Freedom <BR>
  * Pacote: org.freedom.telas <BR>
- * Classe: @(#)FPrincipal.java <BR>
+ * Classe:
+ * @(#)FPrincipal.java <BR>
  * 
- * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para
+ * Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
- * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
- * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste
+ * Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você
+ * pode contatar <BR>
  * o LICENCIADOR ou então pegar uma cópia em: <BR>
  * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
- * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
- * de acordo com os termos da LPG-PC <BR> <BR>
- *
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é
+ * preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
  * Comentários para a classe...
  */
 
 package org.freedom.telas;
+
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
@@ -50,267 +57,273 @@ import org.freedom.funcoes.Funcoes;
 import org.freedom.modulos.atd.FAgenda;
 
 public class FPrincipal extends JFrame implements ActionListener {
-   
-   private Connection con = null;
-   public  JMenuBar bar = new JMenuBar();
-   private JToolBar tBar = new JToolBar();
-// public  JMenuPad arquivoMenu = new JMenuPad();
-   private JMenuItem sairMI = new JMenuItem();
-   private Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
-   private JButton btCalc = new JButton(Icone.novo("btCalc.gif"));
-   private JButton btAgenda = new JButton(Icone.novo("btAgenda.gif"));
-   public JPanelPad pinBotoesDir = new JPanelPad();
-   public  Container c = getContentPane();
-   public  JDesktopPane dpArea = new JDesktopPane();
-   public  StatusBar statusBar = new StatusBar();
-   public  FPrincipal() {
-     c.setLayout(new BorderLayout());      
 
-     JPanelPad pn = new JPanelPad(JPanelPad.TP_JPANEL);
-     pn.setLayout(new GridLayout( 1, 1));
+	private Connection con = null;
 
-     setJMenuBar( bar );
+	public JMenuBar bar = new JMenuBar();
 
-     sairMI.setText("Sair");
-     sairMI.setMnemonic('r');
-     
-     c.add(pn);
-          
-     btCalc.setPreferredSize(new Dimension(34,34));
-     btCalc.setToolTipText("Calculadora");
-     btCalc.addActionListener(this);
-     
-     btAgenda.setPreferredSize(new Dimension(34,34));
-     btAgenda.setToolTipText("Agenda");
-     btAgenda.addActionListener(this);
-     pinBotoesDir.setBorder(null);
-     c.add(tBar, BorderLayout.NORTH);
-     tBar.setLayout(new BorderLayout());
-     pinBotoesDir.setPreferredSize(new Dimension(102,34));
-     tBar.add(pinBotoesDir,BorderLayout.EAST);
-     
-     pinBotoesDir.add(btCalc);
-     pinBotoesDir.add(btAgenda); 	
-      
-     montaStatus();
-     
-     setSize((int)tela.getWidth() , (int)tela.getHeight()-50);
+	private JToolBar tBar = new JToolBar();
 
-     setExtendedState(MAXIMIZED_BOTH);
-     c.add( dpArea, BorderLayout.CENTER);
-     sairMI.addActionListener(
-        new ActionListener() {
-           public void actionPerformed( ActionEvent e ) {
-               fecharJanela();
-           }
-        }
-     );
-     addWindowListener(
-       new WindowAdapter() {
-         public void windowClosing( WindowEvent e ) {
-           fecharJanela();
-         }
-       }
-     );
-    
-   }
+	// public JMenuPad arquivoMenu = new JMenuPad();
+	private JMenuItem sairMI = new JMenuItem();
 
-   public void addKeyListerExterno(KeyListener arg0) {
-   	 this.addKeyListener(arg0);
-   	 btCalc.addKeyListener(arg0);
-   	 bar.addKeyListener(arg0);
-   	 tBar.addKeyListener(arg0);
-   	 btAgenda.addKeyListener(arg0);
-   }
-     
-   public void montaStatus() {
-     c.add(statusBar, BorderLayout.SOUTH);
-   }
-   
-   public void remConFilial() {
-   	 String sSQL = "EXECUTE PROCEDURE SGFIMCONSP(?,?,?)";
-   	 try {
-   	 	PreparedStatement ps = con.prepareStatement(sSQL);
-   	 	ps.setInt(1,Aplicativo.iCodEmp);
-   	 	ps.setInt(2,Aplicativo.iCodFilialPad);
-   	 	ps.setString(3,Aplicativo.strUsuario);
-   	 	ps.execute();
-   	 	ps.close();
-   	 	if (!con.getAutoCommit())
-   	 		con.commit();
-   	 }
-   	 catch(SQLException err) {
-		Funcoes.mensagemErro(this,"Erro ao remover filial ativa no banco!\n"+err.getMessage());
-   	 }
-   }
-   
-   public void setConexao(Connection conGeral) {
-      con = conGeral;
-   }
-   public void fecharJanela() {
-     if ( con != null  ) {
-       try {
-		 remConFilial();
-         con.close();
-       }
-       catch (java.sql.SQLException e) {
-          Funcoes.mensagemErro( this,"Não foi possível fechar a conexao com o banco de dados!");
-       }
-     }
-     System.exit(0);
-   }
+	private Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
 
-   public void adicFilha(Container filha) {
-     dpArea.add(filha);
-   }
-   public void adicMenu(JMenuPad menu) {
-     bar.add(menu);           
-   }
+	private JButton btCalc = new JButton(Icone.novo("btCalc.gif"));
 
-/*   public void adicItemArq(JMenuItemPad menu) {
-     arquivoMenu.add(menu);
-   }
+	private JButton btAgenda = new JButton(Icone.novo("btAgenda.gif"));
 
-   public void setMenu() {
-     arquivoMenu.addSeparator();
-     arquivoMenu.add(sairMI);
- 
-   }     
-   public void tiraEmp() {
-     arquivoMenu.remove(0);     
-   }*/
-   public void actionPerformed( ActionEvent evt ) {
-     if (evt.getSource() == btCalc) {
-       Calc calc = new Calc();
-       dpArea.add("Calc",calc);
-       calc.show();
-     }
-     else if (evt.getSource() == btAgenda) {
-        if (this.temTela("Agenda") == false) {
-			FAgenda tela = new FAgenda();
-			this.criatela("Agenda",tela,con);
-        }     	     	
-     }
+	public JPanelPad pinBotoesDir = new JPanelPad();
 
-     }
+	public Container c = getContentPane();
 
-   
-   public boolean temTela(String nome) {
-     boolean retorno = false;
-     int i = 0;
-     
-     JInternalFrame[] telas = dpArea.getAllFrames();
-     JInternalFrame tela = null;
+	public JDesktopPane dpArea = new JDesktopPane();
 
-     while (true) {
+	public StatusBar statusBar = new StatusBar();
 
-        try {
-            tela = telas[i];
-        }
-        catch (java.lang.Exception e) {
-           break;
-        }
-        
-        if (tela == null) {
-           break;
-        } 
-		else if (tela.getName() == null) {
-		   break;
-		} 
-        else if (tela.getName().equals(nome)) {
-	    try {
-		      tela.setSelected(true);
-    	}
-    	catch(Exception e) { }
-        retorno = true;
-        break;
-    }
-        
-        i++; 
-    }
-     
-     return retorno;
-     
-  }
+	public FPrincipal() {
+		c.setLayout(new BorderLayout());
 
-  public JInternalFrame getTela(String nome) {
-	JInternalFrame retorno = null;
-     
-	JInternalFrame[] telas = dpArea.getAllFrames();
-	JInternalFrame tela = null;
-	
-    
-	for (int i=0; i<telas.length; i++) {
+		JPanelPad pn = new JPanelPad(JPanelPad.TP_JPANEL);
+		pn.setLayout(new GridLayout(1, 1));
 
-	   try {
-		   tela = telas[i];
-	   }
-	   catch (java.lang.Exception e) {
-		  break;
-	   }
-        
-	   if (tela == null) {
-		  break;
-	   } 
-	   else if (tela.getName() == null) {
-		  break;
-	   } 
-	   else if (tela.getName().equals(nome)) {
-	     try {
-			 retorno = tela;
-			 break;
-	     }
-	     catch(Exception e) { 
-	       retorno = null;
-	       break;
-	     }
-       }
-    }
-     
-	return retorno;
-     
- }
-  
-  public void criatela(String nome, FFilho comp, Connection cn) {
-    comp.setName(nome);
-    dpArea.add(nome,comp);
-    comp.setConexao(cn);
-    comp.execShow();
-    try {
-      comp.setSelected(true);
-    }
-    catch(Exception e) { } 
-  }
-  public void criatela(String nome, FDialogo comp, Connection cn) {
-    comp.setName(nome);
-    //dpArea.add(nome,comp); 
-    comp.setConexao(cn);
-    comp.setVisible(true);
-  }
+		setJMenuBar(bar);
 
-	/**
-	*  Ajusta a identificação do sistema. <BR>
-	*  @param sDesc - Descrição do sistema.
-	*  @param iCod - Código do sistema.
-	*  @param iMod - Código do módulo.
-	*
-	*/
+		sairMI.setText("Sair");
+		sairMI.setMnemonic('r');
 
-	public void setIdent(String sDesc, int iCod, int iMod) {
-		setTitle(sDesc);
-/*		arquivoMenu.setCodSistema(iCod);
-		arquivoMenu.setCodModulo(iMod);
-		arquivoMenu.setCodMenu(100000000);
-		arquivoMenu.setNivel(0);*/
+		c.add(pn);
+
+		btCalc.setPreferredSize(new Dimension(34, 34));
+		btCalc.setToolTipText("Calculadora");
+		btCalc.addActionListener(this);
+
+		btAgenda.setPreferredSize(new Dimension(34, 34));
+		btAgenda.setToolTipText("Agenda");
+		btAgenda.addActionListener(this);
+		pinBotoesDir.setBorder(null);
+		c.add(tBar, BorderLayout.NORTH);
+		tBar.setLayout(new BorderLayout());
+		pinBotoesDir.setPreferredSize(new Dimension(102, 34));
+		tBar.add(pinBotoesDir, BorderLayout.EAST);
+
+		pinBotoesDir.add(btCalc);
+		pinBotoesDir.add(btAgenda);
+
+		montaStatus();
+
+		setSize((int) tela.getWidth(), (int) tela.getHeight() - 50);
+
+		setExtendedState(MAXIMIZED_BOTH);
+		c.add(dpArea, BorderLayout.CENTER);
+		sairMI.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fecharJanela();
+			}
+		});
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				fecharJanela();
+			}
+		});
+
+	}
+
+	public void addKeyListerExterno(KeyListener arg0) {
+		this.addKeyListener(arg0);
+		btCalc.addKeyListener(arg0);
+		bar.addKeyListener(arg0);
+		tBar.addKeyListener(arg0);
+		btAgenda.addKeyListener(arg0);
+	}
+
+	public void montaStatus() {
+		c.add(statusBar, BorderLayout.SOUTH);
+	}
+
+	public void remConFilial() {
+		String sSQL = "EXECUTE PROCEDURE SGFIMCONSP(?,?,?)";
+		try {
+			PreparedStatement ps = con.prepareStatement(sSQL);
+			ps.setInt(1, Aplicativo.iCodEmp);
+			ps.setInt(2, Aplicativo.iCodFilialPad);
+			ps.setString(3, Aplicativo.strUsuario);
+			ps.execute();
+			ps.close();
+			if (!con.getAutoCommit())
+				con.commit();
+		} catch (SQLException err) {
+			Funcoes.mensagemErro(this,
+					"Erro ao remover filial ativa no banco!\n"
+							+ err.getMessage());
+		}
+	}
+
+	public void setConexao(Connection conGeral) {
+		con = conGeral;
+	}
+
+	public void fecharJanela() {
+		if (con != null) {
+			try {
+				remConFilial();
+				con.close();
+			} catch (java.sql.SQLException e) {
+				Funcoes
+						.mensagemErro(this,
+								"Não foi possível fechar a conexao com o banco de dados!");
+			}
+		}
+		System.exit(0);
+	}
+
+	public void adicFilha(Container filha) {
+		dpArea.add(filha);
+	}
+
+	public void adicMenu(JMenuPad menu) {
+		bar.add(menu);
+	}
+
+	/*
+	 * public void adicItemArq(JMenuItemPad menu) { arquivoMenu.add(menu); }
+	 * 
+	 * public void setMenu() { arquivoMenu.addSeparator();
+	 * arquivoMenu.add(sairMI);
+	 *  } public void tiraEmp() { arquivoMenu.remove(0); }
+	 */
+	public void actionPerformed(ActionEvent evt) {
+		if (evt.getSource() == btCalc) {
+			Calc calc = new Calc();
+			dpArea.add("Calc", calc);
+			calc.show();
+		} else if (evt.getSource() == btAgenda) {
+			if (this.temTela("Agenda") == false) {
+				FAgenda tela = new FAgenda();
+				this.criatela("Agenda", tela, con);
+			}
+		}
+
+	}
+
+	public boolean temTela(String nome) {
+		boolean retorno = false;
+		int i = 0;
+
+		JInternalFrame[] telas = dpArea.getAllFrames();
+		JInternalFrame tela = null;
+
+		while (true) {
+
+			try {
+				tela = telas[i];
+			} catch (java.lang.Exception e) {
+				break;
+			}
+
+			if (tela == null) {
+				break;
+			} else if (tela.getName() == null) {
+				i++;
+				continue;
+			} else if (tela.getName().equals(nome)) {
+				try {
+					tela.setSelected(true);
+				} catch (Exception e) {
+				}
+				retorno = true;
+				break;
+			}
+
+			i++;
+		}
+
+		return retorno;
+
+	}
+
+	public JInternalFrame getTela(String nome) {
+		JInternalFrame retorno = null;
+
+		JInternalFrame[] telas = dpArea.getAllFrames();
+		JInternalFrame tela = null;
+
+		for (int i = 0; i < telas.length; i++) {
+
+			try {
+				tela = telas[i];
+			} catch (java.lang.Exception e) {
+				break;
+			}
+
+			if (tela == null) {
+				break;
+			} else if (tela.getName() == null) {
+				break;
+			} else if (tela.getName().equals(nome)) {
+				try {
+					retorno = tela;
+					break;
+				} catch (Exception e) {
+					retorno = null;
+					break;
+				}
+			}
+		}
+
+		return retorno;
+
+	}
+
+	public void criatela(String nome, FFilho comp, Connection cn) {
+		comp.setName(nome);
+		comp.setTitulo(nome);
+		dpArea.add(nome, comp);
+		comp.setConexao(cn);
+		comp.execShow();
+		try {
+			comp.setSelected(true);
+		} catch (Exception e) {
+		}
+	}
+
+	public void criatela(String nome, FDialogo comp, Connection cn) {
+		comp.setName(nome);
+		comp.setTitulo(nome);
+		//dpArea.add(nome,comp);
+		comp.setConexao(cn);
+		comp.setVisible(true);
 	}
 
 	/**
-	*  Adiciona um componente na barra de ferramentas. <BR>
-	*  @param comp - Componente a ser adicionado.
-	*
-	*/
+	 * Ajusta a identificação do sistema. <BR>
+	 * 
+	 * @param sDesc -
+	 *            Descrição do sistema.
+	 * @param iCod -
+	 *            Código do sistema.
+	 * @param iMod -
+	 *            Código do módulo.
+	 *  
+	 */
+
+	public void setIdent(String sDesc, int iCod, int iMod) {
+		setTitle(sDesc);
+		/*
+		 * arquivoMenu.setCodSistema(iCod); arquivoMenu.setCodModulo(iMod);
+		 * arquivoMenu.setCodMenu(100000000); arquivoMenu.setNivel(0);
+		 */
+	}
+
+	/**
+	 * Adiciona um componente na barra de ferramentas. <BR>
+	 * 
+	 * @param comp -
+	 *            Componente a ser adicionado.
+	 *  
+	 */
 
 	public void adicCompInBar(Component comp, String sAling) {
-		tBar.add(comp,sAling);
+		tBar.add(comp, sAling);
 	}
 }
