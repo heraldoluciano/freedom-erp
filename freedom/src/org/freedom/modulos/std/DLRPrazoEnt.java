668eb46c9ade735dbdb8120cb3bf3b5557a756f3
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-05-20 13:56:04 +0000

    *** empty log message ***

---
+++ freedom/src/org/freedom/modulos/std/DLRPrazoEnt.java
@@ -0,0 +1,54 @@
+/**
+ * @version 14/07/2003 <BR>
+ * @author Setpoint Informática Ltda./Alex Rodrigues <BR>
+ *
+ * Projeto: Freedom <BR>
+ *  
+ * Pacote: org.freedom.modulos.std <BR>
+ * Classe: @(#)DLRTipoPag.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR> <BR>
+ *
+ * Comentários sobre a classe...
+ */
+
+package org.freedom.modulos.std;
+import org.freedom.componentes.JLabelPad;
+
+import org.freedom.componentes.JRadioGroup;
+import org.freedom.telas.FFDialogo;
+
+import java.util.Vector;
+public class DLRPrazoEnt extends FFDialogo {
+  private JRadioGroup rgOrdem = null;
+  private JLabelPad lbOrdem = new JLabelPad("Ordenar por:");
+  private Vector vLabs = new Vector();
+  private Vector vVals = new Vector();
+  public DLRPrazoEnt() {
+    setTitulo("Ordem do Relatório");
+    setAtribos(300,140);
+    vLabs.addElement("Código");
+    vLabs.addElement("Descrição");
+    vVals.addElement("C");
+    vVals.addElement("D");
+    rgOrdem = new JRadioGroup(1,2,vLabs,vVals);
+    rgOrdem.setVlrString("D");
+    adic(lbOrdem,7,0,80,15);
+    adic(rgOrdem,7,20,280,30);
+  }
+  public String getValor() {
+    String sRetorno = "";
+    if (rgOrdem.getVlrString().compareTo("C") == 0 )
+      sRetorno = "CODPE";
+    else if (rgOrdem.getVlrString().compareTo("D") == 0 )
+      sRetorno = "DESCPE";
+    return sRetorno;
+  }
+}
