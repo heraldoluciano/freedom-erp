7a7a81ee72fbf65bf86744e45b377e36586da020
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-05-24 20:46:41 +0000

    Acerto nos menus e bot�es.

--- freedom/src/org/freedom/modulos/pcp/FreedomPCP.java
+++ freedom/src/org/freedom/modulos/pcp/FreedomPCP.java
@@ -44,54 +44,34 @@ public class FreedomPCP extends Aplicativo implements ActionListener {
 
 		super(
 				"iconAtendimento32.gif", "splashPCP.jpg", 1, "Freedom", 5, "Planejamento e Controle da Produção", "freedom.ini", null);
-		addOpcao(-1, TP_OPCAO_MENU, "Arquivo", "", 'A', 100000000, 0, false,
-				null);
-		addOpcao(100000000, TP_OPCAO_MENU, "Tabelas", "", 'T', 100100000, 1,
-				false, null);
-		addOpcao(100000000, TP_OPCAO_MENU, "Preferências", "", 'F', 110100000,
-				1, false, null);
-		addOpcao(100100000, TP_OPCAO_MENU, "Produtos", "", 'P', 100101000, 2,
-				false, null);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Classificação fiscal",
-				"Classificação Fiscal", 'l', 100120020, 3, true,
-				FCLFiscal.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Almoxarifado", "Almoxarifado", 'x',
-				100120030, 3, true, FAlmox.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Grupo", "Grupo", 'r', 100120040, 3,
-				true, FGrupo.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Marca", "Marca", 'c', 100120050, 3,
-				true, FMarca.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Unidade", "Unidade", 'U',
-				100120060, 3, true, FUnidade.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Produto", "Produto", 'P',
-				100120070, 3, true, FProduto.class);
+		addOpcao(-1, TP_OPCAO_MENU, "Arquivo", "", 'A', 100000000, 0, false,null);
+			addOpcao(100000000, TP_OPCAO_MENU, "Tabelas", "", 'T', 100100000, 1,false, null);			
+				addOpcao(100100000, TP_OPCAO_MENU, "Produtos", "", 'P', 100101000, 2,false, null);
+					addOpcao(100101000, TP_OPCAO_ITEM, "Classificação fiscal","Classificação Fiscal", 'l', 100120020, 3, true,FCLFiscal.class);
+					addOpcao(100101000, TP_OPCAO_ITEM, "Almoxarifado", "Almoxarifado", 'x',100120030, 3, true, FAlmox.class);
+					addOpcao(100101000, TP_OPCAO_ITEM, "Grupo", "Grupo", 'r', 100120040, 3,true, FGrupo.class);
+					addOpcao(100101000, TP_OPCAO_ITEM, "Marca", "Marca", 'c', 100120050, 3,true, FMarca.class);
+					addOpcao(100101000, TP_OPCAO_ITEM, "Unidade", "Unidade", 'U',100120060, 3, true, FUnidade.class);
+					addOpcao(100101000, TP_OPCAO_ITEM, "Produto", "Produto", 'P',100120070, 3, true, FProduto.class);
 
-		addSeparador(100100000);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Estrutura", "Estrutura de produto",
-				'E', 100102000, 2, true, FEstrutura.class);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Tipos de recursos",
-				"Tipos de recursos", 'T', 100103000, 2, true, FTipoRec.class);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Recursos de produção",
-				"Recursos de produção", 'R', 100104000, 2, true,
-				FRecursos.class);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Fases de produção",
-				"Fases de produção", 'F', 100105000, 2, true, FFase.class);
+					addSeparador(100100000);
+		
+				addOpcao(100100000, TP_OPCAO_MENU, "PCP","PCP", 'C', 100102000, 1, false, null);
+					addOpcao(100102000, TP_OPCAO_ITEM, "Tipos de recursos","Tipos de recursos", 'T', 100102010, 2, true, FTipoRec.class);
+					addOpcao(100102000, TP_OPCAO_ITEM, "Recursos de produção","Recursos de produção", 'R', 100102020, 2, true,FRecursos.class);
+					addOpcao(100102000, TP_OPCAO_ITEM, "Fases de produção","Fases de produção", 'F', 100102030, 2, true, FFase.class);
+					addOpcao(100102000, TP_OPCAO_ITEM, "Estrutura", "Estrutura de produto",'E', 100102040, 2, true, FEstrutura.class);
+		
+		    addOpcao(100000000, TP_OPCAO_MENU, "Preferências", "", 'F', 110100000,1, false, null);
+				addOpcao(110100000, TP_OPCAO_ITEM, "Preferências gerais","Preferências gerais", 'G', 110105000, 2, true,FPrefereProd.class);
+			
+		addOpcao(-1, TP_OPCAO_MENU, "Produção", "", 'P', 200000000, 0, false,null);
+			addOpcao(200000000, TP_OPCAO_ITEM, "Ordens de produção","Ordens de produção", 'O', 200100000, 1, true, FOP.class);
 
-		addOpcao(110100000, TP_OPCAO_ITEM, "Preferências gerais",
-				"Preferências gerais", 'G', 110101000, 2, true,
-				FPrefereProd.class);
+		addBotao("btEstProduto.gif", "Estrutura de produto","Estrutura de produto", 100102040, FEstrutura.class);
+		addBotao("btProduto.gif", "Produtos", "Produto", 100101000,FProduto.class);
 
-		addOpcao(-1, TP_OPCAO_MENU, "Produção", "", 'P', 200000000, 0, false,
-				null);
-		addOpcao(200000000, TP_OPCAO_ITEM, "Ordens de produção",
-				"Ordens de produção", 'O', 200100000, 1, true, FOP.class);
-
-		addBotao("btEstProduto.gif", "Estrutura de produto",
-				"Estrutura de produto", 100102000, FPrefereProd.class);
-		addBotao("btProduto.gif", "Produtos", "Produto", 100101000,
-				FProduto.class);
-
-		ajustaMenu();
+		ajustaMenu(); 
 		
 		sNomeModulo = "Produção";
 		sMailSuporte = "suporte@stpinf.com";
