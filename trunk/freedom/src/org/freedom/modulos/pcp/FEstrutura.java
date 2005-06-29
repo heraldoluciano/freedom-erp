b9fd962f8e9c0a9de95f8a8d18bd9363299eadef
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-06-29 15:04:34 +0000

    implanta��o dos relatorios

--- trunk/freedom/src/org/freedom/modulos/pcp/FEstrutura.java
+++ trunk/freedom/src/org/freedom/modulos/pcp/FEstrutura.java
@@ -21,21 +21,30 @@
  */ 
 
 package org.freedom.modulos.pcp;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.swing.JButton;
+
 import org.freedom.acao.CarregaEvent;
 import org.freedom.acao.CarregaListener;
 import org.freedom.acao.PostEvent;
 import org.freedom.acao.PostListener;
 import org.freedom.bmps.Icone;
 import org.freedom.componentes.GuardaCampo;
+import org.freedom.componentes.ImprimeOS;
 import org.freedom.componentes.JCheckBoxPad;
+import org.freedom.componentes.JPanelPad;
 import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
-import org.freedom.componentes.JPanelPad;
+import org.freedom.funcoes.Funcoes;
 import org.freedom.telas.FDetalhe;
 
 public class FEstrutura extends FDetalhe implements ActionListener, CarregaListener, PostListener{
@@ -72,6 +81,12 @@ public class FEstrutura extends FDetalhe implements ActionListener, CarregaListe
     setTitulo("Estrutura de produtos");
     setAtribos( 50, 20, 568, 460);
     setAltCab(170);
+    
+    pnGImp.removeAll();
+	pnGImp.setLayout(new GridLayout(1, 2));
+	pnGImp.setPreferredSize(new Dimension(100, 26));
+	pnGImp.add(btPrevimp);
+	pnGImp.add(btImp);
     
 	btFase.setToolTipText("Fases da produção");
     
@@ -170,10 +185,226 @@ public class FEstrutura extends FDetalhe implements ActionListener, CarregaListe
     tab.setTamColuna(50,0);
     tab.setTamColuna(150,2);
     tab.setTamColuna(150,5);
-    
-    
+        
     cbRmaAutoItEst.setEnabled(false);
     
+    btImp.addActionListener(this);
+	btPrevimp.addActionListener(this);
+	
+	setImprimir(true);
+    
+  }
+  private void imprimir(boolean bVisualizar) {
+    ImprimeOS imp = new ImprimeOS("",con);
+    int linPag = imp.verifLinPag()-1;
+    int iTot = 0;
+    String[] sValores;
+    String sWhere = "";
+    imp.montaCab();
+    imp.setTitulo("Relatório de Estrutura do Produto");
+   
+    DLREstrutura dl = new DLREstrutura();
+    dl.setVisible(true);
+    if (dl.OK == false) {
+      dl.dispose();
+      return;
+    }
+    sValores = dl.getValores();
+	dl.dispose();
+    
+	if (sValores[2].equals("A")) {
+		sWhere += " AND E.CODPROD="+txtCodProd.getVlrString();
+		sWhere += " AND E.SEQEST="+txtSeqEst.getVlrString();
+	}
+    
+    if (sValores[1].equals("R")){
+    	
+    	String sSQL = "SELECT E.CODPROD, PD.DESCPROD, E.SEQEST, E.QTDEST, E.DESCEST, E.ATIVOEST, E.CODMODLOTE, E.NRODIASVALID" +
+					  " FROM PPESTRUTURA E, EQPRODUTO PD " +
+					  " WHERE PD.CODEMP=E.CODEMP AND PD.CODFILIAL=E.CODFILIAL AND E.CODPROD=PD.CODPROD " +
+					    sWhere+
+					  " ORDER BY "+sValores[0];
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+		
+		ps = con.prepareStatement(sSQL);
+		rs = ps.executeQuery();
+		  
+		imp.limpaPags();
+		
+		while ( rs.next() ) {
+		   if (imp.pRow()==0) {
+		      imp.impCab(136, true);
+		      imp.say(imp.pRow()+0,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"|"+Funcoes.replicate("-",133)+"|");
+		      imp.say(imp.pRow()+1,0,"| Cód.prod.");
+		      imp.say(imp.pRow()+0,13,"| Descrição do produto");
+		      imp.say(imp.pRow()+0,50,"| Seq.Est.");
+		      imp.say(imp.pRow()+0,60,"| Qtd.");
+		      imp.say(imp.pRow()+0,70,"| Descrição");
+		      imp.say(imp.pRow()+0,101,"| Ativo");
+		      imp.say(imp.pRow()+0,110,"| Mod.Lote");
+		      imp.say(imp.pRow()+0,121,"| Valid");
+		      imp.say(imp.pRow()+0,135,"|");
+		   }
+		   	  
+		   	  imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"|"+Funcoes.replicate("-",133)+"|");
+		   	  imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"| " + rs.getString("CodProd"));
+		      imp.say(imp.pRow()+0,13,"| " + rs.getString("DescProd").substring(0,34));
+		      imp.say(imp.pRow()+0,50,"| " + rs.getString("SeqEst"));
+		      imp.say(imp.pRow()+0,60,"| " + rs.getString("QtdEst"));
+		      imp.say(imp.pRow()+0,70,"| " + rs.getString("DescEst").substring(0,29));
+		      imp.say(imp.pRow()+0,100,"| " + (rs.getString("AtivoEst").equals("S") ? "Sim" : "Não"));
+		      imp.say(imp.pRow()+0,110,"| " + rs.getString("CodModLote"));
+		      imp.say(imp.pRow()+0,121,"| " + rs.getString("NroDiasValid")+" Dias");
+		      imp.say(imp.pRow()+0,135,"|");
+		      
+		   
+		   if (imp.pRow()>=linPag) {
+			      imp.say(imp.pRow()+1,0,""+imp.comprimido());
+			      imp.say(imp.pRow()+0,0,"+"+Funcoes.replicate("=",133)+"+");
+			      imp.incPags();
+			      imp.eject();
+		   }
+		}
+		imp.say(imp.pRow()+1,0,""+imp.comprimido());
+	    imp.say(imp.pRow()+0,0,"+"+Funcoes.replicate("=",133)+"+");
+		
+	    imp.eject();
+		
+		imp.fechaGravacao();
+		
+		if (!con.getAutoCommit())
+			con.commit();
+		dl.dispose();
+		}  
+		catch ( SQLException err ) {
+		Funcoes.mensagemErro(this,"Erro consulta tabela de estrutura do produto!\n"+err.getMessage(),true,con,err);      
+		}
+    }
+    else if (sValores[1].equals("C")){
+    	
+    	String sCodProd = "";
+    	String sSQL = "SELECT E.CODPROD, PD.DESCPROD, E.SEQEST, E.QTDEST, E.DESCEST, E.ATIVOEST," +
+    				  " E.CODMODLOTE, E.NRODIASVALID, IT.SEQITEST, IT.CODPRODPD, PI.DESCPROD, IT.QTDITEST," +
+    				  " IT.CODFASE, F.DESCFASE, IT.RMAAUTOITEST" +
+					  " FROM PPESTRUTURA E, PPITESTRUTURA IT, EQPRODUTO PD, EQPRODUTO PI, PPFASE F " +
+					  " WHERE E.CODPROD=PD.CODPROD AND E.CODEMP=PD.CODEMP AND E.CODFILIAL=PD.CODFILIAL" +
+					  " AND IT.CODPROD=E.CODPROD AND IT.SEQEST=E.SEQEST AND IT.CODEMP=E.CODEMP AND IT.CODFILIAL=E.CODFILIAL" +
+					  " AND IT.CODPRODPD=PI.CODPROD AND IT.CODEMPPD=PI.CODEMP AND IT.CODFILIALPD=PI.CODFILIAL" +
+					  " AND IT.CODFASE=F.CODFASE AND IT.CODEMPFS=F.CODEMP AND IT.CODFILIALFS=F.CODFILIAL" +
+					  sWhere+
+					  " ORDER BY "+sValores[0]+", IT.CODPROD, IT.CODFASE";
+					 
+					  
+    	System.out.println(sSQL);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+		
+		ps = con.prepareStatement(sSQL);
+		rs = ps.executeQuery();
+		
+		sCodProd = txtCodProd.getVlrString();		
+		int cont = 0;
+		imp.limpaPags();
+		
+		while ( rs.next() ) {
+		   if (imp.pRow()==0) {
+		      imp.impCab(136, true);
+		      imp.say(imp.pRow()+ 0,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"|"+Funcoes.replicate(" ",133)+"|");
+		      
+		   }
+		   if (!sCodProd.equals(rs.getString(1))){
+	   		cont=0;
+	   		sCodProd = rs.getString(1);
+		   }
+		   if (sCodProd.equals(rs.getString(1)) && cont==0){
+		      imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"|"+Funcoes.replicate("=",133)+"|");
+		      imp.say(imp.pRow()+1,0,"| Cód.prod.");
+		      imp.say(imp.pRow()+0,13,"| Descrição do produto");
+		      imp.say(imp.pRow()+0,50,"| Seq.Est.");
+		      imp.say(imp.pRow()+0,60,"| Qtd.");
+		      imp.say(imp.pRow()+0,70,"| Descrição");
+		      imp.say(imp.pRow()+0,101,"| Ativo");
+		      imp.say(imp.pRow()+0,110,"| Mod.Lote");
+		      imp.say(imp.pRow()+0,121,"| Valid");
+		      imp.say(imp.pRow()+0,135,"|");		      
+		      imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"|"+Funcoes.replicate("=",133)+"|");
+		      
+		   	  imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"| " + rs.getString(1));
+		      imp.say(imp.pRow()+0,13,"| " + rs.getString(2).substring(0,34));
+		      imp.say(imp.pRow()+0,50,"| " + rs.getString(3));
+		      imp.say(imp.pRow()+0,60,"| " + rs.getString(4));
+		      imp.say(imp.pRow()+0,70,"| " + rs.getString(5).substring(0,29));
+		      imp.say(imp.pRow()+0,100,"| " + (rs.getString(6).equals("S") ? "Sim" : "Não"));
+		      imp.say(imp.pRow()+0,110,"| " + rs.getString(7));
+		      imp.say(imp.pRow()+0,121,"| " + rs.getString(8)+" Dias");
+		      imp.say(imp.pRow()+0,135,"|");
+		      imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"|"+Funcoes.replicate("-",133)+"|");
+		      imp.say(imp.pRow()+1,0,"| Item");
+		      imp.say(imp.pRow()+0,8,"| Cod.prod");
+		      imp.say(imp.pRow()+0,20,"| Descrição do produto");
+		      imp.say(imp.pRow()+0,60,"| Qtd.");
+		      imp.say(imp.pRow()+0,70,"| Cod.fase");
+		      imp.say(imp.pRow()+0,80,"| Descrição da fase");
+		      imp.say(imp.pRow()+0,123,"| Auto Rma");
+		      imp.say(imp.pRow()+0,135,"|");
+		      imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"|"+Funcoes.replicate("-",133)+"|");
+		      cont++;
+		   }
+		   	  
+		   	  imp.say(imp.pRow()+1,0,""+imp.comprimido());
+		      imp.say(imp.pRow()+0,0,"| " + rs.getString(9));
+		      imp.say(imp.pRow()+0,8,"| " + rs.getString(10));
+		      imp.say(imp.pRow()+0,20,"| " + rs.getString(11).substring(0,38));
+		      imp.say(imp.pRow()+0,60,"| " + rs.getString(12));
+		      imp.say(imp.pRow()+0,70,"| " + rs.getString(13));
+		      imp.say(imp.pRow()+0,80,"| " + rs.getString(14).substring(0,38));
+		      imp.say(imp.pRow()+0,123,"|   " + (rs.getString(15).equals("S") ? "Sim" : "Não"));
+		      imp.say(imp.pRow()+0,135,"|");
+		   
+		  		   
+		   if (imp.pRow()>=linPag) {
+			      imp.say(imp.pRow()+1,0,""+imp.comprimido());
+			      imp.say(imp.pRow()+0,0,"+"+Funcoes.replicate("=",133)+"+");
+			      imp.incPags();
+			      imp.eject();
+		   }
+		}
+		imp.say(imp.pRow()+1,0,""+imp.comprimido());
+	    imp.say(imp.pRow()+0,0,"+"+Funcoes.replicate("=",133)+"+");
+	      
+		imp.eject();
+		
+		imp.fechaGravacao();
+		
+		if (!con.getAutoCommit())
+			con.commit();
+		dl.dispose();
+		}  
+		catch ( SQLException err ) {
+		Funcoes.mensagemErro(this,"Erro consulta tabela de insumos do produto!\n"+err.getMessage(),true,con,err);      
+		}
+    }
+    
+    if (bVisualizar) {
+      imp.preview(this);
+    }
+    else {
+      imp.print();
+    }
   }
   private void abreFase() {
     if (fPrim.temTela("Estrutura x Fase")==false) {
@@ -184,8 +415,14 @@ public class FEstrutura extends FDetalhe implements ActionListener, CarregaListe
     }
   }
   public void actionPerformed(ActionEvent evt) {
-    if (evt.getSource() == btFase)
+    if (evt.getSource() == btFase){
         abreFase();
+    }
+    else if (evt.getSource() == btPrevimp) {   	
+        imprimir(true);
+    }
+    else if (evt.getSource() == btImp) 
+      imprimir(false);
     super.actionPerformed(evt);
   }
   public void setConexao(Connection cn) {
