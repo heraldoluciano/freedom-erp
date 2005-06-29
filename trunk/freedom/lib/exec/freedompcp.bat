444887c725149bd60ba74d46cdb6e8c2d792dd4e
los_stpinf <los_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-06-29 19:41:01 +0000

    altera��es no fim para org.freedom

--- trunk/freedom/lib/exec/freedompcp.bat
+++ trunk/freedom/lib/exec/freedompcp.bat
@@ -1,3 +1,3 @@
 cd \opt\freedom
-javaw -classpath jars\freedompcp.jar;jars\jcommon-0.8.7.jar;jars\jfreechart-0.9.12.jar;jars\itext-1.02b.jar;jars\firebirdsql-full.jar;lib\nachocalendar-0.21.jar -DARQINI=freedom.ini  projetos.freedompcp.FreedomPCP
+javaw -classpath jars\freedompcp.jar;jars\jcommon-0.8.7.jar;jars\jfreechart-0.9.12.jar;jars\itext-1.02b.jar;jars\firebirdsql-full.jar;lib\nachocalendar-0.21.jar -DARQINI=freedom.ini  org.freedom.modulos.pcp.FreedomPCP
 
