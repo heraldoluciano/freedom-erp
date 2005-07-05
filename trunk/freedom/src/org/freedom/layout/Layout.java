97e344c6cfc9c241d861ea8d0713bc84a356ca1f
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-07-05 20:34:46 +0000

    Componentes para impress�o de notas fiscais.

---
+++ trunk/freedom/src/org/freedom/layout/Layout.java
@@ -0,0 +1,31 @@
+/**
+ * @version 05/07/2005 <BR>
+ * @author Setpoint Informática Ltda./Robson Sanchez <BR>
+ *
+ * Projeto: Freedom <BR>
+ * Pacote: org.freedom.layout <BR>
+ * Classe: @(#)Layout.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR> <BR>
+ *
+ * Comentários para a classe...
+ */
+
+package org.freedom.layout;
+
+import org.freedom.componentes.ImprimeOS;
+import org.freedom.componentes.NF;
+public class Layout extends Object {
+  public boolean bEntrada = false; 
+  public Layout() { }
+  public boolean imprimir(NF nf, ImprimeOS imp) {
+    return false;
+  }
+}
