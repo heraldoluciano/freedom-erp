7a69e422f5658d97d9f84d16738b463bf4879096
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-08-08 14:47:46 +0000

    Layout desenvolvido no iReport.

--- freedom/src/org/freedom/relatorios/GerContas.java
+++ freedom/src/org/freedom/relatorios/GerContas.java
@@ -63,50 +63,50 @@ public class GerContas extends LeiauteGR {
         setFonte(fnTituloNeg2);
         
       	drawRetangulo(10,30,120,30);//Conta
-      		drawTexto("CONTA",10,48,120,AL_BCEN);      	
+      	drawTexto("CONTA",10,48,120,AL_BCEN);      	
       	drawRetangulo(130,30,30,30);//Cod.
-  			drawTexto("CÓD.",130,48,30,AL_BCEN); 
+  		drawTexto("CÓD.",130,48,30,AL_BCEN); 
       	drawRetangulo(160,30,30,30);//Cidade
-			drawTexto("CID.",160,48,30,AL_BCEN);
+		drawTexto("CID.",160,48,30,AL_BCEN);
       	drawRetangulo(190,30,30,30);//Cat
-      		drawTexto("CAT.",190,48,30,AL_BCEN);
+      	drawTexto("CAT.",190,48,30,AL_BCEN);
       	drawRetangulo(220,30,20,30);//Class
-  			drawTexto("CLA.",220,48,20,AL_BCEN);
+  		drawTexto("CL.",220,48,20,AL_BCEN);
       	drawRetangulo(240,30,180,15);//Contatos 1
-			drawTexto("CONTATOS",240,40,180,AL_BCEN);
+		drawTexto("CONTATOS",240,40,180,AL_BCEN);
       	drawRetangulo(420,30,30,30);//Tot
-      		drawTexto("TOT.",420,48,30,AL_BCEN);
+      	drawTexto("TOT.",420,48,30,AL_BCEN);
        	drawRetangulo(450,30,60,30);//%afto
-      		drawTexto("% AFTO.",450,48,60,AL_BCEN);
+      	drawTexto("% AFTO.",450,48,60,AL_BCEN);
       	drawRetangulo(510,30,60,30);//20%
-      		drawTexto("20 %",570,48,60,AL_BCEN);      		      		      		
+      	drawTexto("20 %",570,48,60,AL_BCEN);      		      		      		
 			
-	      	setFonte(fnTituloNeg3);
+	    setFonte(fnTituloNeg3);
 	      	
 		drawRetangulo(240,45,15,15);//jan
-      		drawTexto("Jan",242,54);
+      	drawTexto("Jan",242,54);
       	drawRetangulo(255,45,15,15);//fev
-  			drawTexto("Fev",257,54);
+  		drawTexto("Fev",257,54);
       	drawRetangulo(270,45,15,15);//mar
-			drawTexto("Mar",272,54);
+		drawTexto("Mar",272,54);
       	drawRetangulo(285,45,15,15);//abr
-      		drawTexto("Abr",287,54);
+      	drawTexto("Abr",287,54);
       	drawRetangulo(300,45,15,15);//mai      	
-      		drawTexto("Mai",302,54);
+      	drawTexto("Mai",302,54);
       	drawRetangulo(315,45,15,15);//jun      	
-      		drawTexto("Jun",317,54);
+      	drawTexto("Jun",317,54);
       	drawRetangulo(330,45,15,15);//jul      	
-      		drawTexto("Jul",332,54);
+      	drawTexto("Jul",332,54);
       	drawRetangulo(345,45,15,15);//ago
-      		drawTexto("Ago",347,54);
+      	drawTexto("Ago",347,54);
       	drawRetangulo(360,45,15,15);//set
-      		drawTexto("Set",362,54);
+      	drawTexto("Set",362,54);
       	drawRetangulo(375,45,15,15);//out
-      		drawTexto("Out",377,54);
+      	drawTexto("Out",377,54);
       	drawRetangulo(390,45,15,15);//nov
-      		drawTexto("Nov",392,54);
+      	drawTexto("Nov",392,54);
       	drawRetangulo(405,45,15,15);//dez
-      		drawTexto("Dez",407,54);      	
+      	drawTexto("Dez",407,54);      	
       	
       	
       	while (rs.next()) {
