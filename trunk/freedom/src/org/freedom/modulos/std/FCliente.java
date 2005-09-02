b50a5b97bd112cfa360f7f26431266899acf5ee7
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2005-09-02 17:47:56 +0000

    *** empty log message ***

--- trunk/freedom/src/org/freedom/modulos/std/FCliente.java
+++ trunk/freedom/src/org/freedom/modulos/std/FCliente.java
@@ -2121,8 +2121,7 @@ public class FCliente extends FTabDados implements RadioGroupListener, PostListe
 		  vCli.addElement(txtAno.getVlrInteger());
 		  vCli.addElement(new Integer(mes));		 
 		  vCli.addElement(buscaAtendente());		 
-		  dl = new DLVisitas(FCliente.this,con);
-		  dl.setCampos(vCli);
+		  dl = new DLVisitas(FCliente.this,con,vCli);
 		  dl.setConexao(con);
 		  dl.carregaTabela(mes,(txtAno.getVlrInteger()).intValue());
 		  dl.setVisible(true);		 
