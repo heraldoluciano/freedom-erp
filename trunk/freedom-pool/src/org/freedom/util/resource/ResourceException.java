7a9a07b074bdb8d9353a08b1b0b3dd7129b3ea0a
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-01-09 16:59:46 +0000

    *** empty log message ***

---
+++ trunk/freedom-pool/src/org/freedom/util/resource/ResourceException.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 05/10/2004
+ * Autor: robson 
+ * Descrição: 
+ */
+package org.freedom.util.resource;
+
+/**
+ * @author robson
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ResourceException extends Exception {
+    public ResourceException(String e) {
+        super(e);
+    }
+}
