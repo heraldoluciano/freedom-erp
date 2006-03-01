7e41274ccb68540236cd9eece7553e811787a026
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-03-01 17:50:51 +0000

    Ajustes relativos a serializa��o

--- freedom-pool/src/org/freedom/util/resource/ResourceException.java
+++ freedom-pool/src/org/freedom/util/resource/ResourceException.java
@@ -12,6 +12,7 @@ package org.freedom.util.resource;
  * Window - Preferences - Java - Code Style - Code Templates
  */
 public class ResourceException extends Exception {
+	private static final long serialVersionUID = 1L;
     public ResourceException(String e) {
         super(e);
     }
