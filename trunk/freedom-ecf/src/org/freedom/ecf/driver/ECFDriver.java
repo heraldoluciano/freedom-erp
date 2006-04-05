ba74f935bc5b32dca91ca0380206b8a98f2efa19
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-04-05 14:41:24 +0000

    *** empty log message ***

--- trunk/freedom-ecf/src/org/freedom/ecf/driver/ECFDriver.java
+++ trunk/freedom-ecf/src/org/freedom/ecf/driver/ECFDriver.java
@@ -1,5 +1,17 @@
 package org.freedom.ecf.driver;
 
-public class ECFDriver {
-
+public abstract class ECFDriver {
+	public static final byte ESC = 27;
+	protected String porta;
+	public ECFDriver() {
+		
+	}
+	public ECFDriver(String porta) {
+		this.porta = porta;
+	}
+	public boolean abrePorta(String porta) {
+		boolean retorno = true;
+		this.porta = porta;
+		return retorno;
+	}
 }
