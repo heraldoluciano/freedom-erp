1605cb21a1fd97e3c78479312f4190c9317d45cf
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-04-15 17:36:37 +0000

    Recuperando as fun��es que j� estavam prontas e foram perdidas.

--- freedom-ecf/src/org/freedom/ecf/driver/ECFDriver.java
+++ freedom-ecf/src/org/freedom/ecf/driver/ECFDriver.java
@@ -6,11 +6,24 @@
  */
 package org.freedom.ecf.driver;
 
+import java.util.Enumeration;
+
+import javax.comm.CommPortIdentifier;
+import javax.comm.PortInUseException;
+import javax.comm.SerialPort;
+import javax.comm.UnsupportedCommOperationException;
+
 public abstract class ECFDriver {
 	public static final byte ESC = 27;
 	public static final byte STX = 2;
+	public static final int TIMEOUT = 60;
+	public static final int BAUDRATE = 9600;
+	public static final int DATABITS = SerialPort.DATABITS_8;
+	public static final int STOPBITS = SerialPort.STOPBITS_1;
+	public static final int PARITY = SerialPort.PARITY_NONE;
 	protected String porta;
 	protected boolean ativada = false;
+	protected SerialPort portaSerial = null;
 	public ECFDriver() {
 		
 	}
@@ -20,9 +33,37 @@ public abstract class ECFDriver {
 	public boolean abrePorta(String porta) {
 		boolean retorno = true;
 		this.porta = porta;
-		
+		portaSerial = abreSerial(porta);
+		if (portaSerial==null)
+			retorno = false;
 		ativada = retorno;
 		return retorno;
+	}
+	public SerialPort abreSerial(String porta) {
+		SerialPort portaSerial = null;
+		Enumeration listaPortas = null;
+		CommPortIdentifier ips = null;
+		listaPortas = CommPortIdentifier.getPortIdentifiers();
+		while (listaPortas.hasMoreElements()) {
+			ips = (CommPortIdentifier) listaPortas.nextElement();
+			if (ips.getName().equalsIgnoreCase(porta)) 
+				break;
+			else
+				ips = null;
+		}
+		if (ips != null) {
+			try {
+				portaSerial = (SerialPort) ips.open("SComm",TIMEOUT);
+				portaSerial.setSerialPortParams(BAUDRATE, DATABITS, STOPBITS, PARITY);
+			}
+			catch(PortInUseException e) {
+				portaSerial = null;
+			}
+			catch (UnsupportedCommOperationException e) {
+				
+			}
+		}
+		return portaSerial;
 	}
 	public boolean getAtivada() {
 		return ativada;
