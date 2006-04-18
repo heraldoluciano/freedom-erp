acf2ee856931ab2b0fd07e127c83ab8dbff34b5f
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-04-18 01:29:46 +0000

    Inclus�o de abertura de portas seriais.

--- freedom-ecf/src/org/freedom/ecf/driver/ECFDriver.java
+++ freedom-ecf/src/org/freedom/ecf/driver/ECFDriver.java
@@ -6,6 +6,9 @@
  */
 package org.freedom.ecf.driver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Enumeration;
 
 import javax.comm.CommPortIdentifier;
@@ -26,24 +29,49 @@ public abstract class ECFDriver {
 	public static final int COM3 = 2;
 	public static final int COM4 = 3;
 	public static final int COM5 = 4;
+	public static final int OS_NONE = -1;
+	public static final int OS_LINUX = 0;
+	public static final int OS_WINDOWS = 1;
+	private static int sistema = -1;
+    private InputStream entrada = null;
+    private OutputStream saida = null;
+	
 	protected String porta;
 	protected boolean ativada = false;
 	protected SerialPort portaSerial = null;
 	public ECFDriver() {
 		
 	}
-	public ECFDriver(String porta) {
-		this.porta = porta;
-		abrePorta(porta);
+	public ECFDriver(int com) {
+		abrePorta(com);
 	}
-	public boolean abrePorta(String porta) {
+	public boolean abrePorta(int com) {
 		boolean retorno = true;
-		this.porta = porta;
+		porta = convPorta(com);
 		portaSerial = abreSerial(porta);
 		if (portaSerial==null)
 			retorno = false;
 		ativada = retorno;
 		return retorno;
+	}
+	public String convPorta(int com) {
+		String porta = null;
+		if (getSistema()==OS_WINDOWS) 
+			porta = "COM"+(com+1);
+		else
+			porta = "/dev/ttyS"+com;
+		return porta;
+	}
+	public int getSistema() {
+		String os = null;
+		if (sistema==OS_NONE) {
+		  os = System.getProperty("os.name").toLowerCase();
+		  if (os.indexOf("linux")>OS_NONE)
+			  sistema = OS_LINUX;
+		  else if (os.indexOf("windows")>OS_NONE)
+			  sistema = OS_LINUX;
+		}
+		return sistema;
 	}
 	public SerialPort abreSerial(String porta) {
 		SerialPort portaSerial = null;
@@ -60,7 +88,12 @@ public abstract class ECFDriver {
 		if (ips != null) {
 			try {
 				portaSerial = (SerialPort) ips.open("SComm",TIMEOUT);
-				portaSerial.setSerialPortParams(BAUDRATE, DATABITS, STOPBITS, PARITY);
+				if (portaSerial!=null) {
+	            	entrada = portaSerial.getInputStream();
+	            	saida = portaSerial.getOutputStream();
+					portaSerial.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_OUT);
+					portaSerial.setSerialPortParams(BAUDRATE, DATABITS, STOPBITS, PARITY);
+				}
 			}
 			catch(PortInUseException e) {
 				portaSerial = null;
@@ -68,12 +101,23 @@ public abstract class ECFDriver {
 			catch (UnsupportedCommOperationException e) {
 				
 			}
+			catch (IOException e) {
+				portaSerial = null;
+			}
 		}
 		return portaSerial;
 	}
 	public boolean getAtivada() {
 		return ativada;
 	}
+
+	public void desativaPorta() {
+		if (portaSerial==null) 
+			portaSerial.close();
+		portaSerial = null;
+		ativada = false;
+	}
+	
 	public abstract byte[] preparaCmd(byte[] CMD);
 	
 	public abstract int leituraX();
