18972abbb289d78f67379b597ae183fce8e479ef
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-05-26 15:06:07 +0000

    Implementa��o para monitorar informa��es referentes ao uso de mem�ria.

--- trunk/freedom/src/org/freedom/telas/FSobre.java
+++ trunk/freedom/src/org/freedom/telas/FSobre.java
@@ -22,54 +22,62 @@ package org.freedom.telas;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import org.freedom.componentes.JLabelPad;
 import org.freedom.componentes.JPanelPad;
 import org.freedom.componentes.JTabbedPanePad;
-
 import org.freedom.bmps.Icone;
 import org.freedom.funcoes.Funcoes;
 
-
 public class FSobre extends FFDialogo {
 	private static final long serialVersionUID = 1L;
+	private JButton btMemoria = new JButton("RMA",Icone.novo("btExecutar.gif"));
+    private JTabbedPanePad tpnSobre = new JTabbedPanePad();
+    private JPanelPad pnSobre = new JPanelPad(JPanelPad.TP_JPANEL,new BorderLayout());
+    private JPanelPad pnEquipe = new JPanelPad(JPanelPad.TP_JPANEL,new BorderLayout());
+    private JPanelPad pnSistema = new JPanelPad(JPanelPad.TP_JPANEL,new BorderLayout());
+    private ImageIcon img = Icone.novo(Aplicativo.strSplash);
+	private long lMemLivre;
+  	private long lMemTotal;
+  	private long lMemUtilizada;
+  	private long lMemMaxima;
 
-  private JTabbedPanePad tpnSobre = new JTabbedPanePad();
-  private JPanelPad pnSobre = new JPanelPad(JPanelPad.TP_JPANEL,new BorderLayout());
-  private JPanelPad pnEquipe = new JPanelPad(JPanelPad.TP_JPANEL,new BorderLayout());
-  ImageIcon img = Icone.novo(Aplicativo.strSplash);
-  public FSobre () {
-  	super(Aplicativo.telaPrincipal);
-    setTitulo("Sobre...");
-	setAtribos(312,330);
+  	public FSobre () {
+  		super(Aplicativo.telaPrincipal);
+  		setTitulo("Sobre...");
+  		setAtribos(312,330);	
+  		setToFrameLayout();
+  		
+  		c.add(tpnSobre,BorderLayout.CENTER);
+  		tpnSobre.addTab("Sobre",pnSobre);
 	
-	setToFrameLayout();
-
-	c.add(tpnSobre,BorderLayout.CENTER);
-	tpnSobre.addTab("Sobre",pnSobre);
+  		JLabelPad lbImg = new JLabelPad(img);
+  		lbImg.setPreferredSize(new Dimension(img.getIconWidth(),img.getIconHeight()));
+  		String sVersao = "";
+    
+  		try {
+  			URL uPath = getClass().getResource("FSobre.class");
+  			JarURLConnection juc = (JarURLConnection)uPath.openConnection();
+  			sVersao = Funcoes.dateToStrDataHora(new Date(juc.getJarEntry().getTime()));  	  
+  		}
+  		catch(Exception err) { };
 	
-	JLabelPad lbImg = new JLabelPad(img);
-	lbImg.setPreferredSize(new Dimension(img.getIconWidth(),img.getIconHeight()));
-    String sVersao = "";
-	try {
-      URL uPath = getClass().getResource("FSobre.class");
-      JarURLConnection juc = (JarURLConnection)uPath.openConnection();
-      sVersao = Funcoes.dateToStrDataHora(new Date(juc.getJarEntry().getTime()));
-	}
-	catch(Exception err) { };
+  		pnSobre.add(lbImg,BorderLayout.NORTH);
+  		pnSobre.add(new JLabelPad ("<HTML><BR> Versão do jar: "+sVersao+"<BR> "+ Aplicativo.sEmpSis+"<BR>" +
+  				(new GregorianCalendar().get(Calendar.YEAR))+"<BR></HTML>"),BorderLayout.CENTER);
 	
-	pnSobre.add(lbImg,BorderLayout.NORTH);
-	pnSobre.add(new JLabelPad ("<HTML><BR>"+
-	        " Versão do jar: "+sVersao+"<BR>"+
-	        " "+Aplicativo.sEmpSis+"<BR>"+
-			(new GregorianCalendar().get(Calendar.YEAR))+"<BR></HTML>"),BorderLayout.CENTER);
-
+  		
+	
+  //  System.gc();
+	
+	
 	tpnSobre.addTab("Equipe "+Aplicativo.sNomeSis ,pnEquipe);
     String sHtmlEquipe = "<HTML><BR><CENTER>";
     for (int i = 0;Aplicativo.vEquipeSis.size()>i;i++){
@@ -77,5 +85,29 @@ public class FSobre extends FFDialogo {
     }
     
 	pnEquipe.add(new JLabelPad (sHtmlEquipe+"</CENTER><BR>"));
+	
+	
+	tpnSobre.addTab("Sistema" ,pnSistema);
+	carregaInfoSis();
+	pnSistema.add(new JLabelPad("<HTML>" +
+ 				"Memória maxima:"+lMemMaxima+" MB" + "<BR>" +
+ 				"Memória total:"+lMemTotal+" MB" + "<BR>" +
+ 				"Memória utilizada:"+lMemUtilizada+" MB" + "<BR>" +
+ 				"Memória livre:"+lMemLivre+" MB"+"<HTML>"),BorderLayout.NORTH);
+    pnSistema.add(btMemoria,BorderLayout.SOUTH);
+	
   }
+  public void carregaInfoSis() {
+		lMemLivre =((Runtime.getRuntime().freeMemory() / 1024) / 1024);
+  		lMemTotal = ((Runtime.getRuntime().totalMemory() / 1024) / 1024);
+  		lMemUtilizada = lMemTotal - lMemLivre;
+  		lMemMaxima = ((Runtime.getRuntime().maxMemory() /1024) / 1024);
+  		System.out.println("Carregando informações de memória..");
+  }
+   public void actionPerformed(ActionEvent evt) {
+        super.actionPerformed(evt);    
+    	if (evt.getSource() == btMemoria) 
+    		carregaInfoSis();
+        }	
+  	
 }    
