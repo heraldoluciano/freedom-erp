e893b51e94271bac132460731f214be1c10b4abc
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-07-05 13:43:15 +0000

    Continua��o da documenta��o.

--- trunk/freedom-pool/src/org/freedom/jdbc/DbConnectionPool.java
+++ trunk/freedom-pool/src/org/freedom/jdbc/DbConnectionPool.java
@@ -8,24 +8,25 @@ import org.freedom.util.resource.AbstractResourcePool;
 
 /**
  * Pool de conexões JDBC. <BR>
- * Este programa é licenciado de acordo com a LGPL
- * (Lesser General Public License), <BR>
- * versão 2.1, Fevereiro de 1999 <BR>
- * A LGPL deve acompanhar todas PUBLICAÇÕES,
- * DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
- * Caso uma cópia da LGPL não esteja disponível
- * junto com este Programa, você pode contatar <BR>
- * o LICENCIADOR ou então pegar uma cópia em:
- * <a href=http://creativecommons.org/licenses/LGPL/2.1/legalcode.pt>
- * Creative Commons</a> <BR>
- * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou
- * ALTERAR este Programa é preciso estar
- * de acordo com os termos da LGPL <BR> <BR>
- * criada: 05/10/2004. <BR>
  * Projeto: freedom-pool <BR>
  * Pacote: org.freedom.jdbc <BR>
- * Classe: DbConnectionPool <BR> <BR>
- * @author Robson Sanchez/Setpoint Informática Ltda.
+ * Classe: DbConnectionPool <BR>
+ * <BR>
+ * Este programa é licenciado de acordo com a LGPL (Lesser General Public
+ * License), <BR>
+ * versão 2.1, Fevereiro de 1999 <BR>
+ * A LGPL deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste
+ * Programa. <BR>
+ * Caso uma cópia da LGPL não esteja disponível junto com este Programa, você
+ * pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <a
+ * href=http://creativecommons.org/licenses/LGPL/2.1/legalcode.pt> Creative
+ * Commons</a> <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é
+ * preciso estar de acordo com os termos da LGPL. <BR>
+ * <BR>
+ * @author Robson Sanchez/Setpoint Informática Ltda. <BR>
+ * criada: 05/10/2004. <BR>
  */
 public class DbConnectionPool extends AbstractResourcePool {
 
--- trunk/freedom-pool/src/org/freedom/util/resource/ResourceKey.java
+++ trunk/freedom-pool/src/org/freedom/util/resource/ResourceKey.java
@@ -12,74 +12,74 @@ package org.freedom.util.resource;
  * Preferences - Java - Code Style - Code Templates
  */
 public class ResourceKey {
-	private Object resource = null;
+   private Object resource = null;
 
-	private String key = null;
+   private String key = null;
 
-	private String sessionID = null;
+   private String sessionID = null;
 
-	private String password = null;
+   private String password = null;
 
-	public ResourceKey(String sessionID, String key, String password,
-			Object resource) {
-		this.resource = resource;
-		this.key = key;
-		this.sessionID = sessionID;
-		this.password = password;
-	}
+   public ResourceKey(String sessionID, String key, String password,
+         Object resource) {
+      this.resource = resource;
+      this.key = key;
+      this.sessionID = sessionID;
+      this.password = password;
+   }
 
-	public Object getResource() {
-		return resource;
-	}
+   public Object getResource() {
+      return resource;
+   }
 
-	public void setResource(Object resource) {
-		this.resource = resource;
-	}
+   public void setResource(Object resource) {
+      this.resource = resource;
+   }
 
-	public boolean isResource(String sessionID, String key) {
-		boolean isresource = false;
-		try {
-			if (sessionID == null)
-				isresource = isResource(key);
-			else
-				isresource = ((this.key == key) && (this.sessionID == sessionID));
-		} catch (Exception e) {
+   public boolean isResource(String sessionID, String key) {
+      boolean isresource = false;
+      try {
+         if (sessionID == null)
+            isresource = isResource(key);
+         else
+            isresource = ((this.key == key) && (this.sessionID == sessionID));
+      } catch (Exception e) {
 
-		}
-		return isresource;
-	}
+      }
+      return isresource;
+   }
 
-	public boolean isResource(String key) {
-		boolean isresource = false;
-		try {
-			isresource = (this.key == key);
-		} catch (Exception e) {
+   public boolean isResource(String key) {
+      boolean isresource = false;
+      try {
+         isresource = (this.key == key);
+      } catch (Exception e) {
 
-		}
-		return isresource;
-	}
+      }
+      return isresource;
+   }
 
-	public String getHashKey() {
-		return sessionID + key;
-	}
+   public String getHashKey() {
+      return sessionID + key;
+   }
 
-	public String getKey() {
-		return this.key;
-	}
+   public String getKey() {
+      return this.key;
+   }
 
-	public String getSessionID() {
-		return this.sessionID;
-	}
+   public String getSessionID() {
+      return this.sessionID;
+   }
 
-	public void setSessionID(String sessionID) {
-		this.sessionID = sessionID;
-	}
+   public void setSessionID(String sessionID) {
+      this.sessionID = sessionID;
+   }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+   public void setPassword(String password) {
+      this.password = password;
+   }
 
-	public String getPassword() {
-		return this.password;
-	}
+   public String getPassword() {
+      return this.password;
+   }
 }
