ac1993c08a4d4e5f871fb08f2d76fe0bb55cf62d
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-07-18 23:48:43 +0000

    *** empty log message ***

--- trunk/freedom-ecf/src/org/freedom/ecf/driver/Teste.java
+++ trunk/freedom-ecf/src/org/freedom/ecf/driver/Teste.java
@@ -4,6 +4,10 @@ import java.util.Date;
 
 public class Teste {
 	
+	public Teste(){
+		System.out.println("teste");
+	}
+	
 	/**
 	 * @param args
 	 */
@@ -15,33 +19,41 @@ public class Teste {
 		System.out.println("Inicio --> " + data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds() );
 		
 		
-		
-		// não ta passando....
-		//ecf.nomeiaDepartamento( 2, "Compras   ");
-		
-		
-		//	não ta passando...
-		//ecf.nomeiaTotalizadorNaoSujeitoICMS(1,"Conta de Luz       ");
+		//ecf.programaCaracterParaAutenticacao(new char[]{1,2,4,8,16,32,64,128,64,32,16,8,4,2,1,129,129,129});
+
+		ecf.autenticacaoDeDocumento();
+		ecf.comprovanteNFiscalNVinculado( "01", 10f,"Cartao Credito  ");
 		
 		
-		ecf.aberturaDeCupom();
+		
+		//ecf.abreComprovanteNFiscalVinculado("Cheque Predatado", 1, 726 );
+		// não vinga...
+		//ecf.usaComprovanteNFiscalVinculado("Testando comprovante nao fiscal vinculado...");
+		
+		
+		
+		//ecf.aberturaDeCupom();
 		
 		//ecf.programaUnidadeMedida("KG");
-		//ecf.aumentaDescItem("Caneta                       0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789" );
-		ecf.vendaItem("0000000000001", "Caneta                       ", "FF", 1f, 0.12f, 0f);
-		//ecf.vendaItemDepartamento("FF", 15.001f, 1f, 0.001f, 0.001f, 1, "UN", "0000000000001", "Caneta                       ");
+		//ecf.aumentaDescItem("Caneta                       0123456789" );
 		
-		ecf.iniciaFechamentoCupom(ECFBematech.DESCONTO_PERCENTUAL,0);
-		ecf.efetuaFormaPagamento(2,0.12f,"");
-		ecf.terminaFechamentoCupom("Obrigado pela Preferencia!!!");
+		//ecf.vendaItem("0000000000001", "Caneta                       ", "FF", 1f, 0.12f, 0f);
+		
+		//ecf.vendaItemTresCasas("0000000000001", "Caneta                       ", "FF", 1f, 0.125f, 0f);
+		
+		//ecf.vendaItemDepartamento("FF", 15.001f, 1f, 0.001f, 0.001f, 3, "UN", "0000000000001", "Caneta");
+		
+		//ecf.iniciaFechamentoCupom(ECFBematech.DESCONTO_PERCENTUAL,0);
+		//ecf.efetuaFormaPagamento(1,1f,"ta pago");
+		//ecf.terminaFechamentoCupom("Obrigado pela Preferencia!!!");
 		
 		
-		//ecf.programaFormaPagamento(new String[]{"Cartao Credito  "});
+		//ecf.programaFormaPagamento("Cartao Credito  ");// 2
+		//ecf.programaFormaPagamento("Cartao Visa     ");// 3
 		
 		data = new Date();
 		System.out.println("Fim -----> " + data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds() );
-		
-		
+				
 		System.exit(0);
 	}
 
