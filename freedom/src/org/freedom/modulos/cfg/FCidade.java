999c92d037c27a5b5c4fc4f6e445e27ebbaa63b8
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-08-15 14:31:35 +0000

    *** empty log message ***

--- freedom/src/org/freedom/modulos/cfg/FCidade.java
+++ freedom/src/org/freedom/modulos/cfg/FCidade.java
@@ -1,12 +1,12 @@
 /**
  * @version 25/06/2004 <BR>
- * @author Setpoint Informática Ltda./Anderson Sanchez <BR>
+ * @author Setpoint Informática Ltda./Alex Rodrigues <BR>
  * 
  * Projeto: Freedom <BR>
  * 
  * Pacote: org.freedom.modulos.cfg <BR>
  * Classe:
- * @(#)FPais.java <BR>
+ * @(#)FCidade.java <BR>
  * 
  * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
@@ -36,31 +36,31 @@ import org.freedom.componentes.ListaCampos;
 import org.freedom.funcoes.Funcoes;
 import org.freedom.telas.FDados;
 
-public class FPais extends FDados implements ActionListener {
+public class FCidade extends FDados implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-	private JTextFieldPad txtCodPais = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodCidade = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
-	private JTextFieldPad txtNomePais = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
+	private JTextFieldPad txtNomeCidade = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
 
-	private JTextFieldPad txtSiglaPais = new JTextFieldPad( JTextFieldPad.TP_STRING, 10, 0 );
+	private JTextFieldPad txtSiglaCidade = new JTextFieldPad( JTextFieldPad.TP_STRING, 10, 0 );
 
-	private JTextFieldPad txtDDIPais = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtDDDCidade = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
-	public FPais() {
+	public FCidade() {
 
 		super();
-		setTitulo( "Cadastro de Paises" );
+		setTitulo( "Cadastro de Cidades" );
 		setAtribos( 50, 50, 410, 125 );
 
 		lcCampos.setUsaME( false );
 
-		adicCampo( txtCodPais, 7, 20, 70, 20, "CodPais", "Cód.pais", ListaCampos.DB_PK, true );
-		adicCampo( txtNomePais, 80, 20, 177, 20, "NomePais", "Nome do pais", ListaCampos.DB_SI, true );
-		adicCampo( txtSiglaPais, 260, 20, 77, 20, "SiglaPais", "Sigla", ListaCampos.DB_SI, true );
-		adicCampo( txtDDIPais, 340, 20, 40, 20, "DDIPais", "DDI", ListaCampos.DB_SI, false );
-		setListaCampos( true, "PAIS", "SG" );
+		adicCampo( txtCodCidade, 7, 20, 70, 20, "CodCid", "Cód.cid.", ListaCampos.DB_PK, true );
+		adicCampo( txtNomeCidade, 80, 20, 177, 20, "NomeCid", "Nome da cidade", ListaCampos.DB_SI, true );
+		adicCampo( txtSiglaCidade, 260, 20, 77, 20, "SiglaCid", "Sigla", ListaCampos.DB_SI, true );
+		adicCampo( txtDDDCidade, 340, 20, 40, 20, "DDDCid", "DDD", ListaCampos.DB_SI, false );
+		setListaCampos( true, "CIDADE", "SG" );
 		
 		btImp.addActionListener( this );
 		btPrevimp.addActionListener( this );
@@ -94,9 +94,9 @@ public class FPais extends FDados implements ActionListener {
 		
 			imp.limpaPags();
 			imp.montaCab();
-			imp.setTitulo( "Relatório de paises cadastrados" );
+			imp.setTitulo( "Relatório de Cidades" );
 			
-			String sSQL = "SELECT CODPAIS,NOMEPAIS,SIGLAPAIS,DDIPAIS FROM SGPAIS ORDER BY NOMEPAIS";
+			String sSQL = "SELECT CODCID,NOMECID,SIGLACID,DDDCID FROM SGCIDADE ORDER BY NOMECID";
 			
 			ps = con.prepareStatement( sSQL );
 			rs = ps.executeQuery();
@@ -113,10 +113,10 @@ public class FPais extends FDados implements ActionListener {
 					imp.impCab( 136, false );
 					
 					imp.say( 0, imp.normal() );					
-					imp.say( 2, "Cód.pais" );
+					imp.say( 2, "Cód.cid." );
 					imp.say( 15, "Nome" );
 					imp.say( 80, "Sigla" );
-					imp.say( 100, "DDI" );
+					imp.say( 100, "DDD" );
 					
 					imp.pulaLinha( 1, imp.normal() );
 					imp.say( 0, Funcoes.replicate( "-", 135 ) );
@@ -124,10 +124,10 @@ public class FPais extends FDados implements ActionListener {
 				}
 				
 				imp.pulaLinha( 1, imp.normal() );
-				imp.say( 2, rs.getString( "CodPais" ) != null ? rs.getString( "CodPais" ) : ""  );
-				imp.say( 15, rs.getString( "NomePais" ) != null ? rs.getString( "NomePais" ) : ""  );
-				imp.say( 80, rs.getString( "SiglaPais" ) != null ? rs.getString( "SiglaPais" ) : ""  );
-				imp.say( 100, rs.getString( "DDIPais" ) != null ? rs.getString( "DDIPais" ) : "" );
+				imp.say( 2, rs.getString( "CODCID" ) != null ? rs.getString( "CODCID" ) : ""  );
+				imp.say( 15, rs.getString( "NOMECID" ) != null ? rs.getString( "NOMECID" ) : ""  );
+				imp.say( 80, rs.getString( "DDDCID" ) != null ? rs.getString( "SIGLACID" ) : ""  );
+				imp.say( 100, rs.getString( "SIGLACID" ) != null ? rs.getString( "DDDCID" ) : "" );
 				
 				
 			}
@@ -147,7 +147,7 @@ public class FPais extends FDados implements ActionListener {
 			
 		} catch ( SQLException err ) {
 			err.printStackTrace();
-			Funcoes.mensagemErro( this, "Erro consulta tabela de paises!" + err.getMessage(), true, con, err );
+			Funcoes.mensagemErro( this, "Erro consulta tabela de cidades!" + err.getMessage(), true, con, err );
 		}
 
 		if ( bVisualizar ) {
--- freedom/src/org/freedom/modulos/cfg/FreedomCFG.java
+++ freedom/src/org/freedom/modulos/cfg/FreedomCFG.java
@@ -8,97 +8,70 @@
  * Classe:
  * @(#)FreedomCFG.java <BR>
  * 
- * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para
- * Programas de Computador), <BR>
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
- * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste
- * Programa. <BR>
- * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você
- * pode contatar <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
  * o LICENCIADOR ou então pegar uma cópia em: <BR>
  * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
- * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é
- * preciso estar <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
  * de acordo com os termos da LPG-PC <BR>
  * <BR>
  * 
  * Tela principal do módulo configurador.
- *  
+ * 
  */
 
 package org.freedom.modulos.cfg;
 
 import org.freedom.funcoes.Funcoes;
 import org.freedom.telas.Aplicativo;
-import org.freedom.telas.FPrincipal;
 import org.freedom.telas.FPrincipalPD;
 
 public class FreedomCFG extends Aplicativo {
 
 	public FreedomCFG() {
-		super("iconConfiguracao32.gif", "splashConf.jpg", 1, "Freedom", 2, "Configurador do Sistema", null,new FPrincipalPD("bgFreedomSTD.jpg"));
-		addOpcao(-1, TP_OPCAO_MENU, "Arquivo", "", 'A', 100000000, 0, false,
-				null);
-		addOpcao(100000000, TP_OPCAO_MENU, "Tabelas", "", 'T', 100100000, 1,
-				false, null);
-		addOpcao(100100000, TP_OPCAO_MENU, "Acesso ao sistema", "", 'A',
-				100101000, 2, false, null);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Grupos", "Grupos", 'G', 100101010,
-				3, true, FGrupo.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Usuarios", "Usuarios", 'U',
-				100101020, 3, true, FUsuario.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Acesso", "Acesso Menu", 'A',
-				100101030, 3, true, FAcesso.class);
-		addOpcao(100101000, TP_OPCAO_ITEM, "Menu", "Menu X Objeto", 'M',
-				100101040, 3, true, FMenuObj.class);
-		addOpcao(100100000, TP_OPCAO_MENU, "Clientes/Conveniados", "", 'C',
-				100102000, 2, false, null);
-		addOpcao(100102000, TP_OPCAO_ITEM, "Grau de instrução",
-				"Grau de Instrução", 'G', 100102010, 3, true, FGrauInst.class);
-		addOpcao(100102000, TP_OPCAO_ITEM, "Paises", "Paises", 'P', 100102020,
-				3, true, FPais.class);
-		addOpcao(100100000, TP_OPCAO_MENU, "Objetos", "", 'O', 100103000, 2,
-				false, null);
-		addOpcao(100103000, TP_OPCAO_ITEM, "Tabela", "Tabelas auxiliares", 'T',
-				100103010, 3, true, FTabela.class);
-		addOpcao(100103000, TP_OPCAO_ITEM, "Objetos aux.",
-				"Vinculo entre tabelas físicas e auxiliares", 'O', 100103020,
-				3, true, FObjetoTb.class);
-		addOpcao(100100000, TP_OPCAO_MENU, "Fluxos", "", 'F', 100104000, 2,
-				false, null);
-		addOpcao(100104000, TP_OPCAO_ITEM, "Processos", "Processos", 'P',
-				100104100, 3, true, FProcesso.class);
-		addOpcao(100104000, TP_OPCAO_ITEM, "Fluxos", "Cadastro de fluxos", 'F',
-				100104110, 3, true, FFluxo.class);
 
-		addOpcao(100000000, TP_OPCAO_MENU, "Ferramentas", "", 'e', 100200000,
-				1, false, null);
-		addOpcao(100200000, TP_OPCAO_ITEM, "Ajuste de Sequencia",
-				"Ajusta sequencia", 'A', 100201000, 2, true, FAjustaSeq.class);
-
-		addOpcao(100000000, TP_OPCAO_MENU, "Preferências", "", 'P', 100300000,
-				1, false, null);
-		addOpcao(100300000, TP_OPCAO_ITEM, "Visual",
-				"Configuração de Visual", 'A', 100301000, 2, true, FVisual.class);
+		super( "iconConfiguracao32.gif", "splashConf.jpg", 1, "Freedom", 2, "Configurador do Sistema", null, new FPrincipalPD( "bgFreedomSTD.jpg" ) );
 		
-		addBotao("barraGrupo.gif", "Cadastro de Grupos", "Grupos", 100101010,
-				FGrupo.class);
-		addBotao("barraUsuario.gif", "Cadastro de Usuarios", "Usuarios",
-				100101020, FUsuario.class);
-		addBotao("barraAcesso.gif", "Controle de Acessos", "Acesso Menu",
-				100101030, FAcesso.class);
+		addOpcao( -1, TP_OPCAO_MENU, "Arquivo", "", 'A', 100000000, 0, false, null );
+			addOpcao( 100000000, TP_OPCAO_MENU, "Tabelas", "", 'T', 100100000, 1, false, null );
+				addOpcao( 100100000, TP_OPCAO_MENU, "Acesso ao sistema", "", 'A', 100101000, 2, false, null );
+					addOpcao( 100101000, TP_OPCAO_ITEM, "Grupos", "Grupos", 'G', 100101010, 3, true, FGrupo.class );
+					addOpcao( 100101000, TP_OPCAO_ITEM, "Usuarios", "Usuarios", 'U', 100101020, 3, true, FUsuario.class );
+					addOpcao( 100101000, TP_OPCAO_ITEM, "Acesso", "Acesso Menu", 'A', 100101030, 3, true, FAcesso.class );
+					addOpcao( 100101000, TP_OPCAO_ITEM, "Menu", "Menu X Objeto", 'M', 100101040, 3, true, FMenuObj.class );
+				addOpcao( 100100000, TP_OPCAO_MENU, "Clientes/Conveniados", "", 'C', 100102000, 2, false, null );
+					addOpcao( 100102000, TP_OPCAO_ITEM, "Grau de instrução", "Grau de Instrução", 'G', 100102010, 3, true, FGrauInst.class );
+					addOpcao( 100102000, TP_OPCAO_ITEM, "Paises", "Paises", 'P', 100102020, 3, true, FPais.class );
+					addOpcao( 100102000, TP_OPCAO_ITEM, "Cidades", "Cidades", 'd', 100102030, 3, true, FCidade.class );
+				addOpcao( 100100000, TP_OPCAO_MENU, "Objetos", "", 'O', 100103000, 2, false, null );
+					addOpcao( 100103000, TP_OPCAO_ITEM, "Tabela", "Tabelas auxiliares", 'T', 100103010, 3, true, FTabela.class );
+					addOpcao( 100103000, TP_OPCAO_ITEM, "Objetos aux.", "Vinculo entre tabelas físicas e auxiliares", 'O', 100103020, 3, true, FObjetoTb.class );
+				addOpcao( 100100000, TP_OPCAO_MENU, "Fluxos", "", 'F', 100104000, 2, false, null );
+					addOpcao( 100104000, TP_OPCAO_ITEM, "Processos", "Processos", 'P', 100104100, 3, true, FProcesso.class );
+					addOpcao( 100104000, TP_OPCAO_ITEM, "Fluxos", "Cadastro de fluxos", 'F', 100104110, 3, true, FFluxo.class );
 
+		addOpcao( 100000000, TP_OPCAO_MENU, "Ferramentas", "", 'e', 100200000, 1, false, null );
+			addOpcao( 100200000, TP_OPCAO_ITEM, "Ajuste de Sequencia", "Ajusta sequencia", 'A', 100201000, 2, true, FAjustaSeq.class );
+
+		addOpcao( 100000000, TP_OPCAO_MENU, "Preferências", "", 'P', 100300000, 1, false, null );
+			addOpcao( 100300000, TP_OPCAO_ITEM, "Visual", "Configuração de Visual", 'A', 100301000, 2, true, FVisual.class );
 		
-		//	  addBotao("btProcessos.gif", "Controle de processos","", 100110000,
+		addBotao( "barraGrupo.gif", "Cadastro de Grupos", "Grupos", 100101010, FGrupo.class );
+		addBotao( "barraUsuario.gif", "Cadastro de Usuarios", "Usuarios", 100101020, FUsuario.class );
+		addBotao( "barraAcesso.gif", "Controle de Acessos", "Acesso Menu", 100101030, FAcesso.class );
+
+		// addBotao("btProcessos.gif", "Controle de processos","", 100110000,
 		// null);
-		//	  addBotao("btFluxo.gif", "Controle de Fluxos","", 100111000, null);
-		FVisual teste = new FVisual(true);
-		teste.setConexao(con);
+		// addBotao("btFluxo.gif", "Controle de Fluxos","", 100111000, null);
+		FVisual teste = new FVisual( true );
+		teste.setConexao( con );
 		teste.show();
 
 		ajustaMenu();
 
-		conIB = conexaoIB(getParameter("driver"), getParameter("bancocfg")); // Inicia
+		conIB = conexaoIB( getParameter( "driver" ), getParameter( "bancocfg" ) ); // Inicia
 		// a
 		// variável
 		// de
@@ -109,28 +82,29 @@ public class FreedomCFG extends Aplicativo {
 		// interno
 		// do
 		// interbase
-		
+
 		sNomeModulo = "Administrativo";
 		sNomeSis = "Freedom";
 		sEmpSis = "Setpoint Informática Ltda.";
 		sMailSuporte = "suporte@stpinf.com";
-		vEquipeSis.add("Robson Sanchez - Supervisão / Analise");
-		vEquipeSis.add("Anderson Sanchez - Supervisão / Programação");
-		vEquipeSis.add("Alex Rodrigues - Programação");
-		vEquipeSis.add("Alexandre Marcondes - Programação");
-		vEquipeSis.add("Fernando Oliveira - Programação");
-		vEquipeSis.add("Moyzes Braz - Arte gráfica");
-		vEquipeSis.add("Leandro Oliveira - Testes / Suporte");
+		vEquipeSis.add( "Robson Sanchez - Supervisão / Analise" );
+		vEquipeSis.add( "Anderson Sanchez - Supervisão / Programação" );
+		vEquipeSis.add( "Alex Rodrigues - Programação" );
+		vEquipeSis.add( "Alexandre Marcondes - Programação" );
+		vEquipeSis.add( "Fernando Oliveira - Programação" );
+		vEquipeSis.add( "Moyzes Braz - Arte gráfica" );
+		vEquipeSis.add( "Leandro Oliveira - Testes / Suporte" );
 	}
 
-	public static void main(String sParams[]) {
+	public static void main( String sParams[] ) {
+
 		try {
-			Aplicativo.setLookAndFeel("freedom.ini");
+			Aplicativo.setLookAndFeel( "freedom.ini" );
 			FreedomCFG freedom = new FreedomCFG();
 			freedom.show();
 
-		} catch (Throwable e) {
-			Funcoes.criaTelaErro("Erro de execução");
+		} catch ( Throwable e ) {
+			Funcoes.criaTelaErro( "Erro de execução" );
 			e.printStackTrace();
 		}
 	}
