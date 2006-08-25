1d529c1e0b363d866304da520f4075d3bae0ebaf
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-08-25 11:48:44 +0000

    *** empty log message ***

--- freedom-ecf/src/org/freedom/ecf/com/Serial.java
+++ freedom-ecf/src/org/freedom/ecf/com/Serial.java
@@ -1,3 +1,27 @@
+/**
+ * @version 1.0.0 - 05/04/2006 <BR>
+ * @author Robson Sanchez/Setpoint Informática Ltda. <BR>
+ *         criada: 19/08/2006. <BR>
+ * 
+ * Projeto: Freedom-ECF <BR>
+ * Pacote: org.freedom.ecf.com <BR>
+ * Classe:
+ * @(#)Serial.java <BR>
+ * @see org.freedom.ecf.driver.AbstractECFDriver
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
+ * Esta classe é um Singleton para acesso e controle a porta serial. <BR>
+ * 
+ */
 package org.freedom.ecf.com;
 
 import java.io.IOException;
@@ -12,175 +36,113 @@ import javax.comm.SerialPort;
 import javax.comm.SerialPortEventListener;
 import javax.comm.UnsupportedCommOperationException;
 
-/**
- * Classe de controle da porta serial <BR>
- * Projeto: freedom-ecf <BR>
- * Pacote: org.freedom.ecf.com <BR>
- * Classe: Serial.java <BR>
- * <BR>
- * Este programa é licenciado de acordo com a LGPL (Lesser General Public
- * License), <BR>
- * versão 2.1, Fevereiro de 1999 <BR>
- * A LGPL deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste
- * Programa. <BR>
- * Caso uma cópia da LGPL não esteja disponível junto com este Programa, você
- * pode contatar <BR>
- * o LICENCIADOR ou então pegar uma cópia em: <a
- * href=http://creativecommons.org/licenses/LGPL/2.1/legalcode.pt> Creative
- * Commons</a> <BR>
- * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é
- * preciso estar de acordo com os termos da LGPL. <BR>
- * <BR>
- * @author Robson Sanchez/Setpoint Informática Ltda. <BR>
- * criada: 19/08/2006. <BR>
- */
+
 public class Serial {
 
 	public static final int TIMEOUT = 1000;
+
 	public static final int BAUDRATE = 9600;
+
 	public static final int DATABITS = SerialPort.DATABITS_8;
+
 	public static final int STOPBITS = SerialPort.STOPBITS_1;
+
 	public static final int PARITY = SerialPort.PARITY_NONE;
+
 	public static final int COM1 = 0;
+
 	public static final int COM2 = 1;
+
 	public static final int COM3 = 2;
+
 	public static final int COM4 = 3;
+
 	public static final int COM5 = 4;
+
 	public static final int OS_NONE = -1;
+
 	public static final int OS_LINUX = 0;
+
 	public static final int OS_WINDOWS = 1;
-    private InputStream entrada = null;
-    private OutputStream saida = null;
-	private String porta;
-	private int portaSel = -1;
-	private boolean ativada = false;
-	private SerialPort portaSerial = null;
-	private int sistema = -1;
-	
-	private static Serial instance;
+
+	private static InputStream entrada = null;
+
+	private static OutputStream saida = null;
+
+	private static String porta;
+
+	private static int portaSel = -1;
+
+	private static boolean ativada = false;
+
+	private static SerialPort portaSerial = null;
+
+	private static int sistema = -1;
+
+	private static Serial instance = null;
+
+	/*
+	 *  Construtor definido como private seguindo o padrão Singleton.
+	 */
 	private Serial() {
+
 		super();
-		// TODO Auto-generated constructor stub
+
 	}
-	
+
 	public static Serial getInstance() {
-		if (instance==null) {
+
+		if ( instance == null ) {
 			instance = new Serial();
 		}
+		
 		return instance;
 	}
-	public boolean ativaPorta( final int com, SerialPortEventListener event ) {
-		
+
+	/**
+	 *  Ativa a porta serial.<BR>
+	 * @param com
+	 * @param event Objeto ouvinte do evento de leitura dos bytes recebidos.<BR>
+	 * @return Verdadeiro caso a porta esteja ativada. <BR>
+	 */
+	public boolean ativaPorta( final int com, final SerialPortEventListener event ) {
+
 		boolean retorno = true;
-		
+
 		if ( com != portaSel || portaSerial == null ) {
+			
 			portaSel = com;
 			porta = convPorta( com );
 			portaSerial = ativaSerial( porta );
-			
+
 			if ( portaSerial == null ) {
 				retorno = false;
-			} else {
-				try {
-					portaSerial.addEventListener(event);
-					portaSerial.notifyOnDataAvailable(true);
-				}
-				catch (TooManyListenersException e) {
-					retorno = false;
-				}
-			}
-			ativada = retorno;
-		}
-		return retorno;
-	}
-	
-	
-	
-	public SerialPort ativaSerial( final String porta ) {
-		
-		SerialPort portaSerial = null;
-		Enumeration listaPortas = null;
-		CommPortIdentifier ips = null;
-		listaPortas = CommPortIdentifier.getPortIdentifiers();
-		
-		while ( listaPortas.hasMoreElements() ) {
-			
-			ips = ( CommPortIdentifier ) listaPortas.nextElement();
-			
-			if ( ips.getName().equalsIgnoreCase( porta ) ) {
-				break;
 			}
 			else {
-				ips = null;
-			}
-			
-		}
-		
-		if ( ips != null ) {
-			
-			try {
-								
-				portaSerial = ( SerialPort ) ips.open( "SComm", TIMEOUT );
 				
-				if ( portaSerial != null ) {
+				try {
 					
-	            	entrada = portaSerial.getInputStream();
-	            	saida = portaSerial.getOutputStream();
-					portaSerial.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_OUT);
-					portaSerial.setSerialPortParams(BAUDRATE, DATABITS, STOPBITS, PARITY);
+					portaSerial.addEventListener( event );
+					portaSerial.notifyOnDataAvailable( true );
 					
+				} catch ( TooManyListenersException e ) {
+					e.printStackTrace();
+					retorno = false;
 				}
 				
-			} catch( PortInUseException e ) {
-				e.printStackTrace();
-			} catch ( UnsupportedCommOperationException e ) {
-				
-			} catch ( IOException e ) {
-				portaSerial = null;
 			}
+			
+			ativada = retorno;
 			
 		}
 		
-		return portaSerial;
-		
+		return retorno;
 	}
-	
-	public String convPorta( final int com ) {
-		
-		final StringBuffer porta = new StringBuffer();
-		
-		if ( getSistema() == OS_WINDOWS ) { 
-			porta.append( "COM" );
-			porta.append( com + 1 );
-		}
-		else {
-			porta.append( "/dev/ttyS" );
-			porta.append( com );
-		}
-		
-		return porta.toString();
-		
-	}
-	
-	public int getSistema() {
-		
-		final String system = System.getProperty( "os.name" ).toLowerCase();
-		
-		if ( sistema == OS_NONE ) {
-					  
-		  if ( system.indexOf( "linux" ) > OS_NONE ) {
-			  sistema = OS_LINUX;
-		  }
-		  else if ( system.indexOf("windows") > OS_NONE ) {
-			  sistema = OS_WINDOWS;
-		  }
-		  
-		}
-		
-		return sistema;
-		
-	}
-	
+
+	/**
+	 * Desativa a porta serial.<BR>
+	 *
+	 */
 	public void desativaPorta() {
 
 		if ( portaSerial == null ) {
@@ -191,16 +153,119 @@ public class Serial {
 		ativada = false;
 
 	}
-	
-	public boolean getAtivada() {
+
+	public String convPorta( final int com ) {
+
+		final StringBuffer porta = new StringBuffer();
+
+		if ( getSistema() == OS_WINDOWS ) {
+			porta.append( "COM" );
+			porta.append( com + 1 );
+		}
+		else {
+			porta.append( "/dev/ttyS" );
+			porta.append( com );
+		}
+
+		return porta.toString();
+
+	}
+
+	/**
+	 * Captura a porta aberta de define os objetos de entrada e saida.<BR>
+	 * @param porta
+	 * @return
+	 */
+	public SerialPort ativaSerial( final String porta ) {
+
+		SerialPort portaSerial = null;
+		Enumeration listaPortas = null;
+		CommPortIdentifier ips = null;
+		listaPortas = CommPortIdentifier.getPortIdentifiers();
+
+		while ( listaPortas.hasMoreElements() ) {
+
+			ips = (CommPortIdentifier) listaPortas.nextElement();
+
+			if ( ips.getName().equalsIgnoreCase( porta ) ) {
+				break;
+			}
+			else {
+				ips = null;
+			}
+
+		}
+
+		if ( ips != null ) {
+
+			try {
+
+				portaSerial = (SerialPort) ips.open( "SComm", TIMEOUT );
+
+				if ( portaSerial != null ) {
+
+					entrada = portaSerial.getInputStream();
+					saida = portaSerial.getOutputStream();
+					portaSerial.setFlowControlMode( SerialPort.FLOWCONTROL_RTSCTS_OUT );
+					portaSerial.setSerialPortParams( BAUDRATE, DATABITS, STOPBITS, PARITY );
+
+				}
+
+			} catch ( PortInUseException e ) {
+				e.printStackTrace();
+			} catch ( UnsupportedCommOperationException e ) {
+				e.printStackTrace();
+			} catch ( IOException e ) {
+				e.printStackTrace();
+				portaSerial = null;
+			}
+
+		}
+
+		return portaSerial;
+
+	}
+
+	/**
+	 * 
+	 * @return o nome do sistema operacional.<BR>
+	 */
+	public int getSistema() {
+
+		final String system = System.getProperty( "os.name" ).toLowerCase();
+
+		if ( sistema == OS_NONE ) {
+
+			if ( system.indexOf( "linux" ) > OS_NONE ) {
+				sistema = OS_LINUX;
+			}
+			else if ( system.indexOf( "windows" ) > OS_NONE ) {
+				sistema = OS_WINDOWS;
+			}
+
+		}
+
+		return sistema;
+
+	}
+
+	/**
+	 * 
+	 * @return Verdadeiro caso a porta esteja ativada. <BR>
+	 */
+	public boolean isAtivada() {
+
 		return ativada;
 	}
+
+	public InputStream getEntrada() {
+
+		return entrada;
+	}
 	
-    public InputStream getEntrada() {
-    	return entrada;
-    }
-    public OutputStream getSaida() {
-    	return saida;
-    }
+	public OutputStream getSaida() {
+
+		return saida;
+	}
 
 }
