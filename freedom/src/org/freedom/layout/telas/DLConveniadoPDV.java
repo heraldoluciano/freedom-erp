49ca554faeb9902804b4afeced73b6015d608288
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-08-25 21:59:17 +0000

    *** empty log message ***

--- freedom/src/org/freedom/layout/telas/DLConveniadoPDV.java
+++ freedom/src/org/freedom/layout/telas/DLConveniadoPDV.java
@@ -1,13 +1,9 @@
 package org.freedom.layout.telas;
 
-import java.sql.Connection;
-
 import javax.swing.JLabel;
 
-import org.freedom.componentes.GuardaCampo;
 import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
-import org.freedom.componentes.ListaCampos;
 import org.freedom.telas.FDialogo;
 
 
@@ -15,26 +11,17 @@ public class DLConveniadoPDV extends FDialogo {
 	
 	private static final long serialVersionUID = 1;
 	
-	private JTextFieldPad txtCodConv = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodBarras = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 	
-	private JTextFieldFK txtNomeConv = new JTextFieldFK( JTextFieldPad.TP_INTEGER, 8, 0 );
-	
-	private ListaCampos lcConv = new ListaCampos( this, "" );
+	private JTextFieldFK txtMatricula = new JTextFieldFK( JTextFieldPad.TP_INTEGER, 8, 0 );
 
 	public DLConveniadoPDV() {
 
 		super();
 		
 		setTitulo( "Conveniado" );
-		setAtribos( 300, 400 );
+		setAtribos( 383, 400 );
 		
-//			FK Conveniado
-		lcConv.add(new GuardaCampo( txtCodConv, "CodConv", "Cód.conv.", ListaCampos.DB_PK, false));
-		lcConv.add(new GuardaCampo( txtNomeConv, "NomeConv", "Nome do coveniado", ListaCampos.DB_SI, false));
-		lcConv.montaSql( false, "CONVENIADO", "AT" );    
-		lcConv.setQueryCommit(false);
-		lcConv.setReadOnly(true);
-		txtCodConv.setTabelaExterna(lcConv);
 		
 		montaTela();
 		
@@ -42,18 +29,12 @@ public class DLConveniadoPDV extends FDialogo {
 	
 	private void montaTela() {
 		
-		adic( new JLabel( "Cód.conv." ), 7, 10, 100, 20 );
-		adic( txtCodConv, 7, 30, 100, 20 );
-		adic( new JLabel( "Nome do conveniado" ), 110, 10, 250, 20 );
-		adic( txtNomeConv, 110, 30, 250, 20 );
+		adic( new JLabel( "Cód.barras" ), 7, 10, 200, 20 );
+		adic( txtCodBarras, 7, 30, 200, 20 );
+		adic( new JLabel( "Matricula" ), 210, 10, 100, 20 );
+		adic( txtMatricula, 210, 30, 150, 20 );
 		
-	}
-	
-	public void setConexao( Connection cn ) {
-
-		super.setConexao(cn);
-		
-		lcConv.setConexao( cn );
+		JLabel linha = new JLabel();
 		
 	}
 	
