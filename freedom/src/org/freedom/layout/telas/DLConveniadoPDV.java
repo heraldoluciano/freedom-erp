de4b455a9dc5aa4524715c1b41697608b00b3bd6
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-08-30 00:27:06 +0000

    *** empty log message ***

--- freedom/src/org/freedom/layout/telas/DLConveniadoPDV.java
+++ freedom/src/org/freedom/layout/telas/DLConveniadoPDV.java
@@ -1,11 +1,15 @@
 package org.freedom.layout.telas;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 
 import org.freedom.componentes.JTextAreaPad;
 import org.freedom.componentes.JTextFieldPad;
+import org.freedom.funcoes.Funcoes;
 import org.freedom.telas.FDialogo;
 
 
@@ -13,7 +17,7 @@ public class DLConveniadoPDV extends FDialogo {
 	
 	private static final long serialVersionUID = 1;
 	
-	private JTextFieldPad txtCodBarras = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodBarras = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 15, 0 );
 	
 	private JTextFieldPad txtMatricula = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 	
@@ -48,8 +52,7 @@ public class DLConveniadoPDV extends FDialogo {
 		super();
 		
 		setTitulo( "Conveniado" );
-		setAtribos( 383, 400 );
-		
+		setAtribos( 383, 400 );		
 		
 		montaTela();
 		
@@ -111,6 +114,140 @@ public class DLConveniadoPDV extends FDialogo {
 		txtPercDesc.setAtivo( false );
 		txtPrecoDesc.setAtivo( false );
 		txtTotal.setAtivo( false );
+		
+	}
+	
+	private void montaICard( final boolean sca ) {
+		
+		F2ICard icard = new F2ICard();
+		
+		icard.setVisible( true );
+		
+		String sICard = null;
+		
+		/*GetICard(bSca: Boolean; docMatric: TDOCSESC): String;
+begin
+ if bSca then
+    Result := Copy(strZero(docMatric.VIA,4),4,1)+strZero(docMatric.CDUOP,4)+StrZero(docMatric.MATRIC,6)+IntToStr(docMatric.MATRICS)
+ else
+    Result := '0'+docMatric.UUCC+StrZero(docMatric.MATRIC,7)+StrZero(docMatric.MATRICS,2);
+end;
+ TDOCSESC = record
+   VIA: Integer;
+   CDUOP: Integer;
+   CAT: String[1];
+   UUCC: String[2];
+   UUEE: String[2];
+   MATRIC: Integer;
+   MATRICS: Integer;
+   DV: String[2];
+   VALIDO: Boolean;
+ end;
+Enviado às 21:07 de Terça-feira
+ 
+Pressione a tecla Enter para enviar sua mensagem.
+*/
+		
+		if ( sca ) {
+			
+			int via = getVia();
+			
+			sICard = Funcoes.strZero( String.valueOf( via ), 4 ) + 
+					 Funcoes.strZero( icard.getUUCC(), 4 ) + 
+					 Funcoes.strZero( icard.getMatricula(), 6 ) + 
+					 icard.getDep();
+			
+		}
+		else {
+			
+			sICard = "0" +
+					 icard.getUUCC() + 
+					 Funcoes.strZero( icard.getMatricula(), 7 ) +
+					 Funcoes.strZero( icard.getDep(), 2 );
+			
+		}
+				
+		txtCodBarras.setVlrString( sICard );
+		
+	}
+	
+	private int getVia() {
+		return 1;
+	}
+	
+	public String getIcard() {
+		
+		return txtCodBarras.getVlrString().trim();		
+	}
+	
+	public void keyPressed( KeyEvent e ) {
+		
+		if ( e.getKeyCode() == KeyEvent.VK_F2 ) {
+			
+			montaICard( false );
+			
+		}
+
+		super.keyPressed( e );
+
+	}
+	
+	public void actionPerformed(ActionEvent evt) {
+		System.exit(0);
+	}
+	
+	private class F2ICard extends FDialogo {
+		
+		private static final long serialVersionUID = 1;
+		
+		private JTextFieldPad txtUUCC = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 4, 0 );
+		
+		private JTextFieldPad txtMatricula = new JTextFieldPad( JTextFieldPad.TP_STRING, 7, 0 );
+		
+		private JTextFieldPad txtDep = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 2, 0 );
+		
+		public F2ICard() {
+			
+			super();
+			
+			setAtribos( 255, 120 );
+			setTitle( "Matricula" );
+			
+			montaTela();
+			
+		}
+		
+		private void montaTela() {
+			
+			adic( new JLabel( "UUCC" ), 7, 0, 50, 20 );
+			adic( txtUUCC, 7, 20, 50, 20 );
+			adic( new JLabel( "Matricula" ), 60, 0, 120, 20 );
+			adic( txtMatricula, 60, 20, 120, 20 );
+			adic( new JLabel( "Dep" ), 183, 0, 50, 20 );
+			adic( txtDep, 183, 20, 50, 20 );
+			
+		}
+		
+		public String getUUCC() {
+			
+			return txtUUCC.getVlrString().trim();
+		}
+		
+		public String getMatricula() {
+			
+			return txtMatricula.getVlrString().trim();
+		}
+		
+		public String getDep() {
+			
+			return txtDep.getVlrString().trim();
+		}
+		
+	}
+	
+	public static void main( String[] args ) {
+		
+		new DLConveniadoPDV().setVisible( true );
 		
 	}
 
