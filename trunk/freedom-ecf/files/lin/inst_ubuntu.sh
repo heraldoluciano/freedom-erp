03a0b048fa862ca62f01218fab5e162f8f496041
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-09-03 22:28:47 +0000

    *** empty log message ***

--- trunk/freedom-ecf/files/lin/inst_ubuntu.sh
+++ trunk/freedom-ecf/files/lin/inst_ubuntu.sh
@@ -4,6 +4,7 @@ diretorio=`pwd`
 cp /tmp/commapi/jar/*.jar /usr/lib/jvm/java-1.5.0-sun/jre/lib/
 cp /tmp/commapi/jar/tools/*.jar /usr/lib/jvm/java-1.5.0-sun/jre/lib
 cp /tmp/commapi/lib/*.so /usr/lib/jvm/java-1.5.0-sun/jre/lib/i386/
+cp /tmp/commapi/docs/javax.comm.properties /usr/lib/jvm/java-1.5.0-sun/jre/lib
 cd /usr/lib
 ln -sf /usr/lib/jvm/java-1.5.0-sun/jre/lib/i386/libLinuxSerialParallel_g.so
 ln -sf /usr/lib/jvm/java-1.5.0-sun/jre/lib/i386/libLinuxSerialParallel.so
