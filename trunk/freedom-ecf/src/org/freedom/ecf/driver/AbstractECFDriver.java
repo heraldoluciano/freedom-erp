acea669d5f0ba3d0d2a27a3be1e3efdc9a0ca8a8
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-11-24 18:36:44 +0000

    comentario na classe

--- trunk/freedom-ecf/src/org/freedom/ecf/driver/AbstractECFDriver.java
+++ trunk/freedom-ecf/src/org/freedom/ecf/driver/AbstractECFDriver.java
@@ -1,6 +1,6 @@
 /*
  * Classe base para impressoras fiscais Autor: Robson Sanchez Data: 05/04/2006
- * 
+ 
  */
 package org.freedom.ecf.driver;
 
@@ -16,6 +16,30 @@ import javax.comm.SerialPortEvent;
 import javax.comm.SerialPortEventListener;
 
 import org.freedom.ecf.com.Serial;
+
+/**
+ * Classe base para impressoras fiscais <BR>
+ * Projeto: freedom-ECF <BR>
+ * Pacote: org.freedom.ecf.driver <BR>
+ * Classe: @(#)AbstractECFDriver.java <BR>
+ * <BR>
+ * Este programa é licenciado de acordo com a LGPL (Lesser General Public
+ * License), <BR>
+ * versão 2.1, Fevereiro de 1999 <BR>
+ * A LGPL deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste
+ * Programa. <BR>
+ * Caso uma cópia da LGPL não esteja disponível junto com este Programa, você
+ * pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <a
+ * href=http://creativecommons.org/licenses/LGPL/2.1/legalcode.pt> Creative
+ * Commons</a> <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é
+ * preciso estar de acordo com os termos da LGPL. <BR>
+ * <BR>
+ * @author Robson Sanchez/Setpoint Informática Ltda. <BR>
+ * criada: 05/04/2006. <BR>
+ *  <BR>
+ */
 
 public abstract class AbstractECFDriver implements SerialPortEventListener {
 
