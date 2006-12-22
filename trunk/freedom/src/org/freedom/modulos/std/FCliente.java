fc51fdfc9e468849daf9edbaa732869265e43520
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2006-12-22 13:00:42 +0000

    adicionado mascara na classe

--- trunk/freedom/src/org/freedom/modulos/std/FCliente.java
+++ trunk/freedom/src/org/freedom/modulos/std/FCliente.java
@@ -624,6 +624,7 @@ public class FCliente extends FTabDados implements RadioGroupListener, PostListe
 		txtCnpjCli.setMascara( JTextFieldPad.MC_CNPJ );
 		txtCepCli.setMascara( JTextFieldPad.MC_CEP );
 		txtFoneCli.setMascara( JTextFieldPad.MC_FONE );
+		txtCelCli.setMascara( JTextFieldPad.MC_FONE );
 		txtFaxCli.setMascara( JTextFieldPad.MC_FONE );
 
 		pinEnt = new JPanelPad( 500, 290 );
