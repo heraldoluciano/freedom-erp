3c52892f081958c16f6b097c031d044cb2dff844
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-01-12 16:48:09 +0000

    imclus�o do layout grafico.

--- freedom/src/org/freedom/modulos/std/FClasCli.java
+++ freedom/src/org/freedom/modulos/std/FClasCli.java
@@ -1,11 +1,12 @@
 /**
  * @version 14/07/2003 <BR>
  * @author Setpoint Informática Ltda./Fernando Oliveira da Silva <BR>
- *
+ * 
  * Projeto: Freedom <BR>
- *  
+ * 
  * Pacote: org.freedom.modulos.std <BR>
- * Classe: @(#)FClasCli.java <BR>
+ * Classe:
+ * @(#)FClasCli.java <BR>
  * 
  * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
@@ -14,12 +15,14 @@
  * o LICENCIADOR ou então pegar uma cópia em: <BR>
  * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
  * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
- * de acordo com os termos da LPG-PC <BR> <BR>
- *
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
  * Comentários sobre a classe...
  */
 
 package org.freedom.modulos.std;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.PreparedStatement;
@@ -32,95 +35,102 @@ import org.freedom.componentes.ListaCampos;
 import org.freedom.funcoes.Funcoes;
 import org.freedom.telas.FDados;
 
-public class FClasCli extends FDados implements ActionListener{
+public class FClasCli extends FDados implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-  private JTextFieldPad txtCodClasCli = new JTextFieldPad(JTextFieldPad.TP_INTEGER,5,0);
-  private JTextFieldPad txtDescClasCli= new JTextFieldPad(JTextFieldPad.TP_STRING,40,0);
-  private JTextFieldPad txtSgTpCli= new JTextFieldPad(JTextFieldPad.TP_STRING,3,0);
-  public FClasCli () {
-  	super();
-    setTitulo("Cadastro de Classificação de Cliente");
-    setAtribos( 50, 50, 350, 165);
-    adicCampo(txtCodClasCli, 7, 20, 70, 20,"CodClasCli","Cód.c.cli.",ListaCampos.DB_PK,null,true);
-    adicCampo(txtDescClasCli, 80, 20, 250, 20,"DescClasCli","Descrição",ListaCampos.DB_SI,null,true);
-    adicCampo(txtSgTpCli, 7, 60, 71, 20,"SiglaClasCli","Sigla.c.cli.", ListaCampos.DB_SI,null, false);
-    setListaCampos( true, "CLASCLI", "VD");
-    btImp.addActionListener(this);
-    btPrevimp.addActionListener(this);
-    lcCampos.setQueryInsert(false);    
-//    btPrevimp.addActionListener(this);
-//    btImp.addActionListener(this);
-    setImprimir(true);
-  }
-  public void actionPerformed(ActionEvent evt) {
-    if (evt.getSource() == btPrevimp) {
-        imprimir(true);
-    }
-    else if (evt.getSource() == btImp) 
-      imprimir(false);
-    super.actionPerformed(evt);
-  }
+	private JTextFieldPad txtCodClasCli = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 5, 0 );
 
-  private void imprimir(boolean bVisualizar) {
-    ImprimeOS imp = new ImprimeOS("",con);
-    int linPag = imp.verifLinPag()-1;
-    imp.montaCab();
-    imp.setTitulo("Relatório de Classificação de Clientes");
-    DLRClasCli dl = new DLRClasCli(this);
-	dl.setVisible(true);
-	if (dl.OK == false) {
-	  dl.dispose();
-	  return;
+	private JTextFieldPad txtDescClasCli = new JTextFieldPad( JTextFieldPad.TP_STRING, 40, 0 );
+
+	private JTextFieldPad txtSgTpCli = new JTextFieldPad( JTextFieldPad.TP_STRING, 3, 0 );
+
+	public FClasCli() {
+
+		super();
+		setTitulo( "Cadastro de Classificação de Cliente" );
+		setAtribos( 50, 50, 350, 165 );
+		adicCampo( txtCodClasCli, 7, 20, 70, 20, "CodClasCli", "Cód.c.cli.", ListaCampos.DB_PK, null, true );
+		adicCampo( txtDescClasCli, 80, 20, 250, 20, "DescClasCli", "Descrição", ListaCampos.DB_SI, null, true );
+		adicCampo( txtSgTpCli, 7, 60, 71, 20, "SiglaClasCli", "Sigla.c.cli.", ListaCampos.DB_SI, null, false );
+		setListaCampos( true, "CLASCLI", "VD" );
+		btImp.addActionListener( this );
+		btPrevimp.addActionListener( this );
+		lcCampos.setQueryInsert( false );
+		btPrevimp.addActionListener(this);
+		btImp.addActionListener(this);
+		setImprimir( true );
 	}
-	String sSQL = "SELECT CODCLASCLI,DESCCLASCLI FROM VDCLASCLI ORDER BY "+dl.getValor();
-    PreparedStatement ps = null;
-    ResultSet rs = null;
-    try {
-      ps = con.prepareStatement(sSQL);
-      rs = ps.executeQuery();
-      imp.limpaPags();
-      while ( rs.next() ) {
-         if (imp.pRow()==0) {
-            imp.impCab(80, false);
-            imp.say(imp.pRow()+0,0,""+imp.normal());
-            imp.say(imp.pRow()+0,0,"");
-            imp.say(imp.pRow()+0,2,"Código");
-            imp.say(imp.pRow()+0,30,"Descrição");
-            imp.say(imp.pRow()+1,0,""+imp.normal());
-            imp.say(imp.pRow()+0,0,Funcoes.replicate("-",80));
-         }
-         imp.say(imp.pRow()+1,0,""+imp.normal());
-         imp.say(imp.pRow()+0,2,rs.getString("CodClasCli"));
-         imp.say(imp.pRow()+0,30,rs.getString("DescClasCli"));
-         if (imp.pRow()>=linPag) {
-            imp.incPags();
-            imp.eject();
-         }
-      }
-      
-      imp.say(imp.pRow()+1,0,""+imp.normal());
-      imp.say(imp.pRow()+0,0,Funcoes.replicate("=",80));
-      imp.eject();
-      
-      imp.fechaGravacao();
-      
-//      rs.close();
-//      ps.close();
-      if (!con.getAutoCommit())
-      	con.commit();
-      dl.dispose();
-    }  
-    catch ( SQLException err ) {
-		Funcoes.mensagemErro(this,"Erro consulta tabela de clascli!\n"+err.getMessage(),true,con,err);      
-    }
-    
-    if (bVisualizar) {
-      imp.preview(this);
-    }
-    else {
-      imp.print();
-    }
-  }
+
+	public void actionPerformed( ActionEvent evt ) {
+
+		if ( evt.getSource() == btPrevimp ) {
+			imprimir( true );
+		}
+		else if ( evt.getSource() == btImp ) {
+			imprimir( false );
+		}
+		super.actionPerformed( evt );
+	}
+
+	private void imprimir( boolean bVisualizar ) {
+
+		ImprimeOS imp = new ImprimeOS( "", con );
+		int linPag = imp.verifLinPag() - 1;
+		imp.montaCab();
+		imp.setTitulo( "Relatório de Classificação de Clientes" );
+		DLRClasCli dl = new DLRClasCli( this );
+		dl.setVisible( true );
+		if ( dl.OK == false ) {
+			dl.dispose();
+			return;
+		}
+		String sSQL = "SELECT CODCLASCLI,DESCCLASCLI FROM VDCLASCLI ORDER BY " + dl.getValor();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = con.prepareStatement( sSQL );
+			rs = ps.executeQuery();
+			imp.limpaPags();
+			while ( rs.next() ) {
+				if ( imp.pRow() == 0 ) {
+					imp.impCab( 80, false );
+					imp.say( imp.pRow() + 0, 0, "" + imp.normal() );
+					imp.say( imp.pRow() + 0, 0, "" );
+					imp.say( imp.pRow() + 0, 2, "Código" );
+					imp.say( imp.pRow() + 0, 30, "Descrição" );
+					imp.say( imp.pRow() + 1, 0, "" + imp.normal() );
+					imp.say( imp.pRow() + 0, 0, Funcoes.replicate( "-", 80 ) );
+				}
+				imp.say( imp.pRow() + 1, 0, "" + imp.normal() );
+				imp.say( imp.pRow() + 0, 2, rs.getString( "CodClasCli" ) );
+				imp.say( imp.pRow() + 0, 30, rs.getString( "DescClasCli" ) );
+				if ( imp.pRow() >= linPag ) {
+					imp.incPags();
+					imp.eject();
+				}
+			}
+
+			imp.say( imp.pRow() + 1, 0, "" + imp.normal() );
+			imp.say( imp.pRow() + 0, 0, Funcoes.replicate( "=", 80 ) );
+			imp.eject();
+
+			imp.fechaGravacao();
+
+			// rs.close();
+			// ps.close();
+			if ( !con.getAutoCommit() )
+				con.commit();
+			dl.dispose();
+		} catch ( SQLException err ) {
+			Funcoes.mensagemErro( this, "Erro consulta tabela de clascli!\n" + err.getMessage(), true, con, err );
+		}
+
+		if ( bVisualizar ) {
+			imp.preview( this );
+		}
+		else {
+			imp.print();
+		}
+	}
 }
