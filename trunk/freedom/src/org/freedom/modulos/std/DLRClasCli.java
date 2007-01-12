e5e31c19f4f851268ae56969ab8658cf283f0724
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-01-12 16:48:09 +0000

    imclus�o do layout grafico.

--- trunk/freedom/src/org/freedom/modulos/std/DLRClasCli.java
+++ trunk/freedom/src/org/freedom/modulos/std/DLRClasCli.java
@@ -1,11 +1,12 @@
 /**
  * @version 14/07/2003 <BR>
  * @author Setpoint Informática Ltda./Fernando Oliveira da Silva <BR>
- *
+ * 
  * Projeto: Freedom <BR>
- *  
+ * 
  * Pacote: org.freedom.modulos.std <BR>
- * Classe: @(#)DLRClasCli.java <BR>
+ * Classe:
+ * @(#)DLRClasCli.java <BR>
  * 
  * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
@@ -14,12 +15,14 @@
  * o LICENCIADOR ou então pegar uma cópia em: <BR>
  * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
  * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
- * de acordo com os termos da LPG-PC <BR> <BR>
- *
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
  * Comentários sobre a classe...
  */
 
 package org.freedom.modulos.std;
+
 import java.awt.Component;
 import java.util.Vector;
 
@@ -32,29 +35,58 @@ public class DLRClasCli extends FFDialogo {
 
 	private static final long serialVersionUID = 1L;
 
-  private JRadioGroup rgOrdem = null;
-  private JLabelPad lbOrdem = new JLabelPad("Ordenar por:");
-  private Vector vLabs = new Vector();
-  private Vector vVals = new Vector();
-  public DLRClasCli(Component cOrig) {
-  	super(cOrig);
-    setTitulo("Ordem do Relatório");
-    setAtribos(300,140);
-    vLabs.addElement("Código");
-    vLabs.addElement("Descrição");
-    vVals.addElement("C");
-    vVals.addElement("D");
-    rgOrdem = new JRadioGroup(1,2,vLabs,vVals);
-    rgOrdem.setVlrString("D");
-    adic(lbOrdem,7,0,80,15);
-    adic(rgOrdem,7,20,280,30);
-  }
-  public String getValor() {
-    String sRetorno = "";
-    if (rgOrdem.getVlrString().compareTo("C") == 0 )
-      sRetorno = "CODCLASCLI";
-    else if (rgOrdem.getVlrString().compareTo("D") == 0 )
-      sRetorno = "DESCCLASCLI";
-    return sRetorno;
-  }
+	private JRadioGroup rgOrdem = null;
+
+	private JRadioGroup rgTipo = null;
+
+	private JLabelPad lbOrdem = new JLabelPad( "Ordenar por:" );
+
+	private Vector vLabs = new Vector();
+
+	private Vector vVals = new Vector();
+
+	private Vector vLabs1 = new Vector();
+
+	private Vector vVals1 = new Vector();
+
+	public DLRClasCli( Component cOrig ) {
+
+		super( cOrig );
+		setTitulo( "Ordem do Relatório" );
+		setAtribos( 300, 190 );
+		vLabs.addElement( "Código" );
+		vLabs.addElement( "Descrição" );
+		vVals.addElement( "C" );
+		vVals.addElement( "D" );
+
+		vVals1.addElement( "T" );
+		vVals1.addElement( "G" );
+		vLabs1.addElement( "Texto" );
+		vLabs1.addElement( "Grafico" );
+
+		rgOrdem.setVlrString( "D" );
+		rgTipo.setVlrString( "G" );
+
+		rgOrdem = new JRadioGroup( 1, 2, vLabs, vVals );
+		rgTipo = new JRadioGroup( 1, 2, vLabs1, vVals1 );
+
+		adic( lbOrdem, 7, 0, 80, 15 );
+		adic( rgOrdem, 7, 20, 270, 30 );
+		adic( rgTipo, 7, 60, 270, 30 );
+
+	}
+
+	public String getValor() {
+
+		String sRetorno = "";
+
+		if ( rgOrdem.getVlrString().compareTo( "C" ) == 0 ) {
+			sRetorno = "CODCLASCLI";
+		}
+		else if ( rgOrdem.getVlrString().compareTo( "D" ) == 0 ) {
+			sRetorno = "DESCCLASCLI";
+		}
+		
+		return sRetorno;
+	}
 }
