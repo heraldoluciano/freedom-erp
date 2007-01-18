41a79c168c6f3a9b849a4f728044b712e42ecffc
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-01-18 11:29:20 +0000

    *** empty log message ***

--- trunk/freedom/src/org/freedom/modulos/std/FTipoFiscCli.java
+++ trunk/freedom/src/org/freedom/modulos/std/FTipoFiscCli.java
@@ -27,18 +27,26 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import net.sf.jasperreports.engine.JasperPrintManager;
+
 import org.freedom.componentes.ListaCampos;
 import org.freedom.componentes.ImprimeOS;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.funcoes.Funcoes;
 import org.freedom.telas.Aplicativo;
 import org.freedom.telas.FDados;
+import org.freedom.telas.FPrinterJob;
+
 public class FTipoFiscCli extends FDados implements ActionListener {
+	
 	private static final long serialVersionUID = 1L;
+	
 	private JTextFieldPad txtCod= new JTextFieldPad(JTextFieldPad.TP_INTEGER,5,0);
+	
 	private JTextFieldPad txtDesc= new JTextFieldPad(JTextFieldPad.TP_STRING,40,0);
 	
 	public FTipoFiscCli () {
+		
 		super();
 		setTitulo("Cadastro de tipos fiscal de clientes");
 		setAtribos(50, 50, 360, 125);
@@ -52,100 +60,143 @@ public class FTipoFiscCli extends FDados implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent evt) {
+		
 		if (evt.getSource() == btPrevimp)
 			imprimir(true);
+		
 		else if (evt.getSource() == btImp) 
 			imprimir(false);
+		
 		super.actionPerformed(evt);
 	}
 	
 	private void imprimir(boolean bVisualizar) {
-		PreparedStatement ps = null;
+	
+		StringBuffer sSQL = new StringBuffer();
 		ResultSet rs = null;
-		String sSQL = null;
-		DLRTipoFiscCli dl = null;
-		ImprimeOS imp = new ImprimeOS("",con);
-		int linPag = 0;
-		int iTot = 0;
-
-		dl = new DLRTipoFiscCli();
-		dl.setVisible(true);
-		if (dl.OK == false) {
-			dl.dispose();
-			return;
-		}
-
+		PreparedStatement ps = null;
+		DLRTipoFiscCli dl = new DLRTipoFiscCli();
+		
+		
 		try {
+			dl.setVisible( true );						
+			if ( dl.OK == false ) {
+				dl.dispose();
+				return;
+		}
 			
-			imp = new ImprimeOS("",con);
-			linPag = imp.verifLinPag()-1;
-			imp.montaCab();
-			imp.setTitulo("Relatório de Tipos Fiscal de Cliente");dl = new DLRTipoFiscCli();
-			imp.limpaPags();
+
+			sSQL.append( "SELECT TP.CODFISCCLI AS CODIGO,TP.DESCFISCCLI AS DESCRICAO," );
+			sSQL.append( "(SELECT COUNT(CLI.CODFISCCLI) FROM LFTIPOFISCCLI CLI " );
+			sSQL.append( "WHERE CLI.CODEMPFC=TP.CODEMP AND CLI.CODFILIALFC=TP.CODFILIAL AND CLI.CODTIPOFISCCLI=TP.CODTIPOFISCCLI) AS QTD " );
+			sSQL.append( "FROM LFTIPOFISCCLI TP " );
+			sSQL.append( "WHERE TP.CODEMP=? AND TP.CODFILIAL=? " );
+			sSQL.append( "ORDER BY "  + dl.getValor() );
 			
-			sSQL = "SELECT TP.CODFISCCLI,TP.DESCFISCCLI, " +
-				   "(SELECT COUNT(CLI.CODCLI) FROM VDCLIENTE CLI " +
-				   							 "WHERE CLI.CODEMPFC=TP.CODEMP AND CLI.CODFILIALFC=TP.CODFILIAL " +
-				   							 "AND CLI.CODFISCCLI = TP.CODFISCCLI) "+
-				   "FROM LFTIPOFISCCLI TP " +
-				   "WHERE TP.CODEMP=? AND TP.CODFILIAL=? " + 
-				   "ORDER BY TP." + dl.getValor();
-			
-			ps = con.prepareStatement(sSQL);
+			ps = con.prepareStatement(sSQL.toString());
 			ps.setInt(1, Aplicativo.iCodEmp);
 			ps.setInt(2, ListaCampos.getMasterFilial("LFTIPOFISCCLI"));
 			rs = ps.executeQuery();
-			while ( rs.next() ) {
-				if (imp.pRow()==0) {
-					imp.impCab(80, false);
-					imp.say(imp.pRow(), 0, imp.normal());
-					imp.say(imp.pRow(), 2, "Cód.fisc.cli.");
-					imp.say(imp.pRow(), 20, "Descrição");
-					imp.say(imp.pRow(), 70, "Qtd.cli.");
-					imp.say(imp.pRow() + 1, 0, imp.normal());
-					imp.say(imp.pRow(), 0, Funcoes.replicate("-",79));
-				}
-				imp.say(imp.pRow() + 1, 0, imp.normal());
-				imp.say(imp.pRow(), 2, rs.getString("CodFiscCli"));
-				imp.say(imp.pRow(), 20, rs.getString("DescFiscCli"));
-				imp.say(imp.pRow(), 70, Funcoes.alinhaDir(rs.getInt(3),8));
-				iTot += rs.getInt(3);
-				if (imp.pRow()>=linPag) {
-					imp.say(imp.pRow() + 1, 0, imp.normal());
-					imp.say(imp.pRow(), 0, Funcoes.replicate("-", 79));
-					imp.incPags();
-					imp.eject();
-				}
-			}
-			  
-			imp.say(imp.pRow() + 1, 0, imp.normal());
-			imp.say(imp.pRow(), 0, Funcoes.replicate("=",79));
-			imp.say(imp.pRow() + 1, 0, imp.normal());
-			imp.say(imp.pRow(), 0, "|");
-			imp.say(imp.pRow(), 50, "Total de clientes:");
-			imp.say(imp.pRow(), 70, Funcoes.alinhaDir(iTot,8));
-			imp.say(imp.pRow(), 80, "|");
-			imp.say(imp.pRow() + 1, 0, imp.normal());
-			imp.say(imp.pRow(), 0, Funcoes.replicate("=",79));
-			imp.eject();  
-			imp.fechaGravacao();
 			
-			if (!con.getAutoCommit())
+			if ( "T".equals( dl.getTipo()) ) {
+				imprimirTexto( bVisualizar, rs );
+			}
+			else if ( "G".equals( dl.getTipo() ) ) {
+				imprimirGrafico( bVisualizar, rs );
+			}
+			
+			rs.close();
+			ps.close();
+			
+			if ( !con.getAutoCommit() ) {
 				con.commit();
+			
+			}
 			dl.dispose();
-		} catch ( SQLException err ) {
-			Funcoes.mensagemErro(this,"Erro consulta tabela fiscal de tipos de cliente!"+err.getMessage(),true,con,err);
+			
+		} catch ( Exception err ) {
+			Funcoes.mensagemErro( this, "Erro ao montar relatório do tipo fiscal de clientes!\n" + err.getMessage(), true, con, err );
 			err.printStackTrace();
-		} finally {
-			ps = null;
-			rs = null;
-			sSQL = null;
-			dl = null;
 		}
-		    
-		if (bVisualizar)
-			imp.preview(this);
-		else
+	}
+	private void imprimirTexto( final boolean bVisualizar, final ResultSet rs ){
+		
+		
+		ImprimeOS imp = new ImprimeOS( "", con );
+		int linPag = imp.verifLinPag() - 1;
+		imp.montaCab();
+	
+		try {
+			
+			imp.limpaPags();
+			imp.montaCab();
+			imp.setTitulo( "Relatório de Classificação do tipo fiscal de cliente" );
+		
+		while ( rs.next() ) {
+			if (imp.pRow()==0) {
+				imp.impCab(80, false);
+				imp.say(imp.pRow(), 0, imp.normal());
+				imp.say(imp.pRow(), 2, "Cód.fisc.cli.");
+				imp.say(imp.pRow(), 20, "Descrição");
+				imp.say(imp.pRow(), 70, "Qtd.cli.");
+				imp.say(imp.pRow() + 1, 0, imp.normal());
+				imp.say(imp.pRow(), 0, Funcoes.replicate("-",79));
+			}
+			
+			imp.say(imp.pRow() + 1, 0, imp.normal());
+			imp.say(imp.pRow(), 2, rs.getString("CodFiscCli"));
+			imp.say(imp.pRow(), 20, rs.getString("DescFiscCli"));
+			imp.say(imp.pRow(), 70, Funcoes.alinhaDir(rs.getInt(3),8));
+		
+			if (imp.pRow()>=linPag) {
+				imp.say(imp.pRow() + 1, 0, imp.normal());
+				imp.say(imp.pRow(), 0, Funcoes.replicate("-", 79));
+				imp.incPags();
+				imp.eject();
+			}
+		}
+		  
+		imp.say(imp.pRow() + 1, 0, imp.normal());
+		imp.say(imp.pRow(), 0, Funcoes.replicate("=",79));
+		imp.say(imp.pRow() + 1, 0, imp.normal());
+		imp.say(imp.pRow(), 0, "|");
+		imp.say(imp.pRow(), 50, "Total de clientes:");
+		imp.say(imp.pRow(), 80, "|");
+		imp.say(imp.pRow() + 1, 0, imp.normal());
+		imp.say(imp.pRow(), 0, Funcoes.replicate("=",79));
+		imp.eject();  
+		imp.fechaGravacao();
+		
+		
+		imp.eject();
+		imp.fechaGravacao();
+
+		if ( bVisualizar ) {
+			imp.preview( this );
+		}
+		else {
 			imp.print();
+		}
+		
+	} catch ( SQLException err ) {
+		Funcoes.mensagemErro( this, "Erro consulta tabela de tipos de cliente!\n" + err.getMessage(), true, con, err );
+		err.printStackTrace();
+		}
+	}
+	
+	public void imprimirGrafico( final boolean bVisualizar, final ResultSet rs ) {
+
+		FPrinterJob dlGr = new FPrinterJob( "relatorios/TipoCli.jasper", "Vendas por Cliente",null,  rs, null, this );
+
+		if ( bVisualizar ) {
+			dlGr.setVisible( true );
+		}
+		else {
+			try {
+				JasperPrintManager.printReport( dlGr.getRelatorio(), true );
+			} catch ( Exception err ) {
+				Funcoes.mensagemErro( this, "Erro na impressão do relatorio do tipo fiscal de clientes!" + err.getMessage(), true, con, err );
+			}
+		}
 	}
 }
