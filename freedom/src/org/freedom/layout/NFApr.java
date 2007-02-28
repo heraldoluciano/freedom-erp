d82cc4a21f293eee47532d4cf03f951bd46e4e69
marco_stpinf <marco_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-02-28 20:22:48 +0000

    *** empty log message ***

--- freedom/src/org/freedom/layout/NFApr.java
+++ freedom/src/org/freedom/layout/NFApr.java
@@ -5,7 +5,7 @@
  * Projeto: Freedom <BR>
  * Pacote: leiautes <BR>
  * Classe: @(#)NFApr.java <BR>
- * 
+ *  
  * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
  * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
