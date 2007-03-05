2873e239120454a66fb3f35f5314d11902a5fc6a
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-03-05 21:36:57 +0000

    adicionado campo

--- trunk/freedom/src/org/freedom/modulos/std/DLFechaParcela.java
+++ trunk/freedom/src/org/freedom/modulos/std/DLFechaParcela.java
@@ -50,21 +50,30 @@ public class DLFechaParcela extends FFDialogo {
 	  
 	private JTextFieldFK txtDescTipoCob = new JTextFieldFK(JTextFieldPad.TP_STRING,40,0);
 	
+	private JTextFieldPad txtCodBanco = new JTextFieldPad(JTextFieldPad.TP_INTEGER,8,0);
+	
+	private JTextFieldFK txtDescBanco = new JTextFieldFK(JTextFieldPad.TP_STRING,40,0);
+	
 	private ListaCampos lcCob = new ListaCampos(this,"CO");
 	
-	public DLFechaParcela( Component cOrig, Connection cn, BigDecimal bgParcItRec, Date dDtVencItRec, BigDecimal bgDescItRec, Integer iCodTipoCob ) {
+	private ListaCampos lcBanco = new ListaCampos(this,"BO");
+	
+	public DLFechaParcela( Component cOrig, Connection cn, BigDecimal bgParcItRec, Date dDtVencItRec, BigDecimal bgDescItRec, Integer iCodTipoCob, Integer iCodBanco ) {
 
 		super( cOrig );
 		this.con = cn;
 		setTitulo( "Parcela" );
-		setAtribos( 100, 100, 350, 180 );
+		setAtribos( 100, 100, 350, 220 );
 		txtParcItRec.setVlrBigDecimal( bgParcItRec );
 		txtDtVencItRec.setVlrDate( dDtVencItRec );
 		txtVlrDescItRec.setVlrBigDecimal( bgDescItRec );
 		txtCodTipoCob.setVlrInteger( iCodTipoCob );
+		txtCodBanco.setVlrInteger( iCodBanco );
+		
 		if ( bgDescItRec == null ) {
 			txtVlrDescItRec.setAtivo( false );
 		}
+		
 		lcCob.add( new GuardaCampo( txtCodTipoCob, "CodTipoCob", "Cód.Tip.Cob",ListaCampos.DB_PK, false ));
 		lcCob.add( new GuardaCampo( txtDescTipoCob,"DescTipoCob", "Descrição Tipo de Cobrança", ListaCampos.DB_SI, false));
 		lcCob.setQueryCommit(false);
@@ -74,27 +83,43 @@ public class DLFechaParcela extends FFDialogo {
 		txtCodTipoCob.setTabelaExterna(lcCob);
 		txtCodTipoCob.setFK(true);
 		txtCodTipoCob.setNomeCampo("CodTipoCob");
+		
+		lcBanco.add( new GuardaCampo( txtCodBanco, "CodBanco", "Cód.banco",  ListaCampos.DB_PK, false));
+		lcBanco.add( new GuardaCampo( txtDescBanco, "NomeBanco", "Nome do banco", ListaCampos.DB_SI, false));
+		lcBanco.setQueryCommit(false);
+		lcBanco.setReadOnly(true);
+		lcBanco.setConexao( cn );
+		lcBanco.montaSql(false, "BANCO", "FN");
+		txtCodBanco.setTabelaExterna(lcBanco);
+		txtCodBanco.setFK(true);
+		txtCodBanco.setNomeCampo("CodBanco");
+		
 		adic( new JLabelPad( "Valor" ), 7, 0, 100, 20 );
 		adic( new JLabelPad( "Vencimento" ), 110, 0, 100, 20 );
 		adic( new JLabelPad( "Desconto" ), 220, 0, 100, 20 );
 		adic( new JLabelPad("Cód.Tp.Cob"),7,40,80,20);
 		adic( new JLabelPad("Descrição do tipo cobrança"),90, 40, 230, 20);
+		adic( new JLabelPad("Cod.Banco"),7, 80, 80, 20);
+		adic( new JLabelPad("Descrição do banco"),90, 80, 230, 20);
 		adic( txtParcItRec, 7, 20, 100, 20 );
 		adic( txtDtVencItRec, 110, 20, 100, 20 );
 		adic( txtVlrDescItRec, 220, 20, 100, 20 );
 		adic(txtCodTipoCob, 7, 60, 80, 20);
 		adic(txtDescTipoCob, 90, 60, 230, 20);
+		adic(txtCodBanco,7, 100, 80, 20);
+		adic(txtDescBanco,90, 100, 230, 20);
 		lcCob.carregaDados();
 		
 	}
 	 
 	public Object[] getValores() {
 
-		Object[] oRetorno = new Object[ 4 ];
+		Object[] oRetorno = new Object[ 5 ];
 		oRetorno[ 0 ] = txtParcItRec.getVlrBigDecimal();
 		oRetorno[ 1 ] = txtDtVencItRec.getVlrDate();
 		oRetorno[ 2 ] = txtVlrDescItRec.getVlrBigDecimal();
 		oRetorno[ 3 ] = txtCodTipoCob.getVlrInteger();
+		oRetorno[ 4 ] = txtCodBanco.getVlrInteger();
 
 		return oRetorno;
 	}
