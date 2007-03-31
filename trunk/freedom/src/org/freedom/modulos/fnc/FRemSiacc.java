48c98d1153bc472f44eecd8e6d168656ea1ef93d
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-03-31 14:16:05 +0000

    Distribuicao de classes.

--- trunk/freedom/src/org/freedom/modulos/fnc/FRemSiacc.java
+++ trunk/freedom/src/org/freedom/modulos/fnc/FRemSiacc.java
@@ -143,7 +143,7 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 	
 	private String tipoRem = "00";
 
-	private Map<EPrefs, Object> prefs = new HashMap<EPrefs, Object>();
+	private Map<SiaccUtil.EPrefs, Object> prefs = new HashMap<SiaccUtil.EPrefs, Object>();
 	
 	private final Vector<String> vVals = new Vector<String>();
 	
@@ -237,305 +237,7 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 
 	}
 
-	private abstract class Reg {
-
-		protected StringBuilder sbreg = new StringBuilder();
-
-		private Reg( char codreg ) {
-
-			this.sbreg.append( codreg );
-		}
-
-		String format( Object obj, ETipo tipo, int tam, int dec ) {
-
-			String retorno = null;
-			String str = null;
-			// String formato = null;
-			if ( obj == null ) {
-				str = "";
-			}
-			else {
-				str = obj.toString();
-			}
-			if ( tipo == ETipo.$9 ) {
-				retorno = Funcoes.transValor( str, tam, dec, true );
-			}
-			else {
-				retorno = Funcoes.adicionaEspacos( str, tam );
-			}
-			return retorno;
-		}
-
-		public String toString() {
-
-			return sbreg.toString();
-		}
-		
-		protected abstract void parseLine(String line);
-	}
-
-	private class RegA extends Reg {
-
-		private RegA( char codrem, Map map ) {
-
-			super( 'A' );
-			this.sbreg.append( codrem );
-			this.sbreg.append( format( map.get( EPrefs.CODCONV ), ETipo.X, 20, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.NOMEEMP ), ETipo.X, 20, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.CODBANCO ), ETipo.$9, 3, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.NOMEBANCO ), ETipo.X, 20, 0 ) );
-			this.sbreg.append( Funcoes.dataAAAAMMDD( new Date() ) );
-			this.sbreg.append( format( map.get( EPrefs.NROSEQ ), ETipo.$9, 6, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.VERLAYOUT ), ETipo.$9, 2, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.IDENTSERV ), ETipo.X, 17, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.CONTACOMPR ), ETipo.$9, 16, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.IDENTAMBCLI ), ETipo.X, 1, 0 ) );
-			this.sbreg.append( format( map.get( EPrefs.IDENTAMBBCO ), ETipo.X, 1, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 27, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( format( "1", ETipo.$9, 6, 0 ) ); // Número sequencial do registro
-			this.sbreg.append( format( "", ETipo.X, 1, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( (char) 13 );
-			this.sbreg.append( (char) 10 );
-			// this.sbreg.append( "\n");
-
-		}
-		
-		protected void parseLine(String line) {};
-	}
-
-	private class RegB extends Reg {
-
-		private final char COD_MOV = '2';
-
-		private RegB( char codreg, StuffCli stfCli ) {
-
-			super( codreg );
-			this.sbreg.append( format( stfCli.getCodigo(), ETipo.$9, 10, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 15, 0 ) ); // Completar a identificação do cliente na empresa
-			this.sbreg.append( format( stfCli.getArgs()[ EColcli.AGENCIACLI.ordinal() ], ETipo.$9, 4, 0 ) );
-			this.sbreg.append( format( stfCli.getArgs()[ EColcli.IDENTCLI.ordinal() ], ETipo.X, 14, 0 ) );
-			this.sbreg.append( Funcoes.dataAAAAMMDD( new Date() ) );
-			this.sbreg.append( format( "", ETipo.X, 96, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( COD_MOV );
-			this.sbreg.append( format( "", ETipo.X, 1, 0 ) ); // Reservado para o futuro
-			// this.sbreg.append( "\n");
-
-			this.sbreg.append( (char) 13 );
-			this.sbreg.append( (char) 10 );
-		}
-
-		protected void parseLine(String line) {};
-		
-	}
-
-	private class RegC extends Reg {
-
-		private final char COD_MOV = '2';
-
-		private RegC( char codreg, StuffCli stfCli, int numSeq ) {
-
-			super( codreg );
-			this.sbreg.append( format( stfCli.getCodigo(), ETipo.$9, 10, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 15, 0 ) ); // Completar a identificação do cliente na empresa
-			this.sbreg.append( format( stfCli.getArgs()[ EColcli.AGENCIACLI.ordinal() ], ETipo.$9, 4, 0 ) );
-			this.sbreg.append( format( stfCli.getArgs()[ EColcli.IDENTCLI.ordinal() ], ETipo.X, 14, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 40, 0 ) ); // Ocorrencia 1
-			this.sbreg.append( format( "", ETipo.X, 40, 0 ) ); // Ocorrencia 2
-			this.sbreg.append( format( "", ETipo.X, 19, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( format( numSeq, ETipo.$9, 6, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( COD_MOV );
-			// this.sbreg.append( "\n");
-			this.sbreg.append( (char) 13 );
-			this.sbreg.append( (char) 10 );
-		}
-
-		protected void parseLine(String line) {};
-
-	}
-
-	private class RegD extends Reg {
-
-		private final char COD_MOV = '0';
-
-		private RegD( char codreg, StuffCli stfCli, int numSeq ) {
-
-			super( codreg );
-			this.sbreg.append( format( stfCli.getCodigo(), ETipo.$9, 10, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 15, 0 ) ); // Completar a identificação do cliente na empresa
-			this.sbreg.append( format( stfCli.getArgs()[ EColcli.AGENCIACLI.ordinal() ], ETipo.$9, 4, 0 ) );
-			this.sbreg.append( format( stfCli.getArgs()[ EColcli.IDENTCLI.ordinal() ], ETipo.X, 14, 0 ) );
-			this.sbreg.append( format( stfCli.getCodigo(), ETipo.$9, 10, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 15, 0 ) ); // Completar a identificação do cliente na empresa
-			this.sbreg.append( format( "", ETipo.X, 60, 0 ) ); // Ocorrencia
-			this.sbreg.append( format( "", ETipo.X, 14, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( format( numSeq, ETipo.$9, 6, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( COD_MOV );
-			// this.sbreg.append( "\n");
-			this.sbreg.append( (char) 13 );
-			this.sbreg.append( (char) 10 );
-		}
-
-		protected void parseLine(String line) {};
-		
-	}
-
-	private class RegE extends Reg {
-
-		private final char COD_MOV = '0';
-
-		private final String COD_MOEDA = "03";
-		
-		private float vlrParc = 0;
-
-		private RegE( char codreg, StuffRec stfRec, int numSeq, int numAgenda ) {
-
-			super( codreg );
-			this.vlrParc =  Float.valueOf(stfRec.getArgs()[ EColrec.VLRAPAG.ordinal() ]);
-			this.sbreg.append( format( stfRec.getArgs()[ EColrec.CODCLI.ordinal() ], ETipo.$9, 10, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 15, 0 ) ); // Completar a identificação do cliente na empresa
-			this.sbreg.append( format( stfRec.getArgs()[ EColrec.AGENCIACLI.ordinal() ], ETipo.$9, 4, 0 ) );
-			this.sbreg.append( format( stfRec.getArgs()[ EColrec.IDENTCLI.ordinal() ], ETipo.X, 14, 0 ) );
-			this.sbreg.append( format( stfRec.getArgs()[ EColrec.DTVENC.ordinal() ], ETipo.$9, 8, 0 ) );
-			this.sbreg.append( format( Funcoes.transValor(new BigDecimal(vlrParc), 15, 2, true), ETipo.$9, 15, 0 ) );
-			this.sbreg.append( COD_MOEDA );
-			this.sbreg.append( format( "", ETipo.X, 60, 0 ) ); // Uso da empresa
-			this.sbreg.append( format( numAgenda, ETipo.$9, 6, 0 ) );
-			this.sbreg.append( format( "", ETipo.X, 8, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( format( numSeq, ETipo.$9, 6, 0 ) ); // Reservado para o futuro
-			this.sbreg.append( COD_MOV );
-			// this.sbreg.append( "\n");
-			this.sbreg.append( (char) 13 );
-			this.sbreg.append( (char) 10 );
-		}
-		
-		private float getVlrparc() {
-			return this.vlrParc;
-		}
-		
-		protected void parseLine(String line) {};
-		
-	}
-
-	private class RegZ extends Reg {
-		private RegZ( int totreg, float vlrtotal, int nroseq ) {
-			super('Z');
-			this.sbreg.append( format( totreg, ETipo.$9, 6, 0) );
-			this.sbreg.append( format( vlrtotal, ETipo.$9, 17, 2) );
-			this.sbreg.append( format( "", ETipo.X, 119, 0)); // Reservado para o futuro
-			this.sbreg.append( format( nroseq, ETipo.$9, 6, 0 ));
-			this.sbreg.append( format( "", ETipo.$9, 1, 0));
-		}
-
-		protected void parseLine(String line) {};
-
-	}
 	
-	private class StuffCli {
-		
-	
-		private String[] stfArgs = null;
-	
-		private Integer codigo = null;
-	
-		private StuffCli( Integer codCli, String[] args ) {
-	
-			// System.out.println(args.length);
-			this.codigo = codCli;
-			this.stfArgs = args;
-		}
-	
-		public String[] getArgs() {
-	
-			return this.stfArgs;
-		}
-	
-		public Integer getCodigo() {
-	
-			return this.codigo;
-		}
-	
-		public boolean equals( Object obj ) {
-	
-			if ( obj instanceof StuffCli )
-				return codigo.equals( ( (StuffCli) obj ).getCodigo() );
-			else
-				return false;
-		}
-	
-		public int hashCode() {
-	
-			return this.codigo.hashCode();
-		}
-	}
-
-	private class StuffRec {
-	
-		private String[] stfArgs = null;
-	
-		private Integer chave1 = null;
-	
-		private Integer chave2 = null;
-	
-		private Integer[] chaveComp = new Integer[ 2 ];
-	
-		private StuffRec( Integer codRec, Integer nParcItRec, String[] args ) {
-	
-			// System.out.println(args.length);
-			this.chave1 = codRec;
-			this.chave2 = nParcItRec;
-			this.chaveComp[ 0 ] = codRec;
-			this.chaveComp[ 1 ] = nParcItRec;
-			this.stfArgs = args;
-		}
-	
-		public String[] getArgs() {
-	
-			return this.stfArgs;
-		}
-	
-		public Integer getCodrec() {
-	
-			return this.chave1;
-		}
-	
-		public Integer getNParcitrec() {
-	
-			return this.chave2;
-		}
-
-		public void setSitremessa(String sit) {
-			this.stfArgs[EColrec.SITREMESSA.ordinal()] = sit;
-		}
-		
-		public boolean equals( Object obj ) {
-	
-			if ( obj instanceof StuffRec )
-				return ( ( chave1.equals( ( (StuffRec) obj ).getCodrec() ) ) && ( chave2.equals( ( (StuffRec) obj ).getNParcitrec() ) ) );
-			else
-				return false;
-		}
-	
-		public int hashCode() {
-	
-			return chaveComp.hashCode();
-		}
-	}
-
-	private enum EPrefs {
-		CODBANCO, NOMEBANCO, CODCONV, NOMEEMP, VERLAYOUT, IDENTSERV, CONTACOMPR, IDENTAMBCLI, IDENTAMBBCO, NROSEQ
-	}
-
-	private enum EColcli {
-		CODBANCO, TIPOFEBRABAN, STIPOFEBRABAN, AGENCIACLI, IDENTCLI, TIPOREMCLI
-	}
-
-	private enum EColrec {
-		CODBANCO, TIPOFEBRABAN, STIPOFEBRABAN, SITREMESSA, CODCLI, AGENCIACLI, IDENTCLI, DTVENC, VLRAPAG
-	}
-
-	private enum ETipo {
-		X, $9
-	}
 
 	private void montaTela() {
 	
@@ -600,16 +302,16 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 			ps.setString( 6, TIPO_FEBRABAN );
 			ResultSet rs = ps.executeQuery();
 			if ( rs.next() ) {
-				prefs.put( EPrefs.CODCONV, rs.getString( EPrefs.CODCONV.toString() ) );
-				prefs.put( EPrefs.NOMEEMP, rs.getString( EPrefs.NOMEEMP.toString() ) );
-				prefs.put( EPrefs.VERLAYOUT, rs.getString( EPrefs.VERLAYOUT.toString() ) );
-				prefs.put( EPrefs.CODBANCO, txtCodBanco.getVlrString() );
-				prefs.put( EPrefs.NOMEBANCO, txtNomeBanco.getVlrString() );
-				prefs.put( EPrefs.IDENTSERV, rs.getString( EPrefs.IDENTSERV.toString() ) );
-				prefs.put( EPrefs.CONTACOMPR, rs.getString( EPrefs.CONTACOMPR.toString() ) );
-				prefs.put( EPrefs.IDENTAMBCLI, rs.getString( EPrefs.IDENTAMBCLI.toString() ) );
-				prefs.put( EPrefs.IDENTAMBBCO, rs.getString( EPrefs.IDENTAMBBCO.toString() ) );
-				prefs.put( EPrefs.NROSEQ, rs.getInt( EPrefs.NROSEQ.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.CODCONV, rs.getString( SiaccUtil.EPrefs.CODCONV.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.NOMEEMP, rs.getString( SiaccUtil.EPrefs.NOMEEMP.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.VERLAYOUT, rs.getString( SiaccUtil.EPrefs.VERLAYOUT.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.CODBANCO, txtCodBanco.getVlrString() );
+				prefs.put( SiaccUtil.EPrefs.NOMEBANCO, txtNomeBanco.getVlrString() );
+				prefs.put( SiaccUtil.EPrefs.IDENTSERV, rs.getString( SiaccUtil.EPrefs.IDENTSERV.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.CONTACOMPR, rs.getString( SiaccUtil.EPrefs.CONTACOMPR.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.IDENTAMBCLI, rs.getString( SiaccUtil.EPrefs.IDENTAMBCLI.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.IDENTAMBBCO, rs.getString( SiaccUtil.EPrefs.IDENTAMBBCO.toString() ) );
+				prefs.put( SiaccUtil.EPrefs.NROSEQ, rs.getInt( SiaccUtil.EPrefs.NROSEQ.toString() ) );
 			}
 			rs.close();
 			ps.close();
@@ -739,8 +441,8 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 		File fileSiacc = null;
 		FileWriter fw = null;
 		BufferedWriter bw = null;
-		HashSet<StuffCli> hsCli = new HashSet<StuffCli>();
-		HashSet<StuffRec> hsRec = new HashSet<StuffRec>();
+		HashSet<SiaccUtil.StuffCli> hsCli = new HashSet<SiaccUtil.StuffCli>();
+		HashSet<SiaccUtil.StuffRec> hsRec = new HashSet<SiaccUtil.StuffRec>();
 	
 		if ( consisteExporta( hsCli, hsRec ) ) {
 	
@@ -781,56 +483,56 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 		return retorno;
 	}
 
-	private void atualizaSitremessaExp(HashSet<StuffCli> hsCli, HashSet<StuffRec> hsRec) {
+	private void atualizaSitremessaExp(HashSet<SiaccUtil.StuffCli> hsCli, HashSet<SiaccUtil.StuffRec> hsRec) {
 		setSitremessa(hsRec, "01");
 		persisteDados( hsCli, hsRec );
 	}
 	
-	private void setSitremessa(HashSet<StuffRec> hsRec, final String sit) {
-		for (StuffRec sr: hsRec) {
+	private void setSitremessa(HashSet<SiaccUtil.StuffRec> hsRec, final String sit) {
+		for (SiaccUtil.StuffRec sr: hsRec) {
 			sr.setSitremessa( sit );
 		}
 	}
 	
-	private boolean gravaRemessa( final BufferedWriter bw, HashSet<StuffCli> hsCli, HashSet<StuffRec> hsRec, String sitRemessa ) {
+	private boolean gravaRemessa( final BufferedWriter bw, HashSet<SiaccUtil.StuffCli> hsCli, HashSet<SiaccUtil.StuffRec> hsRec, String sitRemessa ) {
 
 		boolean retorno = false;
 		try {
-			ArrayList<Reg> list = new ArrayList<Reg>();
-			list.add( new RegA( '1', prefs ) );
+			ArrayList<SiaccUtil.Reg> list = new ArrayList<SiaccUtil.Reg>();
+			list.add( new SiaccUtil().new RegA( '1', prefs ) );
 			int i = 2;
 			int numAgenda = 1;
 			float vlrtotal = 0;
-			RegE e = null;
+			SiaccUtil.RegE e = null;
 
-			for ( StuffCli c : hsCli ) {
-				if ( "B".equals( c.getArgs()[ EColcli.TIPOREMCLI.ordinal() ] ) ) {
-					list.add( new RegB( 'B', c ) );
+			for ( SiaccUtil.StuffCli c : hsCli ) {
+				if ( "B".equals( c.getArgs()[ SiaccUtil.EColcli.TIPOREMCLI.ordinal() ] ) ) {
+					list.add(  new  SiaccUtil().new RegB( 'B', c ) ) ;
 					i++;
 				}
 			}
-			for ( StuffCli c : hsCli ) {
-				if ( "C".equals( c.getArgs()[ EColcli.TIPOREMCLI.ordinal() ] ) ) {
-					list.add( new RegC( 'C', c, i++ ) );
+			for ( SiaccUtil.StuffCli c : hsCli ) {
+				if ( "C".equals( c.getArgs()[ SiaccUtil.EColcli.TIPOREMCLI.ordinal() ] ) ) {
+					list.add( new SiaccUtil().new RegC( 'C', c, i++ ) );
 				}
 			}
-			for ( StuffCli c : hsCli ) {
-				if ( "D".equals( c.getArgs()[ EColcli.TIPOREMCLI.ordinal() ] ) ) {
-					list.add( new RegD( 'D', c, i++ ) );
+			for ( SiaccUtil.StuffCli c : hsCli ) {
+				if ( "D".equals( c.getArgs()[ SiaccUtil.EColcli.TIPOREMCLI.ordinal() ] ) ) {
+					list.add( new SiaccUtil().new RegD( 'D', c, i++ ) );
 				}
 			}
-			for ( StuffRec r : hsRec ) {
-				if ( sitRemessa.indexOf(( r.getArgs()[ EColrec.SITREMESSA.ordinal() ] ))>-1 ) {
-					e = new RegE( 'E', r, i++, numAgenda );
+			for ( SiaccUtil.StuffRec r : hsRec ) {
+				if ( sitRemessa.indexOf(( r.getArgs()[ SiaccUtil.EColrec.SITREMESSA.ordinal() ] ))>-1 ) {
+					e = new SiaccUtil().new RegE( 'E', r, i++, numAgenda );
 					list.add( e );
 					vlrtotal += e.getVlrparc();
 					numAgenda++;
 				}
 			}
 			
-			list.add(new RegZ(i, vlrtotal, i++));
+			list.add(new SiaccUtil().new RegZ(i, vlrtotal, i++));
 			
-			for ( Reg reg : list ) {
+			for ( SiaccUtil.Reg reg : list ) {
 				bw.write( reg.toString() );
 			}
 			bw.flush();
@@ -969,7 +671,7 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 		return retorno;
 	}
 
-	private boolean consisteExporta( HashSet<StuffCli> hsCli, HashSet<StuffRec> hsRec ) {
+	private boolean consisteExporta( HashSet<SiaccUtil.StuffCli> hsCli, HashSet<SiaccUtil.StuffRec> hsRec ) {
 
 		boolean retorno = true;
 		Vector vLinha = null;
@@ -985,9 +687,9 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 						break;
 					}
 				}
-				hsCli.add( new StuffCli( (Integer) vLinha.elementAt( COL_CODCLI ), new String[] { txtCodBanco.getVlrString(), TIPO_FEBRABAN, (String) vLinha.elementAt( COL_STIPOFEBRABAN ), (String) vLinha.elementAt( COL_AGENCIACLI ), (String) vLinha.elementAt( COL_IDENTCLI ),
+				hsCli.add( new SiaccUtil().new StuffCli( (Integer) vLinha.elementAt( COL_CODCLI ), new String[] { txtCodBanco.getVlrString(), TIPO_FEBRABAN, (String) vLinha.elementAt( COL_STIPOFEBRABAN ), (String) vLinha.elementAt( COL_AGENCIACLI ), (String) vLinha.elementAt( COL_IDENTCLI ),
 						(String) vLinha.elementAt( COL_TIPOREMCLI ) } ) );
-				hsRec.add( new StuffRec( (Integer) vLinha.elementAt( COL_CODREC ), (Integer) vLinha.elementAt( COL_NRPARC ),
+				hsRec.add( new SiaccUtil().new StuffRec( (Integer) vLinha.elementAt( COL_CODREC ), (Integer) vLinha.elementAt( COL_NRPARC ),
 				/*
 				 * String codBanco, String tipoFebraban, String stipoFebraban, String sitRemessa {CODBANCO, TIPOFEBRABAN, STIPOFEBRABAN, SITREMESSA, CODCLI, AGENCIACLI, IDENTCLI, DTVENC, VLRPARC}
 				 */
@@ -1007,20 +709,26 @@ public class FRemSiacc extends FFilho implements ActionListener, MouseListener {
 		return retorno;
 	}
 
-	private boolean persisteDados( final HashSet<StuffCli> hsCli, final HashSet<StuffRec> hsRec ) {
+	private boolean persisteDados( final HashSet<SiaccUtil.StuffCli> hsCli, final HashSet<SiaccUtil.StuffRec> hsRec ) {
 
 		boolean retorno = true;
-		for ( StuffCli stfCli : hsCli ) {
-			retorno = updateCliente( stfCli.getCodigo(), stfCli.getArgs()[ EColcli.CODBANCO.ordinal() ], stfCli.getArgs()[ EColcli.TIPOFEBRABAN.ordinal() ], stfCli.getArgs()[ EColcli.STIPOFEBRABAN.ordinal() ], stfCli.getArgs()[ EColcli.AGENCIACLI.ordinal() ], stfCli.getArgs()[ EColcli.IDENTCLI
-					.ordinal() ], stfCli.getArgs()[ EColcli.TIPOREMCLI.ordinal() ] );
+		for ( SiaccUtil.StuffCli stfCli : hsCli ) {
+			retorno = updateCliente( stfCli.getCodigo(), stfCli.getArgs()[ SiaccUtil.EColcli.CODBANCO.ordinal() ], 
+					stfCli.getArgs()[ SiaccUtil.EColcli.TIPOFEBRABAN.ordinal() ], stfCli.getArgs()[ SiaccUtil.EColcli.STIPOFEBRABAN.ordinal() ], 
+					stfCli.getArgs()[ SiaccUtil.EColcli.AGENCIACLI.ordinal() ], stfCli.getArgs()[ SiaccUtil.EColcli.IDENTCLI
+					.ordinal() ], stfCli.getArgs()[ SiaccUtil.EColcli.TIPOREMCLI.ordinal() ] );
 			if ( !retorno ) {
 				retorno = false;
 				break;
 			}
 		}
 		if ( retorno ) {
-			for ( StuffRec stfRec : hsRec ) {
-				retorno = updateReceber( stfRec.getCodrec(), stfRec.getNParcitrec(), stfRec.getArgs()[ EColrec.CODBANCO.ordinal() ], stfRec.getArgs()[ EColrec.TIPOFEBRABAN.ordinal() ], stfRec.getArgs()[ EColrec.STIPOFEBRABAN.ordinal() ], stfRec.getArgs()[ EColrec.SITREMESSA.ordinal() ] );
+			for ( SiaccUtil.StuffRec stfRec : hsRec ) {
+				retorno = updateReceber( stfRec.getCodrec(), stfRec.getNParcitrec(), 
+						stfRec.getArgs()[ SiaccUtil.EColrec.CODBANCO.ordinal() ], 
+						stfRec.getArgs()[ SiaccUtil.EColrec.TIPOFEBRABAN.ordinal() ], 
+						stfRec.getArgs()[ SiaccUtil.EColrec.STIPOFEBRABAN.ordinal() ], 
+						stfRec.getArgs()[ SiaccUtil.EColrec.SITREMESSA.ordinal() ] );
 				if ( !retorno ) {
 					retorno = false;
 					break;
