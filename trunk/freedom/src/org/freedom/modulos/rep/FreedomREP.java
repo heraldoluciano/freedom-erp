d2c2796c44e21b1a730b74c486542eb3672afafa
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-04-04 23:47:12 +0000

    Inclus�o no menu do formulario de esta��o de trabalho e de prefer�ncia gerais.

--- trunk/freedom/src/org/freedom/modulos/rep/FreedomREP.java
+++ trunk/freedom/src/org/freedom/modulos/rep/FreedomREP.java
@@ -56,8 +56,9 @@ public class FreedomREP extends AplicativoRep implements ActionListener {
 				addOpcao( 100100000, TP_OPCAO_ITEM, "Moeda", "Moeda", 'M', 100111000, 2, true, RPMoeda.class );
 				addOpcao( 100100000, TP_OPCAO_ITEM, "Cotação", "Cotação", 'a', 100112000, 2, true, RPCotMoeda.class );
 			addSeparador( 100000000 );
-			addOpcao( 100000000, TP_OPCAO_ITEM, "Preferências", "Preferências", 'P', 100200000, 1, true, null );
-			addOpcao( 100000000, TP_OPCAO_ITEM, "Empresa", "Empresa", 'E', 100200000, 1, true, RPEmpresa.class );
+			addOpcao( 100000000, TP_OPCAO_ITEM, "Preferências", "Preferências", 'P', 100200000, 1, true, RPPrefereGeral.class );
+			addOpcao( 100000000, TP_OPCAO_ITEM, "Estação de trabalho", "Estação de trabalho", 't', 100300000, 1, true, RPEstacao.class );
+			addOpcao( 100000000, TP_OPCAO_ITEM, "Empresa", "Empresa", 'E', 100400000, 1, true, RPEmpresa.class );
 
 		addOpcao( -1, TP_OPCAO_MENU, "Vendas", "", 'V', 200000000, 0, false, null );
 			addOpcao( 200000000, TP_OPCAO_ITEM, "Pedidos", "Pedidos", 'P', 200100000, 1, true, RPPedido.class );
