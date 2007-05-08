6cdeed41273f500285682aec198f91171e80c175
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-08 14:25:01 +0000

    Alterações para fazer a baixa das parcelas.

--- freedom/src/org/freedom/modulos/fnc/SiaccUtil.java
+++ freedom/src/org/freedom/modulos/fnc/SiaccUtil.java
@@ -17,6 +17,13 @@ class SiaccUtil {
 		CODBANCO, TIPOFEBRABAN, STIPOFEBRABAN, SITREMESSA, CODCLI, AGENCIACLI, 
 		IDENTCLI, DTVENC, VLRAPAG, PESSOACLI, CPFCLI, CNPJCLI
 	}
+	
+	enum EParcela {
+		CODREC,NPARCITREC,VLRITREC,VLRDESCITREC,VLRMULTAITREC,VLRJUROSITREC,
+	    VLRPARCITREC,VLRPAGOITREC,VLRAPAGITREC,DTITREC,DTVENCITREC,DTPAGOITREC,
+	    STATUSITREC,CODPLAN,OBSITREC,NUMCONTA,CODBANCO,CODTIPOCOB,DOCLANCAITREC,
+	    FLAG,ANOCC,CODCC,VLRCOMIITREC;
+	}
 
 	enum EPrefs {
 		CODBANCO, NOMEBANCO, CODCONV, NOMEEMP, VERLAYOUT, IDENTSERV, CONTACOMPR, IDENTAMBCLI, IDENTAMBBCO, NROSEQ
@@ -1219,15 +1226,14 @@ class SiaccUtil {
 	
 		private Integer chave2 = null;
 	
-		private Integer[] chaveComp = new Integer[ 2 ];
+		private String chaveComp = null;
 	
 		StuffRec( Integer codRec, Integer nParcItRec, String[] args ) {
 	
 			// System.out.println(args.length);
 			this.chave1 = codRec;
 			this.chave2 = nParcItRec;
-			this.chaveComp[ 0 ] = codRec;
-			this.chaveComp[ 1 ] = nParcItRec;
+			this.chaveComp = "[" + String.valueOf( codRec ) + "][" +String.valueOf( nParcItRec ) +"]";
 			this.stfArgs = args;
 		}
 	
@@ -1262,6 +1268,47 @@ class SiaccUtil {
 	
 			return chaveComp.hashCode();
 		}
+	}
+	
+	class StuffParcela {
+		
+		private Integer codrec = null;
+		private Integer numparcrec = null;
+		private Object[] args = null;
+		
+		private String chaveComp = null;
+		
+		StuffParcela( final Integer codrec, final Integer numparcrec, final Object[] args ) {
+			
+			this.codrec = codrec;
+			this.numparcrec = numparcrec;
+			this.args = args;
+			this.chaveComp = "[" + String.valueOf( codrec ) + "][" +String.valueOf( numparcrec ) +"]";
+		}		
+		
+		public Object[] getArgs() {		
+			return args;
+		}
+		
+		public Integer getCodrec() {		
+			return codrec;
+		}
+		
+		public Integer getNumparcrec() {		
+			return numparcrec;
+		}
+
+		public boolean equals( Object obj ) {
+	
+			if ( obj instanceof StuffParcela )
+				return ( ( codrec.equals( ( (StuffParcela)obj ).getCodrec() ) ) && ( numparcrec.equals( ( (StuffParcela)obj ).getNumparcrec() ) ) );
+			else
+				return false;
+		}
+	
+		public int hashCode() {
+			return chaveComp.hashCode();
+		}		
 	}
 
 	/**
