665580c5df0898765e3c3f1c33ad0eabd852701f
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-11 20:56:17 +0000

    Ajustes dos bats e nomes de bibliotecas.

--- trunk/freedom/lib/exec/freedomfnc.bat
+++ trunk/freedom/lib/exec/freedomfnc.bat
@@ -1,3 +1,2 @@
 cd \opt\freedom
-javaw -classpath lib\freedomfnc.jar;lib\jcommon-0.8.7.jar;lib\jfreechart-0.9.12.jar;lib\itext-1.02b.jar;lib\firebirdsql-full.jar;lib\nachocalendar-0.21.jar;lib\commons-logging.jar;lib\jasperreports-0.6.7.jar;lib\liquidlnf.jar;lib\metouia.jar -DARQINI=freedom.ini org.freedom.modulos.fnc.FreedomFNC
-
+javaw -classpath lib\freedomfnc.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar;lib\liquidlnf.jar;lib\metouia.jar -DARQINI=freedom.ini -DARQLOG=freedomfnc.log org.freedom.modulos.fnc.FreedomFNC
