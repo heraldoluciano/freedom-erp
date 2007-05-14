6c8d0cf512b143a9bd2b4dc129a993edadfb73ec
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-14 16:39:20 +0000

    Nova versão 1.1.3.4 ; Ajuste no campo de código de fornecedor na contabilidade.

--- trunk/freedom/setup/build/geradisco.bat
+++ trunk/freedom/setup/build/geradisco.bat
@@ -8,17 +8,23 @@ mkdir ..\disco\opt\firebird\UDF
 mkdir ..\disco\opt\freedom
 mkdir ..\disco\opt\freedom\lib
 mkdir ..\disco\opt\freedom\icones
+mkdir ..\disco\opt\freedom\log
+mkdir ..\disco\opt\freedom\bin
+mkdir ..\disco\opt\freedom\ini
 del ..\disco\opt\freedom\*.* /Q
 del ..\disco\opt\freedom\lib\*.* /Q
 del ..\disco\opt\freedom\icones\*.* /Q
+del ..\disco\opt\freedom\log\*.* /Q
+del ..\disco\opt\freedom\bin\*.* /Q
+del ..\disco\opt\freedom\ini\*.* /Q
 del ..\disco\opt\firebird\dados\*.* /Q
 del ..\disco\disco.zip /Q
 7z x -o..\disco\opt\firebird\dados\ -tzip ..\..\dados\estrutura\freedom.zip
 copy ..\..\src\org\freedom\udf\udfinstall.bat ..\disco\opt\firebird\UDF
 copy ..\..\src\org\freedom\udf\stp_udf_win.sql ..\disco\opt\firebird\UDF
 copy ..\..\src\org\freedom\udf\stp_udf.dll ..\disco\opt\firebird\UDF
-copy ..\..\src\org\freedom\images\*.ico ..\disco\opt\firebird\icones
+copy ..\..\src\org\freedom\images\*.ico ..\disco\opt\freedom\icones
 copy ..\..\lib\*.jar ..\disco\opt\freedom\lib
-copy ..\..\lib\exec\*.bat ..\disco\opt\freedom
-copy ..\..\freedomwin.ini ..\disco\opt\freedom\freedom.ini
+copy ..\..\lib\exec\*.bat ..\disco\opt\freedom\bin
+copy ..\..\freedomwin.ini ..\disco\opt\freedom\ini\freedom.ini
 7z a -tzip ..\disco\win\disco ..\disco\opt\
