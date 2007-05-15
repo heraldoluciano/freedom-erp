0c63dcf040af61dc6591fbe12a96558f4a679cc0
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-15 13:57:26 +0000

    exclusão da tag

--- tags/1.1.3.5/lib/exec/freedomgms.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /opt/freedom
-java -classpath lib/freedomgms.jar:lib/jcommon-0.8.7.jar:lib/jfreechart-0.9.12.jar:lib/itext-1.02b.jar:lib/firebirdsql-full.jar:lib/nachocalendar-0.21.jar:lib/commons-logging.jar:lib/jasperreports-0.6.7.jar -DARQINI=freedom.ini org.freedom.modulos.gms.FreedomGMS
-
