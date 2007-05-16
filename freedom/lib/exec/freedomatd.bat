f03d0387df73b581b65c3684da907c04afd624c9
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-16 17:12:11 +0000

    Inclusão de libs nos bats.

--- freedom/lib/exec/freedomatd.bat
+++ freedom/lib/exec/freedomatd.bat
@@ -1,2 +1,1 @@
-cd \opt\freedom
-start javaw -classpath lib\freedomatd.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar;lib\liquidlnf.jar;lib\metouia.jar -DARQINI=ini\freedom.ini -DARQLOG=log\freedomatd.log org.freedom.modulos.atd.FreedomATD
+start javaw -classpath lib\freedomatd.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar;lib\liquidlnf.jar;lib\metouia.jar;lib\commons-beanutils.jar;lib\commons-collections.jar;lib\mail.jar -DARQINI=ini\freedom.ini -DARQLOG=log\freedomatd.log org.freedom.modulos.atd.FreedomATD
--- freedom/lib/exec/freedomtmk.bat
+++ freedom/lib/exec/freedomtmk.bat
@@ -1,2 +1,1 @@
-cd \opt\freedom
-start javaw -classpath lib\freedomtmk.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar -DARQINI=ini\freedom.ini -DARQLOG=log\freedomtmk.log org.freedom.modulos.tmk.FreedomTMK
+start javaw -classpath lib\freedomtmk.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar;lib\liquidlnf.jar;lib\metouia.jar;lib\commons-beanutils.jar;lib\commons-collections.jar;lib\mail.jar -DARQINI=ini\freedom.ini -DARQLOG=log\freedomtmk.log org.freedom.modulos.tmk.FreedomTMK
