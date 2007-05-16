2a32aee6cf72492f8706095e36fafa9037c124bf
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-16 17:12:11 +0000

    Inclusão de libs nos bats.

--- trunk/freedom/lib/exec/freedompdv.bat
+++ trunk/freedom/lib/exec/freedompdv.bat
@@ -1,2 +1,1 @@
-cd \opt\freedom
-start javaw -classpath lib\freedompdv.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar;lib\liquidlnf.jar;lib\metouia.jar -DARQINI=ini\freedom.ini -DARQLOG=log\freedompdv.log org.freedom.modulos.pdv.FreedomPDV
+start javaw -classpath lib\freedompdv.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar;lib\liquidlnf.jar;lib\metouia.jar;lib\commons-beanutils.jar;lib\commons-collections.jar;lib\mail.jar -DARQINI=ini\freedom.ini -DARQLOG=log\freedompdv.log org.freedom.modulos.pdv.FreedomPDV
