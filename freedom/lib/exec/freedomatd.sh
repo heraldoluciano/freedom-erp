0a693424894d53956efe9cfb4d7b8780f4212291
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-17 18:45:42 +0000

    Novos scripts de inicialização.

--- freedom/lib/exec/freedomatd.sh
+++ freedom/lib/exec/freedomatd.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-cd /opt/freedom
-java -classpath lib/freedomatd.jar:lib/jcommon-0.8.7.jar:lib/jfreechart-0.9.12.jar:lib/itext-1.02b.jar:lib/firebirdsql-full.jar:lib/nachocalendar-0.21.jar:lib/commons-logging.jar:lib/jasperreports-0.6.7.jar -DARQINI=freedom.ini  org.freedom.modulos.atd.FreedomATD
+java -classpath lib/freedomatd.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/itext.jar:lib/firebirdsql-full.jar:lib/nachocalendar.jar:lib/commons-logging.jar:lib/jasperreports.jar:lib/liquidlnf.jar:lib/metouia.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/mail.jar -DARQINI=ini/freedom.ini -DARQLOG=log/freedomatd.log org.freedom.modulos.atd.FreedomATD
