2b3b9410fb43e9525dfae04d78a8986bf8152934
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-17 18:46:47 +0000

    Script de geração de discos para Linux.

---
+++ trunk/freedom/lib/exec/freedompdv.sh
@@ -0,0 +1,1 @@
+java -classpath lib/freedompdv.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/itext.jar:lib/firebirdsql-full.jar:lib/nachocalendar.jar:lib/commons-logging.jar:lib/jasperreports.jar:lib/liquidlnf.jar:lib/metouia.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/mail.jar -DARQINI=ini/freedom.ini -DARQLOG=log/freedompdv.log org.freedom.modulos.pdv.FreedomPDV
