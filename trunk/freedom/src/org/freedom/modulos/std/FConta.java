9a7ab61ab0c834ab0f15a83401dda32b4d955228
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-19 15:00:49 +0000

    Implementação para permitir controle de acesso por conta.

--- trunk/freedom/src/org/freedom/modulos/std/FConta.java
+++ trunk/freedom/src/org/freedom/modulos/std/FConta.java
@@ -20,17 +20,24 @@
  */
 
 package org.freedom.modulos.std;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.sql.Connection;
 import java.util.Vector;
-
+import javax.swing.JScrollPane;
+import org.freedom.acao.CheckBoxEvent;
+import org.freedom.acao.CheckBoxListener;
 import org.freedom.componentes.GuardaCampo;
 import org.freedom.componentes.JCheckBoxPad;
+import org.freedom.componentes.JPanelPad;
 import org.freedom.componentes.JRadioGroup;
 import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
-import org.freedom.telas.FDados;
-public class FConta extends FDados {
+import org.freedom.componentes.Navegador;
+import org.freedom.componentes.Tabela;
+import org.freedom.telas.FTabDados;
+public class FConta extends FTabDados implements CheckBoxListener {
 
 	private static final long serialVersionUID = 1L;
 
@@ -45,17 +52,37 @@ public class FConta extends FDados {
   private JTextFieldFK txtDescMoeda = new JTextFieldFK(JTextFieldPad.TP_STRING, 50,0);
   private JTextFieldFK txtDescPlan = new JTextFieldFK(JTextFieldPad.TP_STRING, 40, 0);
   private JCheckBoxPad cbAtivaConta = new JCheckBoxPad("Sim","S","N");
-  private Vector vValsTipo = new Vector();
-  private Vector vLabsTipo = new Vector();
+  private JCheckBoxPad chbRestritoTipoMov = new JCheckBoxPad( "Permitir todos os usuários?", "S", "N" );
+  private Vector<String> vValsTipo = new Vector<String>();
+  private Vector<String> vLabsTipo = new Vector<String>();
   private JRadioGroup rgTipo = null;
   private ListaCampos lcBanco = new ListaCampos(this,"BO");
   private ListaCampos lcMoeda = new ListaCampos(this,"MA");
   private ListaCampos lcPlan = new ListaCampos(this,"PN");
+  private ListaCampos lcRestricoes = new ListaCampos( this, "" );
+  private ListaCampos lcUsu = new ListaCampos( this, "US" );
+  
+  private JPanelPad pinGeral = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );
+  private JPanelPad pinCamposGeral = new JPanelPad( 430, 460 );
+  private JPanelPad pnRestricoes = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );
+  private JPanelPad pinDetRestricoes = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );  
+  private Tabela tbRestricoes = new Tabela();
+  private JScrollPane spnRestricoes = new JScrollPane( tbRestricoes );
+  private JPanelPad pinNavRestricoes = new JPanelPad( 680, 30 );
+  private JPanelPad pinCamposRestricoes = new JPanelPad( 430, 460 );
+  private Navegador navRestricoes = new Navegador( true );
+  private JTextFieldPad txtIDUsu = new JTextFieldPad( JTextFieldPad.TP_STRING, 8, 0 );
+  private JTextFieldFK txtNomeUsu = new JTextFieldFK( JTextFieldPad.TP_STRING, 50, 0 );
+  
   public FConta() {
   	super();
     setTitulo("Cadastro de Contas");
-    setAtribos(50,50,415,290);
-
+    setAtribos(50,50,450,360);
+    
+	lcRestricoes.setMaster( lcCampos );
+	lcCampos.adicDetalhe( lcRestricoes );
+	lcRestricoes.setTabela( tbRestricoes );
+	
     lcBanco.add(new GuardaCampo( txtCodBanco, "CodBanco", "Cód.banco", ListaCampos.DB_PK, false));
     lcBanco.add(new GuardaCampo( txtDescBanco, "NomeBanco", "Nome do banco", ListaCampos.DB_SI, false));
     lcBanco.montaSql(false, "BANCO", "FN");    
@@ -76,6 +103,15 @@ public class FConta extends FDados {
     lcPlan.setQueryCommit(false);
     lcPlan.setReadOnly(true);
     txtCodPlan.setTabelaExterna(lcPlan);
+   
+	lcUsu.add( new GuardaCampo( txtIDUsu, "IDUsu", "ID", ListaCampos.DB_PK, txtNomeUsu, false ) );
+	lcUsu.add( new GuardaCampo( txtNomeUsu, "NomeUsu", "Nome nome do usuário", ListaCampos.DB_SI, false ) );
+	lcUsu.montaSql( false, "USUARIO", "SG" );
+	lcUsu.setQueryCommit( false );
+	lcUsu.setReadOnly( true );
+	txtIDUsu.setFK( true );
+	txtIDUsu.setNomeCampo( "IDUsu" );
+	txtIDUsu.setTabelaExterna( lcUsu );
     
     vValsTipo.addElement("B");
     vValsTipo.addElement("C");
@@ -83,6 +119,13 @@ public class FConta extends FDados {
     vLabsTipo.addElement("Caixa");
     rgTipo = new JRadioGroup(1,2,vLabsTipo,vValsTipo);
 
+	pinGeral.setPreferredSize( new Dimension( 430, 560 ) );
+	pinGeral.add( pinCamposGeral, BorderLayout.CENTER );
+
+	setPainel( pinCamposGeral );
+
+	adicTab( "Geral", pinCamposGeral );
+    
     adicCampo(txtNumConta, 7, 20, 110, 20, "NumConta", "Nº da conta", ListaCampos.DB_PK, true);
     adicCampo(txtDescConta, 120, 20, 270, 20, "DescConta", "Descrição da conta", ListaCampos.DB_SI, true);
     adicCampo(txtAgConta, 7, 60, 60, 20, "AgenciaConta", "Agência", ListaCampos.DB_SI, false);
@@ -95,14 +138,58 @@ public class FConta extends FDados {
     adicDescFK(txtDescPlan, 150, 140, 240, 20, "DescPlan", "Descrição do tipo de lançamento");
     adicDB(rgTipo, 7, 180, 200, 30, "TipoConta", "Tipo", true);
     adicDB(cbAtivaConta,220,180,120,20,"ativaconta","Ativa",true);
+	adicDB( chbRestritoTipoMov, 7, 220, 240, 20, "TUSUCONTA", "", true );
     setListaCampos(false,"CONTA", "FN");
-    lcCampos.setQueryInsert(false);    
+    lcCampos.setQueryInsert(false);   
+        
+	setPainel( pinDetRestricoes, pnRestricoes );
+
+	pinDetRestricoes.setPreferredSize( new Dimension( 430, 80 ) );
+	pinDetRestricoes.add( pinNavRestricoes, BorderLayout.SOUTH );
+	pinDetRestricoes.add( pinCamposRestricoes, BorderLayout.CENTER );
+
+	setListaCampos( lcRestricoes );
+	setNavegador( navRestricoes );
+
+	pnRestricoes.add( pinDetRestricoes, BorderLayout.SOUTH );
+	pnRestricoes.add( spnRestricoes, BorderLayout.CENTER );
+	pinNavRestricoes.adic( navRestricoes, 0, 0, 270, 25 );
+
+	setPainel( pinCamposRestricoes );
+
+	adicCampo( txtIDUsu, 7, 20, 80, 20, "IdUsu", "Id", ListaCampos.DB_PF, txtNomeUsu, true );
+	adicDescFK( txtNomeUsu, 90, 20, 250, 20, "NomeUsu", " e nome do usuário" );
+
+	setListaCampos( true, "CONTAUSU", "FN" );
+	lcRestricoes.setQueryInsert( false );
+	lcRestricoes.setQueryCommit( false );
+	lcRestricoes.montaTab();
+
+	txtNumConta.setTabelaExterna( lcRestricoes );
+
+	tbRestricoes.setTamColuna( 80, 0 );
+	tbRestricoes.setTamColuna( 280, 1 );
+	
+	chbRestritoTipoMov.addCheckBoxListener( this );
+	
+		
   }
+  
+  public void valorAlterado( CheckBoxEvent evt ) {	  
+	  if ( evt.getCheckBox() == chbRestritoTipoMov )
+		  if ( evt.getCheckBox().isSelected() )
+			  removeTab( "Restrições de Usuário", pnRestricoes );
+		  else
+			  adicTab( "Restrições de Usuário", pnRestricoes );
+  }
+  
   public void setConexao(Connection cn) {
     super.setConexao(cn);
     lcBanco.setConexao(cn);
     lcMoeda.setConexao(cn);
     lcPlan.setConexao(cn);
+    lcRestricoes.setConexao( cn );
+    lcUsu.setConexao( cn );
   }
 }
 
