645899120eb51ee10c7ab614a8bda1cf0af67628
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-05-22 20:19:09 +0000

    inclusão do campo " site banco "

--- trunk/freedom/src/org/freedom/modulos/std/FBanco.java
+++ trunk/freedom/src/org/freedom/modulos/std/FBanco.java
@@ -29,6 +29,7 @@ import java.sql.SQLException;
 
 import org.freedom.componentes.GuardaCampo;
 import org.freedom.componentes.ImprimeOS;
+import org.freedom.componentes.JLabelPad;
 import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
@@ -42,12 +43,13 @@ public class FBanco extends FDados implements ActionListener {
   private JTextFieldPad txtCodBanco = new JTextFieldPad(JTextFieldPad.TP_STRING,3,0);
   private JTextFieldPad txtNomeBanco = new JTextFieldPad(JTextFieldPad.TP_STRING,40,0);
   private JTextFieldPad txtCodModBol = new JTextFieldPad(JTextFieldPad.TP_INTEGER,8,0);
+  private JTextFieldPad txtSiteBanco = new JTextFieldPad(JTextFieldPad.TP_STRING, 80, 0);
   private JTextFieldFK txtDescModBol = new JTextFieldFK(JTextFieldPad.TP_STRING,50,0);
   private ListaCampos lcModBol = new ListaCampos(this,"MB");
   public FBanco () {
   	super();
     setTitulo("Cadastro de Banco");
-    setAtribos( 50, 50, 350, 165);
+    setAtribos( 50, 50, 350, 195);
 
     lcModBol.add(new GuardaCampo( txtCodModBol, "CodModBol", "Cód.mod.bol.", ListaCampos.DB_PK, txtDescModBol,false));
     lcModBol.add(new GuardaCampo( txtDescModBol, "DescModBol", "Descriçao do modelo de boleto", ListaCampos.DB_SI, null,false));
@@ -59,6 +61,7 @@ public class FBanco extends FDados implements ActionListener {
     adicCampo(txtCodBanco, 7, 20, 70, 20,"CodBanco","Cód.banco",ListaCampos.DB_PK,null,true);
     adicCampo(txtNomeBanco, 80, 20, 250, 20,"NomeBanco","Nome do banco",ListaCampos.DB_SI,null,true);
     adicCampo(txtCodModBol, 7, 60, 70, 20, "CodModBol", "Cód.mod.", ListaCampos.DB_FK, txtDescModBol,false);
+    adicCampo(txtSiteBanco, 7, 100, 325, 20, "SiteBanco", "Site ", ListaCampos.DB_SI, null,false);
     adicDescFK(txtDescModBol, 80, 60, 250, 20, "DescModBol", "Descrição do modelo de boleto");
     setListaCampos( false, "BANCO", "FN");
     btImp.addActionListener(this);
