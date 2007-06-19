214ef2b37d9e02df4dde56b45480124aebac2cf5
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-06-19 15:08:09 +0000

    Continuação da implementação dos boletos.

--- freedom/src/org/freedom/funcoes/Teste.java
+++ freedom/src/org/freedom/funcoes/Teste.java
@@ -14,9 +14,11 @@ public class Teste {
 		
         Date dtBase = Funcoes.encodeDate( 1997, 10, 7 );
         Date dtVencto = Funcoes.encodeDate( 2007, 06, 20 );
-		System.out.println(org.freedom.funcoes.Boleto.geraCodBar( "1",  "9", "9", new Long(3000), 
-				new BigDecimal(100), 
-				 "1", new Long(17), new Long(1),  "3275-1", "5688-0", "17" ));
+		System.out.println(org.freedom.funcoes.Boleto.geraCodBar( "1",  "9", "9", 
+				new Long(Funcoes.getNumDiasAbs( Funcoes.encodeDate( 2007, 06, 22 ), 
+						Funcoes.encodeDate( 1997, 10, 7 ) )), 
+				new BigDecimal(350), 
+				 "0224071", new Long(17), new Long(1),  "3275-1", "5688-0", "18" ));
 
 	}
 
