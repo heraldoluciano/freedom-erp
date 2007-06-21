b5dd0a00e746141269a61c52134fec9c34a07665
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-06-21 19:33:32 +0000

    Ajustes para funcionar com instalador.

--- freedom/lib/exec/freedompdv.sh
+++ freedom/lib/exec/freedompdv.sh
@@ -1,1 +1,3 @@
+EXEDIR=${0%/*}
+cd $EXEDIR/..
 java -classpath lib/freedompdv.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/itext.jar:lib/firebirdsql-full.jar:lib/nachocalendar.jar:lib/commons-logging.jar:lib/jasperreports.jar:lib/liquidlnf.jar:lib/metouia.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/mail.jar:lin/comm.jar:lib/barbecue.jar:lib/ireport.jar -DARQINI=ini/freedom.ini -DARQLOG=log/freedompdv.log org.freedom.modulos.pdv.FreedomPDV
