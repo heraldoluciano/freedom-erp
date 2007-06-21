b35475a840f17847e2d7f9cbcc1395aa75064945
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-06-21 19:33:32 +0000

    Ajustes para funcionar com instalador.

--- trunk/freedom/lib/exec/freedomcfg.sh
+++ trunk/freedom/lib/exec/freedomcfg.sh
@@ -1,1 +1,3 @@
+EXEDIR=${0%/*}
+cd $EXEDIR/..
 java -classpath lib/freedomcfg.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/itext.jar:lib/firebirdsql-full.jar:lib/nachocalendar.jar:lib/commons-logging.jar:lib/jasperreports.jar:lib/liquidlnf.jar:lib/metouia.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/mail.jar:lib/barbecue.jar:lib/ireport.jar -DARQINI=ini/freedom.ini -DARQLOG=log/freedomcfg.log org.freedom.modulos.cfg.FreedomCFG
--- trunk/freedom/lib/exec/freedomgms.sh
+++ trunk/freedom/lib/exec/freedomgms.sh
@@ -1,1 +1,3 @@
+EXEDIR=${0%/*}
+cd $EXEDIR/..
 java -classpath lib/freedomgms.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/itext.jar:lib/firebirdsql-full.jar:lib/nachocalendar.jar:lib/commons-logging.jar:lib/jasperreports.jar:lib/liquidlnf.jar:lib/metouia.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/mail.jar:lib/barbecue.jar:lib/ireport.jar -DARQINI=ini/freedom.ini -DARQLOG=log/freedomgms.log org.freedom.modulos.gms.FreedomGMS
