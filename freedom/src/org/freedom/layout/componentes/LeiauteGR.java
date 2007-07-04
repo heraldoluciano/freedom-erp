0df12876531bce3b77d2c2dda92a0c698dd75bb5
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-07-04 19:23:46 +0000

    Mudança na estrutura de packages.

---
+++ freedom/src/org/freedom/layout/componentes/LeiauteGR.java
@@ -0,0 +1,32 @@
+/**
+ * @version 14/05/2003 <BR>
+ * @author Setpoint Informática Ltda./Fernando Oliveira da Silva <BR>
+ *
+ * Projeto: Freedom <BR>
+ * Pacote: leiautes <BR>
+ * Classe: @(#)LeiauteGR.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR> <BR>
+ *
+ * Comentários para a classe...
+ */
+
+package org.freedom.layout.componentes;
+import java.sql.Connection;
+import java.util.Vector;
+
+import org.freedom.componentes.ImprimeLayout;
+
+public abstract class LeiauteGR extends ImprimeLayout {
+  public LeiauteGR() {} 
+  public abstract void setParam(Vector vParam);  
+  public abstract void setConexao(Connection cn);
+
+}
