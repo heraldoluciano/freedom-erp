7e94de669396a0efb1e23ba57f24adf139e76c80
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-07-05 15:07:45 +0000

    Problema de commmit resolvido.

---
+++ trunk/freedom/src/org/freedom/modulos/fnc/FRemSiacc.java
@@ -0,0 +1,13 @@
+package org.freedom.modulos.fnc;
+
+
+public class FRemSiacc extends FRemFBN {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public FRemSiacc() {
+		
+		super();
+	}
+
+}
