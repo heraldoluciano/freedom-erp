25e1f789f2c97403e72f504ff64fce4e79c73a5e
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-07-13 16:48:58 +0000

    Alterações para enviar email apartir do view do jasperreport.

---
+++ freedom/src/org/freedom/telas/DLEmailBean.java
@@ -0,0 +1,91 @@
+/**
+ * @version 05/2007 <BR>
+ * @author Setpoint Informática Ltda.<BR>
+ * @author Alex Rodrigues
+ * 
+ * Projeto: Freedom <BR>
+ * 
+ * Pacote: org.freedom.modulos.rep <BR>
+ * Classe:
+ * @(#)DLEnviaPedido.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
+ * Formulário para envio do pedido por e-mail.
+ */
+
+package org.freedom.telas;
+
+import javax.swing.JLabel;
+
+import org.freedom.componentes.JCheckBoxPad;
+import org.freedom.componentes.JPanelPad;
+import org.freedom.componentes.JTextFieldPad;
+import org.freedom.funcoes.EmailBean;
+
+public class DLEmailBean extends FFDialogo {
+
+	private static final long serialVersionUID = 1L;
+
+	private JPanelPad panelRodape = null;
+
+	private final JTextFieldPad txtHost = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
+
+	private final JTextFieldPad txtPort = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
+
+	private final JTextFieldPad txtFrom = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
+	
+	private final JCheckBoxPad cbAutenticaSMTP = new JCheckBoxPad( "Autenticar ?", "S", "N" );
+	
+	private final JCheckBoxPad cbSSLSMTP = new JCheckBoxPad( "Usa SSL ?", "S", "N" );
+	
+	private EmailBean mail = new EmailBean();
+
+	public DLEmailBean() {
+
+		super();
+		setTitulo( "Configuraçôes de envio" );
+		setAtribos( 405, 240 );
+		setResizable( false );
+
+		montaTela();
+	}
+
+	private void montaTela() {
+
+		adic( new JLabel( "Servidor SMTP:" ), 10, 10, 300, 20 );
+		adic( txtHost, 10, 30, 300, 20 );
+		adic( new JLabel( "Porta" ), 320, 10, 60, 20 );
+		adic( txtPort, 320, 30, 60, 20 );
+		adic( new JLabel( "Usuario:" ), 10, 50, 370, 20 );
+		adic( txtFrom, 10, 70, 370, 20 );
+		adic( cbAutenticaSMTP, 10, 100, 370, 20 );
+		adic( cbSSLSMTP, 10, 120, 370, 20 );
+	}
+	
+	public EmailBean getEmailBean() {
+		
+		return mail;
+	}
+
+	@Override
+	public void ok() {
+
+		mail.setHost( txtHost.getVlrString() );
+		mail.setPorta( txtPort.getVlrInteger() );
+		mail.setDe( txtFrom.getVlrString() );
+		mail.setUsuario( txtFrom.getVlrString() );
+		mail.setAutentica( cbAutenticaSMTP.getVlrString() );
+		mail.setSsl( cbSSLSMTP.getVlrString() );
+		
+		super.ok();
+	}	
+}
