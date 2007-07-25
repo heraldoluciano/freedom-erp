029d39215d257bfa746bb48beba1f29fe38ac651
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-07-25 19:26:15 +0000

    alteração de campos

--- freedom/src/org/freedom/modulos/std/FAlteraRecibo.java
+++ freedom/src/org/freedom/modulos/std/FAlteraRecibo.java
@@ -44,72 +44,72 @@ import org.freedom.telas.Aplicativo;
 import org.freedom.telas.FFilho;
 
 public class FAlteraRecibo extends FFilho implements ActionListener {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	private JPanelPad pinCli = new JPanelPad(350,100);
-	
+
 	private JPanelPad pnRod = new JPanelPad(JPanelPad.TP_JPANEL,new BorderLayout());
-	
+
 	private JTextFieldPad txtCodVenda = new JTextFieldPad(JTextFieldPad.TP_INTEGER,8,0);
-	
+
 	private JTextFieldPad txtCodRec = new JTextFieldPad(JTextFieldPad.TP_INTEGER,8,0);
-	
+
 	private JTextFieldFK txtVlrParcRec = new JTextFieldFK(JTextFieldFK.TP_DECIMAL, 15, Aplicativo.casasDecFin);
-	
+
 	private JTextFieldFK txtVlrParcItRec = new JTextFieldFK(JTextFieldFK.TP_DECIMAL, 15, Aplicativo.casasDecFin);
-	
+
 	private JTextFieldPad txtReciboItRec = new JTextFieldPad(JTextFieldPad.TP_INTEGER, 8, 0);
-	
+
 	private JTextFieldPad txtNParcItRec = new JTextFieldPad(JTextFieldPad.TP_INTEGER, 8, 0);
-	
+
 	private JButton btTrocaDoc = new JButton(Icone.novo("btTrocaNumero.gif"));
-	
+
 	private JButton btSair = new JButton("Sair",Icone.novo("btSair.gif"));
-	
+
 	private ListaCampos lcReceber = new ListaCampos(this, "");
-	
+
 	private ListaCampos lcItReceber = new ListaCampos(this, "");
-	
+
 	public FAlteraRecibo() {
-		
+
 		super(false);
 		setTitulo("Troca de documento");
 		setAtribos(50,50,350,200);
-		
+
 		btSair.addActionListener(this);
 		btTrocaDoc.addActionListener(this);
-		
+
 		montaTela();
 	}
-	
+
 	public void montaListaCampos(){
-		
+
 		lcReceber.add(new GuardaCampo( txtCodRec, "CodRec", "Cód.rec.", ListaCampos.DB_PK,true));
 		lcReceber.add(new GuardaCampo( txtCodVenda, "CodVenda", "Cód.venda", ListaCampos.DB_SI, false));
 		lcReceber.add(new GuardaCampo( txtVlrParcRec, "VlrParcRec", "Valor Total", ListaCampos.DB_SI, false));
-		
+
 		lcReceber.montaSql(false, "RECEBER", "FN");
 		lcReceber.setReadOnly(true);
 		txtCodRec.setTabelaExterna(lcReceber);
 		txtCodRec.setFK(true);
 		txtCodRec.setNomeCampo("CodRec");
-		
+
 		lcItReceber.add(new GuardaCampo( txtCodRec, "CodRec", "Cód.rec.", ListaCampos.DB_PF, true));
 		lcItReceber.add(new GuardaCampo( txtNParcItRec , "NParcItRec", "Nº parc.", ListaCampos.DB_PK, true));
 		lcItReceber.add( new GuardaCampo( txtVlrParcItRec, "VlrParcItRec", "Vlr.parc.", ListaCampos.DB_SI, false) );
 		lcItReceber.add(  new GuardaCampo( txtReciboItRec, "ReciboItRec", "Nº recibo", ListaCampos.DB_SI, false) );
-	    lcItReceber.setMaster( lcReceber );	
+		lcItReceber.setMaster( lcReceber );	
 		lcItReceber.montaSql(false, "ITRECEBER", "FN");
 		lcItReceber.setReadOnly(true);
-	
+
 		txtNParcItRec.setTabelaExterna(lcItReceber);
 		txtNParcItRec.setFK(true);
 		txtNParcItRec.setNomeCampo("NParcItRec"); 
 	}
-	
+
 	public void montaTela(){
-		
+
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
 
@@ -120,16 +120,16 @@ public class FAlteraRecibo extends FFilho implements ActionListener {
 
 		c.add(pnRod,BorderLayout.SOUTH);
 		c.add(pinCli,BorderLayout.CENTER);
-		
+
 		btTrocaDoc.setToolTipText("Alterar");
 
 		//txtCodVenda.setRequerido(true);
 		txtNParcItRec.setRequerido( true );
-	
+
 		txtVlrParcRec.setSoLeitura( true );
 		txtVlrParcItRec.setSoLeitura( true );
 		txtCodVenda.setSoLeitura( true );
-		
+
 		pinCli.adic( new JLabelPad( "Cód.rec." ), 7, 0, 70, 20 );
 		pinCli.adic( txtCodRec, 7, 20, 70, 20 );
 		pinCli.adic( new JLabelPad( "Nº pedido"), 80, 0, 70, 20 );
@@ -137,58 +137,58 @@ public class FAlteraRecibo extends FFilho implements ActionListener {
 		pinCli.adic( new JLabelPad( "Vlr.total" ), 153, 0, 70, 20 );
 		pinCli.adic( txtVlrParcRec, 153, 20, 70, 20 );
 		pinCli.adic( new JLabelPad( "Nº parcela" ), 7, 40, 100, 20 );
-		pinCli.adic( txtNParcItRec, 7, 60, 100, 20 );
-		pinCli.adic( new JLabelPad( "Vlr.parcela" ), 120, 40, 100, 20 );
-		pinCli.adic( txtVlrParcItRec, 120, 60, 100, 20 );
-		pinCli.adic( btTrocaDoc, 240, 50, 30, 30 );
+		pinCli.adic( txtNParcItRec, 7, 60, 141, 20 );
+		pinCli.adic( new JLabelPad( "Vlr.parcela" ), 153, 40, 70, 20 );
+		pinCli.adic( txtVlrParcItRec, 153, 60, 70, 20 );
+		pinCli.adic( btTrocaDoc, 191, 100, 30, 20 );
 		pinCli.adic( new JLabelPad( "Nº Recibo" ), 7, 80, 73, 20);
 		pinCli.adic( txtReciboItRec , 7, 100, 100, 20 );
 	}
-	
-	private void trocaDoc() {
-		
-		String sImpNota = "";
-		
-	    String sSQL1 = "SELECT RECIBOITREC FROM FNITRECEBER WHERE CODVENDA=? AND CODEMP=? AND CODFILIAL=?";
-	    String sSQL2 = "UPDATE FNITRECEBER SET RECIBOITREC=? WHERE CODVENDA=? AND CODEMP=? AND CODFILIAL=?";
-	   
-	    try {
-	    	
-	   	  PreparedStatement ps = con.prepareStatement(sSQL1);
-	      ps.setInt(1,txtCodRec.getVlrInteger().intValue());
-	      ps.setInt(2,Aplicativo.iCodEmp);
-	      ps.setInt(3,ListaCampos.getMasterFilial("VDVENDA"));
-	      ResultSet rs = ps.executeQuery();
-	    
-	      if (rs.next()) {
-	        sImpNota = rs.getString("DocVenda");
-	      }
-	     
-	      rs.close();
-	      ps.close();
 
-	      ps = con.prepareStatement(sSQL2);
-	      ps.setInt(1,txtCodRec.getVlrInteger().intValue());
-	      ps.setInt(2,txtCodVenda.getVlrInteger().intValue());
-	      ps.setInt(3,Aplicativo.iCodEmp);
-	      ps.setInt(4,ListaCampos.getMasterFilial("VDVENDA"));
-	      ps.executeUpdate();
-	      ps.close();
-				
-	      con.commit();
-	      Funcoes.mensagemInforma( this, "Numero do recibo alterado com Sucesso!" );
-	      txtCodRec.setVlrString( "" );
-			
+	private void trocaDoc() {
+
+		String sImpNota = "";
+
+		String sSQL1 = "SELECT RECIBOITREC FROM FNITRECEBER WHERE CODVENDA=? AND CODEMP=? AND CODFILIAL=?";
+		String sSQL2 = "UPDATE FNITRECEBER SET ReciboItRec=? WHERE CODVENDA=? AND CODEMP=? AND CODFILIAL=?";//"UPDATE FNITRECEBER SET RECIBOITREC=? WHERE CODVENDA=? AND CODEMP=? AND CODFILIAL=?";
+
+		try {
+
+			PreparedStatement ps = con.prepareStatement(sSQL1);
+			ps.setInt(1,txtCodRec.getVlrInteger().intValue());
+			ps.setInt(2,Aplicativo.iCodEmp);
+			ps.setInt(3,ListaCampos.getMasterFilial("FNRECEBER"));
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				sImpNota = rs.getString("VlrParcItRec");
+			}
+
+			rs.close();
+			ps.close();
+
+			ps = con.prepareStatement(sSQL2);
+			ps.setInt(1,txtReciboItRec.getVlrInteger().intValue());
+			ps.setInt(2,txtCodVenda.getVlrInteger().intValue());
+			ps.setInt(3,Aplicativo.iCodEmp);
+			ps.setInt(4,ListaCampos.getMasterFilial("FNRECEBER"));
+			ps.executeUpdate();
+			ps.close();
+
+			con.commit();
+			Funcoes.mensagemInforma( this, "Numero do recibo alterado com Sucesso!" );
+			txtCodRec.setVlrString( "" );
+
 		}
 		catch(SQLException err) {
-			
-			Funcoes.mensagemErro(this,"Erro ao alterar a venda!\n"+err.getMessage(),true,con,err);
+
+			Funcoes.mensagemErro(this,"Erro ao alterar numero do recibo!\n"+err.getMessage(),true,con,err);//"Erro ao alterar a venda!"
 			err.printStackTrace();
 
 		}
 	}
 	public void actionPerformed(ActionEvent evt) { 
-		
+
 		if (evt.getSource() == btSair)
 			dispose();
 
@@ -197,7 +197,7 @@ public class FAlteraRecibo extends FFilho implements ActionListener {
 
 	}
 	public void setConexao(Connection cn) {
-		
+
 		super.setConexao(cn);
 		lcReceber.setConexao(cn);
 		lcItReceber.setConexao( cn );
