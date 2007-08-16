7c96fa83fbf27694858e51db3acc9d1abb44671e
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-08-16 19:02:04 +0000

    criação de classe para gerar dig. verificador;

---
+++ freedom/src/org/freedom/funcoes/EANFactory.java
@@ -0,0 +1,72 @@
+package org.freedom.funcoes;
+
+
+public class EANFactory {
+
+	public String novoEAN13(String codpais, String codemp, String codigo){
+		
+		StringBuilder codbar = new StringBuilder();
+		int tamcod = 0;
+		
+		try {
+			
+			codbar.append( codpais.trim() );
+			codbar.append( codemp.trim() );
+			
+			tamcod = codbar.length()-12;
+			codbar.append( Funcoes.strZero( codigo, tamcod ) );
+			codbar.append( calcDigEAN13( codbar.toString() ));
+		 
+		} catch ( Exception e ) {
+			
+			e.printStackTrace();
+		}
+		
+		return codbar.toString();
+	}
+	
+	public boolean isPar(int val) {
+		boolean retorno = ( (val % 2) == 0 );
+		return retorno;
+	}
+	
+	public String calcDigEAN13 (String codbar){
+		
+		int[] digitos = new int[12];
+		int qtdpar = 0;
+		int qtdimpar = 0;
+		int total = 0;
+		String calc = null;
+		int dig =0;
+		StringBuilder codigo = new StringBuilder();
+		if (codbar == null) {
+			codigo.append( Funcoes.replicate( "0", 12 ) );
+		} else if (codbar.length()>=12) {
+			codigo.append( codbar.substring( 0, 12 ) );
+		} else {
+			codigo.append( codbar );
+			codigo.append( Funcoes.replicate( "0" , 12 - codbar.length() ));
+		}
+		for (int i=11; i>-1; i--) {
+			digitos[11-i] = Integer.parseInt( codigo.substring( i, i+1 ) ) ;
+			if (isPar(i+1)) {
+				qtdpar += digitos[11-i];
+			} else {
+				qtdimpar += digitos[11-i];
+			}
+		}
+		
+		qtdpar *= 3;
+		
+		total = qtdpar + qtdimpar;
+		
+		calc = String.valueOf( total );
+		
+		dig = 10 - Integer.parseInt( calc.substring( calc.length()-1 ) );
+		if ( dig==10 ) {
+			dig = 0;
+		}
+		
+		return  String.valueOf( dig );
+	}
+}
