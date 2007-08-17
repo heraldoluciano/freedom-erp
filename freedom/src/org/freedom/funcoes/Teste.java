edd95fb5d09c57b2438083f7c074fc608a1c233a
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-08-17 15:00:59 +0000

    Ajustes no cálculo do nosso número.

--- freedom/src/org/freedom/funcoes/Teste.java
+++ freedom/src/org/freedom/funcoes/Teste.java
@@ -9,16 +9,16 @@ public class Teste {
 
 	/**
 	 * @param args
-	 */
+	 */ 
 	public static void main( String[] args ) {
 		
         Date dtBase = Funcoes.encodeDate( 1997, 10, 7 );
-        Date dtVencto = Funcoes.encodeDate( 2007, 06, 20 );
+        Date dtVencto = Funcoes.encodeDate( 2007, 8, 25 );
 		System.out.println(org.freedom.funcoes.Boleto.geraCodBar( "1",  "9", "9", 
-				new Long(Funcoes.getNumDiasAbs( Funcoes.encodeDate( 2007, 06, 22 ), 
+				new Long(Funcoes.getNumDiasAbs( Funcoes.encodeDate( 2007, 8, 25 ), 
 						Funcoes.encodeDate( 1997, 10, 7 ) )), 
-				new BigDecimal(350), 
-				 "0224071", new Long(41), new Long(1),  "3275-1", "5688-0", "18", "00" ));
+				new BigDecimal(265), 
+				 "224071", new Long(1133), new Long(1),  "3275-1", "5688-X", "18", "00" ));
 
 	}
 
