d8611c3fb9ac0ab20505cc6205cb5ab668ae0981
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-09-15 15:16:01 +0000

    Correção do cálculo do digito verificador do módulo 10.

--- trunk/freedom/src/org/freedom/funcoes/Teste.java
+++ trunk/freedom/src/org/freedom/funcoes/Teste.java
@@ -13,14 +13,14 @@ public class Teste {
 	public static void main( String[] args ) {
 		
         Date dtBase = Funcoes.encodeDate( 1997, 10, 7 );
-        Date dtVencto = Funcoes.encodeDate( 2007, 8, 25 );
+        Date dtVencto = Funcoes.encodeDate( 2008, 11, 15 );
         Long fatvenc = new Long(Funcoes.getNumDiasAbs( Funcoes.encodeDate( 2007, 8, 23 ), 
 				Funcoes.encodeDate( 1997, 10, 7 ) ));
         BigDecimal vlrtit = new BigDecimal(100);
         String barra = org.freedom.funcoes.Boleto.geraCodBar( "1",  "9", "9", 
 				fatvenc, 
 				vlrtit, 
-				 "1421609", new Long(1), new Long(1),  "3275-1", "5688-X", "17", "00" );
+				 "1421609", new Long(1), new Long(16),  "3275-1", "5688-X", "17", "00" );
         String linhadig = Boleto.geraLinhaDig( barra, fatvenc, vlrtit );
 		System.out.println(barra);
 		System.out.println(barra.length());
@@ -29,7 +29,7 @@ public class Teste {
 		
 		
 		//System.out.println(org.freedom.funcoes.Boleto.geraNossoNumero( "00", "1421609", new Long(1), new Long(1) ));
-        //System.out.println(Boleto.digVerif( "0019340100000000330375031750416060680935011" , 11 ));
+        System.out.println(Boleto.digVerif( "0019340100000000330375031750416060680935011" , 11 ));
 
 	}
 
