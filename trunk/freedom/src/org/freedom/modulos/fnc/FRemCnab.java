4283b5060a79b3f5869e54af74c78c67ba255ee4
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2007-11-01 18:49:41 +0000

    Correção do endereço do cliente.

--- trunk/freedom/src/org/freedom/modulos/fnc/FRemCnab.java
+++ trunk/freedom/src/org/freedom/modulos/fnc/FRemCnab.java
@@ -307,7 +307,11 @@ public class FRemCnab extends FRemFBN {
 		}
 		
 		reg.setRazCli( dadosCliente[ DadosCliente.RAZCLI.ordinal() ] );
-		reg.setEndCli( dadosCliente[ DadosCliente.ENDCLI.ordinal() ] );
+		reg.setEndCli( dadosCliente[ DadosCliente.ENDCLI.ordinal() ].trim()+
+				", " + dadosCliente[ DadosCliente.NUMCLI.ordinal()] +
+				"-"+ dadosCliente[ DadosCliente.BAIRCLI.ordinal()]
+				);
+		
 		//reg.setBairCli( dadosCliente[ DadosCliente.BAIRCLI.ordinal() ] );
 		reg.setCepCli( dadosCliente[ DadosCliente.CEPCLI.ordinal() ] );
 		reg.setCidCli( dadosCliente[ DadosCliente.CIDCLI.ordinal() ] );
