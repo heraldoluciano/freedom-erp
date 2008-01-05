f77710946a41fbe7bb4de24b64dd89d9437e26da
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-01-05 20:47:43 +0000

    Alteração para utilização da nova estrutura do Freedom-ECF, criação de validação para caixa aberto e ajustes no layout.

--- freedom/src/org/freedom/modulos/pdv/FSuprimento.java
+++ freedom/src/org/freedom/modulos/pdv/FSuprimento.java
@@ -25,6 +25,7 @@
 package org.freedom.modulos.pdv;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -33,13 +34,14 @@ import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.swing.BorderFactory;
 import javax.swing.SwingConstants;
 
 import org.freedom.componentes.JLabelPad;
 import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
-import org.freedom.drivers.ECFDriver;
+import org.freedom.ecf.app.ControllerECF;
 import org.freedom.funcoes.Funcoes;
 import org.freedom.telas.Aplicativo;
 import org.freedom.telas.AplicativoPDV;
@@ -49,27 +51,62 @@ public class FSuprimento extends FFDialogo {
 
 	private static final long serialVersionUID = 1L;
 
-	private JTextFieldFK txtData = new JTextFieldFK( JTextFieldPad.TP_DATE, 10, 0 );
+	private final JTextFieldFK txtData = new JTextFieldFK( JTextFieldPad.TP_DATE, 10, 0 );
 
-	private JTextFieldPad txtValor = new JTextFieldPad( JTextFieldPad.TP_DECIMAL, 10, 2 );
+	private final JTextFieldPad txtValor = new JTextFieldPad( JTextFieldPad.TP_DECIMAL, 10, 2 );
 
-	private JTextFieldFK txtDataAnt = new JTextFieldFK( JTextFieldPad.TP_DATE, 10, 0 );
+	private final JTextFieldFK txtDataAnt = new JTextFieldFK( JTextFieldPad.TP_DATE, 10, 0 );
 
-	private JTextFieldFK txtSldAnt = new JTextFieldFK( JTextFieldPad.TP_STRING, 10, 2 );
+	private final JTextFieldFK txtSldAnt = new JTextFieldFK( JTextFieldPad.TP_STRING, 10, 2 );
 
-	private JTextFieldFK txtStatusAnt = new JTextFieldFK( JTextFieldPad.TP_STRING, 10, 0 );
+	private final JTextFieldFK txtStatusAnt = new JTextFieldFK( JTextFieldPad.TP_STRING, 10, 0 );
 
-	private JTextFieldFK txtUsuarioAnt = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
+	private final JTextFieldFK txtUsuarioAnt = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 
-	private JTextFieldFK txtUsuarioAtual = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
-
-	private ECFDriver ecf = new ECFDriver( !AplicativoPDV.usaEcfDriver() );
+	private final JTextFieldFK txtUsuarioAtual = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
+	
+	private final ControllerECF ecf;
+	
+	private char STATUS_OLD = '0';
+	
 
 	public FSuprimento() {
 
 		super( Aplicativo.telaPrincipal );
 		setTitulo( "Suprimento de caixa" );
-		setAtribos( 345, 275 );
+		setAtribos( 435, 270 );
+		
+		ecf = new ControllerECF( 
+				AplicativoPDV.getEcfdriver(), 
+				AplicativoPDV.getPortaECF(), 
+				AplicativoPDV.bModoDemo );
+		
+		montaTela();
+	}
+	
+	private void montaTela() {
+		
+		adic( new JLabelPad( "Data da última operação" ), 7, 10, 200, 20 );
+		adic( txtDataAnt, 7, 30, 200, 20 );
+		adic( new JLabelPad( "Saldo atual do caixa" ), 210, 10, 200, 20 );
+		adic( txtSldAnt, 210, 30, 200, 20 );
+		
+		adic( new JLabelPad( "Último operador" ), 7, 50, 200, 20 );
+		adic( txtUsuarioAnt, 7, 70, 200, 20 );
+		adic( new JLabelPad( "Status atual do caixa" ), 210, 50, 200, 20 );
+		adic( txtStatusAnt, 210, 70, 200, 20 );
+
+		JLabelPad lbLinha = new JLabelPad();
+		lbLinha.setBorder( BorderFactory.createEtchedBorder() );
+
+		adic( lbLinha, 7, 109, 403, 2 );
+
+		adic( new JLabelPad( "Operador atual" ), 7, 120, 200, 20 );
+		adic( txtUsuarioAtual, 7, 140, 200, 20 );
+		adic( new JLabelPad( "Data" ), 210, 120, 100, 20 );
+		adic( txtData, 210, 140, 100, 20 );
+		adic( new JLabelPad( "Valor" ), 313, 120, 97, 20 );
+		adic( txtValor, 313, 140, 97, 20 );
 
 		txtData.setVlrDate( Calendar.getInstance().getTime() );
 		txtValor.setVlrBigDecimal( new BigDecimal( 0 ) );
@@ -77,28 +114,9 @@ public class FSuprimento extends FFDialogo {
 
 		txtValor.setHorizontalAlignment( SwingConstants.RIGHT );
 		txtSldAnt.setHorizontalAlignment( SwingConstants.RIGHT );
-
-		adic( new JLabelPad( "Última operação" ), 7, 5, 95, 20 );
-		adic( txtDataAnt, 7, 25, 90, 20 );
-		adic( new JLabelPad( "Saldo final" ), 104, 5, 80, 20 );
-		adic( txtSldAnt, 104, 25, 80, 20 );
-		adic( new JLabelPad( "Status do caixa" ), 191, 5, 130, 20 );
-		adic( txtStatusAnt, 191, 25, 130, 20 );
-
-		adic( new JLabelPad( "Último operador" ), 7, 45, 150, 20 );
-		adic( txtUsuarioAnt, 7, 65, 150, 20 );
-
-		adic( new JLabelPad( "Operador atual" ), 7, 100, 150, 20 );
-		adic( txtUsuarioAtual, 7, 120, 150, 20 );
-		adic( new JLabelPad( "Data" ), 164, 100, 90, 20 );
-		adic( txtData, 164, 120, 90, 20 );
-
-		adic( new JLabelPad( "Valor do suprimento" ), 7, 140, 120, 20 );
-		adic( txtValor, 7, 160, 120, 20 );
-
 	}
 
-	private void executaQuery() {
+	private void loadInfoCaixa() {
 
 		try {
 
@@ -108,40 +126,41 @@ public class FSuprimento extends FFDialogo {
 			ps.setInt( 3, ListaCampos.getMasterFilial( "PVMOVCAIXA" ) );
 			ps.setDate( 4, Funcoes.dateToSQLDate( new Date() ) );
 			ResultSet rs = ps.executeQuery();
-			
+
 			if ( rs.next() ) {
 				if ( rs.getDate( "DDTAMOVRET" ) == null ) {
 					Funcoes.mensagemErro( this, "Caixa não está aberto!" );
 				}
 				else {
 					txtDataAnt.setVlrDate( rs.getDate( "DDTAMOVRET" ) );
-
 					txtSldAnt.setVlrString( Funcoes.strDecimalToStrCurrency( 10, 2, rs.getString( "NVLRSLDMOV" ) ) );
-					txtUsuarioAnt.setVlrString( rs.getString( "CIDUSU" ) );
-
-					txtStatusAnt.setVlrString( ecf.transStatus( rs.getString( "CTIPOMOV" ).toCharArray()[ 0 ] ) );
+					txtUsuarioAnt.setVlrString( rs.getString( "CIDUSU" ) );	
+					
+					STATUS_OLD = rs.getString( "CTIPOMOV" ).toCharArray()[ 0 ];	
+	
+					txtStatusAnt.setVlrString( Funcoes.transStatusECF( STATUS_OLD ) );
 				}
 			}
 
 			rs.close();
 			ps.close();
-			
+
 			if ( !con.getAutoCommit() ) {
 				con.commit();
 			}
-			
-		} catch ( SQLException err ) {
-			Funcoes.mensagemErro( this, "Ocorreu erro na consulta de movimento de caixa!\n" + err.getMessage(), true, con, err );
+
+		} catch ( SQLException e ) {
+			Funcoes.mensagemErro( this, "Ocorreu erro na consulta de movimento de caixa!\n" + e.getMessage(), true, con, e );
 		}
 
 	}
 
 	private void executaSuprimento() {
-		
-		if ( !AplicativoPDV.bECFTerm || ecf.suprimento( txtValor.getVlrBigDecimal() ) ) {
-		
+
+		if ( ecf.suprimento( txtValor.getVlrBigDecimal() ) ) {
+
 			try {
-				
+
 				PreparedStatement ps = con.prepareStatement( "EXECUTE PROCEDURE PVSUPRIMENTOSP(?,?,?,?,?,?)" );
 
 				ps.setInt( 1, Aplicativo.iCodEmp );
@@ -151,21 +170,24 @@ public class FSuprimento extends FFDialogo {
 				ps.setDate( 5, Funcoes.dateToSQLDate( new Date() ) );
 				ps.setString( 6, Aplicativo.strUsuario );
 				ps.execute();
-				
+
 				ps.close();
-				
+
 				if ( !con.getAutoCommit() ) {
 					con.commit();
 				}
-				
+
 			} catch ( SQLException err ) {
 				err.printStackTrace();
 				Funcoes.mensagemErro( this, "Erro ao executar suprimento!\n" + err.getMessage(), true, con, err );
 			}
-			
-			if ( AplicativoPDV.bECFTerm ) {
-				ecf.abreGaveta();
+
+			if ( !ecf.abrirGaveta() ) {
+				Funcoes.mensagemErro( this, ecf.getMessageLog() );
 			}
+		}
+		else {
+			Funcoes.mensagemErro( this, ecf.getMessageLog() );
 		}
 	}
 
@@ -174,15 +196,46 @@ public class FSuprimento extends FFDialogo {
 		if ( evt.getSource() == btOK ) {
 			executaSuprimento();
 		}
-		
+
 		super.actionPerformed( evt );
+	}
+
+	@ Override
+	public void keyPressed( KeyEvent e ) {
+
+		if ( e.getSource() == btOK && e.getKeyCode() == KeyEvent.VK_ENTER ) {
+			btOK.doClick();
+		}
+		else if ( e.getSource() == btCancel && e.getKeyCode() == KeyEvent.VK_ENTER ) {
+			btCancel.doClick();
+		}
+		else {
+			super.keyPressed( e );
+		}
+	}
+
+	@ Override
+	public void setVisible( boolean arg ) {
+
+		if ( arg ) {
+			if ( STATUS_OLD == 'F' || STATUS_OLD == 'Z' ) {
+				Funcoes.mensagemErro( this, "Caixa Fechado!" );
+				super.setVisible( false );
+			}
+			else {
+				super.setVisible( true );
+			}
+		}
+		else {
+			super.setVisible( arg );
+		}
 	}
 
 	public void setConexao( Connection cn ) {
 
 		super.setConexao( cn );
-		
-		executaQuery();
+
+		loadInfoCaixa();
 	}
 
 }
