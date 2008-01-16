860464126a4873b6c5b988800a459720e634a56a
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-01-16 19:38:10 +0000

    criação de relatório ( iswara ) ultimas vendas por cliente/produto

---
+++ trunk/freedom/src/org/freedom/modulos/std/FRVendCliProd.java
@@ -0,0 +1,155 @@
+/**
+ * @version 16/01/2008 <BR>
+ * @author Setpoint Informática Ltda./Reginaldo Garcia Heua <BR>
+ *
+ * Projeto: Freedom <BR>
+ *  
+ * Pacote: org.freedom.modulos.std <BR>
+ * Classe: @(#)FRVendCliProd.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR> <BR>
+ *
+ * Comentários sobre a classe...
+ * 
+ */
+
+package org.freedom.modulos.std;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import javax.swing.BorderFactory;
+import javax.swing.SwingConstants;
+
+import net.sf.jasperreports.engine.JasperPrintManager;
+
+import org.freedom.componentes.JLabelPad;
+import org.freedom.componentes.JTextFieldPad;
+import org.freedom.componentes.ListaCampos;
+import org.freedom.funcoes.Funcoes;
+import org.freedom.telas.Aplicativo;
+import org.freedom.telas.FPrinterJob;
+import org.freedom.telas.FRelatorio;
+
+public class FRVendCliProd extends FRelatorio {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JTextFieldPad txtDataini = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
+
+	private JTextFieldPad txtDatafim = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
+	
+	public FRVendCliProd(){
+		
+		setTitulo( "Ultimas Vendas de Cliente/Produto" );
+		setAtribos( 50, 50, 345, 190 );
+		montaTela();
+		
+	}
+	
+	private void montaTela(){
+		
+		JLabelPad lbLinha = new JLabelPad();
+		lbLinha.setBorder( BorderFactory.createEtchedBorder() );
+		JLabelPad lbPeriodo = new JLabelPad( "Periodo:", SwingConstants.CENTER );
+		lbPeriodo.setOpaque( true );
+
+		adic( lbPeriodo, 15, 5, 80, 20 );
+		adic( lbLinha, 7, 15, 303, 40 );
+		
+		adic( new JLabelPad( "De:", SwingConstants.CENTER ), 10, 25, 40, 20 );
+		adic( txtDataini, 50, 25, 100, 20 );
+		adic( new JLabelPad( "Até:", SwingConstants.CENTER ), 150, 25, 45, 20 );
+		adic( txtDatafim, 195, 25, 100, 20 );
+		
+		Calendar cPeriodo = Calendar.getInstance();
+		txtDatafim.setVlrDate( cPeriodo.getTime() );
+		cPeriodo.set( Calendar.DAY_OF_MONTH, cPeriodo.get( Calendar.DAY_OF_MONTH ) - 30 );
+		txtDataini.setVlrDate( cPeriodo.getTime() );
+	}
+
+	public void imprimir( boolean bVisualizar ) {
+
+		if ( txtDatafim.getVlrDate().before( txtDataini.getVlrDate() ) ) {
+			Funcoes.mensagemInforma( this, "Data final maior que a data inicial!" );
+			return;
+		}
+		
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		StringBuffer sSQL = new StringBuffer();
+		StringBuffer sCab = new StringBuffer();
+		
+		sCab.append( "de : " + txtDataini.getVlrDate() + "Até : " + txtDatafim.getVlrDate()  );
+	
+		
+		try {
+			
+			sSQL.append( "SELECT C.RAZCLI, V.CODCLI, P.DESCPROD, IV.CODPROD, " );
+			sSQL.append( "MAX(V.DTEMITVENDA) DTEMITVENDA, MAX(V.DOCVENDA) DOCVENDA, " );
+			sSQL.append( "MAX(V.SERIE) SERIE, " );
+			sSQL.append( "MAX (IV.VLRLIQITVENDA/(CASE WHEN IV.QTDITVENDA=0 THEN 1 ELSE IV.QTDITVENDA END)) PRECOVENDA " );
+			sSQL.append( "FROM VDCLIENTE C, VDVENDA V, VDITVENDA IV, EQPRODUTO P " );
+			sSQL.append( "WHERE C.CODEMP=V.CODEMPCL AND C.CODFILIAL=V.CODFILIALCL AND " ); 
+			sSQL.append( "C.CODCLI=V.CODCLI AND C.CODEMP=? AND C.CODFILIAL=? AND " );
+			sSQL.append( "IV.CODEMP=V.CODEMP AND IV.CODFILIAL=V.CODFILIAL AND " );
+			sSQL.append( "IV.TIPOVENDA=V.TIPOVENDA AND IV.CODVENDA=V.CODVENDA AND " ); 
+			sSQL.append( "P.CODEMP=IV.CODEMPPD AND P.CODFILIAL=IV.CODFILIALPD AND " );
+			sSQL.append( "P.CODPROD=IV.CODPROD AND " ); 
+			sSQL.append( "V.DTEMITVENDA BETWEEN ? AND ? " );
+			sSQL.append( "GROUP BY C.RAZCLI, V.CODCLI, P.DESCPROD, IV.CODPROD " );
+			
+			ps = con.prepareStatement( sSQL.toString() );
+			
+			ps.setInt( 1, Aplicativo.iCodEmp ); 
+			ps.setInt( 2, ListaCampos.getMasterFilial( "VDCLIENTE" ) ); 
+			ps.setDate( 3, Funcoes.strDateToSqlDate( txtDataini.getVlrString() ) );
+			ps.setDate( 4, Funcoes.strDateToSqlDate( txtDatafim.getVlrString() ) );
+			rs = ps.executeQuery();
+			
+			imprimiGrafico( bVisualizar, rs, sCab.toString() );
+
+			if ( !con.getAutoCommit() ) {
+				con.commit();
+			}
+			
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			Funcoes.mensagemInforma( this, "Erro ao buscar dados da venda!" );
+		}
+	}
+	
+	private void imprimiGrafico( final boolean bVisualizar, final ResultSet rs, final String sCab ) {
+		
+		FPrinterJob dlGr = null;
+		HashMap<String, Object> hParam = new HashMap<String, Object>();
+
+		hParam.put( "CODEMP", Aplicativo.iCodEmp );
+		hParam.put( "CODFILIAL", ListaCampos.getMasterFilial( "VDVENDA" ) );
+		hParam.put( "RAZAOEMP", Aplicativo.sEmpSis );
+		hParam.put( "FILTROS", sCab );
+
+		dlGr = new FPrinterJob( "relatorios/UltVendCli.jasper", "Ultimas Vendas por Cliente/Produto", sCab, rs, hParam, this );
+
+		if ( bVisualizar ) {
+			dlGr.setVisible( true );
+		}
+		else {
+			try {
+				JasperPrintManager.printReport( dlGr.getRelatorio(), true );
+			} catch ( Exception err ) {
+				Funcoes.mensagemErro( this, "Erro na impressão de relatório de Cliente por Produto!" + err.getMessage(), true, con, err );
+			}
+		}
+	}
+
+}
