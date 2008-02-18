273ae7bec3d50aeb89bbff40cc126f79f6e0834e
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-02-18 18:43:21 +0000

    acerto na identação de código.

--- freedom/src/org/freedom/modulos/grh/FNivelCurso.java
+++ freedom/src/org/freedom/modulos/grh/FNivelCurso.java
@@ -1,11 +1,12 @@
 /**
  * @version 28/01/2008 <BR>
  * @author Setpoint Informática Ltda./Anderson Sanchez <BR>
- *
+ * 
  * Projeto: Freedom <BR>
- *  
+ * 
  * Pacote: org.freedom.modulos.grh <BR>
- * Classe: @(#)FNivelCurso.java <BR>
+ * Classe:
+ * @(#)FNivelCurso.java <BR>
  * 
  * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
@@ -14,44 +15,43 @@
  * o LICENCIADOR ou então pegar uma cópia em: <BR>
  * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
  * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
- * de acordo com os termos da LPG-PC <BR> <BR>
- *
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
  * Formulário para cadastro de níveis de cursos, para uso nas funções de recrutamento e seleção.
  * 
  */
 
 package org.freedom.modulos.grh;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
 import org.freedom.telas.FDados;
 
-public class FNivelCurso extends FDados implements ActionListener {
+public class FNivelCurso extends FDados {
+
 	private static final long serialVersionUID = 1L;
-	private JTextFieldPad txtCodNivelCurso = new JTextFieldPad(JTextFieldPad.TP_INTEGER,5,0);
-	private JTextFieldPad txtDescNivelCurso = new JTextFieldPad(JTextFieldPad.TP_STRING,60,0);
-	
-	public FNivelCurso () {
+
+	private JTextFieldPad txtCodNivelCurso = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 5, 0 );
+
+	private JTextFieldPad txtDescNivelCurso = new JTextFieldPad( JTextFieldPad.TP_STRING, 60, 0 );
+
+	public FNivelCurso() {
+
 		super();
-		setTitulo("Cadastro de níveis de cursos");
-		setAtribos( 50, 50, 380, 135);
-		adicCampo(txtCodNivelCurso, 7, 20, 70, 20,"CodNivelCurso","Cód.Nível", ListaCampos.DB_PK, true);
-		adicCampo(txtDescNivelCurso, 80, 20, 250, 20,"DescNivelCurso","Descrição do nível", ListaCampos.DB_SI, true);
-		setListaCampos( true, "NIVELCURSO", "RH");
-		btImp.addActionListener(this);
-		btPrevimp.addActionListener(this);
-		lcCampos.setQueryInsert(false);    
-		setImprimir(true);
+		setTitulo( "Cadastro de níveis de cursos" );
+		setAtribos( 50, 50, 380, 135 );
+		
+		montaTela();
+		
+		setImprimir( false );
 	}
 	
-	public void actionPerformed(ActionEvent evt) {
-		if (evt.getSource() == btPrevimp)
-			imprimir(true);
-		else if (evt.getSource() == btImp) 
-			imprimir(false);
-		super.actionPerformed(evt);
+	private void montaTela() {
+
+		adicCampo( txtCodNivelCurso, 7, 20, 70, 20, "CodNivelCurso", "Cód.Nível", ListaCampos.DB_PK, true );
+		adicCampo( txtDescNivelCurso, 80, 20, 250, 20, "DescNivelCurso", "Descrição do nível", ListaCampos.DB_SI, true );
+		setListaCampos( true, "NIVELCURSO", "RH" );
+		lcCampos.setQueryInsert( false );
 	}
-	
-	private void imprimir(boolean bVisualizar) {}
 }
--- freedom/src/org/freedom/modulos/grh/FreedomGRH.java
+++ freedom/src/org/freedom/modulos/grh/FreedomGRH.java
@@ -8,22 +8,18 @@
  * Classe:
  * @(#)FreedomGRH.java <BR>
  * 
- * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para
- * Programas de Computador), <BR>
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
- * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste
- * Programa. <BR>
- * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você
- * pode contatar <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
  * o LICENCIADOR ou então pegar uma cópia em: <BR>
  * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
- * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é
- * preciso estar <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
  * de acordo com os termos da LPG-PC <BR>
  * <BR>
  * 
  * Tela principal para o módulo de gestão de recursos humanos.
- *  
+ * 
  */
 
 package org.freedom.modulos.grh;
@@ -38,56 +34,48 @@ import org.freedom.telas.LoginPD;
 public class FreedomGRH extends AplicativoPD {
 
 	public FreedomGRH() {
-		super("iconAtendimento32.gif", "splashGRH.jpg", 1, "Freedom", 9, "Gestão de Recursos Humandos", null,new FPrincipalPD(null, "bgFreedom2.jpg"),LoginPD.class);
 
-		addOpcao(-1, TP_OPCAO_MENU, "Arquivo", "", 'A', 100000000, 0, false,
-				null);
-		addOpcao(100000000, TP_OPCAO_MENU, "Tabelas", "", 'T', 100100000, 1,
-				false, null);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Empregados", "Empregados", 'E',
-				100101000, 2, true, FEmpregado.class);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Turnos", "Turnos", 'r', 100102000,
-				2, true, FTurnos.class);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Funçao", "Função", 'F', 100103000,
-				2, true, FFuncao.class);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Departamento", "Departamento", 'D',
-				100104000, 2, true, FDepto.class);
-		addSeparador(100100000);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Area", "Area", 'a',
-				100105000, 2, true, FArea.class);
-		addOpcao(100100000, TP_OPCAO_ITEM, "Nível/Curso", "Nível/Curso", 'N',
-				100106000, 2, true, FNivelCurso.class);
-		addSeparador(100100000);
-		addOpcao( 100100000, TP_OPCAO_ITEM, "Estados civis", "Estados civis", 'i', 
-				100107000, 2, true, FEstadoCivil.class );
-		
-		
-		addBotao("barraConveniados.gif", "Empregados", "Empregados", 100101000,
-				FEmpregado.class);
+		super( "iconAtendimento32.gif", "splashGRH.jpg", 1, "Freedom", 9, "Gestão de Recursos Humandos", null, new FPrincipalPD( null, "bgFreedom2.jpg" ), LoginPD.class );
+
+		addOpcao( -1, TP_OPCAO_MENU, "Arquivo", "", 'A', 100000000, 0, false, null );
+		addOpcao( 100000000, TP_OPCAO_MENU, "Tabelas", "", 'T', 100100000, 1, false, null );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Empregados", "Empregados", 'E', 100100100, 2, true, FEmpregado.class );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Turnos", "Turnos", 'T', 100100200, 2, true, FTurnos.class );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Funçao", "Função", 'F', 100100300, 2, true, FFuncao.class );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Departamento", "Departamento", 'D', 100100400, 2, true, FDepto.class );
+			addSeparador( 100100000 );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Area", "Area", 'A', 100100500, 2, true, FArea.class );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Nível/Curso", "Nível/Curso", 'N', 100100600, 2, true, FNivelCurso.class );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Curso", "Curso", 'C', 100100700, 2, true, FCurso.class );
+			addSeparador( 100100000 );
+			addOpcao( 100100000, TP_OPCAO_ITEM, "Estados civis", "Estados civis", 's', 100100800, 2, true, FEstadoCivil.class );
+
+		addBotao( "barraConveniados.gif", "Empregados", "Empregados", 100101000, FEmpregado.class );
 
 		ajustaMenu();
-		
+
 		sNomeModulo = "Recursos Humanos";
 		sMailSuporte = "suporte@stpinf.com";
 		sNomeSis = "Freedom";
 		sEmpSis = "Setpoint Informática Ltda.";
-		vEquipeSis.add("Robson Sanchez - Supervisão / Analise");
-		vEquipeSis.add("Anderson Sanchez - Supervisão / Programação");
-		vEquipeSis.add("Alex Rodrigues - Programação");
-		vEquipeSis.add("Alexandre Marcondes - Programação");
-		vEquipeSis.add("Fernando Oliveira - Programação");
-		vEquipeSis.add("Moyzes Braz - Arte gráfica");
-		vEquipeSis.add("Reginaldo Garcia - Testes / Suporte");
-		
+		vEquipeSis.add( "Robson Sanchez - Supervisão / Analise" );
+		vEquipeSis.add( "Anderson Sanchez - Supervisão / Programação" );
+		vEquipeSis.add( "Alex Rodrigues - Programação" );
+		vEquipeSis.add( "Alexandre Marcondes - Programação" );
+		vEquipeSis.add( "Fernando Oliveira - Programação" );
+		vEquipeSis.add( "Moyzes Braz - Arte gráfica" );
+		vEquipeSis.add( "Reginaldo Garcia - Testes / Suporte" );
+
 	}
 
-	public static void main(String sParams[]) {
+	public static void main( String sParams[] ) {
+
 		try {
-			Aplicativo.setLookAndFeel("freedom.ini");
+			Aplicativo.setLookAndFeel( "freedom.ini" );
 			FreedomGRH freedom = new FreedomGRH();
 			freedom.show();
-		} catch (Throwable e) {
-			Funcoes.criaTelaErro("Erro de execução");
+		} catch ( Throwable e ) {
+			Funcoes.criaTelaErro( "Erro de execução" );
 			e.printStackTrace();
 		}
 	}
