c5afc10414d758fa043cdbaf73c0a6edf9dd2df4
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-03-25 21:01:13 +0000

    Alterações para implementação da interface ControllerTefEvent; Implementação da rotina de requisição de venda e confirmação de impressão de comprovante.

--- freedom-ecf/src/org/freedom/tef/driver/text/VisaTextTef.java
+++ freedom-ecf/src/org/freedom/tef/driver/text/VisaTextTef.java
@@ -28,13 +28,22 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
+
+import sun.misc.MessageUtils;
+
+import static org.freedom.tef.driver.text.TextTefProperties.*;
 
 
 public class VisaTextTef extends TextTef  {
 	
-	private long indentification = 1;
+	private long nextIndentification = 1;
+	
+	private long indentification = nextIndentification;
+	
+	private List<String> responseToPrint;
 
 	
 	public VisaTextTef() {
@@ -48,32 +57,27 @@ public class VisaTextTef extends TextTef  {
 	}
 
 	@Override
-	protected void initializeTextTef() throws Exception {
+	protected synchronized void initializeTextTef() throws Exception {
 		
 		if ( getTextTefProperties() == null ) {
 			throw new NullPointerException( "Properties para TEF não especificadas!" );
 		}
 		else {
 			
-			getTextTefProperties().setProperty( TextTefProperties.ARQ_TMP,      TextTefProperties.ARQ_TMP );
-			getTextTefProperties().setProperty( TextTefProperties.ARQ_SEND,     TextTefProperties.ARQ_SEND );
-			getTextTefProperties().setProperty( TextTefProperties.ARQ_RESPONSE, TextTefProperties.ARQ_RESPONSE );
-			getTextTefProperties().setProperty( TextTefProperties.ARQ_ACTIVE,   TextTefProperties.ARQ_ACTIVE );
-			getTextTefProperties().setProperty( TextTefProperties.ARQ_STATUS,   TextTefProperties.ARQ_STATUS );
+			set( ARQ_TMP,      ARQ_TMP );
+			set( ARQ_SEND,     ARQ_SEND );
+			set( ARQ_RESPONSE, ARQ_RESPONSE );
+			set( ARQ_ACTIVE,   ARQ_ACTIVE );
+			set( ARQ_STATUS,   ARQ_STATUS );
 		
-        	fileTemp     = new File( getTextTefProperties().get( TextTefProperties.PATH_SEND ) + "/"
-                                   + getTextTefProperties().get( TextTefProperties.ARQ_TMP ) ) ;
-        	fileSend     = new File( getTextTefProperties().get( TextTefProperties.PATH_SEND ) + "/"
-                                   + getTextTefProperties().get( TextTefProperties.ARQ_SEND ) ) ;
-        	fileResponse = new File( getTextTefProperties().get( TextTefProperties.PATH_RESPONSE ) + "/"
-                                   + getTextTefProperties().get( TextTefProperties.ARQ_RESPONSE ) ) ;
-        	fileStatus   = new File( getTextTefProperties().get( TextTefProperties.PATH_RESPONSE ) + "/"
-                                   + getTextTefProperties().get( TextTefProperties.ARQ_STATUS ) ) ;
-        	fileActive   = new File( getTextTefProperties().get( TextTefProperties.PATH_RESPONSE ) + "/"
-                                   + getTextTefProperties().get( TextTefProperties.ARQ_ACTIVE ) ) ;
+        	fileTemp     = new File( get( PATH_SEND )     + "/" + get( ARQ_TMP ) ) ;
+        	fileSend     = new File( get( PATH_SEND )     + "/" + get( ARQ_SEND ) ) ;
+        	fileResponse = new File( get( PATH_RESPONSE ) + "/" + get( ARQ_RESPONSE ) ) ;
+        	fileStatus   = new File( get( PATH_RESPONSE ) + "/" + get( ARQ_STATUS ) ) ;
+        	fileActive   = new File( get( PATH_RESPONSE ) + "/" + get( ARQ_ACTIVE ) ) ;
         	
         	if ( ! isActive() ) {
-        		throw new Exception( "Gerenciador tef não está ativo!" );
+        		throw new Exception( "Gerenciador TEF não está ativo!" );
         	}
         	/*else if ( existActiveTEF( ADM, fileResponse, 1, null ) ) {
         		readReturn();
@@ -83,86 +87,180 @@ public class VisaTextTef extends TextTef  {
 	}
 
 	@Override
-	protected boolean validateTextTefProperties( final TextTefProperties textTefProperties ) throws Exception {
-		return true;
-	}
-
-	@Override
-	public boolean isActive() throws Exception {
+	public synchronized boolean isActive() throws Exception {
 		
 		boolean isActive = false;
+		incrementIndentificationActual();
 		
-		if ( fileActive == null
-				|| ( ! fileActive.exists() || ! fileActive.canRead() ) ) {
-			return isActive;
-		}
-	
-		try {
-			Properties properties = new Properties();
-			properties.load( new FileInputStream( fileActive ) );
-			if ( properties.getProperty( TextTefProperties.HEADER, "" ).equals( "TEF" ) ) {
-				isActive = true;
-				loadTextTefPropertie( properties );
-			}
-			properties.clear();
-		} catch ( IOException e ) {
-			throw new Exception( "Não foi possível verificar se o gerenciador TEF está ativo!", e );
-		}
+		List<String> request = new ArrayList<String>();
+		
+		request.add( HEADER          + " = " + ATV );
+		request.add( INDENTIFICATION + " = " + indentification );
+		request.add( TRAILER         + " = " + 0 );
+		
+		isActive = send( request, fileTemp, fileSend ) && existFileStatus( ATV, indentification );
 		
 		return isActive;
 	}
 
 	@Override
-	public boolean readResponseToPrint() throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean requestAdministrator() throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean requestCancel( final String nsu, 
-								  final String rede, 
-								  final Date data, 
-								  final BigDecimal value ) throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean requestSale( final Integer numberDoc, 
-			                    final BigDecimal value ) throws Exception {
-		
-		boolean actionReturn = false;		
-		final long indentification = this.indentification++;
+	public synchronized boolean requestSale( final Integer numberDoc, 
+			                    			 final BigDecimal value ) throws Exception {		
+		if ( fileSend.exists() ) {
+			fileSend.delete();
+		}
+			
+		incrementIndentificationActual();
 		final DecimalFormat df = new DecimalFormat( "0.00" );
 		
 		List<String> request = new ArrayList<String>();
 		
-		request.add( TextTefProperties.HEADER          + " = " + TextTef.CRT );
-		request.add( TextTefProperties.INDENTIFICATION + " = " + indentification );
-		request.add( TextTefProperties.DOCUMENT        + " = " + numberDoc );
-		request.add( TextTefProperties.VALUE           + " = " + df.format( value ) );
-		request.add( TextTefProperties.EOT             + " = " + 0 );
-		
-		actionReturn = send( request );
-
-		if ( ! actionReturn
-				|| ! existFileStatus( TextTef.CRT, indentification )
-					|| ! existFileResponse( TextTef.CRT, indentification ) ) {
-			actionReturn = false;
-		}
-		else {
-			readReturn();
-		}
-		
-		return actionReturn;
+		request.add( HEADER          + " = " + CRT );
+		request.add( INDENTIFICATION + " = " + indentification );
+		request.add( DOCUMENT        + " = " + numberDoc );
+		request.add( VALUE           + " = " + df.format( value ) );
+		request.add( TRAILER         + " = " + 0 );
+				
+		return send( request, fileTemp, fileSend ) && existFileStatus( CRT, indentification );
 	}
 
-	private void loadTextTefPropertie( final File file ) throws Exception {
+	public synchronized boolean readResponseSale() throws Exception {
+	    
+		boolean readresponse = false;
+	    long indentific = -1;
+	    int changes = 3;
+	    final TextTefProperties clear = new TextTefProperties();
+	    
+	    while ( indentific != indentification && changes-- > 0 ) {			
+	    	
+	    	// aguardando recebimento do arquivo IntPos.001
+	    	if ( existFileResponse( CRT, indentification ) ) {
+	    	
+	    		// Obtém dados do arquivo IntPos.001
+			    loadTextTefPropertie( fileResponse );
+			    
+			    indentific = Integer.parseInt( get( INDENTIFICATION, "0" ) );
+			    
+			    // Verifica se o campo 001-000 é iguál, do contrário 
+			    // despresa o arquivo e continua do aguardo do recebimento do arquivo.
+			    if ( indentific == indentification ) {
+			    	readresponse = true;
+			    	break;
+			    }
+			    else {
+			    	fileResponse.delete();
+			    	this.responseToPrint = null;
+			    	loadTextTefPropertie( clear );
+			    }
+	    	}
+	    }
+	    
+	    if ( indentific != indentification ) {
+	    	throw new Exception( "Apos 3 tentativas, a aplicação não consegui ler o arquivo de resposta." );
+	    }
+	    
+	    return readresponse;
+	}
+
+	@Override
+	public synchronized List<String> getResponseToPrint() throws Exception {
 		
-		if ( file != null ) {
+		if ( this.responseToPrint == null ) {
+			
+			this.responseToPrint = new ArrayList<String>();
+    		final int amountLines = Integer.parseInt( get( AMOUNT_LINES ) );		
+    		String tmp = null;
+    		
+    		for ( int i = 1; i <= amountLines; i++ ) {
+    			tmp = get( RESPONSE_TO_PRINT + String.format( "%03d", i ) );
+    			if ( tmp != null ) {
+    				this.responseToPrint.add( tmp.replaceAll( "\"", "" ) );
+    			}
+    		}
+		}
+		
+		return this.responseToPrint;
+	}
+
+	@Override
+	public synchronized boolean confirmationOfSale() throws Exception {
+		
+		if ( fileResponse.exists() ) {
+			fileResponse.delete();
+		}
+		
+		List<String> request = new ArrayList<String>();
+		
+		request.add( HEADER          + " = " + CNF );
+		request.add( INDENTIFICATION + " = " + indentification );
+		request.add( DOCUMENT        + " = " + get( DOCUMENT ) );
+		request.add( NET_NAME        + " = " + get( NET_NAME ) );
+		request.add( NSU             + " = " + get( NSU ) );
+		request.add( FINISHING       + " = " + get( FINISHING ) );
+		request.add( TRAILER         + " = " + 0 );
+		
+		boolean confirmationOfSale = send( request, fileTemp, fileSend );
+		
+		if ( confirmationOfSale ) {
+			if ( fileStatus.exists() ) {
+				fileStatus.delete();
+			}
+		}
+				
+		return confirmationOfSale;
+	}
+
+	@Override
+	public synchronized boolean notConfirmationOfSale() throws Exception {
+		
+		if ( fileResponse.exists() ) {
+			fileResponse.delete();
+		}
+		
+		List<String> request = new ArrayList<String>();
+		
+		request.add( HEADER          + " = " + NCN );
+		request.add( INDENTIFICATION + " = " + indentification );
+		request.add( DOCUMENT        + " = " + get( DOCUMENT ) );
+		request.add( NET_NAME        + " = " + get( NET_NAME ) );
+		request.add( NSU             + " = " + get( NSU ) );
+		request.add( FINISHING       + " = " + get( FINISHING ) );
+		request.add( TRAILER         + " = " + 0 );
+		
+		boolean notConfirmationOfSale = send( request, fileTemp, fileSend );
+		
+		if ( notConfirmationOfSale ) {
+			if ( fileStatus.exists() ) {
+				fileStatus.delete();
+			}
+		}
+				
+		return notConfirmationOfSale;
+	}
+
+	@Override
+	public synchronized boolean requestCancel( final String nsu, 
+            							 	   final String rede, 
+            								   final Date data, 
+            								   final BigDecimal value ) throws Exception {
+		return false;
+	}
+
+	@Override
+	public synchronized boolean requestAdministrator() throws Exception {
+		return false;
+	}
+
+	private synchronized long incrementIndentificationActual() {
+		
+		this.indentification = this.nextIndentification++;
+		
+		return this.indentification;
+	}
+
+	private synchronized void loadTextTefPropertie( final File file ) throws Exception {
+		
+		if ( file != null && file.exists() ) {
 			
 			Properties properties = new Properties();
 			FileInputStream fis = new FileInputStream( file );
@@ -172,50 +270,62 @@ public class VisaTextTef extends TextTef  {
 		}
 	}
 
-	private void loadTextTefPropertie( final Properties properties ) {
+	private synchronized void loadTextTefPropertie( final Properties properties ) throws Exception {
 		
 		if ( properties == null ) {
 			return;
 		}
 		else {
-			for ( String key : TextTefProperties.getKeyList() ) {
-				if ( TextTefProperties.PATH_SEND.equals( key ) ||
-					 TextTefProperties.PATH_RESPONSE.equals( key ) ||
-					 TextTefProperties.ARQ_TMP.equals( key ) ||
-					 TextTefProperties.ARQ_SEND.equals( key ) ||
-					 TextTefProperties.ARQ_RESPONSE.equals( key ) || 
-					 TextTefProperties.ARQ_ACTIVE.equals( key ) ||
-					 TextTefProperties.ARQ_STATUS.equals( key ) ) {
-					continue;
+			final List<String> keyNoDeleteble = new ArrayList<String>();
+			keyNoDeleteble.add( PATH_SEND );
+			keyNoDeleteble.add( PATH_RESPONSE );
+			keyNoDeleteble.add( ARQ_TMP );
+			keyNoDeleteble.add( ARQ_SEND );
+			keyNoDeleteble.add( ARQ_RESPONSE ); 
+			keyNoDeleteble.add( ARQ_ACTIVE );
+			keyNoDeleteble.add( ARQ_STATUS );
+			String key = null;
+			
+			properties : for ( Enumeration<Object> e = properties.keys() ; e.hasMoreElements() ; ) {
+				key = (String) e.nextElement();
+				for ( String k : keyNoDeleteble ) {
+					if ( k.equals( key ) ) {
+						continue properties;
+					}
 				}
-				else {
-					getTextTefProperties().set( key, properties.getProperty( key, "" ) );
+				try {
+					set( key, properties.getProperty( key, "" ) );
+				} catch ( IllegalArgumentException err ) {
+					MessageUtils.err( err.getMessage() );
 				}
 			}
 		}
 	}
 	
-	private boolean send( final List<String> send ) throws Exception {
+	private synchronized boolean send( final List<String> send,
+						               final File tmp,
+						               final File file ) throws Exception {
 		
-		if ( send == null ) {
+		if ( send == null || tmp == null || file == null ) {
 			return false;
 		}
 		
 	    boolean actionReturn = false;
 	
-		if ( fileTemp.exists() ) {
-			fileTemp.delete();
+		if ( tmp.exists() ) {
+			tmp.delete();
 		}
+		
 		try {
 			
-			PrintStream printStream = new PrintStream( new FileOutputStream( fileTemp ) );
+			PrintStream printStream = new PrintStream( new FileOutputStream( tmp ) );
 			
 			for ( String s : send ) {
 				printStream.println( s );
 			}
 			
 			printStream.close();						
-			fileTemp.renameTo( fileSend );	
+			tmp.renameTo( file );	
 			
 			actionReturn = true;
 			
@@ -226,59 +336,43 @@ public class VisaTextTef extends TextTef  {
 		return actionReturn;
 	}
 
-	//Se não existe linhas para imprimir então apaga o arquivo,
-	//caso contrário só irá apagar o arquivo na confirmação.
-	private void readReturn() throws Exception {
-		
-	    loadTextTefPropertie( fileResponse );
-	    
-	    if ( Integer.parseInt( 
-	    		getTextTefProperties().getProperty( TextTefProperties.AMOUNT_LINES, "0") ) == 0 ) {
-	    	fileResponse.delete();
-	    }
-	
-	    fileStatus.delete();
-	}
-
-	private boolean existFile( final int changes, 
-							   final File file, 
-							   final String header, 
-							   final long indentification ) throws Exception {
-		
+	private synchronized boolean existFile( final int changes, 
+							                final File file, 
+							                final String header, 
+							                final long indentification ) throws Exception {		
 	    boolean existFile = false;
 	    int c = changes;
-	    do {			
-			Thread.sleep( 1000 );
-			System.out.println( "Chances = " + c );
-		} while ( c-- > 0 ); {
+	    while ( c-- > 0 ) {
+	    	System.out.println( "Procurando ... [ " + c + " ] " + file.getName() );
 			if ( file.exists() && file.canRead() ) {
 				Properties properties = new Properties();
 				FileInputStream fileInputStream = new FileInputStream( file );
 				properties.load( fileInputStream );
 				fileInputStream.close();
-				if ( properties.getProperty( TextTefProperties.HEADER, "" ).equals( header )
-						&& ( ( indentification > 0 && 
-							   properties.getProperty( TextTefProperties.INDENTIFICATION, "" )
-							  		.equals( String.valueOf( indentification ) ))
-							 || indentification == 0 ) ) {
-					existFile = true;
-					c = 0;
-					System.out.println( "Encontrou " + file.getName() );
+				if ( properties.getProperty( HEADER, "" ).equals( header ) ) {
+					if ( indentification >= 0 
+							|| properties.getProperty( INDENTIFICATION, "" )
+							  		.equals( String.valueOf( indentification ) ) ) {
+    					existFile = true;
+    					c = 0;
+					}
 				}
 				properties.clear();
-				System.out.println( "Não encontrou " + file.getName() );
 			}
+			Thread.sleep( 1000 );
 		}
 	    
 	    return existFile;
 	}
 
-	private boolean existFileStatus( final String header, final long indentification ) throws Exception {
+	private synchronized boolean existFileStatus( final String header, 
+			                                      final long indentification ) throws Exception {
 		
 	    return existFile( 7, fileStatus, header, indentification );
 	}
 
-	private boolean existFileResponse( final String header, final long indentification ) throws Exception {
+	private synchronized boolean existFileResponse( final String header, 
+			                                        final long indentification ) throws Exception {
 		
 		return existFile( 180, fileResponse, header, indentification );
 	}
