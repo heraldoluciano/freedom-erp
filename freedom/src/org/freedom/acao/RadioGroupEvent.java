fccc6b732313632563e829a15e457b97b2b0b376
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-04-02 17:29:48 +0000

    alteração para saber o objeto qerador do evento.

--- freedom/src/org/freedom/acao/RadioGroupEvent.java
+++ freedom/src/org/freedom/acao/RadioGroupEvent.java
@@ -1,10 +1,11 @@
 /**
  * @version 12/05/2003 <BR>
  * @author Setpoint Informática Ltda./Fernando Oliveira da Silva <BR>
- *
+ * 
  * Projeto: Freedom <BR>
  * Pacote: org.freedom.acao <BR>
- * Classe: @(#)RadioGroupEvent.java <BR>
+ * Classe:
+ * @(#)RadioGroupEvent.java <BR>
  * 
  * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
  * versão 2.1.0 ou qualquer versão posterior. <BR>
@@ -13,24 +14,43 @@
  * o LICENCIADOR ou então pegar uma cópia em: <BR>
  * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
  * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
- * de acordo com os termos da LPG-PC <BR> <BR>
- *
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
  * Comentários da classe.....
  */
 
 package org.freedom.acao;
+
 import javax.swing.JRadioButton;
+
 public class RadioGroupEvent {
-  private JRadioButton rButton = null;
-  private int Indice = -1;
-  public RadioGroupEvent(JRadioButton rb, int ind) {
-    rButton = rb;
-    Indice = ind;
-  }
-  public JRadioButton getRadioButton() {
-    return rButton;
-  }
-  public int getIndice() {
-    return Indice;
-  }
+	
+	private Object source = null;
+
+	private JRadioButton rButton = null;
+
+	private int Indice = -1;
+
+	public RadioGroupEvent( JRadioButton rb, int ind, Object source ) {
+
+		rButton = rb;
+		Indice = ind;
+		this.source = source;
+	}
+
+	public JRadioButton getRadioButton() {
+
+		return rButton;
+	}
+
+	public int getIndice() {
+
+		return Indice;
+	}
+	
+	public Object getSource() {
+	
+		return source;
+	}
 }
