ce58a7991ccd88de202726aca7a2893a1958b952
arlm <arlm@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-04-04 20:11:39 +0000

    Correções gerais para produção de jars

--- freedom/src/org/freedom/modulos/std/FRMovProd.java
+++ freedom/src/org/freedom/modulos/std/FRMovProd.java
@@ -27,7 +27,6 @@ package org.freedom.modulos.std;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.Vector;
 
