f821a11bfcad73c5a5b6149005245839bc0592f5
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-13 14:31:01 +0000

    Mascara para formatação de entrada de dados.

---
+++ freedom-infra/src/org/freedom/infra/x/util/text/Mask.java
@@ -0,0 +1,116 @@
+package org.freedom.infra.x.util.text;
+
+
+public class Mask {
+	
+	public static final char CHAR = 'A';
+	
+	public static final char INTEGER = '0';
+	
+	public static final char NEGATIVE = '-';
+	
+	public static final char DECIMAL = ',';
+	
+	public static final char ALL = '#';
+	
+	private int length = 0;
+
+	private char[] chars = null;
+	
+	
+	public Mask( final String mask ) {
+		
+		length = mask.length();		
+		chars = mask.toCharArray();		
+	}
+
+	public char[] getChars() {
+
+		char[] retorno = new char[ chars.length ];
+		System.arraycopy( chars, 0, retorno, 0, retorno.length );
+		
+		return retorno;
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public void setLength( final int length ) {
+		this.length = length;
+	}
+
+	public String getMask() {
+		return new String( chars );
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	//                                                                            //
+	//                           MASCARAS PRÉ-DEFINIDAS                           //
+	//                                                                            //
+	////////////////////////////////////////////////////////////////////////////////
+	
+	public static Mask createInteger( final int size ) {
+		
+		StringBuilder mask = new StringBuilder();
+		
+		int count = 0;
+		while ( count++ < size ) {
+			mask.append( "0" );
+		}
+		
+		return new Mask( mask.toString() );
+	}
+	
+	public static Mask createDecimal( final int size, final int precision ) {
+		
+		StringBuilder mask = new StringBuilder();
+		
+		int count = 0;
+		while ( count++ < size-precision ) {
+			mask.append( "0" );
+		}
+		
+		mask.append( "," );
+		count = 0;
+		
+		while ( count++ < precision ) {
+			mask.append( "0" );
+		}
+		
+		return new Mask( mask.toString() );
+	}
+	
+	public static Mask createString( final int size ) {
+		
+		StringBuilder mask = new StringBuilder();
+		
+		int count = 0;
+		while ( count++ < size ) {
+			mask.append( "#" );
+		}
+		
+		return new Mask( mask.toString() );
+	}
+	
+	public static Mask createDateDDMMYYYY() {
+				
+		return new Mask( "00/00/0000" );
+	}
+	
+	public static Mask createDateYYYYMMDD() {
+				
+		return new Mask( "0000/00/00" );
+	}
+	
+	public static Mask createTimeHHMMSS() {
+				
+		return new Mask( "00:00:00" );
+	}
+	
+	public static Mask createTimeHHMMSSmmm() {
+				
+		return new Mask( "00:00:00.000" );
+	}
+
+}
