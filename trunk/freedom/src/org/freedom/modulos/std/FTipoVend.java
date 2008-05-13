19d5c493214bde4192c65ebe272009d2d22872d2
felipe_stpinf <felipe_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-13 17:56:16 +0000

    Adionado comentário sobre a classe

--- trunk/freedom/src/org/freedom/modulos/std/FTipoVend.java
+++ trunk/freedom/src/org/freedom/modulos/std/FTipoVend.java
@@ -1,3 +1,26 @@
+/**
+ * @version 13/05/2008 <BR>
+ * @author Setpoint Informática Ltda./Felipe Daniel Elias <BR>
+ * 
+ * Projeto: Freedom <BR>
+ * 
+ * Pacote: org.freedom.modulos.std <BR>
+ * Classe: FTipoVend
+ * @(#)FClasCli.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
+ * Comentários sobre a classe...
+ */
+
 package org.freedom.modulos.std;
 
 import org.freedom.componentes.JTextFieldPad;
