6b1b98cca5cf4238005d303cded0404f9f16f705
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-16 17:51:06 +0000

    Padronização de objetos centrais de aplicação.

---
+++ trunk/freedom-infra/src/org/freedom/infra/controller/Aplication.java
@@ -0,0 +1,78 @@
+package org.freedom.infra.controller;
+
+import java.awt.Window;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public abstract class Aplication {
+
+	private Window window;
+	
+	private File fileIni;
+	
+	private final Properties properties = new Properties();	
+	
+	
+	public Aplication() {
+		
+		super();
+		
+		init();	
+		initParameters();		
+		show();	
+	}
+	
+	protected abstract void init();
+	
+	protected void initParameters() {
+
+		try {
+			if ( fileIni == null ) {
+				fileIni = new File( System.getProperty( "INIT_FILE", "" ) ) ;
+			}
+			
+			if ( fileIni != null && fileIni.exists() ) {
+				FileInputStream fis = new FileInputStream( fileIni );
+				properties.load( fis );
+				fis.close();
+			}
+		}
+		catch ( FileNotFoundException e ) {
+			e.printStackTrace();
+		}
+		catch ( IOException e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void show() {		
+		if ( window != null ) {
+			window.setVisible( true );
+		}
+	}
+	
+	public Window getWindow() {	
+		return window;
+	}
+	
+	public void setWindow( Window window ) {	
+		this.window = window;
+	}
+	
+	public File getFileIni() {	
+		return fileIni;
+	}
+	
+	public void setFileIni( File fileIni ) {	
+		this.fileIni = fileIni;
+	}
+	
+	public Object getPropertie( Object propertie ) {
+		return properties.get( propertie );
+	}
+
+}
