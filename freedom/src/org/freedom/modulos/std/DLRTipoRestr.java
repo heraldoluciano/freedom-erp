b3196e80ab532676975973925d707a36693a3bb4
felipe_stpinf <felipe_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-20 20:02:32 +0000

    Criação de dialog para impressão de relatório, referente a regra fiscal.

---
+++ freedom/src/org/freedom/modulos/std/DLRTipoRestr.java
@@ -0,0 +1,91 @@
+/**
+ * @version 20/05/2003 <BR>
+ * @author Setpoint Informática Ltda./Felipe Daniel Elias <BR>
+ * 
+ * Projeto: Freedom <BR>
+ * 
+ * Pacote: org.freedom.modulos.std <BR>
+ * Classe:
+ * @(#)DLRClasCli.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
+ * Comentários sobre a classe...
+ */
+
+package org.freedom.modulos.std;
+
+import java.awt.Component;
+import java.util.Vector;
+
+import org.freedom.componentes.JLabelPad;
+
+import org.freedom.componentes.JRadioGroup;
+import org.freedom.telas.FFDialogo;
+
+public class DLRTipoRestr extends FFDialogo {
+
+	private static final long serialVersionUID = 1L;
+
+	private JRadioGroup<?, ?> rgOrdem = null;
+
+	private JRadioGroup<?, ?> rgTipo = null;
+
+	private JLabelPad lbOrdem = new JLabelPad( "Ordenar por:" );
+
+	public DLRTipoRestr( Component cOrig ) {
+
+		super( cOrig );
+		setTitulo( "Ordem do Relatório" );
+		setAtribos( 300, 190 );
+
+		Vector<String> vLabs = new Vector<String>();
+		Vector<String> vVals = new Vector<String>();
+
+		vLabs.addElement( "Código" );
+		vLabs.addElement( "Descrição" );
+		vVals.addElement( "C" );
+		vVals.addElement( "D" );
+		rgOrdem = new JRadioGroup<String, String>( 1, 2, vLabs, vVals );
+		rgOrdem.setVlrString( "D" );
+
+		Vector<String> vLabs1 = new Vector<String>();
+		Vector<String> vVals1 = new Vector<String>();
+
+		vVals1.addElement( "G" );
+		vVals1.addElement( "T" );
+		vLabs1.addElement( "Grafico" );
+		vLabs1.addElement( "Texto" );
+		rgTipo = new JRadioGroup<String, String>( 1, 2, vLabs1, vVals1 );
+		rgTipo.setVlrString( "G" );
+
+		adic( lbOrdem, 7, 0, 80, 15 );
+		adic( rgOrdem, 7, 20, 270, 30 );
+		adic( rgTipo, 7, 60, 270, 30 );
+	}
+
+	public String getOrdem() {
+
+		String sRetorno = "DESCTPRESTR";
+
+		if ( rgOrdem.getVlrString().compareTo( "C" ) == 0 ) {
+			sRetorno = "CODTPRESTR";
+		}
+
+		return sRetorno;
+	}
+	
+	public String getTipo() {
+
+		return rgTipo.getVlrString();
+	}	
+
+}
