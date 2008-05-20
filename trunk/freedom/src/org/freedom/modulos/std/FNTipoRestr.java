b615caca1672eae5849b6182d247383d377d9bda
felipe_stpinf <felipe_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-20 20:01:35 +0000

    Criação de tela para cadastro de tipo de restrição, e impressão de relatório.

---
+++ trunk/freedom/src/org/freedom/modulos/std/FNTipoRestr.java
@@ -0,0 +1,181 @@
+/**
+ * @version 20/05/2008 <BR>
+ * @author Setpoint Informática Ltda./Felipe Daniel Elias <BR>
+ * 
+ * Projeto: Freedom <BR>
+ * 
+ * Pacote: org.freedom.modulos.std <BR>
+ * Classe: FTipoVend
+ * @(#)FClasCli.java <BR>
+ * 
+ * Este programa é licenciado de acordo com a LPG-PC (Licença Pública Geral para Programas de Computador), <BR>
+ * versão 2.1.0 ou qualquer versão posterior. <BR>
+ * A LPG-PC deve acompanhar todas PUBLICAÇÕES, DISTRIBUIÇÕES e REPRODUÇÕES deste Programa. <BR>
+ * Caso uma cópia da LPG-PC não esteja disponível junto com este Programa, você pode contatar <BR>
+ * o LICENCIADOR ou então pegar uma cópia em: <BR>
+ * Licença: http://www.lpg.adv.br/licencas/lpgpc.rtf <BR>
+ * Para poder USAR, PUBLICAR, DISTRIBUIR, REPRODUZIR ou ALTERAR este Programa é preciso estar <BR>
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
+ * Comentários sobre a classe...
+ */
+package org.freedom.modulos.std;
+
+import net.sf.jasperreports.engine.JasperPrintManager;
+
+import org.freedom.componentes.ImprimeOS;
+import org.freedom.componentes.JTextFieldPad;
+import org.freedom.componentes.ListaCampos;
+import org.freedom.funcoes.Funcoes;
+import org.freedom.telas.Aplicativo;
+import org.freedom.telas.FDados;
+import org.freedom.telas.FPrinterJob;
+
+import java.awt.event.ActionEvent;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class FNTipoRestr extends FDados {
+
+	private static final long serialVersionUID = 1L;
+
+	private JTextFieldPad txtCodTipoRest = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 5, 0 );
+
+	private JTextFieldPad txtDescTipoRest = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
+
+	public FNTipoRestr() {
+
+		super();
+		setAtribos( 50, 50, 370, 120 );
+		setTitulo( "Tipo de Restrição" );
+
+		adicCampo( txtCodTipoRest, 7, 20, 70, 20, "CODTPRESTR", "Cód.rest.", ListaCampos.DB_PK, true );
+		adicCampo( txtDescTipoRest, 80, 20, 250, 20, "DESCTPRESTR", "Descrição da Restrição", ListaCampos.DB_SI, true );
+		setListaCampos( true, "TIPORESTR", "FN" );
+		setImprimir( true );
+
+		btImp.addActionListener( this );
+		btPrevimp.addActionListener( this );
+
+	}
+
+	public void actionPerformed( ActionEvent evt ) {
+
+		if ( evt.getSource() == btPrevimp ) {
+			imprimir( true );
+		}
+		else if ( evt.getSource() == btImp ) {
+			imprimir( false );
+		}
+		super.actionPerformed( evt );
+	}
+
+	private void imprimir( boolean bVisualizar ) {
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sSQL = null;
+		DLRTipoRestr dl = new DLRTipoRestr( this );
+
+		try {
+			dl.setVisible( true );
+			if ( dl.OK == false ) {
+				dl.dispose();
+				return;
+			}
+
+			sSQL = "SELECT CODTPRESTR, DESCTPRESTR FROM FNTIPORESTR WHERE CODEMP=? AND CODFILIAL=? ORDER BY " + dl.getOrdem();
+			ps = con.prepareStatement( sSQL );
+			ps.setInt( 1, Aplicativo.iCodEmp );
+			ps.setInt( 2, ListaCampos.getMasterFilial( "FNTIPORESTR" ) );
+			rs = ps.executeQuery();
+
+			if ( "T".equals( dl.getTipo() ) ) {
+				imprimirTexto( bVisualizar, rs );
+			}
+			else if ( "G".equals( dl.getTipo() ) ) {
+				imprimirGrafico( bVisualizar, rs );
+			}
+
+			rs.close();
+			ps.close();
+
+			if ( !con.getAutoCommit() ) {
+				con.commit();
+			}
+			dl.dispose();
+		} catch ( Exception err ) {
+			err.printStackTrace();
+			Funcoes.mensagemErro( this, "Erro ao montar relatorio!\n" + err.getMessage(), true, con, err );
+		}
+	}
+
+	private void imprimirTexto( final boolean bVisualizar, final ResultSet rs ) {
+
+		ImprimeOS imp = new ImprimeOS( "", con );
+		int linPag = imp.verifLinPag() - 1;
+		imp.montaCab();
+		imp.setTitulo( "Relatório de Tipo de restrição" );
+
+		try {
+
+			imp.limpaPags();
+
+			while ( rs.next() ) {
+				if ( imp.pRow() == 0 ) {
+					imp.impCab( 80, false );
+					imp.say( imp.pRow() + 0, 0, "" + imp.normal() );
+					imp.say( imp.pRow() + 0, 0, "" );
+					imp.say( imp.pRow() + 0, 2, "Código" );
+					imp.say( imp.pRow() + 0, 30, "Descrição" );
+					imp.say( imp.pRow() + 1, 0, "" + imp.normal() );
+					imp.say( imp.pRow() + 0, 0, Funcoes.replicate( "-", 80 ) );
+
+				}
+				imp.say( imp.pRow() + 1, 0, "" + imp.normal() );
+				imp.say( imp.pRow() + 0, 2, rs.getString( "CODTPRESTR" ) );
+				imp.say( imp.pRow() + 0, 30, rs.getString( "DESCTPRESTR" ) );
+				imp.say( imp.pRow() + 0, 82, "|" + imp.normal() );
+				if ( imp.pRow() >= linPag ) {
+					imp.incPags();
+					imp.eject();
+				}
+			}
+
+			imp.say( imp.pRow() + 1, 0, "" + imp.normal() );
+			imp.say( imp.pRow() + 0, 0, Funcoes.replicate( "=", 80 ) );
+
+			imp.eject();
+			imp.fechaGravacao();
+
+			if ( bVisualizar ) {
+				imp.preview( this );
+			}
+			else {
+				imp.print();
+			}
+		} catch ( Exception err ) {
+			err.printStackTrace();
+			Funcoes.mensagemErro( this, "Erro ao montar relatorio!\n" + err.getMessage(), true, con, err );
+		}
+	}
+
+	public void imprimirGrafico( final boolean bVisualizar, final ResultSet rs ) {
+
+		FPrinterJob dlGr = new FPrinterJob( "relatorios/TipoRestr.jasper", "Tipo de restrição ", null, rs, null, this );
+
+		if ( bVisualizar ) {
+			dlGr.setVisible( true );
+		}
+		else {
+			try {
+
+				JasperPrintManager.printReport( dlGr.getRelatorio(), true );
+			} catch ( Exception err ) {
+				err.printStackTrace();
+				Funcoes.mensagemErro( this, "Erro ao montar relatorio!\n" + err.getMessage(), true, con, err );
+			}
+		}
+	}
+}
