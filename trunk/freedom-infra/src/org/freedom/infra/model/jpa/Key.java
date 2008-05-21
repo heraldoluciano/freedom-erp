4214e3dbc5e8d13d5412829ec628e88a597325f2
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-21 20:47:32 +0000

    Continuação da implementação de chave composta JPA.

--- trunk/freedom-infra/src/org/freedom/infra/model/jpa/Key.java
+++ trunk/freedom-infra/src/org/freedom/infra/model/jpa/Key.java
@@ -1,12 +1,68 @@
 package org.freedom.infra.model.jpa;
 
-public class Key {
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-	Object[] keys = null;
-	public Key(Object... keys) {
-		this.keys = keys;
+public class Key implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final int COL_KEY = 0;
+	public static final int COL_VALUE = 1;
+	private Object[][] keys = null;
+	private String internalKey = null;
+	
+	public Key(Object[][] keys) {
+		setKeys(keys);
 	}
-	
-	
+
+	public Object[][] getKeys() {
+		return keys;
+	}
+
+	public void setKeys(Object[][] keys) {
+		this.keys = keys;
+		if (keys!=null) {
+			StringBuilder buffer = new StringBuilder();
+			for (int i=0; i<keys.length; i++) {
+			   Object key = keys[i][COL_KEY];	
+			   Object value = keys[i][COL_VALUE];
+			   try {
+				   Method method = this.getClass().getMethod("set" + key, value.getClass());
+				   method.invoke(this, value);
+			   } catch (NoSuchMethodException e) {
+				   e.printStackTrace(); 
+			   } catch (InvocationTargetException e) {
+				   e.printStackTrace();
+			   } catch (IllegalAccessException e) {
+				   e.printStackTrace();
+			   }
+               buffer.append(value.toString());
+               buffer.append(" ");
+			}
+		    setInternalKey( buffer.toString() );
+		}
+		
+	}
+
+	public int hashCode() {
+		int hashCode = 0;
+		if ( (keys!=null) && (internalKey!=null) ) {
+			hashCode = internalKey.hashCode();
+			//hashCode = keys.hashCode();
+		}
+		return hashCode;
+	}
+
+	public String getInternalKey() {
+		return internalKey;
+	}
+
+	public void setInternalKey(String internalKey) {
+		this.internalKey = internalKey;
+	}
 
 }
