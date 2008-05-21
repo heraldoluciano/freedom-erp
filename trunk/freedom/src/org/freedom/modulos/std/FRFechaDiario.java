160ce437502e381017f20895ee3e61778e9bc536
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-21 13:16:35 +0000

    correção de bug.

--- trunk/freedom/src/org/freedom/modulos/std/FRFechaDiario.java
+++ trunk/freedom/src/org/freedom/modulos/std/FRFechaDiario.java
@@ -160,9 +160,9 @@ public class FRFechaDiario extends FRelatorio {
 			sSQL.append( "WHERE IR.CODEMP=? AND IR.CODFILIAL=? AND IR.PDVITREC='S' AND IR.STATUSITREC='RP' AND " );
 			sSQL.append( "IR.DTPAGOITREC BETWEEN ? AND ? AND " );
 			sSQL.append( "IR.CODEMP=R.CODEMP AND IR.CODFILIAL=R.CODFILIAL AND IR.CODREC=R.CODREC AND " );
-			sSQL.append( "R.CODEMPPG=P.CODEMP AND R.CODFILIALPG=P.CODFILIAL AND R.CODPLANOPAG=P.CODPLANOPAG " );
+			sSQL.append( "R.CODEMPPG=P.CODEMP AND R.CODFILIALPG=P.CODFILIAL AND R.CODPLANOPAG=P.CODPLANOPAG AND " ); // ADICIONADO AND NO FINAL DA LINHA
 			if ( !"".equals( idusu ) ) {
-				sSQL.append( " CP.IDUSUINS=? AND " );
+				sSQL.append( " IR.IDUSUINS=?" ); // AO INVÉS DE CP.IDUSUINS, FOI COLOCADO IR.IDUSUINS
 			}
 			sSQL.append( "GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9 " );
 			sSQL.append( "ORDER BY 1, 2, 3, 4, 5, 6, 7, 8, 9 " );
@@ -193,7 +193,7 @@ public class FRFechaDiario extends FRelatorio {
 				sCab.append( " - Caixa: " + codcaixa );
 			}
 			if ( !"".equals( idusu ) ) {
-				sSQL.append( " V.IDUSUINS=? AND " );
+				sSQL.append( " C.IDUSUINS=? AND " ); //AO INVÉS DE V.IDUSUINS, FOI COLOCADO C.IDUSUINS
 				sCab.append( " - Usuário: " + idusu );
 			}
 
@@ -240,7 +240,7 @@ public class FRFechaDiario extends FRelatorio {
 			sSQL.append( "WHERE IR.CODEMP=? AND IR.CODFILIAL=? AND " );
 			sSQL.append( "IR.DTPAGOITREC BETWEEN ? AND ? AND IR.PDVITREC='S' AND IR.STATUSITREC='RP' AND " );
 			if ( !"".equals( idusu ) ) {
-				sSQL.append( " CP.IDUSUINS=? AND " );
+				sSQL.append( " IR.IDUSUINS=? AND " ); //AO INVÉS DE CP.IDUSUINS, FOI COLOCADO IR.IDUSUINS
 			}
 			sSQL.append( "IR.CODEMP=R.CODEMP AND IR.CODFILIAL=R.CODFILIAL AND IR.CODREC=R.CODREC AND " );
 			sSQL.append( "R.CODEMPPG=P.CODEMP AND R.CODFILIALPG=P.CODFILIAL AND R.CODPLANOPAG=P.CODPLANOPAG AND " );
