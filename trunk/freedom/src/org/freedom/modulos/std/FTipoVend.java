7c50a68d9f22c28af3fa670cdf9658847b06739c
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-23 20:17:54 +0000

    alteração em tamanho de campo.

--- trunk/freedom/src/org/freedom/modulos/std/FTipoVend.java
+++ trunk/freedom/src/org/freedom/modulos/std/FTipoVend.java
@@ -48,10 +48,10 @@ public class FTipoVend extends FDados {
 	public FTipoVend() {
 
 		super();
-		setAtribos( 50, 50, 350, 165 );
+		setAtribos( 50, 50, 365, 120 );
 		setTitulo( "Cadastro de Tipos de Comissionados" );
-		adicCampo( txtCodTipoVenda, 7, 20, 70, 20, "CODTIPOVEND", "Cód.tp.comis.", ListaCampos.DB_PK, true );
-		adicCampo( txtDescTipoVenda, 80, 20, 250, 20, "DESCTIPOVEND", "Descrição do tipo de comissionado", ListaCampos.DB_SI, true );
+		adicCampo( txtCodTipoVenda, 7, 20, 80, 20, "CODTIPOVEND", "Cód.tp.comis.", ListaCampos.DB_PK, true );
+		adicCampo( txtDescTipoVenda, 90, 20, 250, 20, "DESCTIPOVEND", "Descrição do tipo de comissionado", ListaCampos.DB_SI, true );
 		setListaCampos( true, "TIPOVEND", "VD" );
 		setImprimir( true );
 		btImp.addActionListener( this );
