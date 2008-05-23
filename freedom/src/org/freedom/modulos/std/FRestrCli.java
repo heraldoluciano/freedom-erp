d95de67f34708565a2395b1c85d49f0bd3f6f487
felipe_stpinf <felipe_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-05-23 19:11:07 +0000

    Conclusão da tela restrição de clientes.

--- freedom/src/org/freedom/modulos/std/FRestrCli.java
+++ freedom/src/org/freedom/modulos/std/FRestrCli.java
@@ -1,5 +1,5 @@
 /**
- * @version 20/05/2008 <BR>
+ * @version 23/05/2008 <BR>
  * @author Setpoint Informática Ltda./Felipe Daniel Elias <BR>
  *
  * Projeto: Freedom <BR>
@@ -22,13 +22,24 @@
 
 package org.freedom.modulos.std;
 import java.sql.Connection;
+import java.util.Date;
 
+import javax.swing.JScrollPane;
+
+import org.freedom.acao.CarregaEvent;
+import org.freedom.acao.CarregaListener;
+import org.freedom.acao.InsertEvent;
+import org.freedom.acao.InsertListener;
+import org.freedom.componentes.GuardaCampo;
+import org.freedom.componentes.JLabelPad;
+import org.freedom.componentes.JTextAreaPad;
+import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
 import org.freedom.componentes.JPanelPad;
 import org.freedom.telas.FDetalhe;
 
-public class FRestrCli extends FDetalhe {
+public class FRestrCli extends FDetalhe implements CarregaListener, InsertListener  {
 	
 	private static final long serialVersionUID = 1L;
 
@@ -41,7 +52,25 @@ public class FRestrCli extends FDetalhe {
 	private JTextFieldPad txtRazCli = new JTextFieldPad(JTextFieldPad.TP_STRING,50,0);
 	
 	private JTextFieldPad txtSeqRestr = new JTextFieldPad(JTextFieldPad.TP_INTEGER, 8, 0);
-	//private ListaCampos lcCli = new ListaCampos(this);
+	
+	private JTextFieldPad txtCodTipoRestr = new JTextFieldPad(JTextFieldPad.TP_INTEGER, 8, 0);
+	
+	private JTextFieldFK txtDescTipoRestr = new JTextFieldFK(JTextFieldFK.TP_STRING,50,0);
+	
+	private JTextFieldPad txtSitRestr = new JTextFieldPad(JTextFieldPad.TP_STRING,1,0);
+	
+	private JTextFieldPad txtDataRestr = new JTextFieldPad(JTextFieldPad.TP_DATE,10,0);
+	
+	private JTextFieldPad txtDataCancRestr = new JTextFieldPad(JTextFieldPad.TP_DATE,10,0);
+	
+	private JTextFieldPad txtLegenda = new JTextFieldPad(JTextFieldPad.TP_STRING,50,0);
+	
+	private JTextAreaPad txaObs= new JTextAreaPad(512);
+	
+	private JScrollPane spnObs = new JScrollPane(txaObs);
+	
+	private ListaCampos lcTipoRestr = new ListaCampos(this, "TR");
+	
 	
 	public FRestrCli () {
 		
@@ -52,39 +81,95 @@ public class FRestrCli extends FDetalhe {
 		setListaCampos(lcCampos);
 		setPainel( pinCab, pnCliCab);
 		
+		navRod.setAtivo( 5, false );
+		
 
 		adicCampo( txtCodCli, 7, 20, 100, 20,"Codcli","Cód.cli", ListaCampos.DB_PF,true);
 		adicCampo( txtRazCli, 110, 20, 220, 20,"Razcli","Razão social", ListaCampos.DB_SI, false);  
 		setListaCampos( true, "CLIENTE", "VD" );
 		lcCampos.setQueryCommit(false);
 		lcCampos.setReadOnly(true);
-		//txtCodCli.setTabelaExterna(lcCli);
 		
-		setAltDet( 90 );
+		
+		lcTipoRestr.add(new GuardaCampo( txtCodTipoRestr, "CodTpRestr", "Cód.tp.restr.",  ListaCampos.DB_PK, false));
+		lcTipoRestr.add(new GuardaCampo( txtDescTipoRestr, "DescTpRestr", "Descriçao da restrição",  ListaCampos.DB_SI, false));
+		lcTipoRestr.montaSql(false, "TIPORESTR", "FN");    
+		lcTipoRestr.setQueryCommit(false);
+		lcTipoRestr.setReadOnly(true);
+		txtCodTipoRestr.setTabelaExterna(lcTipoRestr);
+		
+		setAltDet( 150 );
 		pinDet = new JPanelPad( 600, 90 );
 		setPainel( pinDet, pnDet );
 		setListaCampos( lcDet );
 		setNavegador( navRod );
 
-		adicCampo( txtSeqRestr, 7, 20, 60, 20, "SeqRestr", "Item", ListaCampos.DB_PK, true );
-		//adicCampo( txtPercItemPag, 70, 20, 97, 20, "PercPag", "Percento", ListaCampos.DB_SI, true );
-		//adicCampo( txtDiasItemPag, 170, 20, 57, 20, "DiasPag", "Dias", ListaCampos.DB_SI, false );
-		//adicCampo( txtDescItemPag, 230, 20, 143, 20, "DescParcPag", "Descrição", ListaCampos.DB_SI, false );
-
+		txtLegenda.setSoLeitura( true );
+		txtSitRestr.setSoLeitura( true );
+		adicCampo( txtSeqRestr, 7, 20, 30, 20, "SeqRestr", "Item", ListaCampos.DB_PK, true );
+		adicCampo( txtCodTipoRestr, 40, 20, 80, 20, "CodTpRestr", "Cód.tp.restr.", ListaCampos.DB_FK, txtDescTipoRestr, true );
+		adicDescFK( txtDescTipoRestr, 123, 20, 200, 20, "DescTpRestr", "Descriçao da restrição" );
+		adic( new JLabelPad ("Situação"), 326, 0, 200, 20 );
+		adic( txtLegenda, 326, 20, 200, 20 );
+		adicCampo( txtSitRestr, 529, 20, 30, 20, "SitRestr", "", ListaCampos.DB_SI, false );
+		adicCampo( txtDataRestr, 7, 60, 100, 20, "DtRestr", "Data cadastro", ListaCampos.DB_SI, true );
+		adicCampo( txtDataCancRestr, 7, 100, 100, 20, "DtCancRestr", "Data canc.", ListaCampos.DB_SI, false );
+		adicDBLiv( txaObs, 113, 60, 450, 60,"ObsRestr","Observação",false);
+		adic( new JLabelPad ("Observação"), 113, 40, 200, 20 );
+	   // adic(spnObs, 113, 60, 450, 60 );
 		setListaCampos( true, "RESTRICAO", "FN" );
 		lcDet.setQueryInsert( true );
-
-		//navRod.setAtivo( 4, false );
-		//navRod.setAtivo( 5, false );
+		
 		montaTab();
+		tab.setTamColuna( 200, 2 );
+		tab.setTamColuna( 130, 4 );
+		tab.setTamColuna( 130, 5 );
+		tab.setTamColuna( 300, 6 );
 		
 		btImp.addActionListener(this);
 		btPrevimp.addActionListener(this);
+		lcTipoRestr.addCarregaListener( this );
+		lcDet.addCarregaListener( this );
+		lcDet.addInsertListener( this );
 	}
 	
 	public void setConexao(Connection con) {
 		
 		super.setConexao( con );
+		lcTipoRestr.setConexao( con );
+	
+	}
+
+	public void afterCarrega( CarregaEvent cevt ) {
+
+		
+		if( cevt.getListaCampos() == lcDet ){
+			
+			if( "I".equals( txtSitRestr.getVlrString())){
+				txtLegenda.setVlrString( "Inclusão" );
+			}else if("C".equals( txtSitRestr.getVlrString())){
+				txtLegenda.setVlrString( "Cancelamento" );
+			}
+			txtDataCancRestr.setEnabled( true );
+		}
+	}
+
+	public void beforeCarrega( CarregaEvent cevt ) {
+
+		
+	}
+
+	public void afterInsert( InsertEvent ievt ) {
+
+		if( ievt.getListaCampos() == lcDet ){
+			txtLegenda.setVlrString( "" );
+			txtDataCancRestr.setEnabled( false );
+			txtDataRestr.setVlrDate( new Date() );
+		}
+	}
+
+	public void beforeInsert( InsertEvent ievt ) {
+
 	
 	}
 }
