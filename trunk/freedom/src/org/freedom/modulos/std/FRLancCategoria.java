d0f5bd9cdac1ed483eb006de46bd684c52409772
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-06-05 17:51:31 +0000

    criação de filtros.

--- trunk/freedom/src/org/freedom/modulos/std/FRLancCategoria.java
+++ trunk/freedom/src/org/freedom/modulos/std/FRLancCategoria.java
@@ -23,8 +23,10 @@ package org.freedom.modulos.std;
 
 
 import java.awt.event.ActionListener;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
@@ -35,6 +37,9 @@ import javax.swing.SwingConstants;
 
 import net.sf.jasperreports.engine.JasperPrintManager;
 
+import org.freedom.componentes.GuardaCampo;
+import org.freedom.componentes.JLabelPad;
+import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
 import org.freedom.funcoes.Funcoes;
@@ -51,12 +56,36 @@ public class FRLancCategoria extends FRelatorio implements ActionListener{
 
 	private JTextFieldPad txtDatafim = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
 	
+	private JTextFieldPad txtCodCC = new JTextFieldPad( JTextFieldPad.TP_STRING, 19, 0 );
+	
+	private JTextFieldFK txtDescCC = new JTextFieldFK( JTextFieldPad.TP_STRING, 50, 0 );
+	
+	private JTextFieldFK txtSiglaCC = new JTextFieldFK( JTextFieldPad.TP_STRING, 10, 0 );
+	
+	private JTextFieldPad txtCodConta = new JTextFieldPad( JTextFieldPad.TP_STRING, 10, 0 );
+	
+	private JTextFieldFK txtDescConta = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
+	
+	private JTextFieldPad txtCodPlan = new JTextFieldPad( JTextFieldPad.TP_STRING, 13, 0 );
+	
+	private JTextFieldFK txtDescPlan = new JTextFieldFK(JTextFieldPad.TP_STRING, 40, 0);
+	
+	private ListaCampos lcCC = new ListaCampos( this );
+	
+	private ListaCampos lcConta = new ListaCampos( this );
+	
+	private ListaCampos lcPlan = new ListaCampos(this);
+	
+	int iAnoBase = 0;
+
+	
 	public FRLancCategoria(){
 		
 		setTitulo( "Lançamentos por categoria" );
-		setAtribos( 80, 80, 350, 150 );
+		setAtribos( 80, 80, 350, 300 );
 		
 		montaTela();
+		montaListaCampos();
 	}
 	
 	private void montaTela(){
@@ -66,7 +95,7 @@ public class FRLancCategoria extends FRelatorio implements ActionListener{
 		adic( periodo, 25, 10, 80, 20 );
 		JLabel borda = new JLabel();
 		borda.setBorder( BorderFactory.createEtchedBorder() );
-		adic( borda, 7, 20, 296, 45 );
+		adic( borda, 7, 20, 313, 45 );
 		adic( txtDataini, 25, 35, 110, 20 );
 		adic( new JLabel( "até", SwingConstants.CENTER ), 135, 35, 40, 20 );
 		adic( txtDatafim, 175, 35, 110, 20 );
@@ -75,6 +104,64 @@ public class FRLancCategoria extends FRelatorio implements ActionListener{
 		txtDatafim.setVlrDate( cPeriodo.getTime() );
 		cPeriodo.set( Calendar.DAY_OF_MONTH, cPeriodo.get( Calendar.DAY_OF_MONTH ) - 30 );
 		txtDataini.setVlrDate( cPeriodo.getTime() );
+		
+		adic( new JLabelPad( "Cód. CC" ),7, 65, 80, 20 );
+		adic( txtCodCC, 7, 85, 80, 20 );
+		adic( new JLabelPad( "Descrição do Centro de custo" ),90, 65, 230, 20 );
+		adic( txtDescCC, 90, 85, 230, 20 );
+		adic( new JLabelPad("Cód.Conta"),7, 105, 80, 20 );
+		adic( txtCodConta, 7, 125, 80, 20 );
+		adic( new JLabelPad("Descrição da  Conta"),90, 105, 230, 20 );
+		adic( txtDescConta, 90, 125, 230, 20 );
+		adic( new JLabelPad("Cód.Plan."),7, 145, 80, 20 );
+		adic( txtCodPlan, 7, 165, 80, 20 );
+		adic( new JLabelPad("Descrição do planejamento"),90, 145, 230, 20 );
+		adic( txtDescPlan, 90, 165, 230, 20 );
+		
+		
+	}
+	
+	private void montaListaCampos(){
+		
+		/*******************
+		 * Centro de custo *
+		 *******************/
+		 
+		lcCC.add( new GuardaCampo( txtCodCC, "CodCC", "Cód.cc.", ListaCampos.DB_PK, false ) );
+		lcCC.add( new GuardaCampo( txtSiglaCC, "SiglaCC", "Sigla", ListaCampos.DB_SI, false ) );
+		lcCC.add( new GuardaCampo( txtDescCC, "DescCC", "Descrição do centro de custo", ListaCampos.DB_SI, false ) );
+		lcCC.setReadOnly( true );
+		lcCC.montaSql( false, "CC", "FN" );
+		txtCodCC.setTabelaExterna( lcCC );
+		txtCodCC.setFK( true );
+		txtCodCC.setNomeCampo( "CodCC" );
+		txtSiglaCC.setListaCampos( lcCC );
+		
+		/*********
+		 * Conta *
+		 *********/
+		
+		lcConta.add( new GuardaCampo( txtCodConta, "NumConta", "Cód.conta", ListaCampos.DB_PK, false ) );
+		lcConta.add( new GuardaCampo( txtDescConta, "DescConta", "Descrição da conta", ListaCampos.DB_SI, false ) );
+		lcConta.add( new GuardaCampo( txtCodPlan, "CodPlan", "Cod.plan.", ListaCampos.DB_SI, false ) );
+		lcConta.montaSql( false, "CONTA", "FN" );
+		lcConta.setReadOnly( true );
+		txtCodConta.setTabelaExterna( lcConta );
+		txtCodConta.setFK( true );
+		txtCodConta.setNomeCampo( "NumConta" );
+		
+		/****************
+		 * Planejamento *
+		 * *************/
+		
+		lcPlan.add(new GuardaCampo(txtCodPlan, "CodPlan", "Cód.plan",ListaCampos.DB_PK, false));
+		lcPlan.add(new GuardaCampo(txtDescPlan, "DescPlan","Descrição do planejamento", ListaCampos.DB_SI, false));
+		lcPlan.montaSql(false, "PLANEJAMENTO", "FN");
+		lcPlan.setWhereAdic("NIVELPLAN=6");
+		lcPlan.setReadOnly(true);
+		txtCodPlan.setTabelaExterna(lcPlan);
+		txtCodPlan.setFK(true);
+		txtCodPlan.setNomeCampo("CodPlan");
 	}
 
 	public void imprimir( boolean bVisualizar ){
@@ -82,6 +169,7 @@ public class FRLancCategoria extends FRelatorio implements ActionListener{
 		StringBuilder sSQL = new StringBuilder();
 		ResultSet rs = null;
 		StringBuilder sCab = new StringBuilder();
+		StringBuilder sWhere = new StringBuilder();
 			
 		
 		if ( txtDatafim.getVlrDate().before( txtDataini.getVlrDate() ) ) {
@@ -90,7 +178,24 @@ public class FRLancCategoria extends FRelatorio implements ActionListener{
 			return;
 		}
 		
-		sCab.append( "Periodo: " + txtDataini.getVlrString() + " " + " Até " + " " + txtDatafim.getVlrString() ); 
+		if( ! "".equals( txtCodCC.getVlrString().trim() )){
+			
+			sWhere.append( "AND SL.CODCC= " + txtCodCC.getVlrString() );
+			sCab.append( " Centro de custo:  " + txtCodCC.getVlrString() + "  " + txtDescCC.getVlrString() );
+		}
+		
+		if( ! "".equals( txtCodConta.getVlrString() )){
+			
+			sWhere.append( "AND " );
+			
+		}
+		
+		if( ! "".equals( txtCodPlan.getVlrString() )){
+			
+			sWhere.append( " AND SL.CODPLAN= " + txtCodPlan.getVlrString() );
+		}
+
+		sCab.append( "  Periodo: " + txtDataini.getVlrString() + " " + " Até " + " " + txtDatafim.getVlrString() ); 
 		
 		sSQL.append( " SELECT SL.CODPLAN, PL.DESCPLAN, SL.DATASUBLANCA, SL.HISTSUBLANCA, L.DOCLANCA, SL.VLRSUBLANCA,CC.DESCCC, C.DESCCONTA " );
 		sSQL.append( "FROM FNSUBLANCA SL " );
@@ -100,6 +205,7 @@ public class FRLancCategoria extends FRelatorio implements ActionListener{
 		sSQL.append( "LEFT OUTER JOIN FNCONTA C ON PL.CODEMP=SL.CODEMPPN AND PL.CODFILIAL=SL.CODFILIALPN AND PL.CODPLAN=SL.CODPLAN AND " );
 		sSQL.append( "C.CODEMPPN=PL.CODEMP AND C.CODFILIALPN=PL.CODFILIAL AND C.CODPLAN=PL.CODPLAN " );
 		sSQL.append( "WHERE SL.CODEMP=? AND SL.CODFILIAL=? AND SL.DATASUBLANCA BETWEEN ? AND ? " );
+		sSQL.append( sWhere.toString() );
 		sSQL.append( "ORDER BY SL.CODPLAN, SL.DATASUBLANCA" );
 		
 		try {
@@ -145,4 +251,33 @@ public class FRLancCategoria extends FRelatorio implements ActionListener{
 				}
 			}
 		}
+		
+		public void setConexao( Connection cn ) {
+			
+			super.setConexao( cn );
+			iAnoBase = buscaAnoBaseCC();
+			lcConta.setConexao( cn );
+			lcPlan.setConexao( cn );
+			lcCC.setConexao( cn );
+			lcCC.setWhereAdic( "ANOCC=" + iAnoBase );
+		}
+	
+		private int buscaAnoBaseCC() {
+
+			int iRet = 0;
+			String sSQL = "SELECT ANOCENTROCUSTO FROM SGPREFERE1 WHERE CODEMP=? AND CODFILIAL=?";
+			try {
+				PreparedStatement ps = con.prepareStatement( sSQL );
+				ps.setInt( 1, Aplicativo.iCodEmp );
+				ps.setInt( 2, ListaCampos.getMasterFilial( "SGPREFERE1" ) );
+				ResultSet rs = ps.executeQuery();
+				if ( rs.next() )
+					iRet = rs.getInt( "ANOCENTROCUSTO" );
+				rs.close();
+				ps.close();
+			} catch ( SQLException err ) {
+				Funcoes.mensagemErro( this, "Erro ao buscar o ano-base para o centro de custo.\n" + err.getMessage(), true, con, err );
+			}
+			return iRet;
+		}	
 	}
