ef6abf12d8b37aa397ff29b9803d940e50ea9bc9
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-06-13 19:07:08 +0000

    Implementação de histórico padrão para baixas de contas a receber e pagar.

--- trunk/freedom/src/org/freedom/componentes/ObjetoHistorico.java
+++ trunk/freedom/src/org/freedom/componentes/ObjetoHistorico.java
@@ -1,19 +1,57 @@
 package org.freedom.componentes;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Date;
 
 import org.freedom.funcoes.Funcoes;
+import org.freedom.telas.Aplicativo;
 
 
 public class ObjetoHistorico {
 	private String historicocodificado;
 	private String documento;
+	private String portador;
 	private BigDecimal valor;
 	private String serie;
 	private Date data;
 	private String historicodecodificado;
+	
+	public ObjetoHistorico() {
+		super();
+	}
 
+	public ObjetoHistorico(final Integer codhist, Connection con) {
+		super();
+		
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = con.prepareStatement( "SELECT TXAHISTPAD FROM FNHISTPAD WHERE CODEMP=? AND CODFILIAL=? AND CODHIST=?" );
+			ps.setInt( 1, Aplicativo.iCodEmp );
+			ps.setInt( 2, ListaCampos.getMasterFilial( "FNHISTPAD" ) );
+			ps.setInt( 2, codhist );
+
+			rs = ps.executeQuery();
+
+			if ( rs.next() ) {
+				setHistoricocodificado( rs.getString( 1 ) );
+			}
+			
+			rs.close();
+			ps.close();
+
+			if ( !con.getAutoCommit() ) {
+				con.commit();
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	protected void decodeHistorico( ) {
 		
 		String decoded = null;
@@ -28,6 +66,7 @@ public class ObjetoHistorico {
 				tmp = tmp.replaceAll( "<VALOR>", getValor() != null ? String.valueOf( getValor() ) :  "0.00"  ) ;
 				tmp = tmp.replaceAll( "<SERIE>", getSerie() != null ? getSerie() : "" );
 				tmp = tmp.replaceAll( "<DATA>", getData() !=null ? Funcoes.dateToStrDate( getData() ) : "");
+				tmp = tmp.replaceAll( "<PORTADOR>", getPortador() != null ? getPortador() : "" );
 				
 				decoded = tmp;
 			}
@@ -35,8 +74,8 @@ public class ObjetoHistorico {
 				e.printStackTrace();
 			}
 		}
-		
-		setHistoricoDecodificado( decoded );
+		setHistoricodecodificado( decoded );
+
 	}
 
 	
@@ -64,18 +103,6 @@ public class ObjetoHistorico {
 	}
 
 	
-	public String getHistoricoDecodificado() {
-		decodeHistorico();
-		return historicodecodificado;
-	}
-
-	
-	public void setHistoricoDecodificado( String historicodecodificado ) {
-	
-		this.historicodecodificado = historicodecodificado;
-	}
-
-	
 	public String getSerie() {
 	
 		return serie;
@@ -88,18 +115,6 @@ public class ObjetoHistorico {
 	}
 
 	
-	public String getHistoricoCodificado() {
-	
-		return historicocodificado;
-	}
-
-	
-	public void setHistoricoCodificado( String historicocodificado ) {
-	
-		this.historicocodificado = historicocodificado;
-	}
-
-	
 	public BigDecimal getValor() {
 	
 		return valor;
@@ -109,5 +124,47 @@ public class ObjetoHistorico {
 	public void setValor( BigDecimal valor ) {
 	
 		this.valor = valor;
+	}
+
+
+	
+	public String getHistoricocodificado() {
+	
+		return historicocodificado;
+	}
+
+
+	
+	public void setHistoricocodificado( String historicocodificado ) {
+	
+		this.historicocodificado = historicocodificado;
+	}
+
+
+	
+	public String getHistoricodecodificado() {
+		decodeHistorico();
+		return historicodecodificado;
+	}
+
+
+	
+	public void setHistoricodecodificado( String historicodecodificado ) {
+	
+		this.historicodecodificado = historicodecodificado;
+	}
+
+
+	
+	public String getPortador() {
+	
+		return portador;
+	}
+
+
+	
+	public void setPortador( String portador ) {
+	
+		this.portador = portador;
 	}
 }
