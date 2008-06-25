70be38a14828c978ab84ee906a756ca5395eefb7
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-06-25 13:30:44 +0000

    Classe para setar parâmetros de conexão.

---
+++ trunk/freedom-infra/src/org/freedom/infra/model/jpa/AbstractJpaProperties.java
@@ -0,0 +1,10 @@
+package org.freedom.infra.model.jpa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractJpaProperties {
+	public static Map setUser(final int key, String user) {
+		return new HashMap();
+	}
+}
