00dc051577a16442233c242a921ca72467cee536
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-07-17 22:45:52 +0000

    criação de painel para imagem;

--- freedom/src/org/freedom/modulos/pcp/DLAcoesCorretivas.java
+++ freedom/src/org/freedom/modulos/pcp/DLAcoesCorretivas.java
@@ -1,15 +1,30 @@
 package org.freedom.modulos.pcp;
 
+import javax.swing.ImageIcon;
+
+import org.freedom.bmps.Icone;
+import org.freedom.componentes.JLabelPad;
+import org.freedom.componentes.JPanelPad;
 import org.freedom.telas.FFDialogo;
 
 
 public class DLAcoesCorretivas extends FFDialogo {
 
 	private static final long serialVersionUID = 1L;
+	
+	private JPanelPad pnImg = new JPanelPad( 635, 371 );
+	
+	private ImageIcon imgACorretiva = Icone.novo( "ishikawa.jpg" );
+	
+	private JLabelPad lbImg = new JLabelPad( imgACorretiva );
 
 	public DLAcoesCorretivas(){
 		
 		setTitulo( "Acões corretivas" );
-		setAtribos( 350, 300 );
+		setAtribos( 690, 500 );
+		
+		adic( pnImg, 20, 20, 635, 371 );
+		pnImg.tiraBorda();
+		pnImg.adic( lbImg, 0, 0, 635, 371 );
 	}
 }
