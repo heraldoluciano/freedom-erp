64bceea5ef5a568631e6286bf8e1320366eaf8b6
alex_stpinf <alex_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-07-18 15:01:35 +0000

    acerto para "lincar" a imagem.

--- trunk/freedom/src/org/freedom/modulos/pcp/DLAcoesCorretivas.java
+++ trunk/freedom/src/org/freedom/modulos/pcp/DLAcoesCorretivas.java
@@ -1,30 +1,98 @@
 package org.freedom.modulos.pcp;
 
-import javax.swing.ImageIcon;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.sql.Connection;
 
 import org.freedom.bmps.Icone;
 import org.freedom.componentes.JLabelPad;
-import org.freedom.componentes.JPanelPad;
+import org.freedom.modulos.pcp.DLAcaoCorretiva.EMs;
 import org.freedom.telas.FFDialogo;
 
 
-public class DLAcoesCorretivas extends FFDialogo {
+public class DLAcoesCorretivas extends FFDialogo implements MouseListener {
 
 	private static final long serialVersionUID = 1L;
 	
-	private JPanelPad pnImg = new JPanelPad( 635, 371 );
+	private final JLabelPad lbImg = new JLabelPad( Icone.novo( "ishikawa.jpg" ) );
 	
-	private ImageIcon imgACorretiva = Icone.novo( "ishikawa.jpg" );
+	private final JLabelPad lbM1 = new JLabelPad();
 	
-	private JLabelPad lbImg = new JLabelPad( imgACorretiva );
+	private final JLabelPad lbM2 = new JLabelPad();
+	
+	private final JLabelPad lbM3 = new JLabelPad();
+	
+	private final JLabelPad lbM4 = new JLabelPad();
+	
+	private final JLabelPad lbM5 = new JLabelPad();
+	
+	private final JLabelPad lbM6 = new JLabelPad();
+	
+	private Object[] keys;
+	
 
-	public DLAcoesCorretivas(){
+	public DLAcoesCorretivas( Connection con, Object[] keys ) {
 		
 		setTitulo( "Acões corretivas" );
-		setAtribos( 690, 500 );
+		setAtribos( 670, 480 );
+		setConexao( con );
 		
-		adic( pnImg, 20, 20, 635, 371 );
-		pnImg.tiraBorda();
-		pnImg.adic( lbImg, 0, 0, 635, 371 );
+		adic( lbM1, 26, 46, 116, 61 );
+		adic( lbM2, 182, 46, 116, 61 );
+		adic( lbM3, 341, 46, 116, 61 );
+		adic( lbM4, 26, 225, 116, 61 );
+		adic( lbM5, 182, 225, 116, 61 );
+		adic( lbM6, 341, 225, 116, 61 );
+		adic( lbImg, 10, 10, 635, 371 );
+		
+		lbM1.addMouseListener( this );
+		lbM2.addMouseListener( this );
+		lbM3.addMouseListener( this );
+		lbM4.addMouseListener( this );
+		lbM5.addMouseListener( this );
+		lbM6.addMouseListener( this );
+		
+		this.keys = keys;
+	}
+
+	public void mouseClicked( MouseEvent e ) {
+		
+		EMs m = null;
+		
+		if ( e.getSource() == lbM1 ) {
+			m = EMs.MATERIAIS;
+		}
+		else if ( e.getSource() == lbM2 ) {
+			m = EMs.MAQUINA;
+		}
+		else if ( e.getSource() == lbM3 ) {
+			m = EMs.METODO;
+		}
+		else if ( e.getSource() == lbM4 ) {
+			m = EMs.MEIO_AMBIENTE;
+		}
+		else if ( e.getSource() == lbM5 ) {
+			m = EMs.MAO_DE_OBRA;
+		}
+		else if ( e.getSource() == lbM6 ) {
+			m = EMs.MEDIDA;
+		}
+		
+		if ( m != null ) {
+			DLAcaoCorretiva acaoCorretiva = new DLAcaoCorretiva( con, m, keys );
+			acaoCorretiva.setVisible( true );
+		}
+	}
+
+	public void mouseEntered( MouseEvent e ) {
+	}
+
+	public void mouseExited( MouseEvent e ) {
+	}
+
+	public void mousePressed( MouseEvent e ) {
+	}
+
+	public void mouseReleased( MouseEvent e ) {
 	}
 }
