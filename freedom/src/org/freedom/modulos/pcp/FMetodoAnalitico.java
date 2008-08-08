3aa91683ad590600f9ccb84e243b0754ea9fe066
regi_stpinf <regi_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-08-08 18:48:49 +0000

    inclusão de abas.

--- freedom/src/org/freedom/modulos/pcp/FMetodoAnalitico.java
+++ freedom/src/org/freedom/modulos/pcp/FMetodoAnalitico.java
@@ -25,13 +25,23 @@
 
 package org.freedom.modulos.pcp;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.sql.Connection;
+
+import javax.swing.JScrollPane;
+
+import org.freedom.componentes.JPanelPad;
 import org.freedom.componentes.JTextAreaPad;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
-import org.freedom.telas.FDados;
+import org.freedom.componentes.Navegador;
+import org.freedom.componentes.PainelImagem;
+import org.freedom.componentes.Tabela;
+import org.freedom.telas.FTabDados;
 
 
-public class FMetodoAnalitico extends FDados {
+public class FMetodoAnalitico extends FTabDados {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -39,22 +49,129 @@ public class FMetodoAnalitico extends FDados {
 	
 	private JTextFieldPad txtDescMtAnalise = new JTextFieldPad( JTextFieldPad.TP_STRING, 80, 0 );
 	
-	private JTextAreaPad txaObsMtAnalise = new JTextAreaPad();
+	private JTextFieldPad txtTituloMet = new JTextFieldPad( JTextFieldPad.TP_STRING, 100, 0 );
+	
+	private JTextFieldPad txtCodFoto = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
+	private JTextFieldPad txtDescFoto = new JTextFieldPad( JTextFieldPad.TP_STRING, 40, 0 );
+		
+	private JPanelPad pinGeral = new JPanelPad( 330, 200 );
+	
+	private JPanelPad pinMaterial = new JPanelPad( new BorderLayout() );
+	
+	private JPanelPad pinReagentes = new JPanelPad( new BorderLayout() );
+	
+	private JPanelPad pinProced = new JPanelPad( new BorderLayout() );
+	
+	private JPanelPad pinCaract = new JPanelPad( new BorderLayout() );
+	
+	private JPanelPad pinAbas = new JPanelPad( new BorderLayout() );
+	
+	private JPanelPad pinCampos = new JPanelPad( 200, 100 );
+	
+	private JPanelPad pnFoto = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );
+	
+	private JPanelPad pinRodFoto = new JPanelPad( 650, 170 );
+	
+	private JTextAreaPad txaMaterial = new JTextAreaPad();
+
+	private JTextAreaPad txaReagente = new JTextAreaPad();
+	
+	private JTextAreaPad txaProced = new JTextAreaPad();
+	
+	private Navegador navFoto = new Navegador( true );
+	
+	private ListaCampos lcFoto = new ListaCampos( this );
+	
+	private Tabela tabFoto = new Tabela();
+
+	private JScrollPane spnFoto = new JScrollPane( tabFoto );
+	
+	private PainelImagem imFotoProd = new PainelImagem( 65000 );
+	
+	
 	public FMetodoAnalitico(){
 		
 		setTitulo( "Métodos Analíticos" );
-		setAtribos( 50, 50, 380, 220 );
+		setAtribos( 50, 50, 550, 500 );
+		
+		lcFoto.setMaster( lcCampos );
+		lcCampos.adicDetalhe( lcFoto );
+		lcFoto.setTabela( tabFoto );
+		
 		montaTela();
+		
 	}
 	
 	private void montaTela(){
+
+		pnCliente.removeAll();		
+		setPainel( pinCampos );
 		
 		adicCampo( txtCodMtAnalise, 7, 20, 70, 20, "CodMtAnalise", "Cód.Método", ListaCampos.DB_PK, true );
-		adicCampo( txtDescMtAnalise, 80, 20, 260, 20, "DescMtAnalise", "Descrição do método analítico", ListaCampos.DB_SI, true );
-		adicDB( txaObsMtAnalise, 7, 60, 335, 80, "ObsMtAnalise", "Observações", false );
-		
-		setListaCampos( true, "METODOANALISE", "PP" );
+		adicCampo( txtDescMtAnalise, 80, 20, 260, 20, "DescMtAnalise", "Descrição do método analítico", ListaCampos.DB_SI, true );		
+		adicCampo( txtTituloMet, 7, 60, 335, 20, "TituloAnalise", "Titulo", ListaCampos.DB_SI, true );		
 	
+		
+		pinAbas.setPreferredSize( new Dimension( 200, 300 ) );
+		pnCliente.add( pinCampos, BorderLayout.NORTH );
+		
+		/****************
+		 *   Material   *
+		 ****************/		
+
+		adicTab( "Material", pinMaterial );
+		adicDBLiv( txaMaterial, "MatAnalise", "Material", false );
+		pinMaterial.add( txaMaterial );
+		
+		/****************
+		 *   Reagentes  *
+		 ****************/
+		
+	    adicTab( "Reagentes", pinReagentes );
+	    adicDBLiv( txaReagente, "ReagAnalise", "Reagentes", false );
+	    pinReagentes.add( txaReagente );
+		
+		/*******************
+		 *  Procedimentos  *
+		 *******************/
+		
+		adicTab( "Procedimentos", pinProced );
+		adicDBLiv( txaProced, "ProcAnalise", "Procedimento", false );
+		pinProced.add( txaProced );
+				
+		setListaCampos( true, "METODOANALISE", "PP" );
+		
+		/********************
+		 * Caracteristicas  *
+		 ********************/
+		
+		setPainel( pinRodFoto, pnFoto );
+		adicTab( "Cracteristica", pnFoto );
+		setListaCampos( lcFoto );
+		setNavegador( navFoto );
+		pnFoto.add( pinRodFoto, BorderLayout.SOUTH );
+		pnFoto.add( spnFoto, BorderLayout.CENTER );
+		pinRodFoto.adic( navFoto, 0, 140, 270, 25 );
+		
+		adicCampo( txtCodFoto, 7, 20, 70, 20, "CodFotoMTan", "Nº foto", ListaCampos.DB_PK, true );
+		adicCampo( txtDescFoto, 80, 20, 250, 20, "DescFotoMtan", "Descrição da foto", ListaCampos.DB_SI, true );
+		adicDB( imFotoProd, 350, 20, 150, 140, "FotoMTan", "Foto", true );		
+		
+		setListaCampos( true, "FOTOMTAN", "PP" );
+		lcFoto.setQueryInsert( false );
+		lcFoto.setQueryCommit( false );
+		lcFoto.montaTab();
+		
+		tabFoto.setColunaInvisivel( 2 );
+		tabFoto.setTamColuna( 350, 1 );
+		
+		}
+	
+	public void setConexao( Connection con ){
+		
+		super.setConexao( con );
+		lcFoto.setConexao( con );
+		
 	}
 }
