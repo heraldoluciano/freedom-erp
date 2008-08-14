8582b278881f404d59ddb0e1a7c9f8d7656837b1
anderson_stpinf <anderson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-08-14 20:32:43 +0000

    Inclusão de nova biblioteca no classpath (brazilutils)

--- freedom/lib/exec/freedomtmk.sh
+++ freedom/lib/exec/freedomtmk.sh
@@ -1,3 +1,3 @@
 EXEDIR=${0%/*}
 cd $EXEDIR/..
-java -classpath lib/freedomtmk.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/itext.jar:lib/jaybird-full.jar:lib/nachocalendar.jar:lib/commons-logging.jar:lib/jasperreports.jar:lib/liquidlnf.jar:lib/metouia.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/mail.jar:lib/barbecue.jar:lib/ireport.jar:lib/activation.jar -DARQINI=ini/freedom.ini -DARQLOG=log/freedomtmk.log org.freedom.modulos.tmk.FreedomTMK
+java -classpath lib/freedomtmk.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/itext.jar:lib/jaybird-full.jar:lib/nachocalendar.jar:lib/commons-logging.jar:lib/jasperreports.jar:lib/liquidlnf.jar:lib/metouia.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/mail.jar:lib/barbecue.jar:lib/ireport.jar:lib/activation.jar:lib/brazilutils.jar -DARQINI=ini/freedom.ini -DARQLOG=log/freedomtmk.log org.freedom.modulos.tmk.FreedomTMK
