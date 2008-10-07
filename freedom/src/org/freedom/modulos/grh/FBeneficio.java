894b095d1ae02a9f1ec89db040c960c43200eead
felipe_stpinf <felipe_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-10-07 12:16:18 +0000

    Retirados botões de impressão.

--- freedom/src/org/freedom/modulos/grh/FBeneficio.java
+++ freedom/src/org/freedom/modulos/grh/FBeneficio.java
@@ -24,18 +24,10 @@
 
 package org.freedom.modulos.grh;
 
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashMap;
-
-import net.sf.jasperreports.engine.JasperPrintManager;
-
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
-import org.freedom.funcoes.Funcoes;
-import org.freedom.telas.Aplicativo;
 import org.freedom.telas.FDados;
-import org.freedom.telas.FPrinterJob;
 
 public class FBeneficio extends FDados implements ActionListener {
 
@@ -44,65 +36,25 @@ public class FBeneficio extends FDados implements ActionListener {
 	private final JTextFieldPad txtCodBenef = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 5, 0 );
 
 	private final JTextFieldPad txtDescBenef = new JTextFieldPad( JTextFieldPad.TP_STRING, 60, 0 );
-	
+
 	private final JTextFieldPad txtVlrBenef = new JTextFieldPad( JTextFieldPad.TP_NUMERIC, 15, 2 );
-	
 
 	public FBeneficio() {
 
 		super();
 		setTitulo( "Cadastro de Benefícios" );
 		setAtribos( 50, 50, 470, 125 );
-		
+
 		montaTela();
-		
-		btImp.addActionListener( this );
-		btPrevimp.addActionListener( this );
-		
-		setImprimir( true );
 	}
-	
+
 	private void montaTela() {
 
 		adicCampo( txtCodBenef, 7, 20, 70, 20, "CodBenef", "Cód.benef.", ListaCampos.DB_PK, true );
 		adicCampo( txtDescBenef, 80, 20, 260, 20, "DescBenef", "Descrição do benefício", ListaCampos.DB_SI, true );
 		adicCampo( txtVlrBenef, 345, 20, 100, 20, "ValorBenef", "Valor( R$ )", ListaCampos.DB_SI, false );
-		setListaCampos( true, "BENEFICIO", "RH" );		
+		setListaCampos( true, "BENEFICIO", "RH" );
 		lcCampos.setQueryInsert( false );
 	}
 
-	public void actionPerformed( ActionEvent evt ) {
-
-		if ( evt.getSource() == btPrevimp ) {
-			imprimir( true );
-		}
-		else if ( evt.getSource() == btImp ) {
-			imprimir( false );
-		}
-		
-		super.actionPerformed( evt );
-	}
-
-	private void imprimir( boolean bVisualizar ) {
-		
-		FPrinterJob dlGr = null;
-		HashMap<String, Object> hParam = new HashMap<String, Object>();
-
-		hParam.put( "CODEMP", Aplicativo.iCodEmp );
-		hParam.put( "CODFILIAL", ListaCampos.getMasterFilial( "RHBENEFICIO" ) );
-
-		dlGr = new FPrinterJob( "relatorios/grhFuncao.jasper", "Lista de Benefícios", "", this, hParam, con, null, false );
-
-		if ( bVisualizar ) {
-			dlGr.setVisible( true );
-		}
-		else {
-			try {
-				JasperPrintManager.printReport( dlGr.getRelatorio(), true );
-			} catch ( Exception e ) {
-				e.printStackTrace();
-				Funcoes.mensagemErro( this, "Erro na geração do relátorio!" + e.getMessage(), true, con, e );
-			}
-		}
-	}
 }
