f2f1b4bef565f44b4e1be9cdc0f04f01e5126f14
felipe_stpinf <felipe_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-11-11 17:27:55 +0000

    Inserção de combobox para selecionar se fornecedor é CIF ou FOB.

--- trunk/freedom/src/org/freedom/modulos/rep/RPFornecedor.java
+++ trunk/freedom/src/org/freedom/modulos/rep/RPFornecedor.java
@@ -26,8 +26,10 @@
 package org.freedom.modulos.rep;
 
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 import org.freedom.componentes.JCheckBoxPad;
+import org.freedom.componentes.JRadioGroup;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
 import org.freedom.telas.FDados;
@@ -45,10 +47,10 @@ public class RPFornecedor extends FDados implements ActionListener {
 	private final JTextFieldPad txtCnpjFor = new JTextFieldPad( JTextFieldPad.TP_STRING, 14, 0 );
 
 	private final JTextFieldPad txtInscFor = new JTextFieldPad( JTextFieldPad.TP_STRING, 15, 0 );
-	
+
 	private final JTextFieldPad txtEndFor = new JTextFieldPad( JTextFieldPad.TP_STRING, 45, 0 );
 
-	//private final JTextFieldPad txtNumCli = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	// private final JTextFieldPad txtNumCli = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
 	private final JTextFieldPad txtCidFor = new JTextFieldPad( JTextFieldPad.TP_STRING, 30, 0 );
 
@@ -67,47 +69,67 @@ public class RPFornecedor extends FDados implements ActionListener {
 	private final JTextFieldPad txtEmailFor = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
 
 	private final JTextFieldPad txtCodRepFor = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
-	
+
 	private final JCheckBoxPad cbAtivo = new JCheckBoxPad( "     Ativo", "S", "N" );
+
+	private JRadioGroup<String, String> TipoForn;
 
 	public RPFornecedor() {
 
 		super( false );
-		setTitulo( "Cadastro de fornecedores" );		
-		setAtribos( 50, 50, 435, 380 );
-		
+		setTitulo( "Cadastro de fornecedores" );
+		setAtribos( 50, 50, 435, 430 );
+
+		montaRadioGrupos();
 		montaTela();
+
 		setListaCampos( true, "FORNECEDOR", "RP" );
-		
+
 		txtCnpjFor.setMascara( JTextFieldPad.MC_CNPJ );
 		txtCepFor.setMascara( JTextFieldPad.MC_CEP );
 		txtFoneFor.setMascara( JTextFieldPad.MC_FONE );
 		txtFaxFor.setMascara( JTextFieldPad.MC_FONE );
+
 	}
-	
+
+	private void montaRadioGrupos() {
+
+		Vector<String> labs = new Vector<String>();
+		labs.add( "CIF" );
+		labs.add( "FOB" );
+		Vector<String> vals = new Vector<String>();
+		vals.add( "C" );
+		vals.add( "F" );
+		TipoForn = new JRadioGroup<String, String>( 1, 2, labs, vals );
+
+	}
+
 	private void montaTela() {
-		
-		adicCampo( txtCodFor, 7, 30, 100, 20, "CodFor", "Cód.For.", ListaCampos.DB_PK, true );
-		adicCampo( txtRazFor, 110, 30, 200, 20, "RazFor", "Razão social do fornecedor", ListaCampos.DB_SI, true );
+
+		adicDB( TipoForn, 7, 25, 300, 30, "TipoFor", "Tipo", false );
+		adicCampo( txtCodFor, 7, 80, 100, 20, "CodFor", "Cód.For.", ListaCampos.DB_PK, true );
+		adicCampo( txtRazFor, 110, 80, 295, 20, "RazFor", "Razão social do fornecedor", ListaCampos.DB_SI, true );
 		adicDB( cbAtivo, 325, 30, 80, 20, "AtivFor", "", true );
+
+		adicCampo( txtNomeFor, 7, 120, 300, 20, "NomeFor", "Nome do fantasia", ListaCampos.DB_SI, true );
+		adicCampo( txtCodRepFor, 313, 120, 95, 20, "CodRepFor", "Cód.rep.for.", ListaCampos.DB_SI, false );
+
+		adicCampo( txtCnpjFor, 7, 160, 200, 20, "CnpjFor", "CNPJ", ListaCampos.DB_SI, false );
+		adicCampo( txtInscFor, 210, 160, 200, 20, "InscFor", "Inscrição", ListaCampos.DB_SI, false );
+
+		adicCampo( txtEndFor, 7, 200, 403, 20, "EndFor", "Endereço", ListaCampos.DB_SI, false );
+
+		adicCampo( txtCidFor, 7, 240, 132, 20, "CidFor", "Cidade", ListaCampos.DB_SI, false );
+		adicCampo( txtBairFor, 142, 240, 132, 20, "BairFor", "Bairro", ListaCampos.DB_SI, false );
+		adicCampo( txtCepFor, 277, 240, 80, 20, "CepFor", "Cep", ListaCampos.DB_SI, false );
+		adicCampo( txtUFFor, 360, 240, 50, 20, "EstFor", "UF", ListaCampos.DB_SI, false );
+
+		adicCampo( txtDDDFor, 7, 280, 52, 20, "DDDFor", "DDD", ListaCampos.DB_SI, false );
+		adicCampo( txtFoneFor, 62, 280, 172, 20, "FoneFor", "Fone", ListaCampos.DB_SI, false );
+		adicCampo( txtFaxFor, 237, 280, 172, 20, "FaxFor", "Fax", ListaCampos.DB_SI, false );
+
+		adicCampo( txtEmailFor, 7, 320, 403, 20, "EmailFor", "E-mail", ListaCampos.DB_SI, false );
+
 		
-		adicCampo( txtNomeFor, 7, 70, 300, 20, "NomeFor", "Nome do fantazia", ListaCampos.DB_SI, true );
-		adicCampo( txtCodRepFor, 310, 70, 100, 20, "CodRepFor", "Cód.rep.for.", ListaCampos.DB_SI, false );
-		
-		adicCampo( txtCnpjFor, 7, 110, 200, 20, "CnpjFor", "CNPJ", ListaCampos.DB_SI, false );
-		adicCampo( txtInscFor, 210, 110, 200, 20, "InscFor", "Inscrição", ListaCampos.DB_SI, false );
-		
-		adicCampo( txtEndFor, 7, 150, 403, 20, "EndFor", "Endereço", ListaCampos.DB_SI, false );
-		
-		adicCampo( txtCidFor, 7, 190, 132, 20, "CidFor", "Cidade", ListaCampos.DB_SI, false );
-		adicCampo( txtBairFor, 142, 190, 132, 20, "BairFor", "Bairro", ListaCampos.DB_SI, false );		
-		adicCampo( txtCepFor, 277, 190, 80, 20, "CepFor", "Cep", ListaCampos.DB_SI, false );
-		adicCampo( txtUFFor, 360, 190, 50, 20, "EstFor", "UF", ListaCampos.DB_SI, false );
-		
-		adicCampo( txtDDDFor, 7, 230, 52, 20, "DDDFor", "DDD", ListaCampos.DB_SI, false );
-		adicCampo( txtFoneFor, 62, 230, 172, 20, "FoneFor", "Fone", ListaCampos.DB_SI, false );		
-		adicCampo( txtFaxFor, 237, 230, 172, 20, "FaxFor", "Fax", ListaCampos.DB_SI, false );
-		
-		adicCampo( txtEmailFor, 7, 270, 403, 20, "EmailFor", "E-mail", ListaCampos.DB_SI, false );
 	}
 }
