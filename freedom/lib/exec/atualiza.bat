e5dae84e10eb1c374a1aaaf85451fb3aa63ae4eb
robson_stpinf <robson_stpinf@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2008-11-20 16:36:16 +0000

    Inclusão de copia da estrutura de banco de dados para a pasta desenv.

--- freedom/lib/exec/atualiza.bat
+++ freedom/lib/exec/atualiza.bat
@@ -1,2 +1,7 @@
 copy \opt\eclipse\workspace\freedom\lib\*.jar \opt\freedom\lib
 copy \opt\eclipse\workspace\freedom\lib\exec\*.bat \opt\freedom\bin
+mkdir \opt\firebird\dados\desenv
+copy \opt\eclipse\workspace\freedom\dados\estrutura\freedom.zip \opt\firebird\dados\desenv\freedom.zip
+cd \opt\firebird\dados\desenv
+7z e -y freedom.zip
+
