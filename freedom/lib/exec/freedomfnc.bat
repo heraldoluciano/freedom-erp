bfd71e578e02ab284d14d5964369582a776bab18
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-07-20 20:37:42 +0000

    Padronização

--- freedom/lib/exec/freedomfnc.bat
+++
@@ -1,2 +0,0 @@
-cd \opt\freedom
-start javaw -classpath lib\freedomfnc.jar;lib\jcommon.jar;lib\jfreechart.jar;lib\itext.jar;lib\firebirdsql-full.jar;lib\nachocalendar.jar;lib\commons-logging.jar;lib\jasperreports.jar;lib\liquidlnf.jar;lib\metouia.jar -DARQINI=ini\freedom.ini -DARQLOG=log\freedomfnc.log org.freedom.modulos.fnc.FreedomFNC
