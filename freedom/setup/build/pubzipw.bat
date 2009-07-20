81b9507dbc315e99706185d7e758877d2c650e2f
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-07-20 22:54:18 +0000

    Deleção para novo import.

--- freedom/setup/build/pubzipw.bat
+++
@@ -1,5 +0,0 @@
-@echo "Publica‡Æo do disco de instala‡Æo Windows"
-rem "call geradisco.bat"
-rem "copy ..\disco\win\disco.zip .\freedom_1_1_3_0.zip"
-ftp -s:ftpzip.ftp -A upload.sourceforge.net
-rem "del .\freedom_1_1_3_0.zip /Q"
