d2514baf6f633af5a69ed8a15af7957091eddb93
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-07-20 21:17:42 +0000

    Importação inicial Google Code.

--- trunk/freedom/setup/build/geradisco.bat
+++ trunk/freedom/setup/build/geradisco.bat
@@ -7,24 +7,27 @@ mkdir ..\disco\opt\firebird\dados
 mkdir ..\disco\opt\firebird\UDF
 mkdir ..\disco\opt\freedom
 mkdir ..\disco\opt\freedom\lib
-mkdir ..\disco\opt\freedom\icones
+mkdir ..\disco\opt\freedom\ico
 mkdir ..\disco\opt\freedom\log
 mkdir ..\disco\opt\freedom\bin
 mkdir ..\disco\opt\freedom\ini
 del ..\disco\opt\freedom\*.* /Q
 del ..\disco\opt\freedom\lib\*.* /Q
-del ..\disco\opt\freedom\icones\*.* /Q
+del ..\disco\opt\freedom\ico\*.* /Q
 del ..\disco\opt\freedom\log\*.* /Q
 del ..\disco\opt\freedom\bin\*.* /Q
 del ..\disco\opt\freedom\ini\*.* /Q
 del ..\disco\opt\firebird\dados\*.* /Q
 del ..\disco\disco.zip /Q
-7z x -o..\disco\opt\firebird\dados -tzip ..\..\dados\estrutura\freedom.zip
+\opt\7-Zip\7z x -o..\disco\opt\firebird\dados -tzip ..\..\dados\estrutura\freedom.zip
+\opt\firebird\bin\gbak -C -R -P 4096 ..\disco\opt\firebird\dados\freedom.fbk localhost:/opt/firebird/dados/freedom.fdb -user sysdba -pass masterkey
+copy \opt\firebird\dados\freedom.fdb ..\disco\opt\firebird\dados
 copy ..\..\src\org\freedom\udf\udfinstall.bat ..\disco\opt\firebird\UDF
 copy ..\..\src\org\freedom\udf\stp_udf_win.sql ..\disco\opt\firebird\UDF
 copy ..\..\src\org\freedom\udf\stp_udf.dll ..\disco\opt\firebird\UDF
-copy ..\..\src\org\freedom\images\*.ico ..\disco\opt\freedom\icones
+copy ..\..\icones\novos\ico\*.ico ..\disco\opt\freedom\ico
 copy ..\..\lib\*.jar ..\disco\opt\freedom\lib
 copy ..\..\lib\exec\*.bat ..\disco\opt\freedom\bin
 copy ..\..\freedomwin.ini ..\disco\opt\freedom\ini\freedom.ini
-7z a -tzip ..\disco\win\disco ..\disco\opt\
+\opt\7-Zip\7z a -tzip ..\disco\win\disco ..\disco\opt\
+
--- trunk/freedom/setup/build/geradisco.sh
+++ trunk/freedom/setup/build/geradisco.sh
@@ -1,4 +1,4 @@
-@echo "Criando disco de instalacao"
+echo "Criando disco de instalacao"
 mkdir ../disco
 mkdir ../disco/lin
 mkdir ../disco/opt
@@ -7,13 +7,13 @@ mkdir ../disco/opt/firebird/dados
 mkdir ../disco/opt/firebird/UDF
 mkdir ../disco/opt/freedom
 mkdir ../disco/opt/freedom/lib
-mkdir ../disco/opt/freedom/icones
+mkdir ../disco/opt/freedom/ico
 mkdir ../disco/opt/freedom/log
 mkdir ../disco/opt/freedom/bin
 mkdir ../disco/opt/freedom/ini
 rm ../disco/opt/freedom/* 
 rm ../disco/opt/freedom/lib/* 
-rm ../disco/opt/freedom/icones/* 
+rm ../disco/opt/freedom/ico/* 
 rm ../disco/opt/freedom/log/*
 rm ../disco/opt/freedom/bin/*
 rm ../disco/opt/freedom/ini/*
@@ -23,8 +23,10 @@ unzip -d ../disco/opt/firebird/dados/ ../../dados/estrutura/freedom.zip
 cp ../../src/org/freedom/udf/udfinstall.sh ../disco/opt/firebird/UDF/
 cp ../../src/org/freedom/udf/stp_udf_lin.sql ../disco/opt/firebird/UDF/
 cp ../../src/org/freedom/udf/stp_udf.so ../disco/opt/firebird/UDF/
-cp ../../src/org/freedom/images/*.ico ../disco/opt/freedom/icones/
+cp ../../icones/novos/ico/*.ico ../disco/opt/freedom/ico/
 cp ../../lib/*.jar ../disco/opt/freedom/lib/
 cp ../../lib/exec/*.sh ../disco/opt/freedom/bin/
 cp ../../freedomlin.ini ../disco/opt/freedom/ini/freedom.ini
-zip ../disco/lin/disco ../disco/opt/*
+cd ../disco
+rm lin/disco.zip
+zip -r lin/disco ./opt/*
