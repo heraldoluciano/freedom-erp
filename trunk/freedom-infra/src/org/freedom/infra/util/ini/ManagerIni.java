e6de4de7339fa104528c5f3dd9960afed4ecbe9f
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-07-20 20:55:15 +0000

    Novos

--- trunk/freedom-infra/src/org/freedom/infra/util/ini/ManagerIni.java
+++ trunk/freedom-infra/src/org/freedom/infra/util/ini/ManagerIni.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 /**
  * Projeto: <a href="http://sourceforge.net/projects/freedom-erp/">Freedom-infra</a> <br>
  * Este programa é licenciado de acordo com a LPG-PC <br>
- * (Licença Pública Geral para Programas de Computador) versão 2.1.0 ou qualquer versão posterior. <br>
+ * modifica-lo dentro dos termos da Licença Pública Geral GNU como publicada pela Fundação do Software Livre (FSF); <BR>
  * <br>
  *
  * Gerenciador para fluxo em arquivo de parametros.<br>
