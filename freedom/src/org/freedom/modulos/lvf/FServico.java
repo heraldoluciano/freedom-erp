3cc7af7d7c4050de04cf94c9854acedd906fc794
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-07-22 18:53:22 +0000

    Exclusão de import desnecessário.

--- freedom/src/org/freedom/modulos/lvf/FServico.java
+++ freedom/src/org/freedom/modulos/lvf/FServico.java
@@ -27,12 +27,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.freedom.componentes.JTextAreaPad;
-import org.freedom.componentes.ListaCampos;
 import org.freedom.componentes.ImprimeOS;
+import org.freedom.componentes.JTextAreaPad;
 import org.freedom.componentes.JTextFieldPad;
+import org.freedom.componentes.ListaCampos;
 import org.freedom.funcoes.Funcoes;
-import org.freedom.telas.Aplicativo;
 import org.freedom.telas.FDados;
 
 public class FServico extends FDados implements ActionListener{
