e744d8e64332e152dba5991e26938e0d26c05461
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-08-17 20:25:57 +0000

    Implementações referentes à devolução de compra.

--- freedom-fw1/src/org/freedom/modulos/std/DLBuscaCompra.java
+++ freedom-fw1/src/org/freedom/modulos/std/DLBuscaCompra.java
@@ -22,18 +22,25 @@
 package org.freedom.modulos.std;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.HashMap;
 
-import javax.swing.ImageIcon;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
 
 import org.freedom.acao.TabelaSelEvent;
 import org.freedom.acao.TabelaSelListener;
+import org.freedom.bmps.Icone;
+import org.freedom.componentes.JLabelPad;
+import org.freedom.componentes.JPanelPad;
+import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
 import org.freedom.componentes.Tabela;
 import org.freedom.funcoes.Funcoes;
@@ -41,23 +48,43 @@ import org.freedom.infra.model.jdbc.DbConnection;
 import org.freedom.telas.FFDialogo;
 
 public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
-    public Tabela tab = new Tabela();
-	private JScrollPane spnCentro = new JScrollPane(tab); 
+    
 	private static final long serialVersionUID = 1L;   
+	
+	public Tabela tab = new Tabela();
+    
+	private JScrollPane spnCentro = new JScrollPane(tab); 
+	
+	private JPanelPad pnCab = new JPanelPad(new Dimension( 500, 65 ));		
+	
 	private Integer coditcompra = new Integer(0);
+	
 	private Integer codcompra = new Integer(0);
+	
 	private Integer codvenda = new Integer(0);	
+	
 	private Integer codfor = null;
+	
 	private Integer codempfr = null;
+	
 	private Integer codfilialfr = null;
-	boolean bRet = false;
-	public int iPadrao = 0;
+	
+	private boolean bRet = false;
+	
 	private ListaCampos lcItens = null; 
+	
 	private ListaCampos lcProd = null;
 	
+	private JTextFieldPad txtDataIni = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
+	
+	private JTextFieldPad txtDataFim = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );	
+	
+	private JButton btExec = new JButton( Icone.novo( "btExecuta.gif" ) );
+	
 	public DLBuscaCompra(ListaCampos lcItens, ListaCampos lcProd, Integer codvenda, DbConnection con ) {
-		setTitulo("Pesquisa auxiliar");
-		setAtribos( 550, 260);
+		
+		setTitulo("Selecione um íten de compra para devolução");
+		setAtribos( 550, 360);
 		setResizable(true);
 		    
 		setConexao( con );
@@ -67,37 +94,52 @@ public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
 		this.codvenda = codvenda;
 
 		setAtribos( 575, 260);
-			
+
+		c.add( pnCab, BorderLayout.NORTH);
 		c.add( spnCentro, BorderLayout.CENTER);
 		
-		tab.adicColuna("Doc");
-		tab.adicColuna("Data");    
+		tab.adicColuna("Data");  
+		tab.adicColuna("Doc");		  
 		tab.adicColuna("Cód.Cp.");
 		tab.adicColuna("Cód.It.Cp.");
+		tab.adicColuna("Lote");
 		tab.adicColuna("Qtd.");   	  
 		tab.adicColuna("Preço");   	
 		tab.adicColuna("Qtd.Dev.");
-		tab.setTamColuna(60,0);
-		tab.setTamColuna(75,1);
-		tab.setTamColuna(75,1);
-		tab.setTamColuna(50,3);
-		tab.setTamColuna(70,4);
-		tab.setTamColuna(70,5);   	 	 
-		tab.setTamColuna(70,6);
-//		tab.addTabelaSelListener(this); 
 		
-		setTitulo("Itens de compra");
-//		tab.addKeyListener(this);
+		tab.setTamColuna(75,0);
+		tab.setTamColuna(60,1);		
+		tab.setTamColuna(75,2);
+		tab.setTamColuna(50,3);
+		tab.setTamColuna(50,4);
+		tab.setTamColuna(90,5);		
+		tab.setTamColuna(70,6);   	 	 
+		tab.setTamColuna(70,7);
+		
+		JPanelPad pnPeriodo = new JPanelPad();
+		pnPeriodo.setBorder(BorderFactory.createTitledBorder("Período"));
+		pnPeriodo.adic(new JLabelPad("De:"), 4,0,20,20);
+		pnPeriodo.adic(txtDataIni, 25, 0, 75, 20);
+		pnPeriodo.adic(new JLabelPad("Até:"), 105,0,25,20);
+		pnPeriodo.adic(txtDataFim, 130, 0, 75, 20);
+		
+		pnCab.adic(pnPeriodo, 4, 4, 220, 50);
+		pnCab.adic(btExec, 224, 12, 30, 40 );
+		
+		Date dtRef = new Date();
+		
+		txtDataIni.setVlrDate(Funcoes.subtraiMes(dtRef, 1));
+		txtDataFim.setVlrDate(dtRef);
+		
+				
+		tab.addTabelaSelListener(this); 		
+		tab.addKeyListener(this);
 		
 		buscaForneced();
 		buscaCompras();
 		
 	}
-	
-	public int getLinhaPadrao(){
-		return iPadrao;
-	}
-	
+		
 	public HashMap<String,Integer> getValor() {
 		HashMap<String, Integer> ret = new HashMap<String, Integer>();
 		try {
@@ -123,7 +165,7 @@ public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
 		bRet = false;
 		StringBuilder sql = new StringBuilder();
 		     
-		sql.append( "select cp.doccompra, cp.dtemitcompra, ic.codcompra, ic.coditcompra, ic.qtditcompra, ic.precoitcompra, ");
+		sql.append( "select cp.doccompra, cp.dtemitcompra, ic.codcompra, ic.coditcompra, ic.codlote , ic.qtditcompra, ic.precoitcompra, ");
 		sql.append( "coalesce((select sum(dv.qtddev) from cpdevolucao dv where dv.codemp=ic.codemp "); 
 		sql.append( "and dv.codfilial=ic.codfilial and dv.codcompra=ic.codcompra and dv.coditcompra=ic.coditcompra),0) qtddev ");
 		sql.append( "from ");
@@ -133,6 +175,7 @@ public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
 		sql.append( "where ");
 		sql.append( "cp.codempfr=? and cp.codfilialfr=? and cp.codfor=? and ");
 		sql.append( "ic.codemppd=? and ic.codfilialpd=? and ic.codprod=? ");
+		sql.append( "order by cp.dtemitcompra desc");
  
 		try {
 			ps = con.prepareStatement(sql.toString());
@@ -154,10 +197,11 @@ public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
 			while (rs.next()) {
 			
 				tab.adicLinha();
-				tab.setValor(rs.getString("doccompra") != null ? rs.getString("doccompra") : "", irow , icol++);
-				tab.setValor(rs.getString("dtemitcompra") != null ? rs.getString("dtemitcompra").trim() : "", irow , icol++);
+				tab.setValor(rs.getString("dtemitcompra") != null ? Funcoes.dateToStrDate( rs.getDate("dtemitcompra")) : "", irow , icol++);
+				tab.setValor(rs.getString("doccompra") != null ? rs.getString("doccompra") : "", irow , icol++);				
 				tab.setValor(rs.getString("codcompra") != null ? rs.getString("codcompra") : "", irow , icol++);
 				tab.setValor(rs.getString("coditcompra") != null ? rs.getString("coditcompra") : "", irow , icol++);
+				tab.setValor(rs.getString("codlote") != null ? rs.getString("codlote") : "", irow , icol++);
 				tab.setValor(rs.getString("qtditcompra") != null ? Funcoes.strDecimalToStrCurrency(13,2,rs.getString("qtditcompra")) : "", irow , icol++);
 				tab.setValor(rs.getString("precoitcompra") != null ? Funcoes.strDecimalToStrCurrency(13,2,rs.getString("precoitcompra")) : "", irow , icol++);
 				tab.setValor(rs.getString("qtddev") != null ? Funcoes.strDecimalToStrCurrency(13,2,rs.getString("qtddev")) : "", irow , icol++);
@@ -166,16 +210,15 @@ public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
 				icol = 0; 
 			}
 			 	      	
+			if(irow>0) {
+				tab.requestFocus();
+				tab.setLinhaSel(0); 
+			}
+			
 			rs.close();
 			ps.close();
 			con.commit();
-			/*
-			if((bRet) && (lcItens.getStatus() == 2)) {
-				tab.requestFocus();
-				tab.setLinhaSel(iPadrao); 
-				setVisible(true);      		
-			}			 
-			*/
+			
 		} 
 		catch (SQLException err) {
 			Funcoes.mensagemErro(this,"Erro ao buscar filiais almoxarifados e saldos!\n"+err.getMessage(),true,con,err);
@@ -190,31 +233,29 @@ public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
 	}
 	   
 	public void actionPerformed(ActionEvent evt) {
-		getValores();
+//		getValores();
 		super.actionPerformed(evt);
 	}
 	
-	public synchronized void valorAlterado(TabelaSelEvent tsevt) {       
-		try {   	
-			if (tsevt.getTabela() == tab)
-				getValores();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}	
-	}
+	public synchronized void valorAlterado(TabelaSelEvent tsevt) { } 
 	
 	public void keyPressed(KeyEvent kevt) {
 		if ( kevt.getSource() == tab && kevt.getKeyCode() == KeyEvent.VK_ENTER) {        
-			if (tab.getNumLinhas() > 0 && btOK.isEnabled()) 
+			if (tab.getNumLinhas() > 0 && btOK.isEnabled()) { 
 				btOK.doClick();
-			else if (!btOK.isEnabled()) {
-				if (tab.getLinhaSel()==tab.getNumLinhas()-1)
-					tab.setLinhaSel(tab.getNumLinhas()-2);
-				else
-					tab.setLinhaSel(tab.getLinhaSel()-1);
 			}
-		} else if (kevt.getKeyCode() == KeyEvent.VK_ESCAPE)
+			else if (!btOK.isEnabled()) {
+				if (tab.getLinhaSel()==tab.getNumLinhas()-1) {
+					tab.setLinhaSel(tab.getNumLinhas()-2);
+				}
+				else {
+					tab.setLinhaSel(tab.getLinhaSel()-1);
+				}
+			}
+		} 
+		else if (kevt.getKeyCode() == KeyEvent.VK_ESCAPE) {
 			btCancel.doClick();
+		}
 	}    
 	
 	public void ok(){		
@@ -222,20 +263,7 @@ public class DLBuscaCompra extends FFDialogo implements TabelaSelListener {
 		coditcompra = new Integer(Integer.parseInt(tab.getValueAt(tab.getLinhaSel(),3).toString()));
 		super.ok();
 	}
-	
-	public void getValores(){
-		if (tab.getNumLinhas() > 0) {
-			if (bRet) {			
-				if(this.isVisible()){
-					if (((ImageIcon)tab.getValueAt(tab.getLinhaSel(),5)).getDescription().equals("N")) 
-						btOK.setEnabled(false);
-					else	
-						btOK.setEnabled(true);
-				}
-			}
-		}    
-	}
-	
+		
 	private void buscaForneced() {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
