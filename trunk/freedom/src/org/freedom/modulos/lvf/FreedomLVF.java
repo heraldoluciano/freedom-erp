241c5ce696599382bee6f0e264637699d25d4f66
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-09-18 17:50:10 +0000

    Mudança de package da classe FTabICMS.

--- trunk/freedom/src/org/freedom/modulos/lvf/FreedomLVF.java
+++ trunk/freedom/src/org/freedom/modulos/lvf/FreedomLVF.java
@@ -42,7 +42,6 @@ import org.freedom.modulos.std.FRVendasIcms;
 import org.freedom.modulos.std.FRegraFiscal;
 import org.freedom.modulos.std.FSetor;
 import org.freedom.modulos.std.FSintegra;
-import org.freedom.modulos.std.FTabICMS;
 import org.freedom.modulos.std.FTipoCli;
 import org.freedom.modulos.std.FTipoFor;
 import org.freedom.modulos.std.FVendedor;
