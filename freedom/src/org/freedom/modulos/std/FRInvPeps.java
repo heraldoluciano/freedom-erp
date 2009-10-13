af3d133e60369d33f69a6db95e31304661b3f5bc
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2009-10-13 12:53:51 +0000

    Implementação de coalesce na query, a fim de garantir a execução do relatório, prevenindo null pointer exception...

--- freedom/src/org/freedom/modulos/std/FRInvPeps.java
+++ freedom/src/org/freedom/modulos/std/FRInvPeps.java
@@ -205,7 +205,7 @@ public class FRInvPeps extends FRelatorio {
         
   		//iCodAlmox = txt
   		sSql = "SELECT "+sCpCodigo+",DESCPROD,SLDPROD,CUSTOUNIT,CUSTOTOT "
-  		 			+ ",CODFABPROD,CODBARPROD,ATIVOPROD " 
+  		 			+ ",COALESCE(CODFABPROD,0),COALESCE(CODBARPROD,0),ATIVOPROD " 
   		 			+ "FROM EQRELPEPSSP(?,?,?,?,?,?,?,?,?,?,?,?,?) " 
   		 			+ swhere2
   		 			+ swhere3 + swhere + " ORDER BY "+(rgOrdem.getVlrString().equals("D")?"DESCPROD":sCpCodigo);
