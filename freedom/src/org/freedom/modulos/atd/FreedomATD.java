22a53be5795419b7d5e1dbc330149cdec5e03c50
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-01-11 10:55:35 +0000

    Correção em ícone do orçamento.

--- freedom/src/org/freedom/modulos/atd/FreedomATD.java
+++ freedom/src/org/freedom/modulos/atd/FreedomATD.java
@@ -88,7 +88,7 @@ public class FreedomATD extends AplicativoPD implements ActionListener {
 		addOpcao(1200000000, TP_OPCAO_ITEM, "Termo de Recebimento", "Termo de Recebimento", 'e', 120500000, 1, true, FRTermReceb.class);
 
 		addBotao("btAtendimento.gif", "Atendimento", "Atendimento", 110100000, FAtendimento.class);
-		addBotao("btOrcamento.gif", "Orçamento", "Orcamento", 120100000, FOrcamento.class);			
+		addBotao("btOrcamento.png", "Orçamento", "Orcamento", 120100000, FOrcamento.class);			
 		addBotao("btConsOrcamento.gif", "Pesquisa Orçamento", "Pesquisa Orcamentos", 120200000, FConsOrc.class);
 		addBotao("barraConveniados.gif", "Conveniados", "Conveniados", 100101040, FConveniado.class);
 		addBotao("btAprovaOrc.gif", "Aprovações de Orçamantos", "Aprova Orcamento", 120300000, FAprovaOrc.class);
