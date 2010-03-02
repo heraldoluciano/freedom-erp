20cea87865899ff523dc636e249d0c0e72e7d6a7
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-03-02 12:37:55 +0000

    Implementações relativas ao cadastro de balanças/estações de trabalho.

--- trunk/freedom/src/org/freedom/modulos/std/FEstacao.java
+++ trunk/freedom/src/org/freedom/modulos/std/FEstacao.java
@@ -23,26 +23,37 @@
 
 package org.freedom.modulos.std;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
-import org.freedom.infra.model.jdbc.DbConnection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Vector;
 
+import javax.comm.SerialPort;
+import javax.swing.JScrollPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import org.freedom.acao.PostEvent;
 import org.freedom.acao.PostListener;
 import org.freedom.componentes.GuardaCampo;
 import org.freedom.componentes.JCheckBoxPad;
+import org.freedom.componentes.JComboBoxPad;
+import org.freedom.componentes.JPanelPad;
 import org.freedom.componentes.JRadioGroup;
+import org.freedom.componentes.JTabbedPanePad;
 import org.freedom.componentes.JTextFieldFK;
 import org.freedom.componentes.JTextFieldPad;
 import org.freedom.componentes.ListaCampos;
-import org.freedom.componentes.JPanelPad;
+import org.freedom.componentes.Navegador;
+import org.freedom.componentes.Tabela;
 import org.freedom.funcoes.Funcoes;
+import org.freedom.infra.functions.SystemFunctions;
+import org.freedom.infra.model.jdbc.DbConnection;
 import org.freedom.telas.Aplicativo;
 import org.freedom.telas.FDetalhe;
 
-public class FEstacao extends FDetalhe implements PostListener, ActionListener {
+public class FEstacao extends FDetalhe implements PostListener, ActionListener, ChangeListener  {
 
 	private static final long serialVersionUID = 1L;
 
@@ -67,7 +78,7 @@ public class FEstacao extends FDetalhe implements PostListener, ActionListener {
 	private JCheckBoxPad cbNfeEst = new JCheckBoxPad( "Emite NFE?", "S", "N" );
 	
 	private JTextFieldPad txtNroImp = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 5, 0 );
-
+	
 	private JTextFieldPad txtCodImp = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
 	private JTextFieldPad txtCodPapel = new JTextFieldPad( JTextFieldPad.TP_STRING, 10, 0 );
@@ -88,56 +99,95 @@ public class FEstacao extends FDetalhe implements PostListener, ActionListener {
 
 	private Vector<String> vLabTipoUsoImp = new Vector<String>();
 
-	private JRadioGroup<?, ?> rgTipoUsoImp = null;
+	private JRadioGroup<?,?> rgTipoUsoImp = null;
+	
+	private JTabbedPanePad tpnGeral = new JTabbedPanePad();
+	
+	private JPanelPad panelImpressorasCampos = new JPanelPad( 500, 80 );
+
+	private JPanelPad panelImpressoras = new JPanelPad( new GridLayout( 1, 1 ) );
+
+	private JPanelPad panelBalancasCampos = new JPanelPad( 500, 80 );
+
+	private JPanelPad panelBalancas = new JPanelPad( new GridLayout( 1, 1 ) );
+	
+	public Tabela tabDetBal = new Tabela();
+	
+	public JScrollPane spTabDetBal = new JScrollPane( tabDetBal );
+	
+	public Navegador navDetBal = new Navegador( true );
+	
+	private ListaCampos lcDetBal = new ListaCampos( this );
+	
+	private JTextFieldPad txtNroBal = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 5, 0 );
+	
+	private JTextFieldPad txtParityBal = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 1, 0 );
+	
+	private Vector<String> vValModelosBal = new Vector<String>();
+	
+	private Vector<String> vLabModelosBal = new Vector<String>();
+	
+	private Vector<Integer> vValPortasBal = new Vector<Integer>();
+	
+	private Vector<String> vLabPortasBal = new Vector<String>();
+	
+	private Vector<Integer> vValSpeedBal = new Vector<Integer>();
+	
+	private Vector<String> vLabSpeedBal = new Vector<String>();
+	
+	private Vector<Integer> vValBitsBal = new Vector<Integer>();
+	
+	private Vector<String> vLabBitsBal = new Vector<String>();
+
+	private Vector<Integer> vValStopBitsBal = new Vector<Integer>();
+	
+	private Vector<String> vLabStopBitsBal = new Vector<String>();
+	
+	private Vector<Integer> vValParidadeBal = new Vector<Integer>();
+	
+	private Vector<String> vLabParidadeBal = new Vector<String>();
+	
+	private JComboBoxPad cbModeloBal = new JComboBoxPad( vLabModelosBal, vValModelosBal, JComboBoxPad.TP_STRING, 50, 0 );
+	
+	private JComboBoxPad cbPortaBal = new JComboBoxPad( vLabPortasBal, vValPortasBal, JComboBoxPad.TP_INTEGER, 1, 0 );
+	
+	private JComboBoxPad cbSpeedBal = new JComboBoxPad( vLabSpeedBal, vValSpeedBal, JComboBoxPad.TP_INTEGER, 5, 0 );
+	
+	private JComboBoxPad cbParidadeBal = new JComboBoxPad( vLabParidadeBal, vValParidadeBal, JComboBoxPad.TP_INTEGER, 1, 0 );
+	
+	private JComboBoxPad cbBitsBal = new JComboBoxPad( vLabBitsBal, vValBitsBal, JComboBoxPad.TP_INTEGER, 1, 0 );
+	
+	private JComboBoxPad cbStopBitsBal = new JComboBoxPad( vLabStopBitsBal, vValStopBitsBal, JComboBoxPad.TP_INTEGER, 1, 0 );
+	
+
+
 
 	public FEstacao() {
 
 		setTitulo( "Cadastro de estações de trabalho" );
 		setAtribos( 50, 10, 550, 520 );
+		
 		pinCab = new JPanelPad( 530, 50 );
+		
+		montaValores();
+		
+		montaListacampos();
+		
+		lcDetBal.setMaster( lcCampos );
+		lcCampos.adicDetalhe( lcDetBal );
+		lcDetBal.setTabela( tabDetBal );
+		navDetBal.setName( "DetalheBalanca" );
+		lcDetBal.setNavegador( navDetBal );
+		
+		montaTela();
+		
+		adicListeners();
+		
+		montaTabelas();
+		
 	}
-
-	private void montaTela() {
-
-		// adicTab("Estação", pinEst);
-
-		lcImp.add( new GuardaCampo( txtCodImp, "CodImp", "Cód.imp.", ListaCampos.DB_PK, false ) );
-		lcImp.add( new GuardaCampo( txtDescImp, "DescImp", "Descrição da impressora", ListaCampos.DB_SI, false ) );
-		lcImp.add( new GuardaCampo( txtCodPapel, "CodPapel", "Cód.papel", ListaCampos.DB_SI, false ) );
-		lcImp.montaSql( false, "IMPRESSORA", "SG" );
-		lcImp.setQueryCommit( false );
-		lcImp.setReadOnly( true );
-		txtCodImp.setTabelaExterna( lcImp );
-
-		lcPapel.add( new GuardaCampo( txtCodPapel, "CodPapel", "Cód.papel.", ListaCampos.DB_PK, false ) );
-		lcPapel.add( new GuardaCampo( txtDescPapel, "DescPapel", "Descrição do papel", ListaCampos.DB_SI, false ) );
-		lcPapel.montaSql( false, "PAPEL", "SG" );
-		lcPapel.setQueryCommit( false );
-		lcPapel.setReadOnly( true );
-		txtCodPapel.setTabelaExterna( lcPapel );
-
-		lcCampos.addPostListener( this );
-		pinCab = new JPanelPad( 740, 130 );
-		setListaCampos( lcCampos );
-		setAltCab( 130 );
-		setPainel( pinCab, pnCliCab );
-		cbModoDemoEst.setVlrString( "N" );
-		cbNfeEst.setVlrString( "N" );
-
-		adicCampo( txtCodEst, 7, 20, 80, 20, "Codest", "Nº estação", ListaCampos.DB_PK, true );
-		adicCampo( txtDescEst, 90, 20, 257, 20, "Descest", "Descrição da estação de trabalho", ListaCampos.DB_SI, true );
-		adicDB( cbModoDemoEst, 350, 20, 170, 20, "ModoDemoEst", "Modo", true );
-		adicCampo( txtFonteTxt , 7, 60, 270, 20, "FonteTxt", "Fonte para visualização de relatórios texto", ListaCampos.DB_SI, false );
-		adicCampo( txtTamFonteTxt , 280, 60, 65, 20, "TamFonteTxt", "Tamanho", ListaCampos.DB_SI, false );
-		adicDB( cbNfeEst, 350, 60, 150, 20, "NfeEst", "NFE", true );
-		setListaCampos( true, "ESTACAO", "SG" ); 
-		lcCampos.setQueryInsert( false );
-
-		setAltDet( 230 );
-		pinDet = new JPanelPad( 740, 230 );
-		setPainel( pinDet, pnDet );
-		setListaCampos( lcDet );
-		setNavegador( navRod );
+	
+	private void montaValores() {
 		
 		vValTipoUsoImp.addElement( "NF" );
 		vValTipoUsoImp.addElement( "NS" );
@@ -146,6 +196,7 @@ public class FEstacao extends FDetalhe implements PostListener, ActionListener {
 		vValTipoUsoImp.addElement( "RG" );
 		vValTipoUsoImp.addElement( "ET" );
 		vValTipoUsoImp.addElement( "TO" );
+		
 		vLabTipoUsoImp.addElement( "Nota fiscal" );
 		vLabTipoUsoImp.addElement( "Nota fiscal - serviço" );
 		vLabTipoUsoImp.addElement( "Pedido" );
@@ -153,8 +204,180 @@ public class FEstacao extends FDetalhe implements PostListener, ActionListener {
 		vLabTipoUsoImp.addElement( "Relatório gráfico" );
 		vLabTipoUsoImp.addElement( "Etiquetas" );
 		vLabTipoUsoImp.addElement( "Todos" );
+
+		vLabModelosBal.addElement( "<Selecione um modelo>" );
+		vLabModelosBal.addElement( "Filizola BP" );
+		vLabModelosBal.addElement( "Rodoviária EPM SP-2400" );
+		
+		vValModelosBal.addElement( "" );
+		vValModelosBal.addElement( "Filizola BP" );
+		vValModelosBal.addElement( "Rodoviária EPM SP-2400" );
+		
+		vLabPortasBal.addElement( "<Selecione uma porta>" );
+		
+		if(SystemFunctions.OS_LINUX == SystemFunctions.getOS()) {						
+			vLabPortasBal.addElement( "/dev/ttyS0" );
+			vLabPortasBal.addElement( "/dev/ttyS1" );
+			vLabPortasBal.addElement( "/dev/ttyS2" );
+			vLabPortasBal.addElement( "/dev/ttyS3" );			
+		}
+		else {		
+			vLabPortasBal.addElement( "COM1" );
+			vLabPortasBal.addElement( "COM2" );
+			vLabPortasBal.addElement( "COM3" );
+			vLabPortasBal.addElement( "COM4" );			
+		}
+		
+		vValPortasBal.addElement( -1 );
+		vValPortasBal.addElement( 0 );
+		vValPortasBal.addElement( 1 );
+		vValPortasBal.addElement( 2 );
+		vValPortasBal.addElement( 3 );
+
+		vLabSpeedBal.addElement( "2400" );
+		vLabSpeedBal.addElement( "4800" );
+		vLabSpeedBal.addElement( "7200" );		
+		vLabSpeedBal.addElement( "9600" );
+		vLabSpeedBal.addElement( "12000" );		
+		
+		vValSpeedBal.addElement( 2400 );
+		vValSpeedBal.addElement( 4800 );
+		vValSpeedBal.addElement( 7200 );		
+		vValSpeedBal.addElement( 9600 );
+		vValSpeedBal.addElement( 12000 );		
+
+		vLabParidadeBal.addElement( "Sem paridade" );
+		vLabParidadeBal.addElement( "Par" );		
+		vLabParidadeBal.addElement( "Impar" );
+		
+		vValParidadeBal.addElement( SerialPort.PARITY_NONE );
+		vValParidadeBal.addElement( SerialPort.PARITY_EVEN );
+		vValParidadeBal.addElement( SerialPort.PARITY_ODD );
+
+		vLabBitsBal.addElement( "5 bits" );
+		vLabBitsBal.addElement( "6 bits" );
+		vLabBitsBal.addElement( "7 bits" );
+		vLabBitsBal.addElement( "8 bits" );
+		
+		vValBitsBal.addElement( SerialPort.DATABITS_5 );
+		vValBitsBal.addElement( SerialPort.DATABITS_6 );
+		vValBitsBal.addElement( SerialPort.DATABITS_7 );
+		vValBitsBal.addElement( SerialPort.DATABITS_8 );
+
+		vLabStopBitsBal.addElement( "1 bit" );
+		vLabStopBitsBal.addElement( "1,5 bit" );
+		vLabStopBitsBal.addElement( "2 bits" );
+		
+		vValStopBitsBal.addElement( SerialPort.STOPBITS_1 );		
+		vValStopBitsBal.addElement( SerialPort.STOPBITS_1_5 );		
+		vValStopBitsBal.addElement( SerialPort.STOPBITS_2 );
+		
+//		porta.setSerialPortParams( 9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_2, SerialPort.PARITY_EVEN );
 		
 		rgTipoUsoImp = new JRadioGroup<String, String>( 3, 3, vLabTipoUsoImp, vValTipoUsoImp );
+		
+		cbModeloBal = new JComboBoxPad( vLabModelosBal, vValModelosBal, JComboBoxPad.TP_STRING, 50, 0 );
+			
+		cbPortaBal = new JComboBoxPad( vLabPortasBal, vValPortasBal, JComboBoxPad.TP_INTEGER, 1, 0 );
+		
+		cbSpeedBal = new JComboBoxPad( vLabSpeedBal, vValSpeedBal, JComboBoxPad.TP_INTEGER, 5, 0 );
+		
+		cbParidadeBal = new JComboBoxPad( vLabParidadeBal, vValParidadeBal, JComboBoxPad.TP_INTEGER, 1, 0 );
+		
+		cbBitsBal = new JComboBoxPad( vLabBitsBal, vValBitsBal, JComboBoxPad.TP_INTEGER, 1, 0 ); 
+		
+		cbStopBitsBal = new JComboBoxPad( vLabStopBitsBal, vValStopBitsBal, JComboBoxPad.TP_INTEGER, 1, 0 );
+		
+	}
+
+	
+	private void montaListacampos() {
+		
+		/********************
+		 *  IMPRESSORA  *
+		 ********************/	
+		lcImp.add( new GuardaCampo( txtCodImp, "CodImp", "Cód.imp.", ListaCampos.DB_PK, false ) );
+		lcImp.add( new GuardaCampo( txtDescImp, "DescImp", "Descrição da impressora", ListaCampos.DB_SI, false ) );
+		lcImp.add( new GuardaCampo( txtCodPapel, "CodPapel", "Cód.papel", ListaCampos.DB_SI, false ) );
+		lcImp.montaSql( false, "IMPRESSORA", "SG" );
+		lcImp.setQueryCommit( false );
+		lcImp.setReadOnly( true );
+		txtCodImp.setTabelaExterna( lcImp );
+
+		/********************
+		 *  PAPEL  *
+		 ********************/	
+		lcPapel.add( new GuardaCampo( txtCodPapel, "CodPapel", "Cód.papel.", ListaCampos.DB_PK, false ) );
+		lcPapel.add( new GuardaCampo( txtDescPapel, "DescPapel", "Descrição do papel", ListaCampos.DB_SI, false ) );
+		lcPapel.montaSql( false, "PAPEL", "SG" );
+		lcPapel.setQueryCommit( false );
+		lcPapel.setReadOnly( true );
+		txtCodPapel.setTabelaExterna( lcPapel );
+			
+	}
+	
+	private void montaTabelas() {
+
+		// Tabela de impressoras
+		
+		tab.setTamColuna( 50, 0 );
+		tab.setTamColuna( 70, 1 );
+		tab.setTamColuna( 230, 2 );
+		tab.setTamColuna( 70, 3 );
+		tab.setTamColuna( 230, 4 );
+		
+		// Tabela de Balanças
+		
+		tabDetBal.setTamColuna( 50, 0 );
+		tabDetBal.setTamColuna( 200, 1 );
+		tabDetBal.setTamColuna( 50, 2 );
+		tabDetBal.setTamColuna( 60, 3 );
+		tabDetBal.setTamColuna( 60, 4 );
+		tabDetBal.setTamColuna( 60, 5 );
+		tabDetBal.setTamColuna( 60, 6 );
+		tabDetBal.setTamColuna( 60, 7 );
+		
+	}
+	
+	private void montaTela() {
+		
+		/********************
+		 *  CABEÇALHO  *
+		 ********************/	
+		
+		pinCab = new JPanelPad( 740, 130 );
+		setListaCampos( lcCampos );
+		setAltCab( 130 );
+		setPainel( pinCab, pnCliCab );
+		cbModoDemoEst.setVlrString( "N" );
+		cbNfeEst.setVlrString( "N" );
+		
+		adicCampo( txtCodEst, 7, 20, 80, 20, "Codest", "Nº estação", ListaCampos.DB_PK, true );
+		adicCampo( txtDescEst, 90, 20, 257, 20, "Descest", "Descrição da estação de trabalho", ListaCampos.DB_SI, true );
+		adicDB( cbModoDemoEst, 350, 20, 170, 20, "ModoDemoEst", "Modo", true );
+		adicCampo( txtFonteTxt , 7, 60, 270, 20, "FonteTxt", "Fonte para visualização de relatórios texto", ListaCampos.DB_SI, false );
+		adicCampo( txtTamFonteTxt , 280, 60, 65, 20, "TamFonteTxt", "Tamanho", ListaCampos.DB_SI, false );
+		adicDB( cbNfeEst, 350, 60, 150, 20, "NfeEst", "NFE", true );
+		setListaCampos( true, "ESTACAO", "SG" ); 
+		lcCampos.setQueryInsert( false );		
+
+		pnDet.add( tpnGeral );
+		
+		setAltDet( 250 );
+		
+		/*************************
+		 *  DETALHE IMPRESSORAS  *
+		 *************************/	
+		
+		tpnGeral.addTab( "Impressoras", panelImpressoras );
+
+		setPainel( panelImpressorasCampos );
+		
+		panelImpressoras.add( panelImpressorasCampos );
+
+		setListaCampos( lcDet );
+		setNavegador( navRod );
+		
 		adicCampo( txtNroImp, 7, 20, 80, 20, "NroImp", "Nº imp.", ListaCampos.DB_PK, true );
 		adicCampo( txtCodImp, 90, 20, 80, 20, "CodImp", "Cód.imp.", ListaCampos.DB_FK, txtDescImp, true );
 		adicDescFK( txtDescImp, 173, 20, 300, 20, "DescImp", "Descrição da impressora" );
@@ -166,17 +389,49 @@ public class FEstacao extends FDetalhe implements PostListener, ActionListener {
 		adicDB( cbImpGrafica, 363, 100, 150, 20, "ImpGrafica", "Gráfica", true );
 		adicDB( rgTipoUsoImp, 7, 140, 500, 70, "TipoUsoImp", "Tipo uso", true );
 
-		lcDet.addPostListener( this );
 		setListaCampos( true, "ESTACAOIMP", "SG" );
+
 		lcDet.setQueryInsert( false );
+		
 		montaTab();
 
-		tab.setTamColuna( 50, 0 );
-		tab.setTamColuna( 70, 1 );
-		tab.setTamColuna( 230, 2 );
-		tab.setTamColuna( 70, 3 );
-		tab.setTamColuna( 230, 4 );
+		/*************************
+		 *  DETALHE BALANCAS     *
+		 *************************/	
+		
+		tpnGeral.addTab( "Balanças", panelBalancas );
 
+		panelBalancas.add( panelBalancasCampos );
+
+		setListaCampos( lcDetBal );
+		setPainel( panelBalancasCampos );
+		setNavegador( navDetBal );
+		
+		adicCampo( txtNroBal, 7, 20, 60, 24, "NroBal", "Seq.Bal.", ListaCampos.DB_PK, true );		
+		adicDB( cbModeloBal, 70, 20, 200, 24, "ModeloBal", "Modelo", true );		
+		adicDB( cbPortaBal, 273, 20, 160, 24, "PortaBal", "Porta", true );		
+		adicDB( cbSpeedBal, 436, 20, 80, 24, "SpeedBal", "Velocidade", true );
+		
+		adicDB( cbParidadeBal, 7, 64, 115, 24, "ParityBal", "Paridade", true );
+		adicDB( cbBitsBal, 125, 64, 71, 24, "BitsBal", "Bits", true );
+		adicDB( cbStopBitsBal, 199, 64, 71, 24, "StopBitBal", "Stop Bits", true );
+		 
+		setListaCampos( true, "ESTACAOBAL", "SG" );
+		
+		lcDetBal.setQueryInsert( true );
+		
+		lcDetBal.montaTab();
+
+	}
+	
+	private void adicListeners() {
+		
+		tpnGeral.addChangeListener( this );
+		
+		lcCampos.addPostListener( this );
+		
+		lcDet.addPostListener( this );		
+		
 	}
 
 	public void afterPost( PostEvent pevt ) {
@@ -210,11 +465,39 @@ public class FEstacao extends FDetalhe implements PostListener, ActionListener {
 		}
 	}
 
-	public void setConexao( DbConnection cn ) {
 
+	public void setConexao( DbConnection cn ) {
+		
 		super.setConexao( cn );
 		lcImp.setConexao( cn );
 		lcPapel.setConexao( cn );
-		montaTela();
+		lcDetBal.setConexao( cn );
+		
 	}
+	
+	public void stateChanged( ChangeEvent e ) {
+
+		if ( e.getSource() == tpnGeral ) {
+			if ( tpnGeral.getSelectedIndex() == 0 ) { // Clique na aba de impressoras
+				pnMaster.remove( spTabDetBal );
+				pnMaster.add( spTab, BorderLayout.CENTER );
+				pnRodape.remove( navDetBal );
+				pnRodape.add( navRod, BorderLayout.WEST );
+				setSize( getWidth(), getHeight()-1 );
+				setAltDet( 250 );
+			}
+			else if ( tpnGeral.getSelectedIndex() == 1 ) { //Clique na aba de balanças
+				pnMaster.remove( spTab );
+				pnMaster.add( spTabDetBal, BorderLayout.CENTER );
+				pnRodape.remove( navRod );
+				pnRodape.add( navDetBal , BorderLayout.WEST );
+				setSize( getWidth(), getHeight()+1 );
+				setAltDet( 140 );
+			}
+			setVisible( true );
+		}
+	}
+	
+	
+	
 }
