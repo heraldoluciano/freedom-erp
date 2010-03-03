611bab1866289c4a24775bac2644fb9c48146b1c
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-03-03 21:19:09 +0000

    Implementação nos método de busca de produtos para buscar pela referência do produto no fornecedor.

--- freedom-fw1/src/org/freedom/modulos/std/FPrefereGeral.java
+++ freedom-fw1/src/org/freedom/modulos/std/FPrefereGeral.java
@@ -517,6 +517,8 @@ public class FPrefereGeral extends FTabDados implements CheckBoxListener, Action
 	private final JCheckBoxPad cbLancaRMAContr = new JCheckBoxPad( "Permite lançamento de RMA em contrato.", "S", "N" );
 	
 	private final JCheckBoxPad cbInfCPDevolucao = new JCheckBoxPad( "Informar compra na devolução ?", "S", "N" );
+		
+	private final JCheckBoxPad cbUsaBuscGenProdCP = new JCheckBoxPad( "Busca generica do código do produto.", "S", "N" );
 	
 	private final JCheckBoxPad cbAutenticaSMTP = new JCheckBoxPad( "Autenticar ?", "S", "N" );
 	
@@ -1087,6 +1089,7 @@ public class FPrefereGeral extends FTabDados implements CheckBoxListener, Action
 		adicDB( cbPrecoRel, 7, 95, 270, 20, "PrecoCpRel", "",false );
 		adicDB( cbHabiitaCustoCompra, 7, 115, 300, 20, "CustoCompra", "", true );		
 		adicDB( cbInfCPDevolucao, 7, 135, 300, 20, "INFCPDEVOLUCAO", "", true );
+		adicDB( cbUsaBuscGenProdCP, 7, 155, 300, 20, "USABUSCAGENPRODCP", "", true );
 		
 		adicCampo( txtDescClassCp, 11, 200, 250, 20, "ClassCp", "Layout padrão para pedido de compra.", ListaCampos.DB_SI, false );
 		adicCampo( txtObs01, 11, 240, 250, 20, "LabelObs01Cp", "Descrição para campo Obs01.", ListaCampos.DB_SI, false );
