385c6b5c458cea63125df5aa294d804cfe6f3dce
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-03-16 21:08:17 +0000

    Padronização em acesso a atributos da classe Aplicativo.

--- freedom/src/org/freedom/modulos/crm/FEnviaMail.java
+++ freedom/src/org/freedom/modulos/crm/FEnviaMail.java
@@ -52,7 +52,6 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.swing.BorderFactory;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 
@@ -81,7 +80,7 @@ import org.freedom.telas.FFilho;
  */
 public class FEnviaMail extends FFilho implements ActionListener {
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTextFieldPad txtCodAtiv = new JTextFieldPad(JTextFieldPad.TP_INTEGER,8,0);
 	private JTextFieldFK txtDescAtiv = new JTextFieldFK(JTextFieldPad.TP_STRING,40,0);
 	private JTextFieldPad txtArqMen = new JTextFieldPad(JTextFieldPad.TP_STRING,40,0);
@@ -104,39 +103,39 @@ public class FEnviaMail extends FFilho implements ActionListener {
 	String sPass = null;
 	File fArq = null;
 	boolean bEnvia = false;
-    public FEnviaMail() {
-    	super(false);
-    	setTitulo("Envio de e-mail aos contatos");
-    	setAtribos(100,100,375,430);
-    	
-    	lbStatus.setPreferredSize(new Dimension(0,20));
-    	lbStatus.setForeground(Color.BLUE);
-    	pinRod.tiraBorda();
-    	
-    	Vector<String> vVals = new Vector<String>();
-    	vVals.add("A");
+	public FEnviaMail() {
+		super(false);
+		setTitulo("Envio de e-mail aos contatos");
+		setAtribos(100,100,375,430);
+
+		lbStatus.setPreferredSize(new Dimension(0,20));
+		lbStatus.setForeground(Color.BLUE);
+		pinRod.tiraBorda();
+
+		Vector<String> vVals = new Vector<String>();
+		vVals.add("A");
 		vVals.add("C");
 		Vector<String> vLabs = new Vector<String>();
 		vLabs.add("Arquivo no anexo.");
 		vLabs.add("Arquivo no corpo.");
 		rgTipo = new JRadioGroup<String, String>(1,2,vLabs,vVals);
 		rgTipo.setBorder(BorderFactory.createEmptyBorder());
-		
+
 		lbStatus.setBorder(BorderFactory.createEtchedBorder());
 
-    	Container c = getContentPane();
-    	c.setLayout(new BorderLayout());
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
 		c.add(pinGeral,BorderLayout.NORTH);
 		c.add(pnCenter,BorderLayout.CENTER);
 		c.add(pinRod,BorderLayout.SOUTH);
-		
+
 		adicBotaoSair().add(pinRod,BorderLayout.CENTER);
 
 		pnCenter.add(pinArq,BorderLayout.NORTH);
 		pnCenter.add(spnMen,BorderLayout.CENTER);
 		pnCenter.add(lbStatus,BorderLayout.SOUTH);
-		
-				
+
+
 		txtCodAtiv.setTipo(JTextFieldPad.TP_INTEGER,8,0);
 		txtDescAtiv.setTipo(JTextFieldPad.TP_STRING,40,0);
 		lcAtiv.add(new GuardaCampo( txtCodAtiv, "CodAtiv", "Cód.ativ.", ListaCampos.DB_PK, false));
@@ -146,7 +145,7 @@ public class FEnviaMail extends FFilho implements ActionListener {
 		txtCodAtiv.setFK(true);
 		lcAtiv.setReadOnly(true);
 		lcAtiv.montaSql(false, "ATIVIDADE", "TK");
-		
+
 		pinGeral.adic(new JLabelPad("Cód.ativ."),7,0,290,20);
 		pinGeral.adic(txtCodAtiv,7,20,80,20);
 		pinGeral.adic(new JLabelPad("Descrição da atividade"),90,0,290,20);
@@ -155,62 +154,61 @@ public class FEnviaMail extends FFilho implements ActionListener {
 		pinGeral.adic(txtDe,7,60,333,20);
 		pinGeral.adic(new JLabelPad("Assunto:"),7,80,333,20);
 		pinGeral.adic(txtAssunto,7,100,333,20);
-		
+
 		pinArq.adic(new JLabelPad("Arquivo"),7,0,200,20);
 		pinArq.adic(txtArqMen,7,20,313,20);
 		pinArq.adic(btBuscaArq,320,20,20,20);
 		pinArq.adic(rgTipo,7,45,333,30);
-		
+
 		pinRod.adic(btEnviar,7,0,30,30);
 		pinRod.adic(pbAnd,45,5,205,20);
 
 		pbAnd.setStringPainted(true);
 		pbAnd.setMinimum(0);
-		
+
 
 		btBuscaArq.addActionListener(this);
 		btEnviar.addActionListener(this);
-    }
-    private String buscaArq() {
-    	String sRetorno = "";
+	}
+	private String buscaArq() {
+		String sRetorno = "";
 		JFileChooser fcImagem = new JFileChooser();
 		if (fcImagem.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
 			sRetorno = fcImagem.getSelectedFile().getPath();
 		}
 		return sRetorno;
-    }
-    private void abrePrefere() {
+	}
+	private void abrePrefere() {
 		if (!fPrim.temTela("Pref. Gerais")) {
-		  FPrefere tela = new FPrefere();
-		  fPrim.criatela("Pref. Gerais",tela,con);
+			FPrefere tela = new FPrefere();
+			fPrim.criatela("Pref. Gerais",tela,con);
 		} 
-    }
-    private boolean verifSmtp() {
-    	boolean bRet = false;
-    	String sSQL = "SELECT SMTPMAIL,USERMAIL,PASSMAIL FROM SGPREFERE3 WHERE CODEMP=? AND CODFILIAL=?";
-    	try {
-    	  PreparedStatement ps = con.prepareStatement(sSQL);
-    	  ps.setInt(1,Aplicativo.iCodEmp);
-    	  ps.setInt(2,Aplicativo.iCodFilial);
-    	  ResultSet rs = ps.executeQuery();
-    	  if (rs.next()) {
-    		sSMTP = rs.getString("SMTPMail") != null ? rs.getString("SMTPMail").trim() : null;  	
-			sUser = rs.getString("UserMail") != null ? rs.getString("UserMail").trim() : null;  	
-			sPass = rs.getString("PassMail") != null ? rs.getString("PassMail").trim() : null;
-		  }
-		  if (sSMTP == null) {
-			if (Funcoes.mensagemConfirma(this,"Não foi cadastrado o servidor de SMTP, gostaria de cadastrar agora?") == JOptionPane.YES_OPTION)
-			 abrePrefere();  	
-    	  }
-		  else
-			bRet = true;
-    	}
-    	catch (SQLException err) {
-    		Funcoes.mensagemErro(this,"Erro ao buscar o as informações de SMTP!\n"+err.getMessage());
-    		err.printStackTrace();
-    	}    	    	
-    	return bRet;
-    }
+	}
+	private boolean verifSmtp() {
+		boolean bRet = false;
+		String sSQL = "SELECT SMTPMAIL,USERMAIL,PASSMAIL FROM SGPREFERE3 WHERE CODEMP=? AND CODFILIAL=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sSQL);
+			ps.setInt(1,Aplicativo.iCodEmp);
+			ps.setInt(2,Aplicativo.iCodFilial);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				sSMTP = rs.getString("SMTPMail") != null ? rs.getString("SMTPMail").trim() : null;  	
+				sUser = rs.getString("UserMail") != null ? rs.getString("UserMail").trim() : null;  	
+				sPass = rs.getString("PassMail") != null ? rs.getString("PassMail").trim() : null;
+			}
+			if (sSMTP == null) {
+				Funcoes.mensagemInforma(null, "Não foi cadastrado o servidor de SMTP!\nUtilize a tela de preferências gerais para configurar.");    	  
+			}
+			else
+				bRet = true;
+		}
+		catch (SQLException err) {
+			Funcoes.mensagemErro(this,"Erro ao buscar o as informações de SMTP!\n"+err.getMessage());
+			err.printStackTrace();
+		}    	    	
+		return bRet;
+	}
 	private boolean verifCab() {
 		boolean bRet = false;
 		if (txtDe == null) {
@@ -231,11 +229,11 @@ public class FEnviaMail extends FFilho implements ActionListener {
 			bRet = true;
 		return bRet;
 	}
-    private void enviar() {
-    	String sWhere = "";
-    	int iConta = 0;
-    	if (!verifSmtp()) 
-    		return;
+	private void enviar() {
+		String sWhere = "";
+		int iConta = 0;
+		if (!verifSmtp()) 
+			return;
 		else if (!verifCab()) 
 			return;
 		else if (!verifCorpo()) 
@@ -243,58 +241,59 @@ public class FEnviaMail extends FFilho implements ActionListener {
 		Properties pp = new Properties();
 		pp.put("mail.smtp.host",sSMTP);
 		Session se = Session.getDefaultInstance(pp);
-		
+
 		pbAnd.setMaximum(1);
 		pbAnd.setValue(iConta = 0);
 
 		fArq = null;		
 		if (!txtArqMen.getVlrString().equals("")) {
-		  fArq = new File(txtArqMen.getVlrString());
-		  if (!fArq.exists()) {
-			Funcoes.mensagemErro(this,"Arquivo não foi encotrado!");
-			return;
-		  }
+			fArq = new File(txtArqMen.getVlrString());
+			if (!fArq.exists()) {
+				Funcoes.mensagemErro(this,"Arquivo não foi encotrado!");
+				return;
+			}
 		}
-		
+
 		String sSQL = "SELECT NOMECTO,EMAILCTO FROM TKCONTATO WHERE CODEMP=? AND CODFILIAL=?"+sWhere;
 		try {
-		  PreparedStatement ps = con.prepareStatement(sSQL);
-		  ps.setInt(1,Aplicativo.iCodEmp);
-		  ps.setInt(2,ListaCampos.getMasterFilial("TKCONTATO"));
-		  ResultSet rs = ps.executeQuery();
-		  bEnvia = true;
-		  while(rs.next()) {
-			String sEmail = rs.getString("EmailCto");
-			if (!bEnvia)
-				break;
-			if (sEmail != null) {
-			  if (rs.getString("NomeCto") != null)
-				sEmail = rs.getString("NomeCto").trim()+" <"+sEmail.trim()+'>';
-			  else
-				sEmail = sEmail.trim(); 
-		      mandaMail(sEmail,se);
-			  pbAnd.setValue(++iConta);
+			PreparedStatement ps = con.prepareStatement(sSQL);
+			ps.setInt(1,Aplicativo.iCodEmp);
+			ps.setInt(2,ListaCampos.getMasterFilial("TKCONTATO"));
+			ResultSet rs = ps.executeQuery();
+			bEnvia = true;
+			while(rs.next()) {
+				String sEmail = rs.getString("EmailCto");
+				if (!bEnvia)
+					break;
+				if (sEmail != null) {
+					if (rs.getString("NomeCto") != null)
+						sEmail = rs.getString("NomeCto").trim()+" <"+sEmail.trim()+'>';
+					else
+						sEmail = sEmail.trim(); 
+					mandaMail(sEmail,se);
+					pbAnd.setValue(++iConta);
+				}
 			}
-          }
-		  rs.close();
-		  ps.close();
+			rs.close();
+			ps.close();
 		}
 		catch(SQLException err) {
 			Funcoes.mensagemErro(this,"Erro ao buscar contatos!\n"+err.getMessage(),true,con,err);
 			err.printStackTrace();
 		}
-		
-		
-    }
-    private void mandaMail(String sTo,Session se) {
+
+
+	}
+	private void mandaMail(String sTo,Session se) {
 		state("Preparando envio para: '"+sTo+"'");
 		try {
+
 			MimeMessage mes = new MimeMessage(se);
 			mes.setFrom(new InternetAddress(txtDe.getVlrString()));
 			mes.setSubject(txtAssunto.getVlrString());
 			mes.setSentDate(new Date());
 			mes.addRecipient(RecipientType.TO,new InternetAddress(sTo));
-			
+
 			BodyPart parte = new MimeBodyPart();
 
 			parte.setText(txaMen.getVlrString());
@@ -303,12 +302,12 @@ public class FEnviaMail extends FFilho implements ActionListener {
 			multipart.addBodyPart(parte);
 
 			if (fArq != null) {
-			  parte = new MimeBodyPart();
-			  FileDataSource orig = new FileDataSource(fArq);
-			  parte.setDataHandler(new DataHandler(orig));
-			  parte.setFileName(fArq.getName());
-			  parte.setDisposition(rgTipo.getVlrString().equals("A") ? Part.ATTACHMENT : Part.INLINE);
-			  multipart.addBodyPart(parte);
+				parte = new MimeBodyPart();
+				FileDataSource orig = new FileDataSource(fArq);
+				parte.setDataHandler(new DataHandler(orig));
+				parte.setFileName(fArq.getName());
+				parte.setDisposition(rgTipo.getVlrString().equals("A") ? Part.ATTACHMENT : Part.INLINE);
+				multipart.addBodyPart(parte);
 			}
 			mes.setContent(multipart);		
 			state("Enviando dados...");
@@ -320,37 +319,38 @@ public class FEnviaMail extends FFilho implements ActionListener {
 			err.getStackTrace();
 			state("Aguardando reenvio.");
 		}
-    }
+	}
+	
 	public void actionPerformed(ActionEvent evt) {
-		 String sArq = "";
-		 if (evt.getSource() == btBuscaArq) {
+		String sArq = "";
+		if (evt.getSource() == btBuscaArq) {
 			sArq = buscaArq();
 			if (!sArq.equals("")) {
 				txtArqMen.setText(sArq);
 			}
-		 }
-		 else if (evt.getSource() == btEnviar) {
+		}
+		else if (evt.getSource() == btEnviar) {
 			ProcessoSec pSec = new ProcessoSec(500,
-			  new Processo() {
+					new Processo() {
 				public void run() {
-				  lbStatus.updateUI();
-				  pbAnd.updateUI();
+					lbStatus.updateUI();
+					pbAnd.updateUI();
 				}
-			  },
-			  new Processo() {
+			},
+			new Processo() {
 				public void run() {
-				  enviar();
+					enviar();
 				}
-			  }
+			}
 			);
 			pSec.iniciar();
-		 }
+		}
 	}
 	public void state(String sStatus) {
 		lbStatus.setText(sStatus);
 	}
-    public void setConexao(DbConnection cn) {
-    	super.setConexao(cn);
-    	lcAtiv.setConexao(cn);
-    }
+	public void setConexao(DbConnection cn) {
+		super.setConexao(cn);
+		lcAtiv.setConexao(cn);
+	}
 }
