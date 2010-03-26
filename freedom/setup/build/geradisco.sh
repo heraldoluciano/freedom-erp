90070c128eb6ede5c8206e67b67162566a75ed37
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-03-26 14:45:51 +0000

    Ajustes.

--- freedom/setup/build/geradisco.sh
+++ freedom/setup/build/geradisco.sh
@@ -19,16 +19,22 @@ rm ../disco/opt/freedom/bin/*
 rm ../disco/opt/freedom/ini/*
 rm ../disco/opt/firebird/dados/*
 rm ../disco/disco.zip
-unzip -d ../disco/opt/firebird/dados/ ../../dados/estrutura/freedom.zip
+unzip -j -d ../disco/opt/firebird/dados/ ../../dados/estrutura/freedom.zip
 cp ../../src/org/freedom/udf/udfinstall.sh ../disco/opt/firebird/UDF/
 cp ../../src/org/freedom/udf/stp_udf_lin.sql ../disco/opt/firebird/UDF/
 cp ../../src/org/freedom/udf/stp_udf.so ../disco/opt/firebird/UDF/
+cp ../../src/org/freedom/udf/udfinstall.bat ../disco/opt/firebird/UDF/
+cp ../../src/org/freedom/udf/stp_udf_win.sql ../disco/opt/firebird/UDF/
+cp ../../src/org/freedom/udf/stp_udf.dll ../disco/opt/firebird/UDF/
 cp ../../icones/novos/png/*.png ../disco/opt/freedom/icon/
+cp ../../icones/novos/ico/*.ico ../disco/opt/freedom/icon/
 cp ../../lib/*.jar ../disco/opt/freedom/lib/
 cp ../../../freedom-infra/lib/*.jar ../disco/opt/freedom/lib/
 cp ../../../freedom-fw1/lib/*.jar ../disco/opt/freedom/lib/
 cp ../../lib/exec/*.sh ../disco/opt/freedom/bin/
-cp ../../freedomlin.ini ../disco/opt/freedom/ini/freedom.ini
+cp ../../lib/exec/*.bat ../disco/opt/freedom/bin/
+cp ../../freedomlin.ini ../disco/opt/freedom/ini/freedomlin.ini
+cp ../../freedomwin.ini ../disco/opt/freedom/ini/freedomwin.ini
 cd ../disco
 rm lin/disco.zip
 zip -r lin/disco ./opt/*
