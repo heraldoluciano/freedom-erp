7c835925e6f923bc71c679287f1cbf11e166c1e8
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-04-12 20:13:41 +0000

    Implementação no trigger de geração de itens de recebimento de mercadorias, para inclusão da coluna refprod.

---
+++ trunk/freedom/src/org/freedom/modulos/fnc/view/dialog/utility/DLInfoPlanoPag.java
@@ -0,0 +1,112 @@
+/**
+ * @version 14/07/2003 <BR>
+ * @author Setpoint Informática Ltda./Fernando Oliveira da Silva <BR>
+ * 
+ * Projeto: Freedom <BR>
+ * 
+ * Pacote: org.freedom.modulos.std <BR>
+ * Classe:
+ * @(#)DLLote.java <BR>
+ * 
+ * Este arquivo é parte do sistema Freedom-ERP, o Freedom-ERP é um software livre; você pode redistribui-lo e/ou <BR>
+ * modifica-lo dentro dos termos da Licença Pública Geral GNU como publicada pela Fundação do Software Livre (FSF); <BR>
+ * na versão 2 da Licença, ou (na sua opnião) qualquer versão. <BR>
+ * Este programa é distribuido na esperança que possa ser  util, mas SEM NENHUMA GARANTIA; <BR>
+ * sem uma garantia implicita de ADEQUAÇÂO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. <BR>
+ * Veja a Licença Pública Geral GNU para maiores detalhes. <BR>
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com este programa, se não, <BR>
+ * de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
+ * Comentários sobre a classe...
+ */
+
+package org.freedom.modulos.fnc.view.dialog.utility;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+
+import org.freedom.funcoes.Funcoes;
+import org.freedom.infra.model.jdbc.DbConnection;
+import org.freedom.library.persistence.GuardaCampo;
+import org.freedom.library.persistence.ListaCampos;
+import org.freedom.library.swing.JLabelPad;
+import org.freedom.library.swing.JTextFieldFK;
+import org.freedom.library.swing.JTextFieldPad;
+import org.freedom.library.swing.dialog.FFDialogo;
+
+public class DLInfoPlanoPag extends FFDialogo {
+
+	private static final long serialVersionUID = 1L;
+
+	private JTextFieldPad txtCodPlanoPag = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+
+	private JTextFieldFK txtDescPlanoPag = new JTextFieldFK( JTextFieldPad.TP_STRING, 50, 0 );
+
+	private JLabelPad lbCodPlanoPag = new JLabelPad( "Cód.Plan." );
+
+	private JLabelPad lbDescPlanoPag = new JLabelPad( "Descrição do plano de pagamento" );
+	
+	private ListaCampos lcPlanoPag = new ListaCampos( this, "PG" );
+	
+	public DLInfoPlanoPag( Component cOrig, DbConnection cn ) {
+
+		super( cOrig );
+		setConexao( cn );
+		setTitulo( "Informe o plano de pagamento" );
+		setAtribos( 400, 220 );
+		
+		montaListaCampos();
+
+		txtCodPlanoPag.setRequerido( true );
+
+		adic( lbCodPlanoPag, 7, 0, 80, 20 );
+		adic( txtCodPlanoPag, 7, 20, 80, 20 );
+
+		adic( lbDescPlanoPag, 90, 0, 250, 20);
+		adic( txtDescPlanoPag, 90, 20, 250, 20);
+		
+		txtCodPlanoPag.requestFocus();
+	}
+
+	private void montaListaCampos() {
+
+		lcPlanoPag.add( new GuardaCampo( txtCodPlanoPag, "CodPlanoPag", "Cód.p.pag.", ListaCampos.DB_PK, false ) );
+		lcPlanoPag.add( new GuardaCampo( txtDescPlanoPag, "DescPlanoPag", "Descrição do plano de pagamento", ListaCampos.DB_SI, false ) );
+		lcPlanoPag.setWhereAdic( "ATIVOPLANOPAG='S' AND CVPLANOPAG IN ('C','A')" );
+		lcPlanoPag.montaSql( false, "PLANOPAG", "FN" );
+		lcPlanoPag.setQueryCommit( false );
+		lcPlanoPag.setReadOnly( true );
+		txtCodPlanoPag.setTabelaExterna( lcPlanoPag );
+		txtCodPlanoPag.setFK( true );
+
+	}
+
+	public Integer getValor() {
+
+		return txtCodPlanoPag.getVlrInteger();
+	}
+	
+
+	public void actionPerformed( ActionEvent evt ) {
+		
+		super.actionPerformed( evt );
+		
+		if ( evt.getSource() == btOK ) {
+			if ( txtCodPlanoPag.getText().trim().length() == 0 ) {
+				Funcoes.mensagemInforma( this, "Informe o plano de pagamento!" );
+				txtCodPlanoPag.requestFocus();
+			}
+		}
+	//	else if ( evt.getSource() == btCancel ) {
+		//	super.actionPerformed( evt );
+		//}
+		
+	}
+	
+	public void setConexao( DbConnection cn) {
+		super.setConexao( cn );
+		lcPlanoPag.setConexao( cn );
+	}
+	
+}
