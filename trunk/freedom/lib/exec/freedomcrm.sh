4ec4b0a35b121f7eb8907da0e2be4cdfacdd0227
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-04-15 18:49:30 +0000

    Mudança em pacotes.

--- trunk/freedom/lib/exec/freedomcrm.sh
+++ trunk/freedom/lib/exec/freedomcrm.sh
@@ -1,3 +1,3 @@
 EXEDIR=${0%/*}
 cd $EXEDIR/..
-java -classpath "lib/*" -DARQINI=ini/freedom.ini -DARQINI=ini/freedom.ini -DARQLOG=log/freedomcrm.log org.freedom.modulos.crm.FreedomCRM
+java -classpath "lib/*" -DARQINI=ini/freedom.ini -DARQLOG=log/freedomcrm.log org.freedom.modulos.crm.FreedomCRM
