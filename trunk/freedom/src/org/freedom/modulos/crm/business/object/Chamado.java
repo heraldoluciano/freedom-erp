6df7383e01151ee33288f551397e1dd723692287
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-04-16 00:12:35 +0000

    Inclusão de chamada para a tela de cadastro de chamados.

--- trunk/freedom/src/org/freedom/modulos/crm/business/object/Chamado.java
+++ trunk/freedom/src/org/freedom/modulos/crm/business/object/Chamado.java
@@ -1,73 +1,70 @@
 package org.freedom.modulos.crm.business.object;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Vector;
 
 import org.freedom.infra.beans.Field;
-import org.freedom.infra.model.jdbc.DbConnection;
-import org.freedom.library.persistence.ListaCampos;
-import org.freedom.library.swing.frame.Aplicativo;
+import org.freedom.infra.pojos.Constant;
 
 
 
 public class Chamado {
 
+	private static final long serialVersionUID = 1L;
+	
 	public enum Campos {
 		CODEMP, CODFILIAL, CODCHAMADO, DESCCHAMADO, DETCHAMADO, OBSCHAMADO, CODEMPCL, CODFILIALCL,
 		CODCLI, SOLICITANTE, PRIORIDADE, CODEMPTC, CODFILIALTC, CODTPCHAMADO, STATUS, DTCHAMADO,
 		DTPREVISAO, QTDHORASPREVISAO, DTCONCLUSAO, DTINS, HINS, IDUSUINS, DTALT, HALT, IDUSUALT
 	}
 	
-	Field codchamado = new Field( "codchamado", null, Types.INTEGER );
-	Field deschamado = new Field( "descchamado", null, Types.CHAR );
-	Field detchamado = new Field( "detchamado", null, Types.CHAR );
-	Field obschamado = new Field( "obschamado", null, Types.CHAR );
-	Field solicitante = new Field( "solicitante", null, Types.CHAR );
-	Field prioridade = new Field( "prioridade", null, Types.INTEGER );
-	Field codtpchamado = new Field( "codtpchamado", null, Types.INTEGER );
-	Field status = new Field( "status", null, Types.CHAR );
-	Field dtchamado = new Field( "dtchamado", null, Types.DATE );
-	Field dtprevisao = new Field( "dtprevisao", null, Types.DATE );
-	Field qtdhorasprevisao = new Field( "qtdhorasprevisao", null, Types.DECIMAL );
-	Field dtconclusao = new Field( "dtconclusao", null, Types.DATE );
+	public static final Constant STATUS_PENDENTE = new Constant("Pendente", "PE" ); 
+	public static final Constant STATUS_EM_ANDAMENTO = new Constant("Em andamento", "EA" );
+	public static final Constant STATUS_CANCELADO = new Constant("Cancelado", "CA" );
+	public static final Constant STATUS_CONCLUIDO = new Constant("Concluído", "CO" );
 	
-	private void montaComboTipoChamado(DbConnection con) {
+	private Field codchamado = new Field( "codchamado", null, Types.INTEGER );
+	private Field deschamado = new Field( "descchamado", null, Types.CHAR );
+	private Field detchamado = new Field( "detchamado", null, Types.CHAR );
+	private Field obschamado = new Field( "obschamado", null, Types.CHAR );
+	private Field solicitante = new Field( "solicitante", null, Types.CHAR );
+	private Field prioridade = new Field( "prioridade", null, Types.INTEGER );
+	private Field codtpchamado = new Field( "codtpchamado", null, Types.INTEGER );
+	private Field status = new Field( "status", null, Types.CHAR );
+	private Field dtchamado = new Field( "dtchamado", null, Types.DATE );
+	private Field dtprevisao = new Field( "dtprevisao", null, Types.DATE );
+	private Field qtdhorasprevisao = new Field( "qtdhorasprevisao", null, Types.DECIMAL );
+	private Field dtconclusao = new Field( "dtconclusao", null, Types.DATE );
 
-		try {
-			
-			PreparedStatement ps = con.prepareStatement( "select codtpchamado, desctpchamado from attipoatendo where codemp=? and codfilial=? order by desctpchamado" );
+	public static Vector<String> getLabels( ) {
 
-			ps.setInt( 1, Aplicativo.iCodEmp );
-			ps.setInt( 2, ListaCampos.getMasterFilial( "CRTIPOCHAMADO" ) );
-
-			
-			ResultSet rs = ps.executeQuery();
-			
-//			vValsTipo.clear();
-			//			vLabsTipo.clear();
-
-			//			vValsTipo.addElement( -1 );
-			//			vLabsTipo.addElement( "<Selecione>" );
-			
-			//			while ( rs.next() ) {
-			//vValsTipo.addElement( rs.getInt( "CodTpAtendo" ) );
-			//				vLabsTipo.addElement( rs.getString( "DescTpAtendo" ) );
-			//			}
-			
-			//			cbTipo.setItensGeneric( vLabsTipo, vValsTipo );
-			
-			//			rs.close();
-			//			ps.close();
-			
-			con.commit();
-			
-		} catch ( SQLException e ) {
-			e.printStackTrace();
-//			Funcoes.mensagemErro( this, "Erro ao carregar os tipos de atendimento!\n" + e.getMessage(), true, con, e );
-		} 
+		Vector<String> ret = new Vector<String>();
+		
+		ret.addElement( "<--Selecione-->" );
+	
+		ret.add( STATUS_PENDENTE.getName() );
+		ret.add( STATUS_EM_ANDAMENTO.getName() );
+		ret.add( STATUS_CANCELADO.getName() );
+		ret.add( STATUS_CONCLUIDO.getName() );
+		
+		return ret;
+		
 	}
+	
+	public static Vector<Object> getValores( ) {
+		
+		Vector<Object> ret = new Vector<Object>();
+		
+		ret.addElement( "" );
+		
+		ret.add( (String) STATUS_PENDENTE.getValue() );
+		ret.add( (String) STATUS_EM_ANDAMENTO.getValue() );
+		ret.add( (String) STATUS_CANCELADO.getValue() );
+		ret.add( (String) STATUS_CONCLUIDO.getValue() );
+		
+		return ret;
 
+	}
+	
 	
 }
