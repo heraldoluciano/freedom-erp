7ac8cbb474133a1b95fc9876c8d6ddf7879060ef
cristianrm <cristianrm@gmail.com>
2010-05-18 14:29:56 +0000

    Adicionado preenchimento de campo no email.

--- freedom/src/org/freedom/modulos/fnc/view/frame/report/FRCartaCobranca.java
+++ freedom/src/org/freedom/modulos/fnc/view/frame/report/FRCartaCobranca.java
@@ -2,23 +2,23 @@
  * @version 16/04/2010 <BR>
  * @author Setpoint Informática Ltda./Anderson Sanchez <BR>
  * 
- * Projeto: Freedom <BR>
+ *         Projeto: Freedom <BR>
  * 
- * Pacote: org.freedom.modulos.std <BR>
- * Classe:
+ *         Pacote: org.freedom.modulos.std <BR>
+ *         Classe:
  * @(#)FRReceber.java <BR>
  * 
- * Este arquivo é parte do sistema Freedom-ERP, o Freedom-ERP é um software livre; você pode redistribui-lo e/ou <BR>
- * modifica-lo dentro dos termos da Licença Pública Geral GNU como publicada pela Fundação do Software Livre (FSF); <BR>
- * na versão 2 da Licença, ou (na sua opnião) qualquer versão. <BR>
- * Este programa é distribuido na esperança que possa ser  util, mas SEM NENHUMA GARANTIA; <BR>
- * sem uma garantia implicita de ADEQUAÇÂO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. <BR>
- * Veja a Licença Pública Geral GNU para maiores detalhes. <BR>
- * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com este programa, se não, <BR>
- * de acordo com os termos da LPG-PC <BR>
+ *                    Este arquivo é parte do sistema Freedom-ERP, o Freedom-ERP é um software livre; você pode redistribui-lo e/ou <BR>
+ *                    modifica-lo dentro dos termos da Licença Pública Geral GNU como publicada pela Fundação do Software Livre (FSF); <BR>
+ *                    na versão 2 da Licença, ou (na sua opnião) qualquer versão. <BR>
+ *                    Este programa é distribuido na esperança que possa ser util, mas SEM NENHUMA GARANTIA; <BR>
+ *                    sem uma garantia implicita de ADEQUAÇÂO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. <BR>
+ *                    Veja a Licença Pública Geral GNU para maiores detalhes. <BR>
+ *                    Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com este programa, se não, <BR>
+ *                    de acordo com os termos da LPG-PC <BR>
  * <BR>
  * 
- * Tela de filtros par ao relatório de carta de cobrança.
+ *                    Tela de filtros par ao relatório de carta de cobrança.
  * 
  */
 
@@ -28,10 +28,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+
 import net.sf.jasperreports.engine.JasperPrintManager;
+
 import org.freedom.acao.RadioGroupEvent;
 import org.freedom.acao.RadioGroupListener;
 import org.freedom.infra.model.jdbc.DbConnection;
+import org.freedom.library.functions.EmailBean;
 import org.freedom.library.functions.Funcoes;
 import org.freedom.library.persistence.GuardaCampo;
 import org.freedom.library.persistence.ListaCampos;
@@ -39,6 +42,7 @@ import org.freedom.library.swing.component.JLabelPad;
 import org.freedom.library.swing.component.JTextFieldFK;
 import org.freedom.library.swing.component.JTextFieldPad;
 import org.freedom.library.swing.frame.Aplicativo;
+import org.freedom.library.swing.frame.FDados;
 import org.freedom.library.swing.frame.FPrinterJob;
 import org.freedom.library.swing.frame.FRelatorio;
 import org.freedom.modulos.fnc.business.component.Juros;
@@ -50,10 +54,12 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 	private JTextFieldPad txtCodCli = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
 	private JTextFieldPad txtCnpjCli = new JTextFieldPad( JTextFieldPad.TP_STRING, 14, 0 );
-	
+
 	private JTextFieldPad txtDiasAtrazo = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 3, 0 );
 
 	private JTextFieldFK txtRazCli = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
+
+	private JTextFieldFK txtEmailCli = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 
 	private ListaCampos lcCli = new ListaCampos( this );
 
@@ -62,11 +68,11 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 	public FRCartaCobranca() {
 
 		super( false );
-		
+
 		setTitulo( "Carta de cobrança" );
 		setAtribos( 40, 50, 420, 130 );
 
-		txtDiasAtrazo.setVlrInteger( new Integer(15) );
+		txtDiasAtrazo.setVlrInteger( new Integer( 15 ) );
 
 		montaListaCampos();
 		montaRadioGroups();
@@ -76,9 +82,12 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 
 	private void montaListaCampos() {
 
+		FDados fDados = new FDados();
+
 		lcCli.add( new GuardaCampo( txtCodCli, "CodCli", "Cód.cli.", ListaCampos.DB_PK, false ) );
 		lcCli.add( new GuardaCampo( txtRazCli, "RazCli", "Razão social do cliente", ListaCampos.DB_SI, false ) );
 		lcCli.add( new GuardaCampo( txtCnpjCli, "CnpjCli", "CNPJ", ListaCampos.DB_SI, false ) );
+		fDados.adicCampoInvisivel( txtCodCli, "CodCli", "Cód.cli.", ListaCampos.DB_FK, txtRazCli, false );
 
 		lcCli.montaSql( false, "CLIENTE", "VD" );
 		lcCli.setReadOnly( true );
@@ -98,10 +107,10 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 		adic( txtCodCli, 7, 20, 80, 20 );
 		adic( new JLabelPad( "Razão social do cliente" ), 90, 0, 210, 20 );
 		adic( txtRazCli, 90, 20, 210, 20 );
-		
+
 		adic( new JLabelPad( "Dias de atrazo" ), 303, 0, 90, 20 );
 		adic( txtDiasAtrazo, 303, 20, 90, 20 );
-		
+
 	}
 
 	public void imprimir( boolean bVisualizar ) {
@@ -116,42 +125,41 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 			sql.append( "cl.codcli, cl.razcli, " );
 			sql.append( "rc.datarec emissao, ir.dtvencitrec vencimento, ir.dtpagoitrec recebimento, " );
 			sql.append( "rc.docrec doc, ir.vlrparcitrec, ir.vlrpagoitrec recebido, ir.vlrapagitrec aberto " );
-			
+
 			sql.append( "from fnitreceber ir, fnreceber rc, vdcliente cl " );
-			
+
 			sql.append( "where " );
 			sql.append( "ir.codemp=rc.codemp and ir.codfilial=rc.codfilial and ir.codrec=rc.codrec " );
 			sql.append( "and cl.codemp=rc.codempcl and cl.codfilial=rc.codfilialcl and cl.codcli=rc.codcli " );
 			sql.append( "and ir.statusitrec not in('RP','CR') " );
 			sql.append( "and ir.dtvencitrec <= cast('today' as date) " );
-			sql.append( "and rc.codemp=? and rc.codfilial=? ");
-			
-			if( txtCodCli.getVlrInteger() > 0 ) {
+			sql.append( "and rc.codemp=? and rc.codfilial=? " );
+
+			if ( txtCodCli.getVlrInteger() > 0 ) {
 				sql.append( " and rc.codempcl=? and rc.codfilialcl=? and rc.codcli=? " );
 			}
-			
-			if( txtDiasAtrazo.getVlrInteger()>0) {
-				sql.append( " and ( ( ( cast('today' as date) ) - ir.dtvencitrec ) ) >= " + txtDiasAtrazo.getVlrInteger()  );
+
+			if ( txtDiasAtrazo.getVlrInteger() > 0 ) {
+				sql.append( " and ( ( ( cast('today' as date) ) - ir.dtvencitrec ) ) >= " + txtDiasAtrazo.getVlrInteger() );
 			}
-			
+
 			sql.append( " order by 1, 3 desc" );
-						
+
 			int param = 1;
-			
+
 			System.out.println( "Query:" + sql.toString() );
-			
+
 			ps = con.prepareStatement( sql.toString() );
 
 			ps.setInt( param++, Aplicativo.iCodEmp );
 			ps.setInt( param++, ListaCampos.getMasterFilial( "FNRECEBER" ) );
-			
-			if( txtCodCli.getVlrInteger() > 0 ) {
+
+			if ( txtCodCli.getVlrInteger() > 0 ) {
 				ps.setInt( param++, lcCli.getCodEmp() );
 				ps.setInt( param++, lcCli.getCodFilial() );
 				ps.setInt( param++, txtCodCli.getVlrInteger() );
-				
-			}
 
+			}
 
 			rs = ps.executeQuery();
 
@@ -159,14 +167,13 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 
 			rs.close();
 			ps.close();
-			
+
 			con.commit();
 
-		} 
-		catch ( SQLException err ) {
+		} catch ( SQLException err ) {
 			Funcoes.mensagemErro( this, "Erro consulta tabela de contas a receber!\n" + err.getMessage(), true, con, err );
 			err.printStackTrace();
-		}		
+		}
 	}
 
 	private void imprimirGrafico( final boolean bVisualizar, final ResultSet rs, final String sCab ) {
@@ -182,10 +189,13 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 		hParam.put( "CIDFILIAL", Aplicativo.empresa.getCidFilial() );
 
 		Juros calcjuros = new Juros();
-		
+
 		hParam.put( "CALCJUROS", calcjuros );
 
-		dlGr = new FPrinterJob( "layout/rel/REL_CARTA_COB_01.jasper", "Carta de cobrança", sCab, rs, hParam, this );
+		EmailBean mail = Aplicativo.getEmailBean();
+		mail.setPara( EmailBean.getEmailCli( txtCodCli.getVlrInteger(), con ) );
+
+		dlGr = new FPrinterJob( "layout/orc/ORC_PD.jasper", null, null, this, hParam, con, mail );
 
 		if ( bVisualizar ) {
 			dlGr.setVisible( true );
@@ -194,7 +204,7 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 			try {
 				JasperPrintManager.printReport( dlGr.getRelatorio(), true );
 			} catch ( Exception err ) {
-				Funcoes.mensagemErro( this, "Erro na impressão do relatório de carta de cobrança!" + err.getMessage(), true, con, err );				
+				Funcoes.mensagemErro( this, "Erro na impressão do relatório de carta de cobrança!" + err.getMessage(), true, con, err );
 			}
 		}
 	}
@@ -205,7 +215,7 @@ public class FRCartaCobranca extends FRelatorio implements RadioGroupListener {
 
 		lcCli.setConexao( cn );
 		lcTipoCli.setConexao( cn );
-		
+
 	}
 
 	public void valorAlterado( RadioGroupEvent evt ) {
