74c7a4eba44073dd8ce0023226f9448d517889bd
anderson.sanchez <anderson.sanchez@gmail.com>
2010-05-19 23:18:43 +0000

    Correções de sincronismo.

--- freedom-infra/src/org/freedom/infra/driver/scale/EpmSP2400.java
+++ freedom-infra/src/org/freedom/infra/driver/scale/EpmSP2400.java
@@ -17,19 +17,19 @@ import org.freedom.infra.functions.StringFunctions;
 public class EpmSP2400 extends AbstractScale  {
 
 	private BigDecimal weight = null;
-	
+
 	private Date date = null;
-	
+
 	private Time time = null;
-	
+
 	public static final String NOME_BAL = "Rodoviária EPM SP-2400";
-	
+
 	private boolean reading = true; // Indica se ainda está lendo o buffer.
-	
+
 	public void initialize( Integer com, Integer timeout, Integer baudrate, Integer databits, Integer stopbits, Integer parity ) {
-		
+
 		this.com = com;
-		
+
 		serialParams.setTimeout( timeout );
 		serialParams.setBauderate( baudrate );
 		serialParams.setDatabits( databits );
@@ -37,15 +37,15 @@ public class EpmSP2400 extends AbstractScale  {
 		serialParams.setParity( parity );
 
 		activePort(this);
-		
+
 		reading = true;
-		
+
 		readReturn();
-		
-		
-		
+
+
+
 	}
-	
+
 	public String getName() {
 		return NOME_BAL;
 	}
@@ -59,49 +59,51 @@ public class EpmSP2400 extends AbstractScale  {
 			e.printStackTrace();
 		}
 	}
-	
-	protected void readReturn() {
-		
+
+	protected synchronized void readReturn() {
+
 		try {
-			
-			
-//			SerialPort  porta = Serial.getInstance().getSerialPort();
-//			porta.addEventListener(this);
-			
-//			porta.removeEventListener();
-			
-//			porta.notifyOnDataAvailable( true );
-			
-			
-			
-//			porta.addEventListener( this );
-			
-			
-//			SerialPort  porta = Serial.getInstance().getSerialPort();
-			
-//			if(porta !=null) {
-//				porta.removeEventListener();
-//				porta.addEventListener(this);
-//			}
+
+
+			//			SerialPort  porta = Serial.getInstance().getSerialPort();
+			//			porta.addEventListener(this);
+
+			//			porta.removeEventListener();
+
+			//			porta.notifyOnDataAvailable( true );
+
+
+
+			//			porta.addEventListener( this );
+
+
+			//			SerialPort  porta = Serial.getInstance().getSerialPort();
+
+			//			if(porta !=null) {
+			//				porta.removeEventListener();
+			//				porta.addEventListener(this);
+			//			}
 
 			while (reading) {
-				Thread.sleep( 5000 );
+				
+				Thread.sleep( 10000 );
+				
 			}
-			
-			
-/*			if(buffer!=null) {
-			
+
+
+			/*			if(buffer!=null) {
+
 				String reading = new String( buffer );			
-				
+
 				System.out.println(reading);
-				
+
 				parseString(reading);				
-				
+
 			}
 			else {
 				System.out.println("Buffer is null!");
 			}*/			
-			
+
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -109,164 +111,150 @@ public class EpmSP2400 extends AbstractScale  {
 		finally {
 			CtrlPort.getInstance().disablePort();
 		}
-		
+
 	}
 
 	public void setDate(Date date) {
-		
+
 		this.date = date;		
 	}
 
-	
+
 	public Date getDate() {
- 
+
 		return date;
-	
+
 	}
 
 	private void setTime(Time time) {
-		
+
 		this.time = time; 
-	
+
 	}
-	
+
 	public Time getTime() {
-		
+
 		return time;		
 	}
-		
+
 	private synchronized void parseString(String str) {
-		
+
 		String strweight = "";
 		String strdate = "";
 		String strtime = "";
-		
-		try {
-			
-			System.out.println("Leitura:" + str);
-			
-			// pega os ultimos 56 caracteres do buffer
-			//if(str.length()>=56) {
-				//str = str.substring(str.length()-56);
-			//}
 
-			//str = StringFunctions.alltrim(str);
-			
-			System.out.println("Limpo..:" + str);
-			
-			int posicaobranca = str.indexOf( "    " );
-			
-			System.out.println("Posicao branca:"+posicaobranca);
-			
-			if(posicaobranca<24 && posicaobranca>-1) {
-				
-				str = str.substring( posicaobranca+4 );
-				
-				System.out.println("Limpo.2.:" + str);
-//				str = StringFunctions.alltrim( str );
-			
-				//str = str.trim();
-				
-				if(str.length()>=24) {
-					
-					str = str.substring( 0, 24 );
-					
-					String validador = str.substring( 11, 12 )  + str.substring( 14, 15 ) + str.substring( 19, 20 );
-					
-					if("//:".equals( validador )) {
-						
-						System.out.println("CARACTER MALDITO:" + ((int) str.charAt(23)) );
-											
-						System.out.println("Finalizou leitura!");
-						
-						System.out.println("String Limpa:" + str);
-						
-						strweight = str.substring( 0,  07 );
-						
-						System.out.println("peso lido: " + strweight);
-						
-						strdate = str.substring( 9,  17 );
-						
-						System.out.println("data lida: " + strdate);
-						
-						strtime = str.substring( 17, str.length()-1 );
-						
-						System.out.println("hora lida antes: " + strtime);
-						
-						setWeight( ConversionFunctions.stringToBigDecimal( strweight ) );
-						setDate( ConversionFunctions.strDate6digToDate( strdate ) );
-						setTime( ConversionFunctions.strTimetoTime( strtime ) );
-	
-						System.out.println("Setou o peso:" + getWeight());
-						System.out.println("Setou a data:" + getDate());
-						System.out.println("Setou a hora:" + getTime());					
-						
-						// Porta deve ser desabilitada para finalizar a leitura dos pesos da balança.
-						
-						buffer = null;
-	
+		try {
+
+			System.out.println("Leitura:" + str);
+
+			//str = str.trim();
+
+			// pega os ultimos 56 caracteres do buffer
+			if(str.length()>56) {
+				buffer = null;
+				//str = str.substring(str.length()-56);
+				int posicaobranca = str.indexOf( "    " );
+
+				if(posicaobranca<24 && posicaobranca>-1) {
+					str = str.substring( posicaobranca+4 );
+					System.out.println("str-sub:"+str);
+					//str = StringFunctions.alltrim( str );
+
+					//str = str.trim();
+
+					if(str.length()>=24) {
+
+						str = str.substring( 0, 24 );
+						System.out.println("str-avaliada:"+str);
+
+						String validador = str.substring( 11, 12 )  + str.substring( 14, 15 ) + str.substring( 19, 20 );
+						System.out.println("str-validador:"+validador);
+
+						if("//:".equals( validador )) {
+
+							System.out.println("CARACTER MALDITO:" + ((int) str.charAt(23)) );
+
+							System.out.println("Finalizou leitura!");
+
+							System.out.println("String Limpa:" + str);
+
+							strweight = str.substring( 0,  07 );
+
+							System.out.println("peso lido: " + strweight);
+
+							strdate = str.substring( 9,  17 );
+
+							System.out.println("data lida: " + strdate);
+
+							strtime = str.substring( 17, str.length()-1 );
+
+							System.out.println("hora lida antes: " + strtime);
+
+							setWeight( ConversionFunctions.stringToBigDecimal( strweight ) );
+							setDate( ConversionFunctions.strDate6digToDate( strdate ) );
+							setTime( ConversionFunctions.strTimetoTime( strtime ) );
+
+							System.out.println("Setou o peso:" + getWeight());
+							System.out.println("Setou a data:" + getDate());
+							System.out.println("Setou a hora:" + getTime());					
+
+							// Porta deve ser desabilitada para finalizar a leitura dos pesos da balança.
+							
+
+							reading = false; // Parar a leitura se leitura estiver OK
+							
+						}
+						else {				
+							System.out.println("String nao validada: " + str);
+						}
+
 					}
-					else {				
-						System.out.println("String nao validada: " + str);
-					}
-					
+
 				}
-				
+
 			}
-			
-			
+
+
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public synchronized void serialEvent( final SerialPortEvent event ) {
 
 		byte[] result = null;
 		byte[] bufferTmp = null;
 		byte[] tmp = null;
-		byte[] peso01 = null;
-		byte[] peso02 = null;
-		byte[] peso03 = null;
-		
+
 		InputStream input = null;
 
 		input = CtrlPort.getInstance().getInput();
 
 		try {
-			
-			reading = true;  // Coloca o flag em estado de leitura
-			
+
 			if ( event.getEventType() == SerialPortEvent.DATA_AVAILABLE ) {
-				
-				Thread.sleep(250);
-				peso01 = lepeso( input );
 
-				Thread.sleep(250);
-				peso02 = lepeso( input );
-
-				Thread.sleep(250);
-				peso03 = lepeso( input );
-				
-//				result = new byte[ 256 ];
+				result = new byte[ input.available() ];
+				//				result = new byte[ 256 ];
 
 				int nodeBytes = 0;
-				
+
 				if ( result != null ) { 
-					
+
 					while ( input.available() > 0 ) {
-						
+
 						nodeBytes = input.read( result );	
-					
+
 					}
 
 					System.out.println("tamanho da cadeia de leitura:" + nodeBytes);
-					
+
 					if ( buffer == null ) {
 						bufferTmp = result;
 					} 
 					else {
+						isRead = true;
 						tmp = buffer;
 						bufferTmp = new byte[ tmp.length + result.length ];
 
@@ -279,50 +267,28 @@ public class EpmSP2400 extends AbstractScale  {
 						}
 					}  
 
-					isRead = true;
-					
 					buffer = bufferTmp;
-					
-					if (buffer.length>=56) {
-						parseString( new String(buffer) );
-						buffer = null;
-					}
-				}
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace() ;// TODO: handle exception
-		} catch ( IOException e ) {
-			e.printStackTrace();
-		} finally {
-			reading = false;
-		}
-	}
-	
-	private byte[] lepeso(InputStream input) {
-		byte[] result = null ;
-		int nodeBytes = 0;
-		try {
-			result = new byte[ input.available() ];
-			if ( result != null ) { 
-				while ( input.available() > 0 ) {
-					nodeBytes = input.read( result );	
+					parseString( new String(buffer) );					
 				}
 			}
 		} catch ( IOException e ) {
 			e.printStackTrace();
 		}
-		return result;
+		finally {
+//			reading = false;
+		}
 	}
-	
+
 	private void setWeight( BigDecimal weight ) {
-		
+
 		this.weight = weight;
-		
+
 	}
-	
+
 	public BigDecimal getWeight() {
-		
+
 		return weight;
 	}
+
 
 }
