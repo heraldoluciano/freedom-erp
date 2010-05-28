9936bcc9037b3f9801e5221aa7e0145fe43dd001
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-05-28 23:07:50 +0000

    Implementação para versionamento da tela de inicialização.

--- trunk/freedom-infra/src/org/freedom/infra/functions/SystemFunctions.java
+++ trunk/freedom-infra/src/org/freedom/infra/functions/SystemFunctions.java
@@ -1,8 +1,12 @@
 package org.freedom.infra.functions;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 public class SystemFunctions {
 
@@ -36,10 +40,34 @@ public class SystemFunctions {
 	    String className = clazz.getName();  
 	    className = className.replaceAll("\\.", "/");  
 	    className = "/" + className + ".class";  
-	    URL url = Class.class.getResource(className);  
+	    URL url = Class.class.getResource(className);
 	    URLConnection urlConnection = url.openConnection();  
 	    java.util.Date lastModified = new java.util.Date(urlConnection.getLastModified());  
 	    return lastModified;  
 	}
+	
+	public static String getVersion(Class<?> clazz) {
+		String versao = "";
+		
+		try {
+			
+			 File file =  new File ( clazz.getProtectionDomain().getCodeSource().getLocation().toURI());			 
+			 JarFile jarfile = new JarFile(file.getAbsolutePath());
+			 
+			 Manifest manifest = jarfile.getManifest();
+			 
+			 Attributes att = manifest.getMainAttributes();
+			 
+			 versao = att.getValue("Signature-Version");			
+			
+		}
+		catch (Exception e) {
+			versao = "Indefinida";
+		}
+		
+		return versao;
+		
+	}
+
 	
 }
