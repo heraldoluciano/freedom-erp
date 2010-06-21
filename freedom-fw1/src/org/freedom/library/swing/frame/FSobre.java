7ce166afe935403440f3011fa79b4403ec05a622
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-06-21 22:39:41 +0000

    Implementações no layout da tela.

--- freedom-fw1/src/org/freedom/library/swing/frame/FSobre.java
+++ freedom-fw1/src/org/freedom/library/swing/frame/FSobre.java
@@ -47,17 +47,19 @@ public class FSobre extends FFDialogo {
 
 	private static final long serialVersionUID = 1L;
 
-	private JButtonPad btMemoria = new JButtonPad( "Reciclar", Icone.novo( "btExecuta.gif" ) );
+	private JButtonPad btReciclarMemoria = new JButtonPad( "Reciclar memória", Icone.novo( "btAtualiza.gif" ) );
 
 	private JTabbedPanePad tpnSobre = new JTabbedPanePad();
 
 	private JPanelPad pnSobre = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );
-
+	
 	private JPanelPad pnEquipe = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );
-
+	
 	private JPanelPad pnSistema = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );
+	
+	private JPanelPad pnNotas = new JPanelPad( JPanelPad.TP_JPANEL, new BorderLayout() );
 
-	private ImageIcon img = Icone.novo( Aplicativo.strSplash );
+	private ImageIcon img = Icone.novo( "freedom_real_520x358.png" );
 
 	private long lMemLivre;
 
@@ -67,9 +69,21 @@ public class FSobre extends FFDialogo {
 
 	private long lMemMaxima;
 
-	private JLabelPad labelEquipe = null;
+	private JLabelPad lbVersao = new JLabelPad();
 	
-	private JLabelPad labelSistema = new JLabelPad();
+	private JScrollPane spVersao = new JScrollPane(lbVersao);
+	
+	private JLabelPad lbEquipe = new JLabelPad();
+	
+	private JScrollPane spEquipe = new JScrollPane(lbEquipe);
+		
+	private JLabelPad lbSistema = new JLabelPad();
+	
+	private JScrollPane spSistema = new JScrollPane(lbSistema);
+	
+	private JLabelPad lbNotas = new JLabelPad();
+	
+	private JScrollPane spNotas = new JScrollPane(lbNotas);
 	
 	private Font font = SwingParams.getFontboldmed();
 	
@@ -78,133 +92,239 @@ public class FSobre extends FFDialogo {
 		super( Aplicativo.telaPrincipal );
 		
 		setTitulo( "Sobre" );
-		setAtribos( 330, 300 );
+		setAtribos( 520, 520 );
 		
 		setToFrameLayout();
-		
-		btMemoria.addActionListener( this );
+		adicListeners();
+
 		c.add( tpnSobre, BorderLayout.CENTER );
+
+		// Aba Sobre
+		
 		tpnSobre.addTab( "Sobre", pnSobre );
+		
+		pnSobre.setBackground(Color.BLACK);
+		lbVersao.setForeground(Color.WHITE);
+		lbVersao.setFont( font );
 
-		JLabelPad lbImg = new JLabelPad( img );
-		lbImg.setPreferredSize( new Dimension( img.getIconWidth(), img.getIconHeight() ) );
-		String sdatacompilacao = "";
+		pnSistema.setBackground(Color.BLACK);
+		lbSistema.setForeground(Color.WHITE);
+		lbSistema.setFont( font );
 
-		try {
-			
-			Date datacompilacao = SystemFunctions.getClassDateCompilation(this.getClass());	
-			
-			sdatacompilacao = Funcoes.dateToStrDataHora( datacompilacao ) ;
-			
-		} 
-		catch ( Exception err ) {
-			err.printStackTrace();
-		}
+		pnEquipe.setBackground(Color.BLACK);
+		lbEquipe.setForeground(Color.WHITE);
+		lbEquipe.setFont( font );
+		
+		pnNotas.setBackground(Color.BLACK);
+		lbNotas.setForeground(Color.WHITE);
+		lbNotas.setFont( font );
 
-		pnSobre.add( lbImg, BorderLayout.NORTH );
-		
-		StringBuilder htmlversao = new StringBuilder();
-		htmlversao.append("<HTML>");
-		
-		htmlversao.append("<BR>");
-		htmlversao.append(" Versão: ");
-		
-		htmlversao.append( SystemFunctions.getVersionSis(this.getClass()) );
-		
-		htmlversao.append("</BR>");	
+		JLabelPad lbImgSobre = new JLabelPad( img );
+		lbImgSobre.setPreferredSize( new Dimension( img.getIconWidth(), img.getIconHeight() ) );
 
-		htmlversao.append("<BR>");
-		htmlversao.append(" Compilação: ");	
-		
-		htmlversao.append( sdatacompilacao );
-		
-		htmlversao.append("</BR>");	
+		JLabelPad lbImgEquipe = new JLabelPad( img );
+		lbImgEquipe.setPreferredSize( new Dimension( img.getIconWidth(), img.getIconHeight() ) );
 
-		htmlversao.append("</HTML>");
-		 
-		JLabelPad pnversao = new JLabelPad(htmlversao.toString());
+		JLabelPad lbImgSistema = new JLabelPad( img );
+		lbImgSistema.setPreferredSize( new Dimension( img.getIconWidth(), img.getIconHeight() ) );
+
+		JLabelPad lbImgNotas = new JLabelPad( img );
+		lbImgNotas.setPreferredSize( new Dimension( img.getIconWidth(), img.getIconHeight() ) );
 		
-		pnSobre.add(pnversao, BorderLayout.CENTER);
+		pnSobre.add( lbImgSobre, BorderLayout.NORTH );
 		
+		lbVersao.setText(getHtmlVersao());
+		lbVersao.setFont(SwingParams.getFontboldmed());
+
+		pnSobre.add(spVersao, BorderLayout.CENTER);
+		
+		// Fim Aba Sobre
+		
+		
+		// Aba Equipe
+
+		
+		lbEquipe.setText(getHtmlEquipe());
+		lbEquipe.setFont( font );
+
 		tpnSobre.addTab( "Equipe", pnEquipe );
 		
-		StringBuffer sHtmlEquipe = new StringBuffer();
+		pnEquipe.add(lbImgEquipe, BorderLayout.NORTH);
+		pnEquipe.add(spEquipe, BorderLayout.CENTER);
 		
-		sHtmlEquipe.append( "<HTML>" );
-		sHtmlEquipe.append( "<TABLE width='300' border=\"1\">" );
+		// Fim Aba Equipe
+		
+		// Aba Sistema (Mémória)
+		
+		lbSistema.setText(getHtmlSistema());
+		
+		tpnSobre.addTab( "Sistema", pnSistema );
+
+		lbSistema.setFont( font );
+		
+		pnSistema.add(lbImgSistema, BorderLayout.NORTH);
+		pnSistema.add(spSistema, BorderLayout.CENTER);
+		
+		pnRodape.add(btReciclarMemoria);
+ 		
+		// Fim Aba Sistema
+
+		// Aba de notas da versão
+		
+		tpnSobre.addTab( "Notas da versão", pnNotas );
+		
+		pnNotas.add(lbImgNotas, BorderLayout.NORTH);
+		pnNotas.add(spNotas, BorderLayout.CENTER);
+		
+		String release_notes = SystemFunctions.getTxtFile("/home/anderson/workspace/freedom/doc/atualiza/v1.2.2.4_v1.2.2.5/", "release_notes.html");
+		
+		lbNotas.setText("<html>" + release_notes + "</html>");
+		lbNotas.setFont(new Font(SwingParams.FONT_PAD, SwingParams.FONT_STYLE_PAD, SwingParams.FONT_SIZE_MIN + 1));
+		
+		// Fim Aba Notas da versão
+		
+	}
+	
+	private void adicListeners() {
+		
+		btReciclarMemoria.addActionListener( this );
+		
+	}
+	
+	private String getHtmlEquipe() {
+		
+		StringBuffer html = new StringBuffer();
+		
+		html.append( "<HTML>" );
+		html.append( "<table style=\"width: 500;\" border=\"1\">");
 		
 		Vector<String> equipesis = Aplicativo.getEquipeSis();
 		
 		for ( int i = 0; equipesis.size() > i; i++ ) {
 			
-			sHtmlEquipe.append( "<TR><CENTER>" );
-			sHtmlEquipe.append( equipesis.elementAt( i ).toString() );
-			sHtmlEquipe.append( "</CENTER></TR>" );
+			html.append( "<TR><CENTER>" );
+			html.append( equipesis.elementAt( i ).toString() );
+			html.append( "</CENTER></TR>" );
 		}
 		
-		sHtmlEquipe.append( "</TABLE></HTML>" );
+		html.append( "</TABLE></HTML>" );
 		
-		labelEquipe = new JLabelPad( sHtmlEquipe.toString() );
-		labelEquipe.setFont( font );
+		return html.toString();
 
-		pnEquipe.add( new JScrollPane( labelEquipe ) , BorderLayout.CENTER );
+	}
+	
+	private String getHtmlVersao() {
+		String ret = "";
+		
+		try {
+			
+			String sdatacompilacao = "";
+			StringBuilder html = new StringBuilder();
+			
+			Date datacompilacao = SystemFunctions.getClassDateCompilation(this.getClass());	
+			
+			sdatacompilacao = Funcoes.dateToStrDataHora( datacompilacao ) ;
 
-		tpnSobre.addTab( "Sistema", pnSistema );
-
-		labelSistema.setFont( font );
-		labelSistema.setForeground( Color.BLUE );
-
-		pnSistema.add( labelSistema, BorderLayout.CENTER );
-		pnSistema.add( btMemoria, BorderLayout.SOUTH );
-
-		carregaInfoSis();
+			html.append("<HTML>");
+			html.append("<table style=\"width: 500;\" border=\"0\">");
+			html.append("<tr>");
+			html.append("<td>");
+					
+			html.append("Versão: ");
+			
+			html.append("</td>");
+			html.append("<td>");
+		
+			html.append( SystemFunctions.getVersionSis(this.getClass()) );
+			
+			html.append("</td>");
+			html.append("</tr>");
+			
+			html.append("<tr>");
+			html.append("<td>");
+			html.append("Compilação:");	
+			html.append("</td>");
+			html.append("<td>");
+			html.append( sdatacompilacao );
+			html.append("</td>");
+			
+			html.append("</tr>");	
+			
+			html.append("</table>");
+			
+			html.append("</HTML>");
+			
+			ret = html.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return ret;
 	}
 
-	public void carregaInfoSis() {
+	private void limpaMemoria() {
+		try {
+			Runtime.getRuntime().gc();
+			System.gc();
+			lbSistema.setText(getHtmlSistema());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String getHtmlSistema() {
+		String ret = "";
+		
+		try {
+			
+			lMemLivre = ( ( Runtime.getRuntime().freeMemory() / 1024 ) / 1024 );
+			lMemTotal = ( ( Runtime.getRuntime().totalMemory() / 1024 ) / 1024 );
+			lMemUtilizada = lMemTotal - lMemLivre;
+			lMemMaxima = ( ( Runtime.getRuntime().maxMemory() / 1024 ) / 1024 );
+			
+			StringBuffer html = new StringBuffer();
+			
+			html.append( "<HTML>" );
+			
+			html.append( "<table style=\"width: 500;\" border=\"1\">");
+			html.append( "<TR>" );
+			html.append( "<TD>Memória maxima:</TD>" );
+			html.append( "<TD>" + lMemMaxima + " MB" + "</TD>" );
+			html.append( "</TR>" );
+			html.append( "<TR>" );
+			html.append( "<TD>Memória total:</TD>" );
+			html.append( "<TD>" + lMemTotal + " MB" + "</TD>" );
+			html.append( "</TR>" );
+			html.append( "<TR>" );
+			html.append( "<TD>Memória utilizada:</TD>" );
+			html.append( "<TD>" + lMemUtilizada + " MB" + "</TD>" );
+			html.append( "</TR>" );
+			html.append( "<TR>" );
+			html.append( "<TD>Memória livre:</TD>" );
+			html.append( "<TD>" + lMemLivre + " MB" + "</TD>" );
+			html.append( "</TR>" );
+			html.append( "</CENTER></TABLE>" );
+			
+			html.append( "</HTML>" );
 
-		Runtime.getRuntime().gc();
-		System.gc();
+			ret = html.toString();
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		lMemLivre = ( ( Runtime.getRuntime().freeMemory() / 1024 ) / 1024 );
-		lMemTotal = ( ( Runtime.getRuntime().totalMemory() / 1024 ) / 1024 );
-		lMemUtilizada = lMemTotal - lMemLivre;
-		lMemMaxima = ( ( Runtime.getRuntime().maxMemory() / 1024 ) / 1024 );
+		return ret;
 		
-		StringBuffer buf = new StringBuffer();
-		
-		buf.append( "<HTML>" );
-		
-		buf.append( "<TABLE width='300' border=\"1\"><CENTER>" );
-		buf.append( "<TR>" );
-		buf.append( "<TD>Memória maxima:</TD>" );
-		buf.append( "<TD>" + lMemMaxima + " MB" + "</TD>" );
-		buf.append( "</TR>" );
-		buf.append( "<TR>" );
-		buf.append( "<TD>Memória total:</TD>" );
-		buf.append( "<TD>" + lMemTotal + " MB" + "</TD>" );
-		buf.append( "</TR>" );
-		buf.append( "<TR>" );
-		buf.append( "<TD>Memória utilizada:</TD>" );
-		buf.append( "<TD>" + lMemUtilizada + " MB" + "</TD>" );
-		buf.append( "</TR>" );
-		buf.append( "<TR>" );
-		buf.append( "<TD>Memória livre:</TD>" );
-		buf.append( "<TD>" + lMemLivre + " MB" + "</TD>" );
-		buf.append( "</TR>" );
-		buf.append( "</CENTER></TABLE>" );
-		
-		buf.append( "</HTML>" );
-		
-		labelSistema.setText( buf.toString() );	
-		
-		repaint();
 	}
 
 	public void actionPerformed( ActionEvent evt ) {
 		
-		if ( evt.getSource() == btMemoria ) {
+		if ( evt.getSource() == btReciclarMemoria ) {
 		
-			carregaInfoSis();
+			limpaMemoria();
 		}
 		
 		super.actionPerformed( evt );
