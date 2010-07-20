6681ff31086bd9de8e5447dd21c580bbbf77618f
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-07-20 15:49:45 +0000

    Alterações na formação do código fonte.

--- trunk/freedom-infra/src/test/TesteTransition.java
+++ trunk/freedom-infra/src/test/TesteTransition.java
@@ -1,4 +1,3 @@
-
 package test;
 
 import java.awt.BorderLayout;
@@ -20,9 +19,9 @@ public class TesteTransition extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JLayeredPane jContentPane = null;
-	
+
 	private Panel fadePanel = null;
-		
+
 	private FadeTransition transition;
 
 	/**
@@ -31,59 +30,59 @@ public class TesteTransition extends JFrame {
 	public TesteTransition() {
 
 		super();
-		
-		transition = new FadeTransition( true, getImagesDescanso(), 5, 4000, 0.05f );
-		
+
+		transition = new FadeTransition(true, getImagesDescanso(), 5, 4000, 0.05f);
+
 		initialize();
-		
+
 		transition.doStart();
 	}
 
 	private void initialize() {
 
-		this.setSize( 800, 600 );
-		this.setContentPane( getJContentPane() );
-		this.setTitle( "Fade Transition" );
-		
+		this.setSize(800, 600);
+		this.setContentPane(getJContentPane());
+		this.setTitle("Fade Transition");
+
 		fadePanel = transition.getFadePanel();
-		//fadePanel = new Panel();
-		fadePanel.setOpaque( true );
-		
-		jContentPane.add( fadePanel );
+		// fadePanel = new Panel();
+		fadePanel.setOpaque(true);
+
+		jContentPane.add(fadePanel);
 	}
 
 	private List<Image> getImagesDescanso() {
-		
+
 		List<Image> imagens = new ArrayList<Image>();
-		
+
 		try {
-			File subdir = new File( "C:\\opt\\eclipse\\workspace\\freedom\\src\\org\\freedom\\images" );
-			if ( subdir.exists() ) {
-				File[] imgs = subdir.listFiles( new FilenameFilter() {
-					public boolean accept( File dir, String name ) {
-						return name.endsWith( ".jpg" ) || name.endsWith( ".gif" );
+			File subdir = new File("C:\\opt\\eclipse\\workspace\\freedom\\src\\org\\freedom\\images");
+			if (subdir.exists()) {
+				File[] imgs = subdir.listFiles(new FilenameFilter() {
+					public boolean accept(File dir, String name) {
+						return name.endsWith(".jpg") || name.endsWith(".gif");
 					}
-				} );
-				int  i = 0;
-				for ( File f : imgs ) {
-					imagens.add( Toolkit.getDefaultToolkit().getImage( f.toString() ) );
+				});
+				int i = 0;
+				for (File f : imgs) {
+					imagens.add(Toolkit.getDefaultToolkit().getImage(f.toString()));
 					i++;
 				}
 			}
 		}
-		catch ( RuntimeException e ) {
+		catch (RuntimeException e) {
 			e.printStackTrace();
 		}
-		
+
 		return imagens;
 	}
 
 	private JLayeredPane getJContentPane() {
 
-		if ( jContentPane == null ) {
+		if (jContentPane == null) {
 			jContentPane = new JLayeredPane();
-			jContentPane.setLayout( new BorderLayout() );
-			jContentPane.setOpaque( true );
+			jContentPane.setLayout(new BorderLayout());
+			jContentPane.setOpaque(true);
 		}
 		return jContentPane;
 	}
