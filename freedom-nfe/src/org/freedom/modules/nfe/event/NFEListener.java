80992d5a7684e4d0f71638b71cf54863ffb510be
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-07-20 15:39:38 +0000

    Alterações na formação do código fonte.

--- freedom-nfe/src/org/freedom/modules/nfe/event/NFEListener.java
+++ freedom-nfe/src/org/freedom/modules/nfe/event/NFEListener.java
@@ -17,20 +17,21 @@ package org.freedom.modules.nfe.event;
 /**
  * Ouvinte de Evento de NF-e.<br>
  * 
- * Esta interface prepara a classe que a implementar, para receber ações do evento de NF-e.<br>
+ * Esta interface prepara a classe que a implementar, para receber ações do
+ * evento de NF-e.<br>
  * 
- * @see	org.freedom.modules.nfe.event.NFEEvent
+ * @see org.freedom.modules.nfe.event.NFEEvent
  * 
  * @author Setpoint Informática Ltda./Robson Sanchez
  * @version 03/08/2009
  */
 public interface NFEListener {
-	
-	public void beforeValidSend( NFEEvent e );
 
-	public void afterValidSend( NFEEvent e );
+	public void beforeValidSend(NFEEvent e);
 
-	public void beforeRunSend( NFEEvent e );
+	public void afterValidSend(NFEEvent e);
 
-	public void afterRunSend( NFEEvent e );
+	public void beforeRunSend(NFEEvent e);
+
+	public void afterRunSend(NFEEvent e);
 }
