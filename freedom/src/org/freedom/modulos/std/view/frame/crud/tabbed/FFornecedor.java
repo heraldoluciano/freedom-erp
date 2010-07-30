f02e1ceb51b79d8cecd445926b1c771076d67d57
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-07-30 17:49:18 +0000

    Implementações referentes a retencao de ir e inss.

--- freedom/src/org/freedom/modulos/std/view/frame/crud/tabbed/FFornecedor.java
+++ freedom/src/org/freedom/modulos/std/view/frame/crud/tabbed/FFornecedor.java
@@ -242,6 +242,7 @@ public class FFornecedor extends FTabDados implements RadioGroupListener, PostLi
 
 	private ListaCampos lcTipoFiscFor = new ListaCampos( this, "FF" );
 
+	
 	public FFornecedor() {
 
 		super();
@@ -406,7 +407,7 @@ public class FFornecedor extends FTabDados implements RadioGroupListener, PostLi
 		adicTab( "Complemento", pinComplemento );
 
 		adicCampo( txtSuframaFor, 7, 20, 137, 20, "suframafor", "SUFRAMA", ListaCampos.DB_SI, false );
-
+		
 		// Painel Observações
 		adicTab( "Observações", panelObservacao );
 		adicDBLiv( txaObs, "ObsFor", "Observações", false );
