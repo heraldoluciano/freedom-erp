ab44b649706dd70cd1b9a15673c6282a53c33fcd
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-08-06 21:47:02 +0000

    Retirada de import desnecessário.

--- trunk/freedom/src/org/freedom/modulos/std/view/frame/crud/special/FManutConFrete.java
+++ trunk/freedom/src/org/freedom/modulos/std/view/frame/crud/special/FManutConFrete.java
@@ -32,23 +32,6 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.math.BigDecimal;
-import org.freedom.infra.model.jdbc.DbConnection;
-import org.freedom.library.functions.Funcoes;
-import org.freedom.library.persistence.GuardaCampo;
-import org.freedom.library.persistence.ListaCampos;
-import org.freedom.library.swing.component.JButtonPad;
-import org.freedom.library.swing.component.JCheckBoxPad;
-import org.freedom.library.swing.component.JLabelPad;
-import org.freedom.library.swing.component.JPanelPad;
-import org.freedom.library.swing.component.JTablePad;
-import org.freedom.library.swing.component.JTextFieldFK;
-import org.freedom.library.swing.component.JTextFieldPad;
-import org.freedom.library.swing.frame.Aplicativo;
-import org.freedom.library.swing.frame.FFilho;
-import org.freedom.modulos.fnc.view.dialog.utility.DLNovoPag;
-import org.freedom.modulos.std.view.frame.crud.tabbed.FFornecedor;
-import org.freedom.modulos.std.view.frame.crud.tabbed.FTransp;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -66,6 +49,21 @@ import javax.swing.event.CellEditorListener;
 import javax.swing.event.ChangeEvent;
 
 import org.freedom.bmps.Icone;
+import org.freedom.infra.model.jdbc.DbConnection;
+import org.freedom.library.functions.Funcoes;
+import org.freedom.library.persistence.GuardaCampo;
+import org.freedom.library.persistence.ListaCampos;
+import org.freedom.library.swing.component.JButtonPad;
+import org.freedom.library.swing.component.JCheckBoxPad;
+import org.freedom.library.swing.component.JLabelPad;
+import org.freedom.library.swing.component.JPanelPad;
+import org.freedom.library.swing.component.JTablePad;
+import org.freedom.library.swing.component.JTextFieldFK;
+import org.freedom.library.swing.component.JTextFieldPad;
+import org.freedom.library.swing.frame.Aplicativo;
+import org.freedom.library.swing.frame.FFilho;
+import org.freedom.modulos.fnc.view.dialog.utility.DLNovoPag;
+import org.freedom.modulos.std.view.frame.crud.tabbed.FTransp;
 
 public class FManutConFrete extends FFilho implements ActionListener {
 
