7fa7077b9c8b0031e2c5f388d3fb061f3b72c53f
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-08-13 21:50:10 +0000

    Inclusão de informações no recibo.

--- trunk/freedom/src/org/freedom/modulos/fnc/view/dialog/report/DLImpReciboPag.java
+++ trunk/freedom/src/org/freedom/modulos/fnc/view/dialog/report/DLImpReciboPag.java
@@ -143,7 +143,8 @@ public class DLImpReciboPag extends FDialogo {
 		sSQL.append( "null codorc, null nomeconv, ip.obsitpag obsorc, p.codcompra docvenda, p.codcompra codvenda, p.codpag reciboitrec, null nomevend, null nomevend2, null nomevend3, null nomevend4, " );
 		
 		if(txtNumCheque.getVlrInteger()>0) {
-			sSQL.append( "(select first 1 ch.vlrcheq from fncheque ch where ch.contacheq=ip.numconta and ch.numcheq=" + txtNumCheque.getVlrInteger() + ") vlrpagoitrec ");	
+			sSQL.append( "(select first 1 ch.vlrcheq from fncheque ch where ch.contacheq=ip.numconta and ch.numcheq=" + txtNumCheque.getVlrInteger() + ") vlrpagoitrec, ");	
+			sSQL.append( "(select first 1 ch.numcheq from fncheque ch where ch.contacheq=ip.numconta and ch.numcheq=" + txtNumCheque.getVlrInteger() + ") numcheq ");
 		}
 		else {
 		
