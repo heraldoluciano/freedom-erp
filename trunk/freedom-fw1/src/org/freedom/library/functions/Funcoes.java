6744f0f4c6825b9995a353ccf473c688ecdb09a2
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-08-17 14:36:10 +0000

    Mudança de package para função de conversão.

--- trunk/freedom-fw1/src/org/freedom/library/functions/Funcoes.java
+++ trunk/freedom-fw1/src/org/freedom/library/functions/Funcoes.java
@@ -1970,16 +1970,8 @@ public class Funcoes {
 	}
 
 	public static String dateToStrDate(Date dVal) {
-		String ret = "";
-		if (dVal != null) {
-			GregorianCalendar cal = new GregorianCalendar();
-			cal.setTime(dVal);
-			int iAno = cal.get(Calendar.YEAR);
-			int iMes = cal.get(Calendar.MONTH) + 1;
-			int iDia = cal.get(Calendar.DAY_OF_MONTH);
-			ret = StringFunctions.strZero(String.valueOf(iDia), 2) + "/" + StringFunctions.strZero(String.valueOf(iMes), 2) + "/" + String.valueOf(iAno);
-		}
-		return ret;
+		
+		return ConversionFunctions.dateToStrDate(dVal);
 
 	}
 
