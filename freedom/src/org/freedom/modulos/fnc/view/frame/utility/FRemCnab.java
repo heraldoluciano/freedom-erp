ef1d27d6a7be0fbdb2651941875f2450b662aba2
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-08-20 20:23:07 +0000

    Atualização CNAB 400 - BANCO DO BRASIL - ALan

--- freedom/src/org/freedom/modulos/fnc/view/frame/utility/FRemCnab.java
+++ freedom/src/org/freedom/modulos/fnc/view/frame/utility/FRemCnab.java
@@ -324,7 +324,7 @@ public class FRemCnab extends FRemFBN {
 		}
 
 		reg.setRazCli( dadosCliente[ DadosCliente.RAZCLI.ordinal() ] );
-		reg.setEndCli( dadosCliente[ DadosCliente.ENDCLI.ordinal() ].trim() + ( dadosCliente[ DadosCliente.NUMCLI.ordinal() ] == null ? " " : ( ", " + dadosCliente[ DadosCliente.NUMCLI.ordinal() ] ) ) + "-" + dadosCliente[ DadosCliente.BAIRCLI.ordinal() ] );
+		reg.setEndCli( dadosCliente[ DadosCliente.ENDCLI.ordinal() ].trim() + ( dadosCliente[ DadosCliente.NUMCLI.ordinal() ] == null ? " " : ( ", " + dadosCliente[ DadosCliente.NUMCLI.ordinal() ].trim() ) ) + "-" + dadosCliente[ DadosCliente.BAIRCLI.ordinal() ].trim() );
 
 		// reg.setBairCli( dadosCliente[ DadosCliente.BAIRCLI.ordinal() ] );
 		reg.setCepCli( dadosCliente[ DadosCliente.CEPCLI.ordinal() ] );
