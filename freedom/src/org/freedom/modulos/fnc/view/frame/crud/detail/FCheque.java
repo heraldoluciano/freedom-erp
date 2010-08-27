adb42d15aa6c63395697ba40fa383b2eacf97922
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-08-27 21:46:20 +0000

    Implementações para impressão do cheque através da tela de cadastro.

--- freedom/src/org/freedom/modulos/fnc/view/frame/crud/detail/FCheque.java
+++ freedom/src/org/freedom/modulos/fnc/view/frame/crud/detail/FCheque.java
@@ -14,13 +14,11 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.Map;
+import java.util.Vector;
 
 import javax.swing.SwingConstants;
-
-import net.sf.jasperreports.engine.JasperPrintManager;
 
 import org.freedom.acao.CarregaEvent;
 import org.freedom.acao.CarregaListener;
@@ -33,7 +31,7 @@ import org.freedom.acao.JComboBoxListener;
 import org.freedom.acao.PostEvent;
 import org.freedom.bmps.Icone;
 import org.freedom.infra.model.jdbc.DbConnection;
-import org.freedom.library.functions.Funcoes;
+import org.freedom.library.component.ImprimeOS;
 import org.freedom.library.persistence.GuardaCampo;
 import org.freedom.library.persistence.ListaCampos;
 import org.freedom.library.swing.component.JButtonPad;
@@ -47,7 +45,6 @@ import org.freedom.library.swing.component.JTextFieldPad;
 import org.freedom.library.swing.component.Navegador;
 import org.freedom.library.swing.frame.Aplicativo;
 import org.freedom.library.swing.frame.FDetalhe;
-import org.freedom.library.swing.frame.FPrinterJob;
 import org.freedom.library.swing.util.SwingParams;
 import org.freedom.modulos.fnc.business.object.Cheque;
 import org.freedom.modulos.fnc.view.frame.crud.tabbed.FConta;
@@ -72,33 +69,33 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 	private JPanelPad panelDetalhe = new JPanelPad();
 
 	private JTextFieldPad txtSeqCheq = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
-	
+
 	private JTextFieldPad txtCodBanc = new JTextFieldPad( JTextFieldPad.TP_STRING, 3, 0 );
 
 	private JTextFieldPad txtContaCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 10, 0 );
-	
+
 	private JTextFieldPad txtAgenciaCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 7, 0 );
 
 	private JTextFieldFK txtDescConta = new JTextFieldFK( JTextFieldPad.TP_STRING, 50, 0 );
 
 	private JTextFieldPad txtDtEmitCheq = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
-	
+
 	private JTextFieldPad txtDtVenctoCheq = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
-	
+
 	private JTextFieldPad txtDtCompCheq = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
 
 	private JTextFieldPad txtNomeEmitCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
-	
+
 	private JTextFieldPad txtNomeFavCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 50, 0 );
-	
+
 	private JTextFieldPad txtNumCheq = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
-	
+
 	private JTextAreaPad txaHistCheq = new JTextAreaPad( 500 );
 
 	private JButtonPad btCompletar = new JButtonPad( Icone.novo( "btOk.gif" ) );
 
 	private JTextFieldFK txtCodPag = new JTextFieldFK( JTextFieldPad.TP_INTEGER, 8, 0 );
-	
+
 	private JTextFieldFK txtDocPag = new JTextFieldFK( JTextFieldPad.TP_INTEGER, 8, 0 );
 
 	private JTextFieldFK txtNParcPag = new JTextFieldFK( JTextFieldPad.TP_INTEGER, 8, 0 );
@@ -108,7 +105,7 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 	private JTextFieldFK txtVlrApagItRec = new JTextFieldFK( JTextFieldPad.TP_DECIMAL, 15, Aplicativo.casasDecFin );
 
 	private JTextFieldFK txtVlrPagoItRec = new JTextFieldFK( JTextFieldPad.TP_DECIMAL, 15, Aplicativo.casasDecFin );
-	
+
 	private JTextFieldPad txtVlrCheq = new JTextFieldPad( JTextFieldPad.TP_DECIMAL, 15, Aplicativo.casasDecFin );
 
 	private JTextFieldFK txtDtPagoItPag = new JTextFieldFK( JTextFieldPad.TP_DATE, 10, 0 );
@@ -116,7 +113,7 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 	private JTextFieldFK txtDtVencItPag = new JTextFieldFK( JTextFieldPad.TP_DATE, 10, 0 );
 
 	private JTextFieldPad txtStatusItPag = new JTextFieldPad( JTextFieldPad.TP_STRING, 2, 0 );
-	
+
 	private JTextFieldPad txtCnpjEmitCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 14, 0 );
 
 	private JTextFieldPad txtCpfEmitCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 11, 0 );
@@ -124,19 +121,19 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 	private JTextFieldPad txtCnpjFavCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 14, 0 );
 
 	private JTextFieldPad txtCpfFavCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 11, 0 );
-	
+
 	private JTextFieldPad txtDDDEmitCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 4, 0 );
 
 	private JTextFieldPad txtDDDFavCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 12, 0 );
 
 	private JTextFieldPad txtFoneEmitCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 8, 0 );
-	
+
 	private JTextFieldPad txtFoneFavCheq = new JTextFieldPad( JTextFieldPad.TP_STRING, 8, 0 );
-	
+
 	private JCheckBoxPad cbPreDatCheq = new JCheckBoxPad( "Pré-datado", "Sim", "Não" );
-	
+
 	private JComboBoxPad cbTipoCheq = null;
-	
+
 	private JComboBoxPad cbSitCheq = null;
 
 	private JLabelPad lbStatus = new JLabelPad( "", SwingConstants.CENTER );
@@ -146,7 +143,7 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 	private ListaCampos lcPlan = new ListaCampos( this, "PN" );
 
 	private ListaCampos lcItPagar = new ListaCampos( this, "" );
-	
+
 	private ListaCampos lcPagar = new ListaCampos( this, "" );
 
 	public FCheque() {
@@ -174,30 +171,29 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 		btPrevimp.addActionListener( this );
 		cbTipoCheq.addComboBoxListener( this );
 
-		
 	}
-	
+
 	private void montaCombos() {
+
 		montaCbTipoCheque();
 		montaCbSitCheque();
 	}
-	
-	private void montaCbTipoCheque( ) {
+
+	private void montaCbTipoCheque() {
 
 		cbTipoCheq.limpa();
 
 		cbTipoCheq.setItens( Cheque.getLabelsTipoCheq(), Cheque.getValoresTipoCheq() );
 
 	}
-	
-	private void montaCbSitCheque( ) {
+
+	private void montaCbSitCheque() {
 
 		cbSitCheq.limpa();
 
 		cbSitCheq.setItens( Cheque.getLabelsSitCheq(), Cheque.getValoresSitCheq() );
 
 	}
-
 
 	private void montaListaCampos() {
 
@@ -209,11 +205,8 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 		txtContaCheq.setTabelaExterna( lcConta, FConta.class.getCanonicalName() );
 		txtContaCheq.setFK( true );
 
-	
-
-		
 		// Itens de contas a pagar
-		
+
 		lcItPagar.add( new GuardaCampo( txtCodPag, "CodPag", "Cód.Pag.", ListaCampos.DB_PK, false ) );
 		lcItPagar.add( new GuardaCampo( txtNParcPag, "NParcPag", "Cód.It.pag.", ListaCampos.DB_PK, false ) );
 		lcItPagar.add( new GuardaCampo( txtDtVencItPag, "DtVencItPag", "Vencimento", ListaCampos.DB_SI, false ) );
@@ -234,9 +227,9 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 		txtNParcPag.setTabelaExterna( lcItPagar, null );
 		txtNParcPag.setFK( true );
 		txtNParcPag.setNomeCampo( "NParcPag" );
-		
-		//Contas a pagar
-		
+
+		// Contas a pagar
+
 		lcPagar.add( new GuardaCampo( txtCodPag, "CodPag", "Cód.Pag.", ListaCampos.DB_PK, txtDocPag, false ) );
 		lcPagar.add( new GuardaCampo( txtDocPag, "DocPag", "Documento", ListaCampos.DB_SI, false ) );
 		lcPagar.montaSql( false, "PAGAR", "FN" );
@@ -245,33 +238,33 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 		txtCodPag.setTabelaExterna( lcPagar, null );
 		txtCodPag.setFK( true );
 		txtCodPag.setNomeCampo( "CodPag" );
-		
+
 	}
 
 	private void montaTela() {
 
 		txtCpfEmitCheq.setMascara( JTextFieldPad.MC_CPF );
 		txtCnpjEmitCheq.setMascara( JTextFieldPad.MC_CNPJ );
-		
+
 		txtCpfFavCheq.setMascara( JTextFieldPad.MC_CPF );
 		txtCnpjFavCheq.setMascara( JTextFieldPad.MC_CNPJ );
-		
+
 		txtFoneEmitCheq.setMascara( JTextFieldPad.MC_FONE );
 		txtFoneFavCheq.setMascara( JTextFieldPad.MC_FONE );
-		
+
 		cbTipoCheq = new JComboBoxPad( null, null, JComboBoxPad.TP_STRING, 2, 0 );
 		cbSitCheq = new JComboBoxPad( null, null, JComboBoxPad.TP_STRING, 2, 0 );
-		
+
 		setAltCab( 260 );
 		setListaCampos( lcCampos );
 		setPainel( panelMaster, pnCliCab );
 
 		JPanelPad pnInfoCheque = new JPanelPad();
 		pnInfoCheque.setBorder( SwingParams.getPanelLabel( "Informações do cheque", Color.BLACK ) );
-		
+
 		JPanelPad pnDatas = new JPanelPad();
 		pnDatas.setBorder( SwingParams.getPanelLabel( "Datas", Color.BLUE ) );
-		
+
 		JPanelPad pnEmitente = new JPanelPad();
 		pnEmitente.setBorder( SwingParams.getPanelLabel( "Informações do emitente", Color.RED ) );
 
@@ -282,53 +275,50 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 		adic( pnDatas, 563, 70, 105, 150 );
 		adic( pnEmitente, 5, 70, 555, 74 );
 		adic( pnFavorecido, 5, 144, 555, 74 );
-		
+
 		/** INFORMACOES DO CHEQUE **/
 		setPainel( pnInfoCheque );
-		
+
 		adicCampo( txtSeqCheq, 7, 15, 40, 20, "seqcheq", "Seq.", ListaCampos.DB_PK, true );
 		adicCampo( txtCodBanc, 50, 15, 40, 20, "codbanc", "Banco", ListaCampos.DB_SI, true );
 		adicCampo( txtContaCheq, 93, 15, 70, 20, "Contacheq", "Nº Conta", ListaCampos.DB_FK, txtDescConta, true );
 		adicCampo( txtNumCheq, 166, 15, 60, 20, "Numcheq", "Nro.Cheq.", ListaCampos.DB_SI, true );
-		adicCampo( txtVlrCheq, 229, 15, 75, 20, "VlrCheq", "Valor", ListaCampos.DB_SI, true );		
-		adicDB( cbTipoCheq, 307, 15, 125, 20, "tipocheq", "Tipo", false );		
+		adicCampo( txtVlrCheq, 229, 15, 75, 20, "VlrCheq", "Valor", ListaCampos.DB_SI, true );
+		adicDB( cbTipoCheq, 307, 15, 125, 20, "tipocheq", "Tipo", false );
 		adicDB( cbSitCheq, 435, 15, 112, 20, "sitcheq", "Status", false );
-		
+
 		adicDB( cbPreDatCheq, 558, 15, 86, 20, "predatcheq", "", true );
 
-		
 		/** INFORMACOES DE DATAS **/
 		setPainel( pnDatas );
 
 		adicCampo( txtDtEmitCheq, 4, 15, 85, 20, "DtEmitCheq", "Emissão", ListaCampos.DB_SI, true );
 		adicCampo( txtDtVenctoCheq, 4, 55, 85, 20, "DtVenctoCheq", "Vencimento", ListaCampos.DB_SI, true );
-		adicCampo( txtDtCompCheq, 4, 95, 85, 20, "DtCompCheq", "Compensação", ListaCampos.DB_SI, true );		
+		adicCampo( txtDtCompCheq, 4, 95, 85, 20, "DtCompCheq", "Compensação", ListaCampos.DB_SI, true );
 
 		/** INFORMACOES DO EMITENTE **/
 		setPainel( pnEmitente );
-		
+
 		adicCampo( txtNomeEmitCheq, 7, 15, 200, 20, "nomeemitcheq", "Nome", ListaCampos.DB_SI, true );
 		adicCampo( txtCnpjEmitCheq, 210, 15, 120, 20, "cnpjemitcheq", "CNPJ", ListaCampos.DB_SI, false );
 		adicCampo( txtCpfEmitCheq, 333, 15, 100, 20, "cpfemitcheq", "CPF", ListaCampos.DB_SI, false );
 		adicCampo( txtDDDEmitCheq, 436, 15, 30, 20, "dddemitcheq", "DDD", ListaCampos.DB_SI, false );
 		adicCampo( txtFoneEmitCheq, 469, 15, 68, 20, "foneemitcheq", "Fone", ListaCampos.DB_SI, false );
-		
-		
+
 		/** INFORMACOES DO FAVORECIDO **/
 		setPainel( pnFavorecido );
-		
+
 		adicCampo( txtNomeFavCheq, 7, 15, 200, 20, "nomeFavcheq", "Nome", ListaCampos.DB_SI, true );
 		adicCampo( txtCnpjFavCheq, 210, 15, 120, 20, "cnpjFavcheq", "CNPJ", ListaCampos.DB_SI, false );
 		adicCampo( txtCpfFavCheq, 333, 15, 100, 20, "cpfFavcheq", "CPF", ListaCampos.DB_SI, false );
 		adicCampo( txtDDDFavCheq, 436, 15, 30, 20, "dddFavcheq", "DDD", ListaCampos.DB_SI, false );
 		adicCampo( txtFoneFavCheq, 469, 15, 68, 20, "foneFavcheq", "Fone", ListaCampos.DB_SI, false );
 
-		
 		setPainel( panelMaster, pnCliCab );
-		
-//		adicDB( txaHistCheq, 7, 100, 539, 60, "histcheq", "Histórico", false );
 
-		setListaCampos( true, "CHEQUE", "FN" ); 
+		// adicDB( txaHistCheq, 7, 100, 539, 60, "histcheq", "Histórico", false );
+
+		setListaCampos( true, "CHEQUE", "FN" );
 		lcCampos.setQueryInsert( false );
 
 		setAltDet( 60 );
@@ -337,19 +327,18 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 		setNavegador( navRod );
 
 		adicCampo( txtCodPag, 7, 20, 50, 20, "CodPag", "Cód.pag.", ListaCampos.DB_PK, false );
-		adicDescFK( txtDocPag, 60, 20, 70, 20, "docpag", "Documento" );		
+		adicDescFK( txtDocPag, 60, 20, 70, 20, "docpag", "Documento" );
 		adicCampo( txtNParcPag, 133, 20, 50, 20, "NParcPag", "Parcela", ListaCampos.DB_PF, false );
-		
+
 		adicDescFK( txtDtVencItPag, 186, 20, 75, 20, "DtVencItPag", "Vencimento" );
 		adicDescFK( txtDtPagoItPag, 264, 20, 75, 20, "DtPagoItPag", "Dt.Pagto." );
-		
+
 		adicDescFK( txtVlrParcItRec, 342, 20, 70, 20, "VlrParcItRec", "Vlr.titulo" );
 		adicDescFK( txtVlrPagoItRec, 415, 20, 70, 20, "VlrPagoItRec", "Vlr.Pago" );
 		adicDescFK( txtVlrApagItRec, 488, 20, 70, 20, "VlrApagItRec", "Vlr.Aberto" );
 
 		adic( lbStatus, 561, 20, 103, 20 );
-		
-		
+
 		setListaCampos( true, "PAGCHEQ", "FN" );
 		lcDet.setQueryInsert( false );
 
@@ -405,86 +394,37 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 		else {
 			lbStatus.setVisible( false );
 		}
-		
+
 	}
 
-	private void concluiBordero() {
+	private synchronized void imprimir( boolean visualizar ) {
 
-		if ( txtSeqCheq.getVlrInteger() <= 0 ) {
-			return;
-		}
+		PreparedStatement ps = null;
 
-		try {
-			PreparedStatement ps = con.prepareStatement( "UPDATE FNBORDERO SET STATUSBOR='BC' WHERE CODEMP=? AND CODFILIAL=? AND CODBOR=?" );
-			ps.setInt( 1, Aplicativo.iCodEmp );
-			ps.setInt( 2, ListaCampos.getMasterFilial( "FNBORDERO" ) );
-			ps.setInt( 3, txtSeqCheq.getVlrInteger() );
+		ImprimeOS imp = new ImprimeOS( "", Aplicativo.getInstace().getConexao(), "CH", true );
 
-			ps.executeUpdate();
-			con.commit();
-		} catch ( SQLException e ) {
-			e.printStackTrace();
-		}
-	}
+		imp.setImpEject( false );
+		Vector<Object> item = null;
+		Map<String, Object> itemMap = null;
 
-	private void imprimir( boolean visualizar ) {
+		// imprime comprimido na coluna 2 para utiliar a linha 0
+		imp.say( 0, 2, imp.comprimido() );
 
-		if ( txtSeqCheq.getVlrInteger() == 0 ) {
-			return;
-		}
+		Cheque cheque = new Cheque( txtSeqCheq.getVlrInteger() );
 
-		StringBuilder sql = new StringBuilder();
-		sql.append( "SELECT CL.CPFCLI,CL.CNPJCLI,CL.FONECLI, B.CODBOR, B.NUMCONTA, C.DESCCONTA, B.NUMCONTABOR, C2.DESCCONTA DESCCONTABOR, B.DTBOR, B.OBSBOR," );
-		sql.append( "IB.CODREC, IB.NPARCITREC, R.CODCLI, CL.RAZCLI, IR.VLRITREC, IR.DTVENCITREC,R.DOCREC,C.AGENCIACONTA " );
-		sql.append( "FROM FNBORDERO B, FNITBORDERO IB, FNCONTA C, FNCONTA C2, " );
-		sql.append( "FNITRECEBER IR, FNRECEBER R, VDCLIENTE CL " );
-		sql.append( "WHERE B.CODEMP=? AND B.CODFILIAL=? AND B.CODBOR=? AND " );
-		sql.append( "C.CODEMP=B.CODEMPCC AND C.CODFILIAL=B.CODFILIALCC AND C.NUMCONTA=B.NUMCONTA AND " );
-		sql.append( "C2.CODEMP=B.CODEMPCB AND C2.CODFILIAL=B.CODFILIALCB AND C2.NUMCONTA=B.NUMCONTABOR AND " );
-		sql.append( "IB.CODEMP=B.CODEMP AND IB.CODFILIAL=B.CODFILIAL AND IB.CODBOR=B.CODBOR AND " );
-		sql.append( "IR.CODEMP=IB.CODEMPRC AND IR.CODFILIAL=IB.CODFILIALRC AND IR.CODREC=IB.CODREC AND IR.NPARCITREC=IB.NPARCITREC AND " );
-		sql.append( "R.CODEMP=IR.CODEMP AND R.CODFILIAL=IR.CODFILIAL AND R.CODREC=IR.CODREC AND " );
-		sql.append( "CL.CODEMP=R.CODEMPCL AND CL.CODFILIAL=R.CODFILIALCL AND CL.CODCLI=R.CODCLI " );
-		sql.append( "ORDER BY IB.NPARCITREC" );
+		itemMap = cheque.montaMap() ;
 
-		ResultSet rs = null;
+		cheque.montaLayoutCheq( imp, itemMap );
 
-		try {
-			PreparedStatement ps = con.prepareStatement( sql.toString() );
-			ps.setInt( 1, Aplicativo.iCodEmp );
-			ps.setInt( 2, ListaCampos.getMasterFilial( "FNBORDERO" ) );
-			ps.setInt( 3, txtSeqCheq.getVlrInteger() );
-
-			rs = ps.executeQuery();
-
-			FPrinterJob dlGr = new FPrinterJob( "relatorios/FBordero.jasper", "Bordero de recebíveis", null, rs, null, this );
-
-			if ( visualizar ) {
-				dlGr.setVisible( true );
-			}
-			else {
-				try {
-					JasperPrintManager.printReport( dlGr.getRelatorio(), true );
-				} catch ( Exception e ) {
-					e.printStackTrace();
-					Funcoes.mensagemErro( this, "Erro na impressão!" + e.getMessage(), true, con, e );
-				}
-			}
-
-			con.commit();
-		} catch ( SQLException e ) {
-			e.printStackTrace();
-			Funcoes.mensagemErro( this, "Erro na pesquisa!" + e.getMessage(), true, con, e );
+		if ( visualizar ) {
+			imp.preview( this );
 		}
 	}
 
 	@ Override
 	public void actionPerformed( ActionEvent e ) {
 
-		if ( e.getSource() == btCompletar ) {
-			concluiBordero();
-		}
-		else if ( e.getSource() == btImp ) {
+		if ( e.getSource() == btImp ) {
 			imprimir( false );
 		}
 		else if ( e.getSource() == btPrevimp ) {
@@ -502,12 +442,11 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 	public void afterCarrega( CarregaEvent e ) {
 
 		showStatus();
-		
-		if(e.getListaCampos() == lcDet) {
+
+		if ( e.getListaCampos() == lcDet ) {
 			lcPagar.carregaDados();
 		}
-		
-		
+
 	}
 
 	public void beforeInsert( InsertEvent e ) {
@@ -538,16 +477,16 @@ public class FCheque extends FDetalhe implements CarregaListener, InsertListener
 	public void setConexao( DbConnection con ) {
 
 		super.setConexao( con );
-		
+
 		lcConta.setConexao( con );
 		lcItPagar.setConexao( con );
 		lcPagar.setConexao( con );
-		
+
 	}
 
 	public void valorAlterado( JComboBoxEvent evt ) {
 
 		// TODO Auto-generated method stub
-		
+
 	}
 }
