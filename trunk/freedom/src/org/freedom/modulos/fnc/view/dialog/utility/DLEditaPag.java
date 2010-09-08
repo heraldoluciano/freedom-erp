e9c69d71fc52c424435cd274da46656e9cb3605a
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-09-08 19:48:27 +0000

    Implementação de mecanismo para exibição dos cheques vinculados.

--- trunk/freedom/src/org/freedom/modulos/fnc/view/dialog/utility/DLEditaPag.java
+++ trunk/freedom/src/org/freedom/modulos/fnc/view/dialog/utility/DLEditaPag.java
@@ -23,24 +23,33 @@
 
 package org.freedom.modulos.fnc.view.dialog.utility;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JScrollPane;
+
+import org.freedom.acao.CarregaEvent;
+import org.freedom.acao.CarregaListener;
 import org.freedom.infra.model.jdbc.DbConnection;
 import org.freedom.library.functions.Funcoes;
 import org.freedom.library.persistence.GuardaCampo;
 import org.freedom.library.persistence.ListaCampos;
 import org.freedom.library.swing.component.JLabelPad;
+import org.freedom.library.swing.component.JPanelPad;
+import org.freedom.library.swing.component.JTabbedPanePad;
+import org.freedom.library.swing.component.JTablePad;
 import org.freedom.library.swing.component.JTextFieldFK;
 import org.freedom.library.swing.component.JTextFieldPad;
 import org.freedom.library.swing.dialog.FFDialogo;
 import org.freedom.library.swing.frame.Aplicativo;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.freedom.acao.CarregaEvent;
-import org.freedom.acao.CarregaListener;
+import org.freedom.modulos.fnc.view.frame.crud.detail.FCheque;
 
 public class DLEditaPag extends FFDialogo implements CarregaListener {
 
@@ -96,14 +105,35 @@ public class DLEditaPag extends FFDialogo implements CarregaListener {
 
 	private final ListaCampos lcTipoCob = new ListaCampos( this, "TC" );
 
+	private JTabbedPanePad tpn = new JTabbedPanePad();
+
+	private JPanelPad pnGeral = new JPanelPad();
+
+	private JPanelPad pnCheques = new JPanelPad( new BorderLayout() );
+
+	private JTablePad tabCheques = new JTablePad();
+
+	private JScrollPane spnCheques = new JScrollPane( tabCheques );
+
+	private JTextFieldPad txtCodPag = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+
+	private JTextFieldPad txtNParcPag = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+
+	private enum enum_grid_cheques {
+		SEQCHEQ, NUMCHEQ, DTEMITCHEQ, DTVENCTOCHEQ, VLRCHEQ, SITCHEQ
+	};
+
 	public DLEditaPag( Component cOrig, boolean edita ) {
 
 		super( cOrig );
-		setTitulo( "Editar" );
-		setAtribos( 360, 420 );
+
+		setTitulo( "Edição de contas a pagar" );
+		setAtribos( 368, 445 );
 
 		montaListaCampos();
 		montaTela();
+		montaGridCheques();
+
 	}
 
 	private void montaListaCampos() {
@@ -163,47 +193,64 @@ public class DLEditaPag extends FFDialogo implements CarregaListener {
 		txtDescPlan.setAtivo( false );
 		txtDtEmis.setAtivo( false );
 
-		adic( new JLabelPad( "Cód.for." ), 7, 0, 250, 20 );
-		adic( txtCodFor, 7, 20, 80, 20 );
-		adic( new JLabelPad( "Razão social do fornecedor" ), 90, 0, 250, 20 );
-		adic( txtRazFor, 90, 20, 250, 20 );
-		adic( new JLabelPad( "Nº conta" ), 7, 40, 250, 20 );
-		adic( txtCodConta, 7, 60, 80, 20 );
-		adic( new JLabelPad( "Descrição da conta" ), 90, 40, 250, 20 );
-		adic( txtDescConta, 90, 60, 250, 20 );
-		adic( new JLabelPad( "Cód.catg." ), 7, 80, 250, 20 );
-		adic( txtCodPlan, 7, 100, 100, 20 );
-		adic( new JLabelPad( "Descrição da categoria" ), 110, 80, 250, 20 );
-		adic( txtDescPlan, 110, 100, 230, 20 );
-		adic( new JLabelPad( "Cód.c.c." ), 7, 120, 250, 20 );
-		adic( txtCodCC, 7, 140, 100, 20 );
-		adic( new JLabelPad( "Descrição do centro de custo" ), 110, 120, 250, 20 );
-		adic( txtDescCC, 110, 140, 230, 20 );
-		adic( new JLabelPad( "Cod.Tp.Cob" ), 7, 160, 80, 20 );
-		adic( txtCodTipoCob, 7, 180, 80, 20 );
-		adic( new JLabelPad( "Descrição do tipo de cobrança" ), 90, 160, 250, 20 );
-		adic( txtDescTipoCob, 90, 180, 250, 20 );
+		Container c = getContentPane();
+		c.setLayout( new BorderLayout() );
+		c.add( pnBordRodape, BorderLayout.SOUTH );
+		c.add( tpn, BorderLayout.CENTER );
 
-		adic( new JLabelPad( "Doc." ), 7, 200, 81, 20 );
-		adic( txtDoc, 7, 220, 81, 20 );
-		adic( new JLabelPad( "Emissão" ), 91, 200, 81, 20 );
-		adic( txtDtEmis, 91, 220, 81, 20 );
-		adic( new JLabelPad( "Vencimento" ), 175, 200, 81, 20 );
-		adic( txtDtVenc, 175, 220, 81, 20 );
-		adic( new JLabelPad( "Vlr.parcela" ), 259, 200, 81, 20 );
-		adic( txtVlrParc, 259, 220, 81, 20 );
+		tpn.addTab( "Informações gerais", pnGeral );
 
-		adic( new JLabelPad( "Vlr.desc." ), 7, 240, 81, 20 );
-		adic( txtVlrDesc, 7, 260, 81, 20 );
-		adic( new JLabelPad( "Vlr.juros." ), 91, 240, 81, 20 );
-		adic( txtVlrJuros, 91, 260, 81, 20 );
-		adic( new JLabelPad( "Vlr.devolução" ), 175, 240, 81, 20 );
-		adic( txtVlrDev, 175, 260, 81, 20 );
-		adic( new JLabelPad( "Vlr.adicional" ), 259, 240, 81, 20 );
-		adic( txtVlrAdic, 259, 260, 81, 20 );
+		// ABA GERAL
 
-		adic( new JLabelPad( "Observações" ), 7, 280, 200, 20 );
-		adic( txtObs, 7, 300, 333, 20 );
+		pnGeral.adic( new JLabelPad( "Cód.for." ), 7, 0, 250, 20 );
+
+		pnGeral.adic( txtCodFor, 7, 20, 80, 20 );
+		pnGeral.adic( new JLabelPad( "Razão social do fornecedor" ), 90, 0, 250, 20 );
+		pnGeral.adic( txtRazFor, 90, 20, 250, 20 );
+		pnGeral.adic( new JLabelPad( "Nº conta" ), 7, 40, 250, 20 );
+		pnGeral.adic( txtCodConta, 7, 60, 80, 20 );
+		pnGeral.adic( new JLabelPad( "Descrição da conta" ), 90, 40, 250, 20 );
+		pnGeral.adic( txtDescConta, 90, 60, 250, 20 );
+		pnGeral.adic( new JLabelPad( "Cód.catg." ), 7, 80, 250, 20 );
+		pnGeral.adic( txtCodPlan, 7, 100, 100, 20 );
+		pnGeral.adic( new JLabelPad( "Descrição da categoria" ), 110, 80, 250, 20 );
+		pnGeral.adic( txtDescPlan, 110, 100, 230, 20 );
+		pnGeral.adic( new JLabelPad( "Cód.c.c." ), 7, 120, 250, 20 );
+		pnGeral.adic( txtCodCC, 7, 140, 100, 20 );
+		pnGeral.adic( new JLabelPad( "Descrição do centro de custo" ), 110, 120, 250, 20 );
+		pnGeral.adic( txtDescCC, 110, 140, 230, 20 );
+		pnGeral.adic( new JLabelPad( "Cod.Tp.Cob" ), 7, 160, 80, 20 );
+		pnGeral.adic( txtCodTipoCob, 7, 180, 80, 20 );
+		pnGeral.adic( new JLabelPad( "Descrição do tipo de cobrança" ), 90, 160, 250, 20 );
+		pnGeral.adic( txtDescTipoCob, 90, 180, 250, 20 );
+
+		pnGeral.adic( new JLabelPad( "Doc." ), 7, 200, 81, 20 );
+		pnGeral.adic( txtDoc, 7, 220, 81, 20 );
+		pnGeral.adic( new JLabelPad( "Emissão" ), 91, 200, 81, 20 );
+		pnGeral.adic( txtDtEmis, 91, 220, 81, 20 );
+		pnGeral.adic( new JLabelPad( "Vencimento" ), 175, 200, 81, 20 );
+		pnGeral.adic( txtDtVenc, 175, 220, 81, 20 );
+		pnGeral.adic( new JLabelPad( "Vlr.parcela" ), 259, 200, 81, 20 );
+		pnGeral.adic( txtVlrParc, 259, 220, 81, 20 );
+
+		pnGeral.adic( new JLabelPad( "Vlr.desc." ), 7, 240, 81, 20 );
+		pnGeral.adic( txtVlrDesc, 7, 260, 81, 20 );
+		pnGeral.adic( new JLabelPad( "Vlr.juros." ), 91, 240, 81, 20 );
+		pnGeral.adic( txtVlrJuros, 91, 260, 81, 20 );
+		pnGeral.adic( new JLabelPad( "Vlr.devolução" ), 175, 240, 81, 20 );
+		pnGeral.adic( txtVlrDev, 175, 260, 81, 20 );
+		pnGeral.adic( new JLabelPad( "Vlr.adicional" ), 259, 240, 81, 20 );
+		pnGeral.adic( txtVlrAdic, 259, 260, 81, 20 );
+
+		pnGeral.adic( new JLabelPad( "Observações" ), 7, 280, 200, 20 );
+		pnGeral.adic( txtObs, 7, 300, 333, 20 );
+
+		// ABA CHEQUES
+
+//		tpn.addTab( "Cheques", pnCheques );
+
+		pnCheques.add( spnCheques, BorderLayout.CENTER );
+
 	}
 
 	public void setValores( String[] sVals, boolean bLancaUsu ) {
@@ -223,6 +270,10 @@ public class DLEditaPag extends FFDialogo implements CarregaListener {
 		txtObs.setVlrString( sVals[ 12 ] );
 		txtCodTipoCob.setVlrString( sVals[ 13 ] );
 		txtVlrDev.setVlrString( sVals[ 14 ] );
+
+		txtCodPag.setVlrString( sVals[ 15 ] );
+		txtNParcPag.setVlrString( sVals[ 16 ] );
+
 		txtVlrParc.setAtivo( bLancaUsu );
 	}
 
@@ -278,10 +329,109 @@ public class DLEditaPag extends FFDialogo implements CarregaListener {
 			con.commit();
 		} catch ( SQLException err ) {
 			err.printStackTrace();
+			Funcoes.mensagemErro( this, "Erro ao buscar lista de cheques\n" + err.getMessage(), true, con, err );
+		}
+
+		return iRet;
+	}
+
+	private int carregaCheques() {
+
+		int iRet = 0;
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		StringBuilder sql = new StringBuilder();
+
+		try {
+
+			sql.append( "select ch.vlrcheq, ch.numcheq, ch.sitcheq, ch.seqcheq, pc.codpag, ch.dtemitcheq, ch.dtvenctocheq from fnpagcheq pc, fncheque ch " );
+			sql.append( "where ch.codemp=pc.codempch and ch.codfilial=pc.codfilialch and ch.seqcheq=pc.seqcheq " );
+			sql.append( "and pc.codemp=? and pc.codfilial=? and pc.codpag=? and pc.nparcpag=?" );
+
+			ps = con.prepareStatement( sql.toString() );
+
+			ps.setInt( 1, Aplicativo.iCodEmp );
+			ps.setInt( 2, ListaCampos.getMasterFilial( "FNITRECEBER" ) );
+			ps.setInt( 3, txtCodPag.getVlrInteger() );
+			ps.setInt( 4, txtNParcPag.getVlrInteger() );
+
+			rs = ps.executeQuery();
+
+			tabCheques.limpa();
+
+			for ( int i = 0; rs.next(); i++ ) {
+
+				tabCheques.adicLinha();
+
+				tabCheques.setValor( rs.getInt( enum_grid_cheques.SEQCHEQ.name() ), i, enum_grid_cheques.SEQCHEQ.ordinal() );
+				tabCheques.setValor( rs.getInt( enum_grid_cheques.NUMCHEQ.name() ), i, enum_grid_cheques.NUMCHEQ.ordinal() );
+				tabCheques.setValor( rs.getDate( enum_grid_cheques.DTEMITCHEQ.name() ), i, enum_grid_cheques.DTEMITCHEQ.ordinal() );
+				tabCheques.setValor( rs.getDate( enum_grid_cheques.DTVENCTOCHEQ.name() ), i, enum_grid_cheques.DTVENCTOCHEQ.ordinal() );
+				tabCheques.setValor( Funcoes.bdToStr( rs.getBigDecimal( enum_grid_cheques.VLRCHEQ.name() ), Aplicativo.casasDecFin ), i, enum_grid_cheques.VLRCHEQ.ordinal() );
+				tabCheques.setValor( rs.getString( enum_grid_cheques.SITCHEQ.name() ), i, enum_grid_cheques.SITCHEQ.ordinal() );
+
+			}
+			
+			tpn.remove(pnCheques);
+			
+			if(tabCheques.getNumLinhas()>0) {
+				tpn.addTab( "Cheques", pnCheques );
+			}
+
+			rs.close();
+			ps.close();
+
+			con.commit();
+		} catch ( SQLException err ) {
+			err.printStackTrace();
 			Funcoes.mensagemErro( this, "Erro ao buscar o ano-base para o centro de custo.\n" + err.getMessage(), true, con, err );
 		}
 
 		return iRet;
+	}
+
+	private void montaGridCheques() {
+
+		tabCheques.addMouseListener( new MouseAdapter() {
+
+			public void mouseClicked( MouseEvent mevt ) {
+
+				if ( mevt.getSource() == tabCheques && mevt.getClickCount() == 2 )
+					abreCheque();
+			}
+		} );
+
+		tabCheques.adicColuna( "Seq." );
+		tabCheques.adicColuna( "Número" );
+		tabCheques.adicColuna( "Dt.Emissão" );
+		tabCheques.adicColuna( "Dt.Vencto." );
+		tabCheques.adicColuna( "Valor" );
+		tabCheques.adicColuna( "Sit." );
+
+		tabCheques.setColunaInvisivel( enum_grid_cheques.SEQCHEQ.ordinal() );
+		tabCheques.setTamColuna( 60, enum_grid_cheques.NUMCHEQ.ordinal() );
+		tabCheques.setTamColuna( 80, enum_grid_cheques.DTEMITCHEQ.ordinal() );
+		tabCheques.setTamColuna( 80, enum_grid_cheques.DTVENCTOCHEQ.ordinal() );
+		tabCheques.setTamColuna( 80, enum_grid_cheques.VLRCHEQ.ordinal() );
+		tabCheques.setTamColuna( 20, enum_grid_cheques.SITCHEQ.ordinal() );
+
+	}
+
+	private void abreCheque() {
+
+		if ( tabCheques.getLinhaSel() > -1 ) {
+			FCheque tela = null;
+			if ( Aplicativo.telaPrincipal.temTela( FCheque.class.getName() ) ) {
+				tela = (FCheque) Aplicativo.telaPrincipal.getTela( FCheque.class.getName() );
+			}
+			else {
+				tela = new FCheque();
+				Aplicativo.telaPrincipal.criatela( "Cheque", tela, con );
+			}
+			tela.exec( (Integer) tabCheques.getValor( tabCheques.getLinhaSel(), enum_grid_cheques.SEQCHEQ.ordinal() ) );
+		}
+	
 	}
 
 	public void beforeCarrega( CarregaEvent cevt ) {
@@ -306,5 +456,6 @@ public class DLEditaPag extends FFDialogo implements CarregaListener {
 		lcTipoCob.carregaDados();
 		lcCC.setConexao( cn );
 		lcCC.carregaDados();
+		carregaCheques();
 	}
 }
