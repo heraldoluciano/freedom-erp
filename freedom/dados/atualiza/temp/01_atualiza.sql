2067e0115ebccfe8c451842b788c050e13fb6710
anderson.sanchez <anderson.sanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-11-12 20:11:19 +0000

    Script de atualização para versão 1.2.4.2.

--- freedom/dados/atualiza/temp/01_atualiza.sql
+++ freedom/dados/atualiza/temp/01_atualiza.sql
@@ -1,10 +1,30 @@
-/* Server Version: LI-V6.3.3.4870 Firebird 1.5.  ODS Version: 10.1. */
+/* Server Version: UI-V6.3.6.5026 Firebird 1.5.  ODS Version: 10.1. */
 SET NAMES NONE;
 
 SET SQL DIALECT 3;
 
-CONNECT 'localhost:/opt/firebird/dados/desenv/1.2.3.8/freedom.fdb' USER 'SYSDBA' PASSWORD 'masterkey';
-
 SET AUTODDL ON;
 
+/* Alter Procedure... */
+/* Alter (SGRETVERSAO) */
+SET TERM ^ ;
 
+ALTER PROCEDURE SGRETVERSAO RETURNS(VERSAO VARCHAR(30))
+ AS
+begin
+    versao = '1.2.4.2 (12/11/2010)';
+    suspend;
+end
+^
+
+/* Alter Procedure... */
+/* Alter (SGRETVERSAO) */
+ALTER PROCEDURE SGRETVERSAO RETURNS(VERSAO VARCHAR(30))
+ AS
+begin
+    versao = '1.2.4.2 (12/11/2010)';
+    suspend;
+end
+^
+
+commit;
