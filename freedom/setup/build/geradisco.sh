dcc6f7efd38e4d238f88c8ad1f7d98ef3b9aca8c
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2010-12-09 14:30:18 +0000

    Compatibilização com novas versões.

--- freedom/setup/build/geradisco.sh
+++ freedom/setup/build/geradisco.sh
@@ -1,40 +1,86 @@
-echo "Criando disco de instalacao"
-mkdir ../disco
-mkdir ../disco/lin
-mkdir ../disco/opt
-mkdir ../disco/opt/firebird
-mkdir ../disco/opt/firebird/dados
-mkdir ../disco/opt/firebird/UDF
-mkdir ../disco/opt/freedom
-mkdir ../disco/opt/freedom/lib
-mkdir ../disco/opt/freedom/icon
-mkdir ../disco/opt/freedom/log
-mkdir ../disco/opt/freedom/bin
-mkdir ../disco/opt/freedom/ini
-rm ../disco/opt/freedom/* 
-rm ../disco/opt/freedom/lib/* 
-rm ../disco/opt/freedom/icon/* 
-rm ../disco/opt/freedom/log/*
-rm ../disco/opt/freedom/bin/*
-rm ../disco/opt/freedom/ini/*
-rm ../disco/opt/firebird/dados/*
-rm ../disco/disco.zip
-unzip -j -d ../disco/opt/firebird/dados/ ../../dados/estrutura/freedom.zip
-cp ../../src/org/freedom/udf/udfinstall.sh ../disco/opt/firebird/UDF/
-cp ../../src/org/freedom/udf/stp_udf_lin.sql ../disco/opt/firebird/UDF/
-cp ../../src/org/freedom/udf/stp_udf.so ../disco/opt/firebird/UDF/
-cp ../../src/org/freedom/udf/udfinstall.bat ../disco/opt/firebird/UDF/
-cp ../../src/org/freedom/udf/stp_udf_win.sql ../disco/opt/firebird/UDF/
-cp ../../src/org/freedom/udf/stp_udf.dll ../disco/opt/firebird/UDF/
-cp ../../icones/novos/png/*.png ../disco/opt/freedom/icon/
-cp ../../icones/novos/ico/*.ico ../disco/opt/freedom/icon/
-cp ../../lib/*.jar ../disco/opt/freedom/lib/
-cp ../../../freedom-infra/lib/*.jar ../disco/opt/freedom/lib/
-cp ../../../freedom-fw1/lib/*.jar ../disco/opt/freedom/lib/
-cp ../../lib/exec/*.sh ../disco/opt/freedom/bin/
-cp ../../lib/exec/*.bat ../disco/opt/freedom/bin/
-cp ../../freedomlin.ini ../disco/opt/freedom/ini/freedomlin.ini
-cp ../../freedomwin.ini ../disco/opt/freedom/ini/freedomwin.ini
-cd ../disco
-rm lin/disco.zip
-zip -r lin/disco ./opt/*
+#!/bin/sh
+# Script para geração de discos de instalação
+
+VERSION=$1
+
+fn_get_version() {
+    if [ -Z $VERSION ]; then
+       echo "Entre com o número da versão: "
+       read VERSION
+       if [ -Z $VERSION ]; then
+          echo "Versão não selecionada !"
+          fn_end_script
+       fi
+    fi
+}
+
+fn_create_all_dir()
+{
+    echo "Criando diretórios..."
+    if [ -d "../disco" ]; then
+       rm -R "../disco"
+    fi
+    fn_create_dir "../disco/opt/firebird/dados"
+    fn_create_dir "../disco/opt/firebird/UDF"
+    fn_create_dir "../disco/opt/freedom/lib"
+    fn_create_dir "../disco/opt/freedom/ico"
+    fn_create_dir "../disco/opt/freedom/log"
+    fn_create_dir "../disco/opt/freedom/bin"
+    fn_create_dir "../disco/opt/freedom/ini"
+    echo "Fim da criação de diretórios !"
+}
+
+fn_create_dir()
+{
+    NEWDIR=$1
+    echo "Criando diretório $NEWDIR"
+    if [ ! -d $NEWDIR ]; then
+       mkdir -p $NEWDIR
+    fi
+}
+
+fn_unzip_db() 
+{
+    unzip -j -d ../disco/opt/firebird/dados/ ../../dados/estrutura/freedom.zip
+}
+
+fn_copy_files() 
+{
+    echo "Copiando arquivos ..."
+    cp ../../src/org/freedom/udf/udfinstall.sh ../disco/opt/firebird/UDF/
+    cp ../../src/org/freedom/udf/stp_udf_lin.sql ../disco/opt/firebird/UDF/
+    cp ../../src/org/freedom/udf/stp_udf.so ../disco/opt/firebird/UDF/
+    cp ../../src/org/freedom/udf/udfinstall.bat ../disco/opt/firebird/UDF/
+    cp ../../src/org/freedom/udf/stp_udf_win.sql ../disco/opt/firebird/UDF/
+    cp ../../src/org/freedom/udf/stp_udf.dll ../disco/opt/firebird/UDF/
+    cp ../../icones/novos/png/*.png ../disco/opt/freedom/ico/
+    cp ../../icones/novos/ico/*.ico ../disco/opt/freedom/ico/
+    cp ../../lib/*.jar ../disco/opt/freedom/lib/
+    cp ../../../freedom-infra/lib/*.jar ../disco/opt/freedom/lib/
+    cp ../../../freedom-fw1/lib/*.jar ../disco/opt/freedom/lib/
+    cp ../../lib/exec/lin/*.sh ../disco/opt/freedom/bin/
+    cp ../../lib/exec/win/*.bat ../disco/opt/freedom/bin/
+    cp ../../lib/exec/osx/*.app ../disco/opt/freedom/bin/
+    cp ../../freedomlin.ini ../disco/opt/freedom/ini/freedomlin.ini
+    cp ../../freedomwin.ini ../disco/opt/freedom/ini/freedomwin.ini
+    echo "Fim da cópia de arquivos !"
+}
+
+fn_zip_disk()
+{
+    cd ../disco
+    zip -r "freedom_full_$VERSION.zip" ./opt/*
+}
+
+fn_end_script() 
+{
+    echo "Script concluído !"
+    exit 0
+}
+
+fn_get_version
+fn_create_all_dir
+fn_unzip_db
+fn_copy_files
+fn_zip_disk
+fn_end_script
