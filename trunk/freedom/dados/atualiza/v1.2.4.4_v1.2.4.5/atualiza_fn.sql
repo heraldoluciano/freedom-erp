e93f82c390482d6787ff4e7b1fd83292cc3a1b69
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-03-01 22:06:12 +0000

    Inclusão de atualização da data de competência em lanca e sublanca, buscando do contas a pagar e receber.

--- trunk/freedom/dados/atualiza/v1.2.4.4_v1.2.4.5/atualiza_fn.sql
+++ trunk/freedom/dados/atualiza/v1.2.4.4_v1.2.4.5/atualiza_fn.sql
@@ -30,9 +30,18 @@ update fnpagtocomi set emmanut='S', dtcomppcomi=datapcomi;
 update fnpagtocomi set emmanut='N';
 commit work;
 update fnlanca set emmanut='S', dtcomplanca=datalanca;
+update fnlanca l set dtcomplanca=(select dtcomppag from fnpagar p
+        where p.codemp=l.codemppg and p.codfilial=l.codfilialpg and p.codpag=l.codpag)
+     where exists ( select * from fnpagar p
+       where p.codemp=l.codemppg and p.codfilial=l.codfilialpg and p.codpag=l.codpag);
+update fnlanca l set dtcomplanca=(select dtcomprec from fnreceber r
+        where r.codemp=l.codemprc and r.codfilial=l.codfilialrc and r.codrec=l.codrec)
+     where exists ( select * from fnreceber r
+       where r.codemp=l.codemprc and r.codfilial=l.codfilialrc and r.codrec=l.codrec);
 update fnlanca set emmanut='N';
 commit work;
-update fnsublanca set emmanut='S', dtcompsublanca=datasublanca;
+update fnsublanca s set s.emmanut='S', s.dtcompsublanca=(select dtcomplanca from fnlanca l
+       where l.codemp=s.codemp and l.codfilial=s.codfilial and l.codlanca=s.codlanca);
 update fnsublanca set emmanut='N';
 commit work;
 
