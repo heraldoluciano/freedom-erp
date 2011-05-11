a6f5235f9857ae8bba0c3d12ddfa18cfa8366b4b
heraldo.luciano <heraldo.luciano@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-05-11 21:24:26 +0000

    Ajustes nos atalhos módulo PCP.

--- freedom/src/org/freedom/modulos/pcp/FreedomPCP.java
+++ freedom/src/org/freedom/modulos/pcp/FreedomPCP.java
@@ -88,9 +88,9 @@ public class FreedomPCP extends AplicativoPD implements ActionListener {
 		addOpcao( -1, TP_OPCAO_MENU, "Arquivo", "", 'A', 100000000, 0, false, null );
 		addOpcao( 100000000, TP_OPCAO_MENU, "Tabelas", "", 'T', 100100000, 1, false, null );
 		addOpcao( 100100000, TP_OPCAO_MENU, "Produtos", "", 'P', 100101000, 2, false, null );
-		addOpcao( 100101000, TP_OPCAO_ITEM, "Almoxarifado", "Almoxarifado", 'x', 100120030, 3, true, FAlmox.class );
-		addOpcao( 100101000, TP_OPCAO_ITEM, "Grupo", "Grupo", 'r', 100120040, 3, true, FGrupoProd.class );
-		addOpcao( 100101000, TP_OPCAO_ITEM, "Marca", "Marca", 'c', 100120050, 3, true, FMarca.class );
+		addOpcao( 100101000, TP_OPCAO_ITEM, "Almoxarifado", "Almoxarifado", 'A', 100120030, 3, true, FAlmox.class );
+		addOpcao( 100101000, TP_OPCAO_ITEM, "Grupo", "Grupo", 'G', 100120040, 3, true, FGrupoProd.class );
+		addOpcao( 100101000, TP_OPCAO_ITEM, "Marca", "Marca", 'M', 100120050, 3, true, FMarca.class );
 		addOpcao( 100101000, TP_OPCAO_ITEM, "Unidade", "Unidade", 'U', 100120060, 3, true, FUnidade.class );
 		addOpcao( 100101000, TP_OPCAO_ITEM, "Produto", "Produto", 'P', 100120070, 3, true, FProduto.class );
 
@@ -101,41 +101,41 @@ public class FreedomPCP extends AplicativoPD implements ActionListener {
 		addOpcao( 100102000, TP_OPCAO_ITEM, "Recursos de produção", "Recursos de produção", 'R', 100102020, 2, true, FRecursos.class );
 		addOpcao( 100102000, TP_OPCAO_ITEM, "Fases de produção", "Fases de produção", 'F', 100102030, 2, true, FFase.class );
 		addOpcao( 100102000, TP_OPCAO_ITEM, "Estrutura", "Estrutura de produto", 'E', 100102040, 2, true, FEstrutura.class );
-		addOpcao( 100102000, TP_OPCAO_ITEM, "Modelos de Lote", "Modelo de lote", 'E', 100102050, 2, true, FModLote.class );
-		addOpcao( 100102000, TP_OPCAO_ITEM, "Tipo de Analise", "Tipo de Analise", 'A', 100102060, 2, true, FTipoAnalise.class );
-		addOpcao( 100102000, TP_OPCAO_ITEM, "Método Analitico", "Método Analitico", 'A', 100102070, 2, true, FMetodoAnalitico.class );
+		addOpcao( 100102000, TP_OPCAO_ITEM, "Modelos de Lote", "Modelo de lote", 'M', 100102050, 2, true, FModLote.class );
+		addOpcao( 100102000, TP_OPCAO_ITEM, "Tipo de Analise", "Tipo de Analise", 'I', 100102060, 2, true, FTipoAnalise.class );
+		addOpcao( 100102000, TP_OPCAO_ITEM, "Método Analitico", "Método Analitico", 'O', 100102070, 2, true, FMetodoAnalitico.class );
 
-		addOpcao( 100000000, TP_OPCAO_MENU, "Preferências", "", 'F', 110100000, 1, false, null );
-		addOpcao( 110100000, TP_OPCAO_ITEM, "Preferências de Produção", "Preferências de Produção", 'u', 110105000, 2, true, FPrefereProd.class );
+		addOpcao( 100000000, TP_OPCAO_MENU, "Preferências", "", 'P', 110100000, 1, false, null );
+		addOpcao( 110100000, TP_OPCAO_ITEM, "Preferências de Produção", "Preferências de Produção", 'P', 110105000, 2, true, FPrefereProd.class );
 
 		addOpcao( -1, TP_OPCAO_MENU, "Produção", "", 'P', 200000000, 0, false, null );
 		addOpcao( 200000000, TP_OPCAO_ITEM, "Ordens de produção", "Ordens de produção", 'O', 200100000, 1, true, FOP.class );
 		addOpcao( 200000000, TP_OPCAO_ITEM, "Simulação de OP", "Simulação de OP", 'S', 200200000, 1, true, FSimulaOP.class );
-		addOpcao( 200000000, TP_OPCAO_ITEM, "Acompanhamento da produção", "Acompanhamento da produção", 'o', 200300000, 1, true, FAcompanhaProd.class );
-		addOpcao( 200000000, TP_OPCAO_ITEM, "Planejamento mestre da produção (Pull)", "Planejamento mestre da produção (Pull)", 'P', 200400000, 1, true, FPMP_Pull.class );
-		addOpcao( 200000000, TP_OPCAO_ITEM, "Planejamento mestre da produção (Push)", "Planejamento mestre da produção (Push)", 'P', 200500000, 1, true, FPMP_Push.class );
+		addOpcao( 200000000, TP_OPCAO_ITEM, "Acompanhamento da produção", "Acompanhamento da produção", 'A', 200300000, 1, true, FAcompanhaProd.class );
+		addOpcao( 200000000, TP_OPCAO_ITEM, "Planejamento mestre da produção (Pull)", "Planejamento mestre da produção (Pull)", 'U', 200400000, 1, true, FPMP_Pull.class );
+		addOpcao( 200000000, TP_OPCAO_ITEM, "Planejamento mestre da produção (Push)", "Planejamento mestre da produção (Push)", 'H', 200500000, 1, true, FPMP_Push.class );
 		addSeparador( 200000000 );
-		addOpcao( 200000000, TP_OPCAO_ITEM, "Contra Prova", "Contra prova", 'P', 200400000, 1, true, FContraProva.class );
+		addOpcao( 200000000, TP_OPCAO_ITEM, "Contra Prova", "Contra prova", 'C', 200400000, 1, true, FContraProva.class );
 		addSeparador( 200000000 );
 		addOpcao( 200000000, TP_OPCAO_ITEM, "Requisição de material", "Requisição de material", 'R', 200500000, 1, true, FRma.class );
 		addOpcao( 200000000, TP_OPCAO_ITEM, "Pesquisa requisição de material", "Pesquisa requisição de material", 'P', 200600000, 1, true, FConsRMA.class );
-		addOpcao( 200000000, TP_OPCAO_ITEM, "Pesquisa RMA por item", "Pesquisa RMA por item", 'i', 200700000, 1, true, FConsRmaItem.class );
+		addOpcao( 200000000, TP_OPCAO_ITEM, "Pesquisa RMA por item", "Pesquisa RMA por item", 'S', 200700000, 1, true, FConsRmaItem.class );
 		addOpcao( 200000000, TP_OPCAO_ITEM, "Baixa RMA via Cód.Barras", "Baixa RMA", 'B', 200800000, 1, true, FBaixaRMACodBar.class );
 		addSeparador( 200000000 );
 		addOpcao( 200000000, TP_OPCAO_MENU, "Listagens", "", 'L', 200900000, 1, false, null );
 		addOpcao( 200900000, TP_OPCAO_ITEM, "Certificação de análise", "Certificação de análise", 'C', 200900100, 1, true, FRCertAnalise.class );
-		addOpcao( 200900000, TP_OPCAO_ITEM, "Análises", "Análises", 'C', 200900200, 1, true, FRAnalise.class );
-		addOpcao( 200900000, TP_OPCAO_ITEM, "Contra-Provas", "Contra-Provas", 'C', 200900300, 1, true, FRContraProva.class );
+		addOpcao( 200900000, TP_OPCAO_ITEM, "Análises", "Análises", 'A', 200900200, 1, true, FRAnalise.class );
+		addOpcao( 200900000, TP_OPCAO_ITEM, "Contra-Provas", "Contra-Provas", 'O', 200900300, 1, true, FRContraProva.class );
 		addOpcao( 200900000, TP_OPCAO_ITEM, "Produção", "Produção", 'P', 200900400, 1, true, FRProducao.class );
-		addOpcao( 200900000, TP_OPCAO_ITEM, "Estruturas por item", "Estruturas por item", 'z', 200900500, 1, true, FREstruturaItem.class );
-		addOpcao( 200900000, TP_OPCAO_ITEM, "Comissionamento/produtividade", "Comissionamento/produtividade", 'C', 200900600, 1, true, FRComisProd.class );
+		addOpcao( 200900000, TP_OPCAO_ITEM, "Estruturas por item", "Estruturas por item", 'E', 200900500, 1, true, FREstruturaItem.class );
+		addOpcao( 200900000, TP_OPCAO_ITEM, "Comissionamento/produtividade", "Comissionamento/produtividade", 'M', 200900600, 1, true, FRComisProd.class );
 		
 		addOpcao( 200900000, TP_OPCAO_MENU, "Listagens FSC", "", 'F', 201000000, 1, false, null );
 		addOpcao( 201000000, TP_OPCAO_ITEM, "Consumo de matéria prima", "Consumo de matéria prima", 'C', 201000100, 1, true, FRConsumoMatFSC.class );
-		addOpcao( 201000000, TP_OPCAO_ITEM, "Produção por grupo", "Produção por grupo", 'g', 201000200, 1, true, FRProducaoGrupoFSC.class );
-		addOpcao( 201000000, TP_OPCAO_ITEM, "Balanço de produção", "Balanço de produção", 'g', 201000300, 1, true, FRBalancoProdFSC.class );
-		addOpcao( 201000000, TP_OPCAO_ITEM, "Balanço de produção por grupo", "Balanço de produção por grupo", 'p', 201000400, 1, true, FRBalancoProdGrupoFSC.class );
-		addOpcao( 201000000, TP_OPCAO_ITEM, "Ordens de produção", "Ordens de produção", 's', 201000500, 1, true, FREncomendasProducaoFSC.class );
+		addOpcao( 201000000, TP_OPCAO_ITEM, "Produção por grupo", "Produção por grupo", 'P', 201000200, 1, true, FRProducaoGrupoFSC.class );
+		addOpcao( 201000000, TP_OPCAO_ITEM, "Balanço de produção", "Balanço de produção", 'B', 201000300, 1, true, FRBalancoProdFSC.class );
+		addOpcao( 201000000, TP_OPCAO_ITEM, "Balanço de produção por grupo", "Balanço de produção por grupo", 'A', 201000400, 1, true, FRBalancoProdGrupoFSC.class );
+		addOpcao( 201000000, TP_OPCAO_ITEM, "Ordens de produção", "Ordens de produção", 'O', 201000500, 1, true, FREncomendasProducaoFSC.class );
 		
 		addOpcao( -1, TP_OPCAO_MENU, "Estoque", "", 'E', 400000000, 0, false, null );
 		addOpcao( 400000000, TP_OPCAO_ITEM, "Kardex", "Kardex", 'K', 400100000, 1, true, FKardex.class );
@@ -147,14 +147,14 @@ public class FreedomPCP extends AplicativoPD implements ActionListener {
 		addOpcao( 400000000, TP_OPCAO_ITEM, "Reprocessa estoque", "Reprocessa estoque", 'R', 400600000, 1, true, FProcessaEQ.class );
 		addSeparador( 400000000 );
 		addOpcao( 400000000, TP_OPCAO_MENU, "Listagens", "", 'L', 400700000, 1, false, null );
-		addOpcao( 400700000, TP_OPCAO_ITEM, "Estoque mínimo", "Estoque Mínimo", 's', 400701000, 2, true, FREstoqueMin.class );
+		addOpcao( 400700000, TP_OPCAO_ITEM, "Estoque mínimo", "Estoque Mínimo", 'E', 400701000, 2, true, FREstoqueMin.class );
 		addOpcao( 400700000, TP_OPCAO_ITEM, "Produtos/Movimentos", "Listagem de Produtos", 'P', 400702000, 2, true, FRMovProd.class );
 		addOpcao( 400700000, TP_OPCAO_ITEM, "Vencimentos de lote", "Vencimento Lote", 'V', 400703000, 2, true, FRVencLote.class );
-		addOpcao( 400700000, TP_OPCAO_ITEM, "Saldos de lote", "Saldos de Lote", 'l', 400704000, 2, true, FRSaldoLote.class );
+		addOpcao( 400700000, TP_OPCAO_ITEM, "Saldos de lote", "Saldos de Lote", 'S', 400704000, 2, true, FRSaldoLote.class );
 		addOpcao( 400700000, TP_OPCAO_ITEM, "Demanda", "Demanda", 'D', 400705000, 2, true, FRDemanda.class );
 		addOpcao( 400700000, TP_OPCAO_ITEM, "Conferência", "Conferência de Estoque", 'C', 400706000, 2, true, FRConfEstoq.class );
 		addOpcao( 400700000, TP_OPCAO_ITEM, "Inventário PEPS", "Inventário PEPS", 'I', 400707000, 2, true, FRInvPeps.class );
-		addOpcao( 400700000, TP_OPCAO_ITEM, "Custo de Produção", "Custo de Produção", 'u', 400708000, 2, true, FRCustoProducao.class );
+		addOpcao( 400700000, TP_OPCAO_ITEM, "Custo de Produção", "Custo de Produção", 'U', 400708000, 2, true, FRCustoProducao.class );
 
 		addBotao( "btEstProduto.gif", "Estrutura de produto", "Estrutura de produto", 100102040, FEstrutura.class );
 		addBotao( "btProduto.gif", "Produtos", "Produto", 100101000, FProduto.class );
