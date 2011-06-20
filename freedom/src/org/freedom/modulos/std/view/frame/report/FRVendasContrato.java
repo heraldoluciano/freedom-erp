4481fffe66e174e337f8fbb68f18806561a3a4c4
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-06-20 20:44:20 +0000

    Relatório de vendas x contratos.

---
+++ freedom/src/org/freedom/modulos/std/view/frame/report/FRVendasContrato.java
@@ -0,0 +1,194 @@
+/**
+ * @version 20/06/2011 <BR>
+ * @author Setpoint Informática Ltda.<BR>
+ * 
+ *         Projeto: Freedom <BR>
+ * 
+ *         Pacote: org.freedom.modulos.std.view.frame.report <BR>
+ *         Classe:
+ * @(#)FRVendasContrato.java <BR>
+ * 
+ *                        Este arquivo é parte do sistema Freedom-ERP, o Freedom-ERP é um software livre; você pode redistribui-lo e/ou <BR>
+ *                        modifica-lo dentro dos termos da Licença Pública Geral GNU como publicada pela Fundação do Software Livre (FSF); <BR>
+ *                        na versão 2 da Licença, ou (na sua opnião) qualquer versão. <BR>
+ *                        Este programa é distribuido na esperança que possa ser util, mas SEM NENHUMA GARANTIA; <BR>
+ *                        sem uma garantia implicita de ADEQUAÇÂO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. <BR>
+ *                        Veja a Licença Pública Geral GNU para maiores detalhes. <BR>
+ *                        Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com este programa, se não, <BR>
+ *                        de acordo com os termos da LPG-PC <BR>
+ * <BR>
+ * 
+ *                        Comentários sobre a classe...
+ * 
+ */
+
+package org.freedom.modulos.std.view.frame.report;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Types;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.SwingConstants;
+
+import net.sf.jasperreports.engine.JasperPrintManager;
+
+import org.freedom.infra.model.jdbc.DbConnection;
+import org.freedom.library.functions.Funcoes;
+import org.freedom.library.persistence.GuardaCampo;
+import org.freedom.library.persistence.ListaCampos;
+import org.freedom.library.swing.component.JLabelPad;
+import org.freedom.library.swing.component.JRadioGroup;
+import org.freedom.library.swing.component.JTextFieldFK;
+import org.freedom.library.swing.component.JTextFieldPad;
+import org.freedom.library.swing.frame.Aplicativo;
+import org.freedom.library.swing.frame.FPrinterJob;
+import org.freedom.library.swing.frame.FRelatorio;
+
+public class FRVendasContrato extends FRelatorio {
+
+	private static final long serialVersionUID = 1L;
+
+	private JTextFieldPad txtDataini = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
+
+	private JTextFieldPad txtDatafim = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
+
+	private JTextFieldPad txtCodCli = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+
+	private JTextFieldFK txtRazCli = new JTextFieldFK( JTextFieldPad.TP_STRING, 50, 0 );
+
+	private ListaCampos lcCli = new ListaCampos( this, "CL" );
+
+	public FRVendasContrato() {
+
+		super( false );
+		setTitulo( "Ultimas Vendas de Cliente/Produto" );
+		setAtribos( 50, 50, 355, 350 );
+
+		montaListaCampos();
+		montaTela();
+	}
+
+	private void montaListaCampos() {
+
+		lcCli.add( new GuardaCampo( txtCodCli, "CodCli", "Cód.cli.", ListaCampos.DB_PK, false ) );
+		lcCli.add( new GuardaCampo( txtRazCli, "RazCli", "Razão social do cliente", ListaCampos.DB_SI, false ) );
+		txtCodCli.setTabelaExterna( lcCli, null );
+		txtCodCli.setNomeCampo( "CodCli" );
+		txtCodCli.setFK( true );
+		lcCli.setReadOnly( true );
+		lcCli.montaSql( false, "CLIENTE", "VD" );
+
+	}
+
+	private void montaTela() {
+
+		JLabelPad lbLinha = new JLabelPad();
+		lbLinha.setBorder( BorderFactory.createEtchedBorder() );
+		JLabelPad lbPeriodo = new JLabelPad( "Periodo:", SwingConstants.CENTER );
+		lbPeriodo.setOpaque( true );
+
+		adic( lbPeriodo, 15, 5, 80, 20 );
+		adic( lbLinha, 7, 15, 320, 45 );
+
+		adic( new JLabelPad( "De:", SwingConstants.CENTER ), 17, 30, 40, 20 );
+
+		adic( txtDataini, 57, 30, 100, 20 );
+
+		adic( new JLabelPad( "Até:", SwingConstants.CENTER ), 157, 30, 45, 20 );
+		adic( txtDatafim, 202, 30, 100, 20 );
+
+		adic( new JLabelPad( "Cód.Cliente" ), 7, 110, 90, 20 );
+		adic( txtCodCli, 7, 130, 90, 20 );
+		adic( new JLabelPad( "Razão social do cliente" ), 100, 110, 227, 20 );
+		adic( txtRazCli, 100, 130, 227, 20 );
+
+		Calendar cPeriodo = Calendar.getInstance();
+		txtDatafim.setVlrDate( cPeriodo.getTime() );
+		cPeriodo.set( Calendar.DAY_OF_MONTH, cPeriodo.get( Calendar.DAY_OF_MONTH ) - 30 );
+		txtDataini.setVlrDate( cPeriodo.getTime() );
+	}
+
+	public void imprimir( boolean bVisualizar ) {
+
+		if ( txtDatafim.getVlrDate().before( txtDataini.getVlrDate() ) ) {
+			Funcoes.mensagemInforma( this, "Data final maior que a data inicial!" );
+			return;
+		}
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		StringBuffer sSQL = new StringBuffer();
+		StringBuffer sCab = new StringBuffer();
+		StringBuffer sWhereCli = new StringBuffer();
+		StringBuffer sWhereComiss = new StringBuffer();
+
+		sCab.append( "de : " + Funcoes.dateToStrDate( txtDataini.getVlrDate() ) + "Até : " + Funcoes.dateToStrDate( txtDatafim.getVlrDate() ) );
+
+		try {
+
+			sSQL.append( "select razcli_ret razcli, codcli_ret codcli, ");
+			sSQL.append( "codprod_ret codprod, " );
+			sSQL.append( "dtemitvenda_ret dtemitvenda, docvenda_ret docvenda, serie_ret serie, precovenda_ret precovenda " );
+			sSQL.append( "from vdretultvdcliprod (?,?,?,?,?,?,?,?,?) " );
+
+			ps = con.prepareStatement( sSQL.toString() );
+
+			ps.setInt( 1, Aplicativo.iCodEmp );
+
+			if ( txtRazCli.getVlrString().trim().length() > 0 ) {
+				ps.setInt( 2, txtCodCli.getVlrInteger() );
+			}
+			else {
+				ps.setNull( 2, Types.INTEGER );
+			}
+
+			ps.setDate( 3, Funcoes.strDateToSqlDate( txtDataini.getVlrString() ) );
+			ps.setDate( 4, Funcoes.strDateToSqlDate( txtDatafim.getVlrString() ) );
+
+			rs = ps.executeQuery();
+
+			imprimiGrafico( bVisualizar, rs, sCab.toString() );
+
+			con.commit();
+
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			Funcoes.mensagemInforma( this, "Erro ao buscar dados da venda!" );
+		}
+	}
+
+	private void imprimiGrafico( final boolean bVisualizar, final ResultSet rs, final String sCab ) {
+
+		FPrinterJob dlGr = null;
+		HashMap<String, Object> hParam = new HashMap<String, Object>();
+
+		hParam.put( "CODEMP", Aplicativo.iCodEmp );
+		hParam.put( "CODFILIAL", ListaCampos.getMasterFilial( "VDVENDA" ) );
+		hParam.put( "RAZAOEMP", Aplicativo.empresa.toString() );
+		hParam.put( "FILTROS", sCab );
+
+		dlGr = new FPrinterJob( "relatorios/UltVendCli.jasper", "Ultimas Vendas por Cliente/Produto", sCab, rs, hParam, this );
+
+		if ( bVisualizar ) {
+			dlGr.setVisible( true );
+		}
+		else {
+			try {
+				JasperPrintManager.printReport( dlGr.getRelatorio(), true );
+			} catch ( Exception err ) {
+				Funcoes.mensagemErro( this, "Erro na impressão de relatório de Cliente por Produto!" + err.getMessage(), true, con, err );
+			}
+		}
+	}
+
+	public void setConexao( DbConnection cn ) {
+
+		super.setConexao( cn );
+		lcCli.setConexao( cn );
+
+	}
+}
