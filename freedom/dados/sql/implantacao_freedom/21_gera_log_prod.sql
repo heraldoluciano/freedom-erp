5427982a31be09f2a826f0248413d9d4709b9654
robsonsanchez <robsonsanchez@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-06-29 20:33:20 +0000

    Exclusão de variávels desnecessárias no desempacota.sh e ajustes no 21_gera_log_prod.sql

--- freedom/dados/sql/implantacao_freedom/21_gera_log_prod.sql
+++ freedom/dados/sql/implantacao_freedom/21_gera_log_prod.sql
@@ -3,4 +3,3 @@ insert into eqprodutolog
 select pd.codemp, pd.codfilial, pd.codprod, 1, precobaseprod, precobaseprod
 from eqproduto pd
 where not exists(select seqlog from eqprodutolog where codemp=pd.codemp and codfilial=pd.codfilial and codprod=pd.codprod );
-
