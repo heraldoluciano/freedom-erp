db18c47744345e561c9acf1e384561b256d98cd8
brunolnascimento <brunolnascimento@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-07-25 21:01:47 +0000

    Inclusão de checkbox na tela origem de contato(CRM)

--- freedom/src/org/freedom/modulos/crm/view/frame/crud/plain/FOrigContato.java
+++ freedom/src/org/freedom/modulos/crm/view/frame/crud/plain/FOrigContato.java
@@ -22,6 +22,7 @@
 package org.freedom.modulos.crm.view.frame.crud.plain;
 
 import org.freedom.library.persistence.ListaCampos;
+import org.freedom.library.swing.component.JCheckBoxPad;
 import org.freedom.library.swing.component.JTextFieldPad;
 import org.freedom.library.swing.frame.FDados;
 
@@ -32,12 +33,14 @@ public class FOrigContato extends FDados {
 	private JTextFieldPad txtCodOrigCont = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 10, 0 );
 
 	private JTextFieldPad txtDescOrigCont = new JTextFieldPad( JTextFieldPad.TP_STRING, 40, 0 );
+	
+	private JCheckBoxPad cbWeb = new JCheckBoxPad( "Publicar na web", "S", "N" );
 
 	public FOrigContato() {
 
 		super();
 		setTitulo( "Origem do contato" );
-		setAtribos( 15, 30, 400, 150 );
+		setAtribos( 15, 30, 400, 165 );
 		montaTela();
 	}
 
@@ -45,6 +48,7 @@ public class FOrigContato extends FDados {
 
 		adicCampo( txtCodOrigCont, 7, 25, 70, 20, "CodOrigCont", "Cód.Orig.", ListaCampos.DB_PK, true );
 		adicCampo( txtDescOrigCont, 80, 25, 290, 20, "DescOrigCont", "Origem do contato", ListaCampos.DB_SI, true );
+		adicDB( cbWeb, 7, 50, 120, 20, "web", "", true );
 
 		setListaCampos( true, "ORIGCONT", "TK" );
 	}
