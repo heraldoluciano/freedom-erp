1232480bafc987962bdaf196494890fb36bf0c99
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-08-12 18:42:51 +0000

    Inclusão de condição para evitar sobreposição de códigos de municípios.

--- trunk/freedom/dados/sql/atualiza_cidade_cadastros.sql
+++ trunk/freedom/dados/sql/atualiza_cidade_cadastros.sql
@@ -10,7 +10,8 @@ set emp.codpais=(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(emp.cidemp))=rtrim(lower(mu.nomemunic))),
  emp.codmunic=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(emp.ufemp))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(emp.cidemp))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(emp.cidemp))=rtrim(lower(mu.nomemunic)))
+ where emp.codmunic is null;
 
 /* Atualiza o cadastro de filiais */
 
@@ -23,7 +24,8 @@ set fil.codpais=(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(fil.cidfilial))=rtrim(lower(mu.nomemunic))),
  fil.codmunic=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(fil.uffilial))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(fil.cidfilial))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(fil.cidfilial))=rtrim(lower(mu.nomemunic)))
+ where fil.codmunic is null;
 
 /* Atualiza o cadastro de clientes endereço principal*/
 
@@ -36,7 +38,8 @@ set cli.codpais=(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(cli.cidcli))=rtrim(lower(mu.nomemunic))),
  cli.codmunic=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(cli.ufcli))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(cli.cidcli))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(cli.cidcli))=rtrim(lower(mu.nomemunic)))
+ where cli.codmunic is null;
 
 /* Atualiza o cadastro de clientes endereço de entrega*/
 
@@ -49,7 +52,8 @@ set cli.codpaisent =(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(cli.cident))=rtrim(lower(mu.nomemunic))),
  cli.codmunicent=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(cli.ufent))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(cli.cident))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(cli.cident))=rtrim(lower(mu.nomemunic)))
+ where cli.codmunicent is null;
 
 /* Atualiza o cadastro de clientes endereço de cobrança*/
 
@@ -62,7 +66,8 @@ set cli.codpaiscob =(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(cli.cidcob))=rtrim(lower(mu.nomemunic))),
  cli.codmuniccob=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(cli.ufcob))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(cli.cidcob))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(cli.cidcob))=rtrim(lower(mu.nomemunic)))
+ where cli.codmuniccob is null;
 
 update cpforneced forn
 set forn.codpais=(select mu.codpais from sgmunicipio mu
@@ -73,7 +78,8 @@ set forn.codpais=(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(forn.cidfor))=rtrim(lower(mu.nomemunic))),
  forn.codmunic=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(forn.uffor))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(forn.cidfor))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(forn.cidfor))=rtrim(lower(mu.nomemunic)))
+ where forn.codmunic is null;
 
 update vdtransp t
 set t.codpais=(select mu.codpais from sgmunicipio mu
@@ -84,7 +90,8 @@ set t.codpais=(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(t.cidtran))=rtrim(lower(mu.nomemunic))),
  t.codmunic=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(t.uftran))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(t.cidtran))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(t.cidtran))=rtrim(lower(mu.nomemunic)))
+ t.codmunic is null;
 
 update tkcontato tk
 set tk.codpais=(select mu.codpais from sgmunicipio mu
@@ -95,7 +102,8 @@ set tk.codpais=(select mu.codpais from sgmunicipio mu
                  and rtrim(lower(tk.cidcto))=rtrim(lower(mu.nomemunic))),
  tk.codmunic=(select mu.codmunic from sgmunicipio mu
                  where rtrim(lower(tk.ufcto))=rtrim(lower(mu.siglauf))
-                 and rtrim(lower(tk.cidcto))=rtrim(lower(mu.nomemunic)));
+                 and rtrim(lower(tk.cidcto))=rtrim(lower(mu.nomemunic)))
+ tk.codmunic is null;
 
 commit work
 
