a2eee62bfc7c89b3318b49fe3ebdbe3468c0761a
robson <robson@stpinf.com>
2011-08-17 01:26:08 +0000

    Inclusão de colunas para faturamento parcial de orçamentos.

--- freedom/dados/sql/atualiza_qtdafat_itorc.sql
+++ freedom/dados/sql/atualiza_qtdafat_itorc.sql
@@ -1,2 +1,12 @@
-update vditorcamento i set i.emmanut='S' , qtdfat=0, qtdafat=0;
-update vditorcamento i set i.emmanut='N' where i.emanut='S';
+update vditorcamento i set emmanut='S' , qtdfatitorc=0, qtdafatitorc=0
+  where qtdfatitorc is null or qtdafatitorc is null;
+
+update vditorcamento i set i.emmanut='S', i.qtdfatitorc=i.qtditorc
+  where i.fatitorc='S';
+  
+update vditorcamento i set i.emmanut='S', i.qtdafatitorc=i.qtditorc
+  where i.fatitorc='N';
+
+update vditorcamento i set i.emmanut='N' where i.emmanut='S';
+
+commit work;
