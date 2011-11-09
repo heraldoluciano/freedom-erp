1087adaf2383979f5c09dab1af0a87bdaad21788
brunolnascimento <brunolnascimento@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-11-09 18:20:58 +0000

    Criação da tela Gestão de Projetos(CRM).

--- trunk/freedom/src/org/freedom/modulos/crm/business/object/Contrato.java
+++ trunk/freedom/src/org/freedom/modulos/crm/business/object/Contrato.java
@@ -17,7 +17,13 @@ public class Contrato {
 	public static final Constant SIT_PROJ_CANCELADO_PRESTADOR = new Constant("Canc. prestador ", "CP", Color.RED, Color.BLACK);
 	public static final Constant SIT_PROJ_FINALIZADO = new Constant("Finalizado", "FN", Color.BLUE, Color.WHITE);
 	
+	public static final Constant TP_PROJ_CONTRATO = new Constant("Contrato", "C", Color.WHITE, Color.BLACK);
+	public static final Constant TP_PROJ_PROJETO = new Constant("Projeto", "P", Color.BLUE, Color.WHITE);
+	public static final Constant TP_PROJ_SUB_PROJETO = new Constant("Sub-Projeto", "S", Color.RED, Color.BLACK);
+
 	private static Vector<Constant> listSitproj = null;
+	
+	private static Vector<Constant> listTpproj = null;
 	
 	public static Vector<Constant> getListSitproj() {
 		if (listSitproj==null) {
@@ -35,11 +41,31 @@ public class Contrato {
 		return listSitproj;
 	}
 	
+	public static Vector<Constant> getListTpproj() {
+		if (listTpproj==null) {
+			listTpproj = new Vector<Constant>();
+			listTpproj.add( TP_PROJ_CONTRATO );
+			listTpproj.add( TP_PROJ_PROJETO );
+			listTpproj.add( TP_PROJ_SUB_PROJETO );
+	
+		}
+		return listTpproj;
+	}
+	
 	public static Vector<String> getSitprojName() {
 		Vector<String> result = new Vector<String>();
 		listSitproj = getListSitproj();
 		for (int i=0; i<listSitproj.size(); i++) {
 			result.add( listSitproj.elementAt( i ) .getName());
+		}
+		return result;
+	}
+	
+	public static Vector<String> getTpprojName() {
+		Vector<String> result = new Vector<String>();
+		listTpproj= getListTpproj();
+		for (int i=0; i<listTpproj.size(); i++) {
+			result.add( listTpproj.elementAt( i ) .getName());
 		}
 		return result;
 	}
@@ -49,6 +75,15 @@ public class Contrato {
 		listSitproj = getListSitproj();
 		for (int i=0; i<listSitproj.size(); i++) {
 			result.add( (String) listSitproj.elementAt( i ) .getValue());
+		}
+		return result;
+	}
+	
+	public static Vector<String> getTpprojValue() {
+		Vector<String> result = new Vector<String>();
+		listTpproj = getListTpproj();
+		for (int i=0; i<listTpproj.size(); i++) {
+			result.add( (String) listTpproj.elementAt( i ) .getValue());
 		}
 		return result;
 	}
