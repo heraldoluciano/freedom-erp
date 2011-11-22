6df6c91d8a7bf72c6b9de3535d13093713a3ca29
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-11-22 13:54:34 +0000

    Implementação de query para atualização da tabela fnsublanca com os campos codemprc, codfilialrc, codrec, nparcitrec, codemppg, codfilialpg, codpag e nparcpag, objetivando gerar dados para os relatórios razão de clientes e razão de fornecedores.

--- trunk/freedom/dados/atualiza/v1.2.4.6_v1.2.4.7/fnsublanca_update_codpag.sql
+++ trunk/freedom/dados/atualiza/v1.2.4.6_v1.2.4.7/fnsublanca_update_codpag.sql
@@ -9,6 +9,48 @@ update fnsublanca
 set emmanut='N'
 where emmanut='S';
 
+-- select utilizada para gerar update
+select slp.codlanca, slp.codsublanca, ip.vlrpagoitpag, slp.vlrsublanca, ip.dtpagoitpag, ip.codemp,  ip.codfilial, ip.codpag, ip.nparcpag
+from fnitpagar ip, fnpagar p, fnsublanca slp
+where p.codemp=ip.codemp and p.codfilial=ip.codfilial and p.codpag=ip.codpag and
+slp.codempfr=p.codempfr and slp.codfilialfr=p.codfilialfr and slp.codfor=p.codfor and
+slp.datasublanca=ip.dtpagoitpag and slp.vlrsublanca=ip.vlrpagoitpag and
+ip.statusitpag = 'PP' and
+not exists (select * from fnsublanca sl2
+where sl2.codemppg=ip.codemp and sl2.codfilialpg=ip.codfilial and
+sl2.codpag=ip.codpag and sl2.nparcpag=ip.nparcpag);
+
+update fnsublanca sl set
+sl.emmanut='S',
+sl.codemppg = (select first 1 ip.codemp
+from fnitpagar ip, fnpagar p
+where p.codemp=ip.codemp and p.codfilial=ip.codfilial and p.codpag=ip.codpag and
+sl.codempfr=p.codempfr and sl.codfilialfr=p.codfilialfr and sl.codfor=p.codfor and
+sl.datasublanca=ip.dtpagoitpag and sl.vlrsublanca=ip.vlrpagoitpag and
+ip.statusitpag = 'PP'),
+sl.codfilialpg = (select first 1 ip.codfilial
+from fnitpagar ip, fnpagar p
+where p.codemp=ip.codemp and p.codfilial=ip.codfilial and p.codpag=ip.codpag and
+sl.codempfr=p.codempfr and sl.codfilialfr=p.codfilialfr and sl.codfor=p.codfor and
+sl.datasublanca=ip.dtpagoitpag and sl.vlrsublanca=ip.vlrpagoitpag and
+ip.statusitpag = 'PP'),
+sl.codpag = (select first 1 ip.codpag
+from fnitpagar ip, fnpagar p
+where p.codemp=ip.codemp and p.codfilial=ip.codfilial and p.codpag=ip.codpag and
+sl.codempfr=p.codempfr and sl.codfilialfr=p.codfilialfr and sl.codfor=p.codfor and
+sl.datasublanca=ip.dtpagoitpag and sl.vlrsublanca=ip.vlrpagoitpag and
+ip.statusitpag = 'PP'),
+sl.nparcpag = (select first 1 ip.nparcpag
+from fnitpagar ip, fnpagar p
+where p.codemp=ip.codemp and p.codfilial=ip.codfilial and p.codpag=ip.codpag and
+sl.codempfr=p.codempfr and sl.codfilialfr=p.codfilialfr and sl.codfor=p.codfor and
+sl.datasublanca=ip.dtpagoitpag and sl.vlrsublanca=ip.vlrpagoitpag and
+ip.statusitpag = 'PP')
+where sl.codfor is not null and sl.codpag is null;
+
+update fnsublanca set emmanut='N' where emmanut='S';
+
+
 commit work;
 
 
