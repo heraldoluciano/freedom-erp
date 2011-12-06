5fe3152cafb2c95059b2e84860df2261408cdc0c
brunolnascimento <brunolnascimento@gmail.com>
2011-12-06 13:33:29 +0000

    Criação do pacote dao no modulo gms, assim como DAOColeta e o bean Coleta.

---
+++ freedom/src/org/freedom/modulos/gms/dao/DAOColeta.java
@@ -0,0 +1,63 @@
+package org.freedom.modulos.gms.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.freedom.infra.dao.AbstractDAO;
+import org.freedom.infra.model.jdbc.DbConnection;
+import org.freedom.library.persistence.ListaCampos;
+import org.freedom.library.swing.frame.Aplicativo;
+import org.freedom.modulos.crm.business.object.Atendimento;
+
+
+public class DAOColeta extends AbstractDAO {
+
+	private Object prefs[] = null;
+	
+	
+	public DAOColeta( DbConnection cn ) {
+
+		super( cn );
+		
+	}
+	
+	public void setPrefs(Integer codemp, Integer codfilial) throws SQLException {
+		
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		StringBuilder sql = null;
+		
+		prefs = new Object[ Atendimento.PREFS.values().length];
+		
+		try {
+			sql = new StringBuilder("select pf1.usarefprod, " );
+			sql.append( "coalesce(pf8.codtiporecmerccm,0) codtiporecmerc " );
+			sql.append( "from sgprefere1 pf1 left outer join sgprefere8 pf8 " );
+			sql.append( "on pf8.codemp=pf1.codemp and pf8.codfilial=pf1.codfilial " );
+			sql.append( "where pf1.codemp=? and pf1.codfilial=? " );
+
+			ps = getConn().prepareStatement( sql.toString() );
+			ps.setInt( 1, Aplicativo.iCodEmp);
+			ps.setInt( 2, ListaCampos.getMasterFilial( "SGPREFERE1" ) );
+			rs = ps.executeQuery();
+			
+			if ( rs.next() ) {
+				
+		
+				
+			}
+			rs.close();
+			ps.close();
+			getConn().commit();
+		} finally {
+			ps = null;
+			rs = null;
+			sql = null;
+		}
+	}
+	
+	public Object[] getPrefs() {
+		return this.prefs;
+	}
+}
