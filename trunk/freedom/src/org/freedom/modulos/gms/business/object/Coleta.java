f4f1f0b098589f2440132fd0c1a9cfdc8f2a6a95
brunolnascimento@gmail.com <brunolnascimento@gmail.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2011-12-06 18:52:24 +0000

    Implementação para carregar código da transportadora na tela coleta de materiais a partir do que foi setado nas preferências do GMS.

--- trunk/freedom/src/org/freedom/modulos/gms/business/object/Coleta.java
+++ trunk/freedom/src/org/freedom/modulos/gms/business/object/Coleta.java
@@ -2,6 +2,6 @@ package org.freedom.modulos.gms.business.object;
 
 
 public class Coleta {
-	public static enum PREFS {  USAREFPROD, CODTIPORECMERC, CODPLANOPAG }
+	public static enum PREFS {  USAREFPROD, CODTIPORECMERC, CODPLANOPAG, CODTRAN }
 
 }
