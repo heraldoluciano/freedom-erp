9a8db7db535081854bd1338ac13d1adbb47dfc07
robson <robson@stpinf.com>
2011-12-12 22:38:15 +0000

    Classe DAO para separação de camada de acesso a dados da tela de gerenciamento de campanhas.

---
+++ freedom/src/org/freedom/modulos/crm/dao/DAOCampanha.java
@@ -0,0 +1,15 @@
+package org.freedom.modulos.crm.dao;
+
+import org.freedom.infra.dao.AbstractDAO;
+import org.freedom.infra.model.jdbc.DbConnection;
+
+
+public class DAOCampanha extends AbstractDAO {
+
+	public DAOCampanha( DbConnection connection ) {
+
+		super( connection );
+
+	}
+
+}
