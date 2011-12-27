2c998baa581e9ab4d8baa63f6d50ce8dec3a5ef9
robson <robson@stpinf.com>
2011-12-27 10:52:34 +0000

    Ajustes no job de atualização de sublançamentos.

--- freedom/dados/atualiza/v1.2.4.6_v1.2.4.7/04_fnsublanca_update_tiposublanca.sql
+++ freedom/dados/atualiza/v1.2.4.6_v1.2.4.7/04_fnsublanca_update_tiposublanca.sql
@@ -5,8 +5,8 @@ update fnsublanca sl
       
 update fnsublanca sl 
    set sl.emmanut='S', sl.tiposublanca='J' 
-   where ( codplan=(select codplandc from sgprefere1 p1 where p1.codemp=sl.codemp and p1.codfilial=sl.codfilial ) or 
-      codplan=(select codplandr from sgprefere1 p1 where p1.codemp=sl.codemp and p1.codfilial=sl.codfilial ) );
+   where ( codplan=(select codplanjr from sgprefere1 p1 where p1.codemp=sl.codemp and p1.codfilial=sl.codfilial ) or
+      codplan=(select codplanjp from sgprefere1 p1 where p1.codemp=sl.codemp and p1.codfilial=sl.codfilial ) );
     
 update fnsublanca set emmanut='N' where emmanut='S';
 
