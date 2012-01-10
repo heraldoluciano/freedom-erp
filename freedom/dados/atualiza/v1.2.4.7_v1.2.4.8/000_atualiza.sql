1e6d7bbb136feb99cd487440f80cc55744a984be
robson <robson@stpinf.com>
2012-01-10 16:32:30 +0000

    Job para inclusão das colunas codemppd e codfilialpd na tabela cpprodfor.

--- freedom/dados/atualiza/v1.2.4.7_v1.2.4.8/000_atualiza.sql
+++ freedom/dados/atualiza/v1.2.4.7_v1.2.4.8/000_atualiza.sql
@@ -1,4 +1,5 @@
 alter table cpprodfor add codemppd integer;
 alter table cpprodfor add codfilialpd smallint;
 update cpprodfor set codemppd=codemp, codfilialpd=codfilial;
+delete from cpprodfor pf where not exists(select * from eqproduto p where p.codemp=pf.codemppd and p.codfilial=pf.codfilialpd and p.codprod=pf.codprod);
 commit work;
