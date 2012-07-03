8f4af6696c42187f954fe56892b6265b8b5d4780
robson <robson@stpinf.com>
2012-07-03 19:42:56 +0000

    Arquivos de carga do sistema compatíveis com Java 7.

--- freedom/lib/exec/win/freedomstd.bat
+++ freedom/lib/exec/win/freedomstd.bat
@@ -1,1 +1,22 @@
-start javaw -classpath "..\lib/*" -DARQINI=..\ini\freedom.ini -DARQLOG=..\log\freedomstd.log org.freedom.modulos.std.FreedomSTD
+set execdirmd=%~f0
+set removedmd=
+echo %execdirmd%
+
+:strip_modulo
+
+set removedmd=%execdirmd:~-1%
+
+set execdirmd=%execdirmd:~0,-1%
+
+if NOT "%removedmd%"=="\" goto strip_modulo
+
+SET FREEDOMMD=freedomstd
+SET FREEDOMCL=org.freedom.modulos.std.FreedomSTD
+
+set CMDENV=%execdirmd%\freedomenv 
+
+set CMDFREEDOM=
+
+call %CMDENV% %FREEDOMMD% %FREEDOMCL%
+
+%CMDFREEDOM%
