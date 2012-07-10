3eadc056bd51dc79380c83d7dba663cc81b8ef9c
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2012-07-10 18:36:29 +0000

    Impressão de ficha avaliativa de contato.

--- trunk/freedom/src/org/freedom/modulos/crm/dao/DAOContato.java
+++ trunk/freedom/src/org/freedom/modulos/crm/dao/DAOContato.java
@@ -12,7 +12,7 @@ import org.freedom.library.swing.frame.Aplicativo;
 
 public class DAOContato extends AbstractDAO {
 	private Object prefs[] = null;
-	public enum CONT_PREFS{ USACTOSEQ };
+	public enum CONT_PREFS{ USACTOSEQ, LAYOUTFICHAAVAL };
 	
 	
 	public DAOContato( DbConnection cn) {
@@ -52,6 +52,28 @@ public class DAOContato extends AbstractDAO {
 		return retorno;
 
 	}
+
+	public StringBuilder getSqlFichaAval() {
+		StringBuilder sql= new StringBuilder();
+		sql.append(" select ct.codcontr, ct.dtinicio, ct.dtfim ");
+		sql.append(", ct.codcli, cl.razcli ");
+		sql.append(", cl.cpfcli, cl.agenciacli ");
+		sql.append(", cl.ncontabcocli, itpf.codconv, pf.nomeemp ");
+		sql.append("from vdcontrato ct ");
+		sql.append("inner join vdcliente cl on ");
+		sql.append("cl.codemp=ct.codempcl and cl.codfilial=ct.codfilialcl and cl.codcli=ct.codcli ");
+		sql.append("inner join vdmodcontr mc on ");
+		sql.append("mc.codemp=ct.codempmc and mc.codfilial=ct.codfilialmc and ");
+		sql.append("mc.codmodcontr=ct.codmodcontr ");
+		sql.append("left outer join vditcontrato ic on ");
+		sql.append("ic.codemp=ct.codemp and ic.codfilial=ct.codfilial and ic.codcontr=ct.codcontr ");
+		sql.append("left outer join sgitprefere6 itpf on ");
+		sql.append("itpf.codempbo=mc.codempbo and itpf.codfilialbo=mc.codfilialbo and itpf.codbanco=mc.codbanco ");
+		sql.append("and itpf.tipofebraban=(case when mc.tpmodcontr='S' then '01' else '00' end) ");
+		sql.append("left outer join sgprefere6  pf on ");
+		sql.append("pf.codemp=itpf.codemp and pf.codfilial=itpf.codfilial ");
+		return sql;
+	}
 	
 	public void setPrefs(Integer codemp, Integer codfilial) throws SQLException {
 		PreparedStatement ps = null;
@@ -61,7 +83,7 @@ public class DAOContato extends AbstractDAO {
 		prefs = new Object[ CONT_PREFS.values().length];
 		
 		try {
-			sql = new StringBuilder("select p.usactoseq " );
+			sql = new StringBuilder("select p.usactoseq, p.layoutfichaaval " );
 			sql.append( "from sgprefere3 p "); 
 			sql.append( "where  p.codemp=? and p.codfilial=?" );
 			
@@ -71,7 +93,8 @@ public class DAOContato extends AbstractDAO {
 			rs = ps.executeQuery();
 			if ( rs.next() ) {
 				
-				prefs[ CONT_PREFS.USACTOSEQ.ordinal() ] = rs.getString( CONT_PREFS.USACTOSEQ.toString() );		
+				prefs[ CONT_PREFS.USACTOSEQ.ordinal() ] = rs.getString( CONT_PREFS.USACTOSEQ.toString() );
+				prefs[ CONT_PREFS.LAYOUTFICHAAVAL.ordinal() ] = rs.getString( CONT_PREFS.LAYOUTFICHAAVAL.toString() );
 				
 			}
 			rs.close();
