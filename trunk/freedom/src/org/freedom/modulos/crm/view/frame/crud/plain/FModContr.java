1c90a53b452704bfa1f308232fd0cb25a167fa2c
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2012-07-24 20:26:15 +0000

    Inclusão dos campos de amarração de carteira de cobrança na tabela de modelo de contratos.

--- trunk/freedom/src/org/freedom/modulos/crm/view/frame/crud/plain/FModContr.java
+++ trunk/freedom/src/org/freedom/modulos/crm/view/frame/crud/plain/FModContr.java
@@ -29,29 +29,40 @@ import org.freedom.infra.model.jdbc.DbConnection;
 import org.freedom.library.persistence.GuardaCampo;
 import org.freedom.library.persistence.ListaCampos;
 import org.freedom.library.swing.component.JRadioGroup;
+import org.freedom.library.swing.component.JTextAreaPad;
 import org.freedom.library.swing.component.JTextFieldFK;
 import org.freedom.library.swing.component.JTextFieldPad;
 import org.freedom.library.swing.frame.FDados;
 import org.freedom.modulos.fnc.view.frame.crud.plain.FBanco;
+import org.freedom.modulos.fnc.view.frame.crud.plain.FCartCob;
 
 public class FModContr extends FDados {
 
 	private static final long serialVersionUID = 1L;
 
-	private JTextFieldPad txtCodModContr= new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private final JTextFieldPad txtCodModContr= new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 	
-	private JTextFieldPad txtCodBanco = new JTextFieldPad( JTextFieldPad.TP_STRING, 3, 0 );
+	private final JTextFieldPad txtCodBanco = new JTextFieldPad( JTextFieldPad.TP_STRING, 3, 0 );
 
-	private JTextFieldFK txtNomeBanco = new JTextFieldFK( JTextFieldFK.TP_STRING, 50, 0 );
+	private final JTextFieldFK txtNomeBanco = new JTextFieldFK( JTextFieldFK.TP_STRING, 50, 0 );
 	
-	private JTextFieldPad txtDescModContr = new JTextFieldPad( JTextFieldPad.TP_STRING, 80, 0 );
+	private final JTextFieldPad txtDescModContr = new JTextFieldPad( JTextFieldPad.TP_STRING, 80, 0 );
 	
-	private JTextFieldPad txtLayoutModContr = new JTextFieldPad( JTextFieldPad.TP_STRING, 100, 0 );	
+	private final JTextFieldPad txtLayoutModContr = new JTextFieldPad( JTextFieldPad.TP_STRING, 100, 0 );	
 	
+	private final JTextFieldPad txtCodCartCob = new JTextFieldPad( JTextFieldPad.TP_STRING, 3, 0 );
+	
+	private final JTextFieldFK txtDescCartCob = new JTextFieldFK( JTextFieldPad.TP_STRING, 50, 0 );
+
+	private final JTextAreaPad txaTexto1 = new JTextAreaPad(10000);
+	
+	private final JTextAreaPad txaTexto2 = new JTextAreaPad(10000);
+
 	private JRadioGroup<String, String> rgTpModContr = null;
-	
-	
+		
 	private ListaCampos lcBanco = new ListaCampos( this, "BO" );
+	
+	private final ListaCampos lcCartCob = new ListaCampos( this, "CB" );
 		
 	public FModContr( ) {
 
@@ -73,7 +84,7 @@ public class FModContr extends FDados {
 		rgTpModContr = new JRadioGroup<String, String>( 1, 2, vTpModContrLab, vTpModControVal );
 
 		setTitulo( "Modelo do Contrato" );
-		setAtribos( 50, 50, 500, 300 );
+		setAtribos( 50, 50, 500, 550 );
 		
 		adicCampo( txtCodModContr, 7, 20, 100, 20, "CODMODCONTR", "Cod.Mod.Contr.", ListaCampos.DB_PK, true );		
 		adicCampo( txtDescModContr, 110, 20, 350, 20, "DESCMODCONTR", "Descrição do Modelo de Contrato", ListaCampos.DB_SI, true );
@@ -81,7 +92,11 @@ public class FModContr extends FDados {
 		adicCampo( txtCodBanco, 7, 103, 80, 20, "CODBANCO", "Cód.banco", ListaCampos.DB_FK, txtNomeBanco, false );
 		adicDescFK( txtNomeBanco, 90, 103, 370, 20, "NOMEBANCO", "Nome do banco" );
 		adicDB( rgTpModContr, 7, 143, 300, 30, "TPMODCONTR", "Tipo do modelo do contrato", false );
-		
+		adicCampo( txtCodCartCob, 7, 193, 80, 20, "CodCartCob", "Cód.cart.cob.", ListaCampos.DB_FK, false );
+		adicDescFK( txtDescCartCob, 90, 193, 370, 20, "DescCartCob", "Descrição da carteira de cobraça" );		
+		adicDB( txaTexto1, 7, 233, 470, 90, "TEXTO1", "Texto 1", false );
+		adicDB( txaTexto2, 7, 343, 470, 90, "TEXTO2", "Texto 2", false );
+
 		setListaCampos( true, "MODCONTR", "VD" );
 	
 	}
@@ -94,6 +109,20 @@ public class FModContr extends FDados {
 		lcBanco.setQueryCommit( false );
 		lcBanco.setReadOnly( true );
 		txtCodBanco.setTabelaExterna( lcBanco, FBanco.class.getCanonicalName() );
+		
+		/***************
+		 * FNCARTCOB *
+		 ***************/
+		lcCartCob.add( new GuardaCampo( txtCodCartCob, "CodCartCob", "Cód.cart.cob.", ListaCampos.DB_PK, txtDescCartCob, false ) );
+		lcCartCob.add( new GuardaCampo( txtDescCartCob, "DescCartCob", "Descrição da carteira de cobrança", ListaCampos.DB_SI, false ) );
+		lcCartCob.setWhereAdicSubSel( "CODBANCO=master.CODBANCO" );
+		lcCartCob.montaSql( false, "CARTCOB", "FN" );
+		lcCartCob.setReadOnly( true );
+		txtCodCartCob.setListaCampos( lcCartCob );
+		txtCodCartCob.setTabelaExterna( lcCartCob, FCartCob.class.getCanonicalName() );
+		txtCodCartCob.setNomeCampo( "CodCartCob" );
+		txtCodCartCob.setFK( true );
+		txtDescCartCob.setLabel( "Descrição da carteira de cobrança" );
 
 	}
 	
@@ -101,6 +130,7 @@ public class FModContr extends FDados {
 
 		super.setConexao( cn );
 		lcBanco.setConexao( cn );
+		lcCartCob.setConexao( cn );
 		
 	}
 }
