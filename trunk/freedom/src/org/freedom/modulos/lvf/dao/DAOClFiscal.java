fb2dca7859ed90a802d8005d845310f66d286153
brunolnascimento@gmail.com <brunolnascimento@gmail.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2012-08-02 03:21:04 +0000

    Implementação de mecanismo para copiar classificações fiscais.

--- trunk/freedom/src/org/freedom/modulos/lvf/dao/DAOClFiscal.java
+++ trunk/freedom/src/org/freedom/modulos/lvf/dao/DAOClFiscal.java
@@ -2,7 +2,9 @@ package org.freedom.modulos.lvf.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.apache.poi.hssf.record.formula.eval.ConcatEval;
 import org.freedom.infra.dao.AbstractDAO;
 import org.freedom.infra.model.jdbc.DbConnection;
 
@@ -29,7 +31,7 @@ public class DAOClFiscal extends AbstractDAO {
 		super( cn );
 	}
 	
-	public void cloneClFiscal(){
+	public boolean cloneClFiscal(Integer codemp, Integer codfilial, String newcodfisc, String codfisc) throws SQLException {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		StringBuilder sql = null;
@@ -48,11 +50,95 @@ public class DAOClFiscal extends AbstractDAO {
 			sql.append( "CODFILIALTT,  CODTRATTRIB, CODEMPME, CODFILIALME, CODMENS, SITPISFISC, SITCOFINSFISC, TIPOST, MARGEMVLAGR, CODNCM, CODNBM, EXTIPI, CODSERV ");
 			sql.append( "FROM LFCLFISCAL where CODEMP=? and CODFILIAL=? AND CODFISC=? ");
 			
+			ps = getConn().prepareStatement( sql.toString() );
+			int param = 1;
+			ps.setInt( param++, codemp );
+			ps.setInt( param++, codfilial );
+			ps.setString( param++, newcodfisc );
+			ps.setInt( param++, codemp );
+			ps.setInt( param++, codfilial );
+			ps.setString( param++, codfisc );
+			
+			ps.execute();
+			ps.close();
 			
 		} finally {
-			
+			ps = null;
 		}
+		
+		return true;
 	}
 	
+	public boolean cloneItClFiscal(Integer codemp, Integer codfilial, String newcodfisc, String codfisc, Integer itemclassifisc) throws SQLException {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		StringBuilder sql = null;
+		Integer retorno = new Integer( 0 );
+		
+		try{
+			
+			//Insere dados copiadas na classificação correta.
+			sql = new StringBuilder();
+			sql.append( "INSERT INTO lfitclfiscal ( CODEMP, CODFILIAL, CODFISC, CODITFISC, ORIGFISC, CODEMPTT, CODFILIALTT, TIPOFISC, TPREDICMSFISC, REDFISC, CODTRATTRIB, NOUFITFISC, CODEMPFC, CODFILIALFC, ");
+			sql.append( "CODFISCCLI, ALIQFISC, ALIQFISCINTRA, ALIQLFISC, ALIQIPIFISC, ALIQPISFISC, ALIQCOFINSFISC, ALIQCSOCIALFISC, ALIQIRFISC, ALIQFUNRURALFISC, ALIQIIFISC, CODEMPME, ");
+			sql.append( "CODFILIALME, CODMENS, CODEMPTM, CODFILIALTM, CODTIPOMOV, TIPOST, MARGEMVLAGR, GERALFISC, CODEMPSP, CODFILIALSP, CODSITTRIBPIS, IMPSITTRIBPIS, CODEMPSC, CODFILIALSC,"); 
+			sql.append( "CODSITTRIBCOF, IMPSITTRIBCOF, CODEMPSI, CODFILIALSI, CODSITTRIBIPI, IMPSITTRIBIPI, TPCALCIPI, VLRIPIUNIDTRIB, MODBCICMS,  MODBCICMSST, CODPAIS, SIGLAUF, ");
+			sql.append( "VLRPISUNIDTRIB, VLRCOFUNIDTRIB, TIPOUSOITFISC, REDBASEST, REDBASEFRETE, CODEMPRA, CODFILIALRA, CODREGRA, CODEMPIS,  CODFILIALIS, CODSITTRIBISS, IMPSITTRIBISS,"); 
+			sql.append( "ALIQISSFISC, RETENSAOISS, INDAPURIPI, CODEMPCN, CODFILIALCN, CSOSN, ALIQICMSIMP, PERCCREDPRESIMP, ADICIPIBASEICMS ) ");
+
+			sql.append( "select ?, ?, ?, ?, ORIGFISC, CODEMPTT, CODFILIALTT, TIPOFISC, TPREDICMSFISC, REDFISC, CODTRATTRIB, NOUFITFISC, CODEMPFC, CODFILIALFC, ");
+			sql.append( "CODFISCCLI, ALIQFISC, ALIQFISCINTRA, ALIQLFISC, ALIQIPIFISC, ALIQPISFISC, ALIQCOFINSFISC, ALIQCSOCIALFISC, ALIQIRFISC, ALIQFUNRURALFISC, ALIQIIFISC, CODEMPME, "); 
+			sql.append( "CODFILIALME, CODMENS, CODEMPTM, CODFILIALTM, CODTIPOMOV, TIPOST, MARGEMVLAGR, GERALFISC, CODEMPSP, CODFILIALSP, CODSITTRIBPIS, IMPSITTRIBPIS, CODEMPSC, CODFILIALSC, "); 
+			sql.append( "CODSITTRIBCOF, IMPSITTRIBCOF, CODEMPSI, CODFILIALSI, CODSITTRIBIPI, IMPSITTRIBIPI, TPCALCIPI, VLRIPIUNIDTRIB, MODBCICMS,  MODBCICMSST, CODPAIS, SIGLAUF, ");
+			sql.append( "VLRPISUNIDTRIB, VLRCOFUNIDTRIB, TIPOUSOITFISC, REDBASEST, REDBASEFRETE, CODEMPRA, CODFILIALRA, CODREGRA, CODEMPIS,  CODFILIALIS, CODSITTRIBISS, IMPSITTRIBISS, "); 
+			sql.append( "ALIQISSFISC, RETENSAOISS, INDAPURIPI, CODEMPCN, CODFILIALCN, CSOSN, ALIQICMSIMP, PERCCREDPRESIMP, ADICIPIBASEICMS  from lfitclfiscal where codemp=? and codfilial=? and codfisc=? and coditfisc=? ");
+
+			ps = getConn().prepareStatement( sql.toString() );
+			int param = 1;
+			ps.setInt( param++, codemp );
+			ps.setInt( param++, codfilial );
+			ps.setString( param++, newcodfisc );
+			ps.setInt( param++, itemclassifisc );
+			ps.setInt( param++, codemp );
+			ps.setInt( param++, codfilial );
+			ps.setString( param++, codfisc );
+			ps.setInt( param++, itemclassifisc );
+			
+			ps.execute();
+			ps.close();
+			
+		} finally {
+			ps = null;
+		}
+		
+		return true;
+	}
+
+	public Integer getQtdItemFiscal(Integer codemp, Integer codfilial, String codfisc) throws SQLException{
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		StringBuilder sql = null;
+		Integer quantidade = new Integer( 0 );
+		
+		try{
+			sql = new StringBuilder();
+			sql.append( "select count(*) from lfitclfiscal where codemp=? and codfilial=? and codfisc=? ");
+			ps = getConn().prepareStatement( sql.toString() );
+			int param = 1;
+			ps.setInt( param++, codemp );
+			ps.setInt( param++, codfilial );
+			ps.setString( param++, codfisc );
+			
+			rs = ps.executeQuery();
+			if(rs.next()){
+				quantidade = rs.getInt( 1 );
+			}
+			
+		}finally{
+			rs = null;
+			ps = null;
+		}
+		return quantidade;
+	}
 	
 }
