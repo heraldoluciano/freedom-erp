1c0099c6b770f73899006c020828d39a883c7ba4
brunolnascimento <brunolnascimento@gmail.com>
2012-08-17 14:46:56 +0000

    Criação de amarrações na tela de relatório de contato.

--- freedom/src/org/freedom/modulos/crm/view/dialog/report/DLRCont.java
+++ freedom/src/org/freedom/modulos/crm/view/dialog/report/DLRCont.java
@@ -37,10 +37,16 @@ import org.freedom.library.swing.component.JRadioGroup;
 import org.freedom.library.swing.component.JTextFieldFK;
 import org.freedom.library.swing.component.JTextFieldPad;
 import org.freedom.library.swing.dialog.FFDialogo;
+import org.freedom.modulos.cfg.view.frame.crud.plain.FMunicipio;
+import org.freedom.modulos.cfg.view.frame.crud.plain.FUF;
+import org.freedom.modulos.crm.view.frame.crud.plain.FAtividade;
+import org.freedom.modulos.crm.view.frame.crud.plain.FOrigContato;
+import org.freedom.modulos.std.view.frame.crud.plain.FSetor;
+import org.freedom.modulos.std.view.frame.crud.plain.FTipoCli;
 
 public class DLRCont extends FFDialogo {
 	
-	public static enum VALORES{ ORDEM, OBSERVACAO, DE, A, FISICA, CIDADE, JURIDICA, MODO, SETOR, DESCSETOR, TIPOIMP } 
+	public static enum VALORES{ ORDEM, OBSERVACAO, DE, A, FISICA, CIDADE, JURIDICA, MODO, SETOR, DESCSETOR, TIPOIMP, CODTIPOCLI, SIGLAUF, CODMUNIC, CODGRUP, CODATIV, CODORIGCONT, EDIFICIO   } 
 
 	private static final long serialVersionUID = 1L;
 
@@ -102,74 +108,135 @@ public class DLRCont extends FFDialogo {
 
 	private JTextFieldFK txtDescSetor = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 	
-	private JTextFieldPad txtTipoCli = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodTipoCli = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
 	private JTextFieldFK txtDescTipoCli = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 	
-	private JTextFieldPad txtCodSiglaUF = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtSiglaUF = new JTextFieldPad( JTextFieldPad.TP_STRING, 8, 0 );
 
 	private JTextFieldFK txtDescSiglaUF = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 	
-	private JTextFieldPad txtCodMunicipio = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodMunicipio = new JTextFieldPad( JTextFieldPad.TP_STRING, 8, 0 );
 
 	private JTextFieldFK txtDescMunicipio = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 	
-	private JTextFieldPad txtCodGrupo = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodGrup = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
-	private JTextFieldFK txtDescGrupo = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
+	private JTextFieldFK txtDescGrup = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 
-	private JTextFieldPad txtCodAtividade = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodAtiv = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
-	private JTextFieldFK txtDescAtividade = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
+	private JTextFieldFK txtDescAtiv = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 	
-	private JTextFieldPad txtCodOrigem = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
+	private JTextFieldPad txtCodOrigCont = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
-	private JTextFieldFK txtDescOrigem = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
+	private JTextFieldFK txtDescOrigCont = new JTextFieldFK( JTextFieldPad.TP_STRING, 40, 0 );
 	
-	private JTextFieldPad txtEdificio = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 30, 0 );
+	private JTextFieldPad txtEdificio = new JTextFieldPad( JTextFieldPad.TP_STRING, 30, 0 );
 
 	private ListaCampos lcSetor = new ListaCampos( this );
+	
+	private ListaCampos lcTipoCli = new ListaCampos( this );
+
+	private ListaCampos lcMunic = new ListaCampos( this );
+
+	private ListaCampos lcUF = new ListaCampos( this );
+	
+	private ListaCampos lcGrupFK = new ListaCampos( this );
+	
+	private ListaCampos lcAtivFK = new ListaCampos( this );
+	
+	private ListaCampos lcOrigCont = new ListaCampos( this );
+	
+	
 
 	public DLRCont( Component cOrig, DbConnection cn ) {
 
 		super( cOrig );
 		setTitulo( "Relatório de Contatos" );
 		setAtribos( 480, 690 );
-		vLabs.addElement( "Código" );
-		vLabs.addElement( "Nome" );
-		vVals.addElement( "C" );
-		vVals.addElement( "D" );
-		rgOrdem = new JRadioGroup<String, String>( 1, 2, vLabs, vVals );
-		rgOrdem.setVlrString( "D" );
 
-		vLabsModo.addElement( "Resumido" );
-		vLabsModo.addElement( "Completo" );
-		vValsModo.addElement( "R" );
-		vValsModo.addElement( "C" );
-		rgModo = new JRadioGroup<String, String>( 1, 2, vLabsModo, vValsModo );
-		rgModo.setVlrString( "R" );
+		montaRadioGroup();
+		montaListaCampos();
+		montaTela();
+
+		setConexao(cn);
+
+	}
+	
+	public void montaListaCampos(){
 		
-		
-
-		vLabsTipoImp.addElement( "Gráfico" );
-		vLabsTipoImp.addElement( "Texto" );
-		vValsTipoImp.addElement( "G" );
-		vValsTipoImp.addElement( "T" );
-		rgTipoImp = new JRadioGroup<String, String>( 1, 2, vLabsTipoImp, vValsTipoImp );
-		rgTipoImp.setVlrString( "G" );
-
-		cbObs.setVlrString( "N" );
-		cbFis.setVlrString( "N" );
-		cbJur.setVlrString( "S" );
-
+		/** SETOR */
 		lcSetor.add( new GuardaCampo( txtCodSetor, "CodSetor", "Código", ListaCampos.DB_PK, false ) );
 		lcSetor.add( new GuardaCampo( txtDescSetor, "DescSetor", "Descrição", ListaCampos.DB_SI, false ) );
 		lcSetor.montaSql( false, "SETOR", "VD" );
 		lcSetor.setReadOnly( true );
-		txtCodSetor.setTabelaExterna( lcSetor, null );
+		txtCodSetor.setTabelaExterna( lcSetor, FSetor.class.getCanonicalName() );
 		txtCodSetor.setFK( true );
 		txtCodSetor.setNomeCampo( "CodSetor" );
+		
+		/** TIPO DE CLIENTE */
+		lcTipoCli.add( new GuardaCampo( txtCodTipoCli, "CodTipoCli", "Cód.tp.cli", ListaCampos.DB_PK, false ) );
+		lcTipoCli.add( new GuardaCampo( txtDescTipoCli, "DescTipoCli", "Descrição do tipo de cliente", ListaCampos.DB_SI, false ) );
+		lcTipoCli.montaSql( false, "TIPOCLI", "VD" );
+		lcTipoCli.setReadOnly( true );
+		txtCodTipoCli.setTabelaExterna( lcTipoCli, FTipoCli.class.getCanonicalName() );
+		txtCodTipoCli.setFK( true );
+		txtCodTipoCli.setNomeCampo( "CodTipoCli" );
+		
+		/** MUNICIPIO */
+		lcMunic.setUsaME( false );
+		lcMunic.add( new GuardaCampo( txtCodMunicipio, "CodMunic", "Cód.Munic.", ListaCampos.DB_PK, false ) );
+		lcMunic.add( new GuardaCampo( txtDescMunicipio, "NomeMunic", "Nome Munic.", ListaCampos.DB_SI, false ) );
+		lcMunic.setDinWhereAdic( "SIGLAUF = #S", txtSiglaUF );
+		lcMunic.montaSql( false, "MUNICIPIO", "SG" );
+		lcMunic.setReadOnly( true );
+		txtCodMunicipio.setTabelaExterna( lcMunic, FMunicipio.class.getCanonicalName() );
+		txtCodMunicipio.setFK( true );
+		txtCodMunicipio.setNomeCampo( "CodMunic" );
 
+		/** UF */
+		lcUF.setUsaME( false );
+		lcUF.add( new GuardaCampo( txtSiglaUF, "SiglaUf", "Sigla", ListaCampos.DB_PK, false ) );
+		lcUF.add( new GuardaCampo( txtDescSiglaUF, "NomeUf", "Nome", ListaCampos.DB_SI, false ) );
+		//lcUF.setWhereAdic( "CODPAIS = 76 ");
+		lcUF.montaSql( false, "UF", "SG" );
+		lcUF.setReadOnly( true );
+		txtSiglaUF.setTabelaExterna( lcUF, FUF.class.getCanonicalName() );
+		txtSiglaUF.setFK( true );
+		txtSiglaUF.setNomeCampo( "SiglaUf" );
+				
+		/** INTERESSES */
+		lcGrupFK.add( new GuardaCampo( txtCodGrup, "CodGrup", "Cód.grup.", ListaCampos.DB_PK, false ) );
+		lcGrupFK.add( new GuardaCampo( txtDescGrup, "DescGrup", "Descrição do grupo", ListaCampos.DB_SI, false ) );
+		lcGrupFK.montaSql( false, "GRUPO", "EQ" );
+		lcGrupFK.setReadOnly( true );
+		txtCodGrup.setFK( true );
+		txtCodGrup.setTabelaExterna( lcGrupFK, null );
+		txtCodGrup.setNomeCampo( "CodGrup" );
+		
+		/** ATIVIDADE */
+		lcAtivFK.add( new GuardaCampo( txtCodAtiv, "CodAtiv", "Cód.ativ.", ListaCampos.DB_PK, false ) );
+		lcAtivFK.add( new GuardaCampo( txtDescAtiv, "DescAtiv", "Descrição da atividade", ListaCampos.DB_SI, false ) );
+		lcAtivFK.montaSql( false, "ATIVIDADE", "TK" );
+		lcAtivFK.setReadOnly( true );
+		txtCodAtiv.setFK( true );
+		txtCodAtiv.setTabelaExterna( lcAtivFK, FAtividade.class.getCanonicalName() );
+		txtCodAtiv.setNomeCampo( "CodAtiv" );
+		
+		/** ORIGEM DO CONTATO */
+		lcOrigCont.add( new GuardaCampo( txtCodOrigCont, "CodOrigCont", "Cód.origem", ListaCampos.DB_PK, false ) );
+		lcOrigCont.add( new GuardaCampo( txtDescOrigCont, "DescOrigCont", "descrição da origem", ListaCampos.DB_SI, false ) );
+		lcOrigCont.montaSql( false, "ORIGCONT", "TK" );
+		lcOrigCont.setReadOnly( true );
+		txtCodOrigCont.setFK( true );
+		txtCodOrigCont.setTabelaExterna( lcOrigCont, FOrigContato.class.getCanonicalName() );
+		txtCodOrigCont.setNomeCampo( "CodOrigCont" );
+		
+	}
+	
+	public void montaTela(){
+		
 		pnlbSelec.add( lbSelec );
 		adic( lbOrdem, 7, 5, 180, 20 );
 		adic( rgOrdem, 7, 25, 220, 30 );
@@ -192,23 +259,50 @@ public class DLRCont extends FFDialogo {
 		adic( rgModo, 7, 220, 433, 30 );
 		adic( txtCodSetor, 7, 275, 80, 20, "Cód.setor" );
 		adic( txtDescSetor, 90, 275, 350, 20, "Descrição do setor" );
-		adic( txtTipoCli, 7, 315, 80, 20, "Cód.Cliente" );
+		adic( txtCodTipoCli, 7, 315, 80, 20, "Cód.tp.Cliente" );
 		adic( txtDescTipoCli, 90, 315, 350, 20, "Descrição do tipo de cliente" );
-		adic( txtCodSiglaUF, 7, 355, 80, 20, "Cód.Sigla UF" );
+		adic( txtSiglaUF, 7, 355, 80, 20, "Cód.Sigla UF" );
 		adic( txtDescSiglaUF, 90, 355, 350, 20, "Sigla UF" );
 		adic( txtCodMunicipio, 7, 395, 80, 20, "Cód.Município" );
 		adic( txtDescMunicipio, 90, 395, 350, 20, "Município" );
-		adic( txtCodGrupo, 7, 435, 80, 20, "Cód.Grupo" );
-		adic( txtDescGrupo, 90, 435, 350, 20, "Descrição do Grupo" );
-		adic( txtCodAtividade, 7, 475, 80, 20, "Cód.Atividade" );
-		adic( txtDescAtividade, 90, 475, 350, 20, "Descrição da Atividade" );
-		adic( txtCodOrigem, 7, 515, 80, 20, "Cód.Origem" );
-		adic( txtDescOrigem, 90, 515, 350, 20, "Descrição da Origem" );
+		adic( txtCodGrup, 7, 435, 80, 20, "Cód.Grupo" );
+		adic( txtDescGrup, 90, 435, 350, 20, "Descrição do Grupo" );
+		adic( txtCodAtiv, 7, 475, 80, 20, "Cód.Atividade" );
+		adic( txtDescAtiv, 90, 475, 350, 20, "Descrição da Atividade" );
+		adic( txtCodOrigCont, 7, 515, 80, 20, "Cód.Origem" );
+		adic( txtDescOrigCont, 90, 515, 350, 20, "Descrição da Origem" );
 		adic( txtEdificio, 7, 555, 433, 20, "Edifício" );
 		adic( cbObs, 7, 575, 220, 30 );
 		
+	}
+	
+	public void montaRadioGroup(){
+		
+		vLabs.addElement( "Código" );
+		vLabs.addElement( "Nome" );
+		vVals.addElement( "C" );
+		vVals.addElement( "D" );
+		rgOrdem = new JRadioGroup<String, String>( 1, 2, vLabs, vVals );
+		rgOrdem.setVlrString( "D" );
 
-		lcSetor.setConexao( cn );
+		vLabsModo.addElement( "Resumido" );
+		vLabsModo.addElement( "Completo" );
+		vValsModo.addElement( "R" );
+		vValsModo.addElement( "C" );
+		rgModo = new JRadioGroup<String, String>( 1, 2, vLabsModo, vValsModo );
+		rgModo.setVlrString( "R" );
+		
+		vLabsTipoImp.addElement( "Gráfico" );
+		vLabsTipoImp.addElement( "Texto" );
+		vValsTipoImp.addElement( "G" );
+		vValsTipoImp.addElement( "T" );
+		rgTipoImp = new JRadioGroup<String, String>( 1, 2, vLabsTipoImp, vValsTipoImp );
+		rgTipoImp.setVlrString( "G" );
+
+		cbObs.setVlrString( "N" );
+		cbFis.setVlrString( "N" );
+		cbJur.setVlrString( "S" );
+		
 	}
 
 	public String[] getValores() {
@@ -228,9 +322,25 @@ public class DLRCont extends FFDialogo {
 		sRetorno[ VALORES.SETOR.ordinal() ] = txtCodSetor.getText();
 		sRetorno[ VALORES.DESCSETOR.ordinal() ] = txtDescSetor.getText();
 		sRetorno[ VALORES.TIPOIMP.ordinal() ] = rgTipoImp.getVlrString();
-		
-		
+		sRetorno[ VALORES.CODTIPOCLI.ordinal() ] = txtCodTipoCli.getVlrInteger().toString();
+		sRetorno[ VALORES.SIGLAUF.ordinal() ] = txtSiglaUF.getVlrString();
+		sRetorno[ VALORES.CODMUNIC.ordinal() ] = txtCodMunicipio.getVlrString();
+		sRetorno[ VALORES.CODGRUP.ordinal() ] = txtCodGrup.getVlrString();
+		sRetorno[ VALORES.CODATIV.ordinal() ] = txtCodAtiv.getVlrInteger().toString();
+		sRetorno[ VALORES.CODORIGCONT.ordinal() ] = txtCodOrigCont.getVlrInteger().toString();
+		sRetorno[ VALORES.EDIFICIO.ordinal() ] = txtEdificio.getVlrString();
 		
 		return sRetorno;
+	}
+	
+	public void setConexao(DbConnection cn){
+		lcSetor.setConexao( cn );
+		lcTipoCli.setConexao( cn );
+		lcMunic.setConexao( cn );
+		lcUF.setConexao( cn );
+		lcGrupFK.setConexao( cn );
+		lcAtivFK.setConexao( cn );
+		lcOrigCont.setConexao( cn );
+	
 	}
 }
