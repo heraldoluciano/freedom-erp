5478b3b82b4ac765a9f048a258b56df9bd977753
robson <robson@stpinf.com>
2012-08-24 02:02:45 +0000

    Implementações de layout e sumarização do registro de inventário.

--- freedom/src/org/freedom/modulos/lvf/FreedomLVF.java
+++ freedom/src/org/freedom/modulos/lvf/FreedomLVF.java
@@ -107,15 +107,15 @@ public class FreedomLVF extends AplicativoPD {
 		addOpcao( 200600000, TP_OPCAO_ITEM, "Modelo de NFs", "Modelo de NFs", 'M', 200607000, 2, true, FModNota.class );
 		addOpcao( 200600000, TP_OPCAO_ITEM, "Modelo de Doc. Fiscais", "Modelo de Doc. Fiscais", 'i', 200608000, 2, true, FModDocFisc.class );
 		
-		addOpcao( -1, TP_OPCAO_MENU, "Estoque", "", 't', 300000000, 0, false, null );
-		addOpcao( 300000000, TP_OPCAO_ITEM, "Registro de Inventário", "Registro de Inventário", 'E', 300100000, 1, true, FRRegitroInventario.class );
-		
-		addOpcao( -1, TP_OPCAO_MENU, "Entrada", "", 'E', 400000000, 0, false, null );
-		addOpcao( 400000000, TP_OPCAO_ITEM, "Registro de Entrada", "Registro de Entrada", 'E', 400100000, 1, true, FRRegitroEntrada.class );
+		addOpcao( -1, TP_OPCAO_MENU, "Entrada", "", 'E', 300000000, 0, false, null );
+		addOpcao( 300000000, TP_OPCAO_ITEM, "Registro de Entrada", "Registro de Entrada", 'E', 300100000, 1, true, FRRegitroEntrada.class );
 
-		addOpcao( -1, TP_OPCAO_MENU, "Saida", "", 'S', 500000000, 0, false, null );
-		addOpcao( 500000000, TP_OPCAO_ITEM, "Registro de Saida", "Registro de Saida", 'S', 500100000, 1, true, FRRegitroSaida.class );
-		addOpcao( 500000000, TP_OPCAO_ITEM, "Registro de Duplicatas", "Registro de Duplicatas", 'S', 500200000, 1, true, FRRegDuplicatas.class);
+		addOpcao( -1, TP_OPCAO_MENU, "Saida", "", 'S', 400000000, 0, false, null );
+		addOpcao( 400000000, TP_OPCAO_ITEM, "Registro de Saida", "Registro de Saida", 'S', 400100000, 1, true, FRRegitroSaida.class );
+		addOpcao( 400000000, TP_OPCAO_ITEM, "Registro de Duplicatas", "Registro de Duplicatas", 'S', 400200000, 1, true, FRRegDuplicatas.class);
+
+		addOpcao( -1, TP_OPCAO_MENU, "Estoque", "", 't', 500000000, 0, false, null );
+		addOpcao( 500000000, TP_OPCAO_ITEM, "Registro de Inventário", "Registro de Inventário", 'E', 500100000, 1, true, FRRegitroInventario.class );
 
 		addBotao( "barraUsuario.gif", "Cliente", "Clientes", 100101050, FCliente.class );
 		addBotao( "btForneced.gif", "Fornecedor", "Fornecedor", 100108000, FFornecedor.class );
