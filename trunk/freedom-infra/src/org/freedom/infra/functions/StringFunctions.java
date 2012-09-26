ab75cb98f99581b95fe41130fd22da111fe05fb1
brunolnascimento@gmail.com <brunolnascimento@gmail.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2012-09-26 20:21:00 +0000

    Criação de método para retirar acentos de uma String. Correção na tela de op na busca da unidade do produto e liberação do botão gerar rma.

--- trunk/freedom-infra/src/org/freedom/infra/functions/StringFunctions.java
+++ trunk/freedom-infra/src/org/freedom/infra/functions/StringFunctions.java
@@ -29,6 +29,8 @@ package org.freedom.infra.functions;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class StringFunctions {
 
@@ -243,5 +245,38 @@ public final class StringFunctions {
 		
 		return sRet;
 	}
+	
+	   public static String[] REPLACES = { "a", "e", "i", "o", "u", "c", "A", "E", "I", "O", "U", "C" };  
+	   
+	   public static Pattern[] PATTERNS = null;  
+	  
+	   public static void compilePatterns() {  
+	      PATTERNS = new Pattern[REPLACES.length];  
+	      PATTERNS[0]  = Pattern.compile("[âãáàä]");  
+	      PATTERNS[1]  = Pattern.compile("[éèêë]");  
+	      PATTERNS[2]  = Pattern.compile("[íìîï]");  
+	      PATTERNS[3]  = Pattern.compile("[óòôõö]");  
+	      PATTERNS[4]  = Pattern.compile("[úùûü]");  
+	      PATTERNS[5]  = Pattern.compile("[ç]");  
+	      PATTERNS[6]  = Pattern.compile("[ÂÃÁÀÄ]");  
+	      PATTERNS[7]  = Pattern.compile("[ÉÈÊË]");  
+	      PATTERNS[8]  = Pattern.compile("[ÍÌÎÏ]");  
+	      PATTERNS[9]  = Pattern.compile("[ÓÒÔÕÖ]");  
+	      PATTERNS[10] = Pattern.compile("[ÚÙÛÜ]");  
+	      PATTERNS[11] = Pattern.compile("[Ç]");  
+	   }  
+	
+	   public static String tiraCaracEspecias(String text) {  
+	      if (PATTERNS == null) {  
+	         compilePatterns();  
+	      }  
+	  
+	      String result = text;  
+	      for (int i = 0; i < PATTERNS.length; i++) {  
+	         Matcher matcher = PATTERNS[i].matcher(result);  
+	         result = matcher.replaceAll(REPLACES[i]);  
+	      }  
+	      return result;  
+	   }  
 
 }
