5855229e6a29edf70429559ba91a4484694bdb06
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2012-11-20 17:01:54 +0000

    Ajustes na nota fiscal de importação para evitar dízima periódica.

--- trunk/freedom/src/org/freedom/modulos/gms/view/frame/crud/detail/FCompra.java
+++ trunk/freedom/src/org/freedom/modulos/gms/view/frame/crud/detail/FCompra.java
@@ -2928,8 +2928,11 @@ public class FCompra extends FDetalhe implements PostListener, CarregaListener, 
 			//sql.append( "(ii.vlrad + ii.vlrii + ii.vlripi + ii.vlrpis + ii.vlrcofins + ii.vlrtxsiscomex ) vlrliqitcompra, (ii.vlrad + ii.vlrii ) vlrproditcompra,   ( (ii.vlrad + ii.vlrii ) / qtd) precoitcompra, " );
 			sql.append( "cast(cast(cast(ii.vlrad + ii.vlrii + ii.vlripi + ii.vlrpis + ii.vlrcofins + ii.vlrtxsiscomex ");
 			sql.append( " + (case when lf.adicicmstotnota='S' then ii.vlricms else 0.00 end)" );
-			sql.append( "  as decimal(15,4) ) / ii.qtd as decimal(15,4)) * ii.qtd as decimal(15,4))  vlrliqitcompra, (ii.vlrad) vlrproditcompra,   ( (ii.vlrad ) / ii.qtd) precoitcompra, " );
-		
+			sql.append( "  as decimal(15,4) ) / ii.qtd as decimal(15,4)) * ii.qtd as decimal(15,4))  vlrliqitcompra ");
+			// valor total dos produtos com cálculo para evitar dízima periódica
+			sql.append( ", cast ( cast( (ii.vlrad) vlrproditcompra as decimal(15,4)) / cast(ii.qtd as decimal(15,4)) * cast(ii.qtd as decimal(15,4)) as decimal(15,4)) ");
+			// preço do ítem para evitar dízima periódica
+			sql.append(" ,  cast( cast ( cast( (ii.vlrad) vlrproditcompra as decimal(15,4)) / cast(ii.qtd as decimal(15,4)) * cast(ii.qtd as decimal(15,4)) as decimal(15,4)) / cast(ii.qtd as decimal(15,4)) ) as decimal(15,4)) precoitcompra, " );
 			
 			// Depois da inserção de parâmetro para adicionar ICMS no total da nota ajustar a linha abaixo.
 			//sql.append( "(ii.vlrad + ii.vlrii + ii.vlripi + ii.vlrpis + ii.vlrcofins + ii.vlrtxsiscomex + ii.vlricms ) vlrliqitcompra, (ii.vlrad) vlrproditcompra,   ( (ii.vlrad ) / qtd) precoitcompra, " );
