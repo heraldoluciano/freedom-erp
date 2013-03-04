7d5c61be0f030002de73bfde3473bfc9433a8f35
brunolnascimento@gmail.com <brunolnascimento@gmail.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2013-03-04 19:34:06 +0000

    Criação de query para ajustes no código fiscal  e item fiscal na compra.

--- trunk/freedom/dados/sql/update/ajusta_cpitcompra_lfitclfiscal.sql
+++ trunk/freedom/dados/sql/update/ajusta_cpitcompra_lfitclfiscal.sql
@@ -3,17 +3,27 @@ update cpcompra set emmanut = 'S';
 update cpitcompra ic set codempif = 68, codfilialif = 1,
 codfisc=
 ( select codfisc from lfbuscafiscalsp(ic.codemppd,ic.codfilialpd,ic.codprod,
-68 , 1 , ( select codfor from cpcompra where codcompra = ic.codcompra ) ,
-68 , 1 , ( select codtipomov from cpcompra where codcompra = ic.codcompra ) , 'CP',
+( select codempfr from cpcompra where codcompra = ic.codcompra) ,
+( select codfilialfr from cpcompra where codcompra = ic.codcompra) ,
+( select codfor from cpcompra where codcompra = ic.codcompra ) ,
+( select codemptm from cpcompra where codcompra = ic.codcompra) ,
+( select codfilialtm from cpcompra where codcompra = ic.codcompra),
+( select codtipomov from cpcompra where codcompra = ic.codcompra ) , 'CP',
 null, null, null, null,null )
 )
 ,
 coditfisc=
 ( select coditfisc from lfbuscafiscalsp(ic.codemppd,ic.codfilialpd,ic.codprod,
-68 , 1 , ( select codfor from cpcompra where codcompra = ic.codcompra ) ,
-68 , 1 , ( select codtipomov from cpcompra where codcompra = ic.codcompra ) , 'CP'
+( select codempfr from cpcompra f where codcompra = ic.codcompra),
+( select codfilialfr from cpcompra where codcompra = ic.codcompra) ,
+( select codfor from cpcompra where codcompra = ic.codcompra ) ,
+( select codemptm from cpcompra where codcompra = ic.codcompra),
+( select codfilialtm from cpcompra where codcompra = ic.codcompra),
+( select codtipomov from cpcompra where codcompra = ic.codcompra ) , 'CP'
 , null, null, null, null,null )
 )
 where ic.coditfisc is null;
 
 update cpcompra set emmanut = 'N';
+
+commit work;
