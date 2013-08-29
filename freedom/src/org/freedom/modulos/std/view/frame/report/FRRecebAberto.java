d189117a4421761b79c0d071b8d24dc21cd879e1
robson <robson@stpinf.com>
2013-08-29 19:41:11 +0000

    Implementação de query no relatório de recebimentos em aberto e criação de campos de filtros.

--- freedom/src/org/freedom/modulos/std/view/frame/report/FRRecebAberto.java
+++ freedom/src/org/freedom/modulos/std/view/frame/report/FRRecebAberto.java
@@ -23,11 +23,11 @@
 
 package org.freedom.modulos.std.view.frame.report;
 
-import java.math.BigDecimal;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Vector;
@@ -37,90 +37,82 @@ import javax.swing.SwingConstants;
 
 import net.sf.jasperreports.engine.JasperPrintManager;
 
-import org.freedom.infra.functions.StringFunctions;
 import org.freedom.infra.model.jdbc.DbConnection;
-import org.freedom.library.component.ImprimeOS;
 import org.freedom.library.functions.Funcoes;
-import org.freedom.library.persistence.GuardaCampo;
 import org.freedom.library.persistence.ListaCampos;
-import org.freedom.library.swing.component.JCheckBoxPad;
 import org.freedom.library.swing.component.JLabelPad;
-import org.freedom.library.swing.component.JRadioGroup;
-import org.freedom.library.swing.component.JTextFieldFK;
 import org.freedom.library.swing.component.JTextFieldPad;
 import org.freedom.library.swing.frame.Aplicativo;
-import org.freedom.library.swing.frame.AplicativoPD;
 import org.freedom.library.swing.frame.FPrinterJob;
 import org.freedom.library.swing.frame.FRelatorio;
 import org.freedom.library.type.TYPE_PRINT;
 
-public class FRRecebAberto extends FRelatorio {
+public class FRRecebAberto extends FRelatorio implements FocusListener {
 
 	private static final long serialVersionUID = 1L;
 
 	private JTextFieldPad txtDatafim = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
+	
+	private JTextFieldPad txtMes = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 2, 0);
+	
+	private JTextFieldPad txtAno = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 4, 0);
 
 	private Vector<String> vVals1 = new Vector<String>();
 
 	private Vector<String> vLabs1 = new Vector<String>();
 
 	public FRRecebAberto() {
-
 		setTitulo( "Recebimentos em aberto" );
-		setAtribos( 80, 80, 330, 160 );
-
+		setAtribos( 80, 80, 350, 180 );
 		GregorianCalendar cPeriodo = new GregorianCalendar();
-		txtDatafim.setVlrDate( cPeriodo.getTime() );
-
-		montaListaCampos();
+		txtMes.setVlrInteger( cPeriodo.get(GregorianCalendar.MONTH) + 1 );
+		txtAno.setVlrInteger( cPeriodo.get( GregorianCalendar.YEAR ) );
 		montaTela();
-
+		setPeriodo(txtMes.getVlrInteger(), txtAno.getVlrInteger());
 	}
 
 	public void montaTela() {
-
+		txtDatafim.setAtivo( false );
 		JLabelPad lbLinha = new JLabelPad();
 		lbLinha.setBorder( BorderFactory.createEtchedBorder() );
 		JLabelPad lbPeriodo = new JLabelPad( "Referência:", SwingConstants.CENTER );
 		lbPeriodo.setOpaque( true );
-
 		adic( lbPeriodo, 7, 1, 80, 20 );
 		adic( lbLinha, 5, 10, 300, 45 );
-
-		adic( new JLabelPad( "Recebimentos em aberto até:" ), 10, 25, 200, 20 );
-		adic( txtDatafim, 200, 25, 90, 20 );
-	}
-
-	public void montaListaCampos() {
-
+		adic( new JLabelPad("Mês e Ano: "), 10, 25, 70, 20);
+		adic( txtMes, 90, 25, 30, 20);
+		adic( new JLabelPad("/"), 126, 25, 70, 20);
+		adic( txtAno, 140, 25, 50, 20);
+		adic( new JLabelPad( "Recebimentos em aberto até:" ), 10, 60, 200, 20 );
+		adic( txtDatafim, 200, 60, 90, 20 );
+		txtMes.addFocusListener( this );
+		txtAno.addFocusListener( this );
 	}
 
 	public void setConexao( DbConnection cn ) {
-
 		super.setConexao( cn );
-
 	}
 
 	public void imprimir( TYPE_PRINT bVisualizar ) {
 
-		String sWhere = "";
-		String sCab = "";
-		String sFiltro = "'R1'";
-
-		String sSQL = "select ir.dtitrec, r.docrec, r.codvenda, ir.vlrparcitrec, ir.vlrpagoitrec, ir.vlrapagitrec, ir.dtpagoitrec" 
-			+ " from fnreceber r, fnitreceber ir" 
-			+ " where ir.codemp=r.codemp and ir.codfilial=r.codfilial and ir.codrec=r.codrec " //+ AplicativoPD.carregaFiltro( con, org.freedom.library.swing.frame.Aplicativo.iCodEmp ) 
-			+ " and statusitrec in ('R1','RL')"
-			+ sWhere + " order by ir.dtitrec";
+		StringBuilder sql = new StringBuilder();
+		    sql.append("select ir.dtitrec, ir.dtvencitrec, r.docrec, r.codvenda, ir.vlrparcitrec, ir.vlrpagoitrec, ir.vlrapagitrec, ir.dtpagoitrec "); 
+			sql.append(" from fnreceber r, fnitreceber ir "); 
+			sql.append(" where ir.codemp=r.codemp and ir.codfilial=r.codfilial and ir.codrec=r.codrec ");
+			sql.append(" and statusitrec in ('R1','RL') ");
+			sql.append(" and r.codemp=? and r.codfilial=? and r.dtvencitrec<=? ");
+			sql.append(" order by ir.dtitrec" );
 
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-
 		try {
-
-			ps = con.prepareStatement( sSQL );
-			ps.setDate( 2, Funcoes.dateToSQLDate( txtDatafim.getVlrDate() ) );
+			ps = con.prepareStatement( sql.toString() );
+			int param = 1;
+			ps.setInt( param++, Aplicativo.iCodEmp );
+			ps.setInt( param++, ListaCampos.getMasterFilial( "FNRECEBER" ) );
+			ps.setDate( param++, Funcoes.dateToSQLDate( txtDatafim.getVlrDate() ) );
 			rs = ps.executeQuery();
+			imprimiGrafico( rs, bVisualizar, "Relatório " );
 
 		} catch ( SQLException err ) {
 			Funcoes.mensagemErro( this, "Erro consulta ao relatório de recebimentos em aberto!\n" + err.getMessage(), true, con, err );
@@ -153,106 +145,29 @@ public class FRRecebAberto extends FRelatorio {
 		}
 	}
 
-	private void imprimiTexto( final ResultSet rs, final TYPE_PRINT bVisualizar, final String sCab ) {
+	public void focusGained( FocusEvent e ) {
 
-		ImprimeOS imp = null;
-		int linPag = 0;
-		BigDecimal bTotalDev = new BigDecimal( "0" );
-		int iNumLanca = 0;
-
-		try {
-
-			boolean hasData = false;
-
-			imp = new ImprimeOS( "", con );
-			linPag = imp.verifLinPag() - 1;
-			imp.montaCab();
-			imp.setTitulo( "Relatório de Compras" );
-			imp.addSubTitulo( sCab.toString() );
-			imp.limpaPags();
-
-			while ( rs.next() ) {
-
-				if ( imp.pRow() == linPag ) {
-
-					imp.say( imp.pRow() + 1, 0, "" + imp.comprimido() );
-					imp.say( imp.pRow() + 0, 0, "+" + StringFunctions.replicate( "-", 133 ) + "+" );
-					imp.eject();
-					imp.incPags();
-
-				}
-
-				if ( imp.pRow() == 0 ) {
-
-					imp.montaCab();
-					imp.setTitulo( "Relatório de Inadimplentes" );
-
-					if ( sCab.length() > 0 ) {
-
-						imp.addSubTitulo( sCab );
-					}
-
-					imp.impCab( 136, true );
-
-					imp.say( imp.pRow() + 0, 0, "" + imp.comprimido() );
-					imp.say( imp.pRow() + 0, 0, "|" + StringFunctions.replicate( "-", 133 ) + "|" );
-					imp.say( imp.pRow() + 1, 0, "" + imp.comprimido() );
-					imp.say( imp.pRow() + 0, 0, "| Vencto." );
-					imp.say( imp.pRow() + 0, 13, "|Vlr. da Parc." );
-					imp.say( imp.pRow() + 0, 27, "|Doc.    " );
-					imp.say( imp.pRow() + 0, 39, "|N.Lancto" );
-					imp.say( imp.pRow() + 0, 48, "|N.Pedido" );
-					imp.say( imp.pRow() + 0, 57, "|Data Emis." );
-					imp.say( imp.pRow() + 0, 68, "|Devedor" );
-					imp.say( imp.pRow() + 0, 119, "|Telefone" );
-					imp.say( imp.pRow() + 0, 135, "|" );
-					imp.say( imp.pRow() + 1, 0, "" + imp.comprimido() );
-					imp.say( imp.pRow() + 0, 0, "|" + StringFunctions.replicate( "-", 133 ) + "|" );
-
-				}
-
-				imp.say( imp.pRow() + 1, 0, "" + imp.comprimido() );
-				imp.say( imp.pRow() + 0, 0, "|" );
-				imp.say( imp.pRow() + 0, 2, StringFunctions.sqlDateToStrDate( rs.getDate( "DtVencItRec" ) ) + "" );
-				imp.say( imp.pRow() + 0, 13, "|" + Funcoes.strDecimalToStrCurrency( 13, 2, rs.getString( "VlrparcItRec" ) ) );
-				imp.say( imp.pRow() + 0, 27, "|" + ( Funcoes.copy( rs.getString( 9 ), 0, 1 ).equals( "P" ) ? Funcoes.copy( rs.getString( "CodVenda" ), 0, 8 ) : Funcoes.copy( rs.getString( "DocRec" ), 0, 8 ) ) + "/" + Funcoes.copy( rs.getString( "NParcItRec" ), 0, 2 ) );
-				imp.say( imp.pRow() + 0, 39, "|" + Funcoes.copy( rs.getString( "Codrec" ), 0, 8 ) );
-				imp.say( imp.pRow() + 0, 48, "|" + Funcoes.copy( rs.getString( "Codvenda" ), 0, 8 ) );
-				imp.say( imp.pRow() + 0, 57, "|" + StringFunctions.sqlDateToStrDate( rs.getDate( "DtItRec" ) ) );
-				imp.say( imp.pRow() + 0, 68, "|" + Funcoes.copy( rs.getString( "CodCli" ), 0, 8 ) + "-" + Funcoes.copy( rs.getString( "RazCli" ), 0, 40 ) );
-				imp.say( imp.pRow() + 0, 119, "|" + ( rs.getString( "DDDCli" ) != null ? "(" + rs.getString( "DDDCli" ) + ")" : "" ) + ( rs.getString( "FoneCli" ) != null ? Funcoes.setMascara( rs.getString( "FoneCli" ).trim(), "####-####" ) : "" ).trim() );
-				imp.say( imp.pRow() + 0, 135, "|" );
-
-				bTotalDev = bTotalDev.add( new BigDecimal( rs.getString( "VlrParcItRec" ) ) );
-				iNumLanca++;
-
-			}
-
-			imp.say( imp.pRow() + 1, 0, "" + imp.comprimido() );
-			imp.say( imp.pRow(), 0, "+" + StringFunctions.replicate( "=", 133 ) + "+" );
-			imp.say( imp.pRow() + 1, 0, "" + imp.comprimido() );
-			imp.say( imp.pRow() + 0, 0, "|" );
-			imp.say( imp.pRow() + 0, 40, "Totais Gerais->    Lançamentos: " + StringFunctions.strZero( "" + iNumLanca, 5 ) + "     Total a Receber: " + Funcoes.strDecimalToStrCurrency( 13, 2, "" + bTotalDev ) );
-			imp.say( imp.pRow(), 135, "|" );
-
-			imp.say( imp.pRow() + 1, 0, "" + imp.comprimido() );
-			imp.say( imp.pRow() + 0, 0, "+" + StringFunctions.replicate( "=", 133 ) + "+" );
-
-			imp.eject();
-			imp.fechaGravacao();
-
-			con.commit();
-
-			if ( bVisualizar==TYPE_PRINT.VIEW ) {
-				imp.preview( this );
-			}
-			else {
-				imp.print();
-
-			}
-
-		} catch ( Exception err ) {
-			err.printStackTrace();
-		}
 	}
+
+	public void focusLost( FocusEvent e ) {
+		if ( e.getSource()==txtMes || e.getSource()==txtAno) {
+			setPeriodo(txtMes.getVlrInteger(), txtAno.getVlrInteger());
+		} 
+	}
+	
+	private void setPeriodo(int mes, int ano) {
+		// Objetivo de pegar o último dia do mês
+		GregorianCalendar cPeriodo = new GregorianCalendar();
+		// Seta o calendário para 1 mês a mais, pois o índice começa em zero e o parâmetro em 1
+		cPeriodo.set( GregorianCalendar.MONTH, mes );
+		// Seta o ano para o calendário
+		cPeriodo.set( GregorianCalendar.YEAR, ano );
+		// Set o primeiro dia do mês
+		cPeriodo.set( GregorianCalendar.DAY_OF_MONTH, 1 );
+		// Subtrai um dia na data, o qual será igual ao último dia do mês
+		cPeriodo.add( GregorianCalendar.DAY_OF_MONTH, -1 );
+		// Atribui a data calculada no campo de filtro
+		txtDatafim.setVlrDate( cPeriodo.getTime() );
+	}
+
 }
