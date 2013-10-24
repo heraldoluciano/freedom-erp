580d2bcdd794b53d823b02019c511671bc8bb1e1
robson <robson@stpinf.com>
2013-10-24 12:42:39 +0000

    Implementações para utilização de ID sequencial no componente ListaCampos.

--- freedom/src/org/freedom/modulos/std/view/frame/utility/FTrocaRefprod.java
+++ freedom/src/org/freedom/modulos/std/view/frame/utility/FTrocaRefprod.java
@@ -22,16 +22,27 @@
 
 package org.freedom.modulos.std.view.frame.utility;
 
+import java.sql.SQLException;
+
+import org.freedom.acao.CarregaEvent;
+import org.freedom.acao.CarregaListener;
+import org.freedom.acao.InsertEvent;
+import org.freedom.acao.InsertListener;
+import org.freedom.acao.PostEvent;
+import org.freedom.acao.PostListener;
 import org.freedom.infra.model.jdbc.DbConnection;
+import org.freedom.library.functions.Funcoes;
 import org.freedom.library.persistence.GuardaCampo;
 import org.freedom.library.persistence.ListaCampos;
 import org.freedom.library.swing.component.JPanelPad;
 import org.freedom.library.swing.component.JTextFieldFK;
 import org.freedom.library.swing.component.JTextFieldPad;
+import org.freedom.library.swing.frame.Aplicativo;
 import org.freedom.library.swing.frame.FDetalhe;
 import org.freedom.modulos.gms.view.frame.crud.tabbed.FProduto;
 
-public class FTrocaRefprod extends FDetalhe {
+public class FTrocaRefprod extends FDetalhe implements InsertListener, PostListener, CarregaListener {
+
 	private static final long serialVersionUID = 1L;
 
 	private JTextFieldPad txtId = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 10, 0 );
@@ -40,18 +51,22 @@ public class FTrocaRefprod extends FDetalhe {
 
 	private JTextFieldPad txtCodfilial = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 8, 0 );
 
+	private JTextFieldPad txtMotivo = new JTextFieldPad( JTextFieldPad.TP_STRING, 80, 0 );
+
+	private JTextFieldPad txtDtTroca = new JTextFieldPad( JTextFieldPad.TP_DATE, 10, 0 );
+
 	private JTextFieldPad txtCodprod = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 10, 0 );
 
 	private JTextFieldPad txtRefprodold = new JTextFieldPad( JTextFieldPad.TP_STRING, 20, 0 );
 
 	private JTextFieldPad txtRefprodnew = new JTextFieldPad( JTextFieldPad.TP_STRING, 20, 0 );
 
-	private JTextFieldPad txtMotivo = new JTextFieldPad( JTextFieldPad.TP_STRING, 80, 0 );
-
 	private JTextFieldFK txtDescprod = new JTextFieldFK( JTextFieldPad.TP_STRING, 50, 0 );
 
-	private JTextFieldPad txtId_it = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 10, 0 );
+	private JTextFieldPad txtId_troca = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 10, 0 );
 
+	private JTextFieldPad txtId_it = new JTextFieldPad( JTextFieldPad.TP_INTEGER, 10, 0 );
+	
 	private ListaCampos lcProd = new ListaCampos( this, "PD" );
 
 	private JPanelPad pinCab = new JPanelPad();
@@ -60,16 +75,17 @@ public class FTrocaRefprod extends FDetalhe {
 
 	public FTrocaRefprod() {
 
-		setTitulo( "Cadastro Fluxos" );
-		setAtribos( 50, 50, 450, 350 );
+		setTitulo( "Troca referência dos produtos" );
+		setAtribos( 10, 10, 600, 400 );
 
 		setAltCab( 90 );
 		pinCab = new JPanelPad( 420, 90 );
-		lcCampos.setUsaFI( false );
-		lcCampos.setUsaME( false );
-		lcDet.setUsaFI( false );
-		lcDet.setUsaME( false );
-		
+		//lcCampos.setUsaFI( false );
+		//lcCampos.setUsaME( false );
+		//lcDet.setUsaFI( false );
+		//lcDet.setUsaME( false );
+		//lcCampos.setWhereAdic( " codemp="+Aplicativo.iCodEmp+" and codfilial="+ListaCampos.getMasterFilial( "EQTROCAREFPROD" ) );
+
 		setListaCampos( lcCampos );
 		setPainel( pinCab, pnCliCab );
 
@@ -82,30 +98,89 @@ public class FTrocaRefprod extends FDetalhe {
 		txtCodprod.setTabelaExterna( lcProd, FProduto.class.getCanonicalName() );
 
 		adicCampo( txtId, 7, 20, 70, 20, "id", "ID.", ListaCampos.DB_PK, true );
-		adicCampo( txtMotivo, 80, 20, 230, 20, "motivo", "Motivo", ListaCampos.DB_SI, true );
-		adicCampoInvisivel( txtCodemp, "codemp", "cód.emp.", ListaCampos.DB_SI , true );
-		adicCampoInvisivel( txtCodfilial, "codfilial", "cód.filial.", ListaCampos.DB_SI , true );
-		setListaCampos( true, "TROCAREFPROD", "EQ" );
+		adicCampo( txtMotivo, 80, 20, 330, 20, "motivo", "Motivo", ListaCampos.DB_SI, true );
+		adicCampo( txtDtTroca, 413, 20, 80, 20, "dttroca", "Data troca", ListaCampos.DB_SI, true);
+		adicCampoInvisivel( txtCodemp, "codemp", "cód.emp.", ListaCampos.DB_SI, true );
+		adicCampoInvisivel( txtCodfilial, "codfilial", "cód.filial.", ListaCampos.DB_SI, true );
+		setListaCampos( false, "TROCAREFPROD", "EQ" );
 
-		setAltDet( 60 );
+		setAltDet( 100 );
 		setPainel( pinDet, pnDet );
 		setListaCampos( lcDet );
 		setNavegador( navRod );
 
 		adicCampo( txtId_it, 7, 20, 40, 20, "id", "ID.", ListaCampos.DB_PK, true );
+		//adicCampoInvisivel( txtId, "id_troca", "id. troca", ListaCampos.DB_PF, true );
+		adicCampoInvisivel( txtId_troca, "id_troca", "id. troca", ListaCampos.DB_FK, true );
 		adicCampo( txtCodprod, 50, 20, 70, 20, "Codprod", "Cód.prod.", ListaCampos.DB_FK, txtDescprod, true );
-		adicDescFK( txtDescprod, 123, 20, 230, 20, "Descprod", "Descrição do produto" );
-		setListaCampos( true, "ITTROCAREFPROD", "EQ" );
+		adicDescFK( txtDescprod, 123, 20, 330, 20, "Descprod", "Descrição do produto" );
+		adicCampo( txtRefprodold, 7, 60, 100, 20, "refprodold", "Referência atual", ListaCampos.DB_SI, true);
+		adicCampo( txtRefprodnew, 110, 60, 100, 20, "refprodnew", "Referência nova", ListaCampos.DB_SI, true);
+		
+		setListaCampos( false, "ITTROCAREFPROD", "EQ" );
 
 		montaTab();
-		tab.setTamColuna( 40, 0 );
-		tab.setTamColuna( 45, 1 );
-		tab.setTamColuna( 350, 2 );
+		int col = 0;
+		tab.setTamColuna( 40, col++ );
+		tab.setTamColuna( 45, col++ );
+		tab.setTamColuna( 350, col++ );
+		lcCampos.addInsertListener( this );
+		lcDet.addInsertListener( this );
+		txtId.setEditable( false );
+		txtId_it.setAtivo( false );
+		txtRefprodold.setAtivo( false );
+		lcCampos.addCarregaListener( this );
+//		txtDttro
 	}
 
 	public void setConexao( DbConnection cn ) {
+
 		super.setConexao( cn );
 		lcProd.setConexao( cn );
 	}
 
+	public void beforeInsert( InsertEvent ievt ) {
+
+	}
+
+	public void afterInsert( InsertEvent ievt ) {
+
+		try {
+			if ( ievt.getListaCampos() == lcCampos ) {
+				txtId.setVlrInteger( lcCampos.gerarSeqId() );
+			} else if (ievt.getListaCampos() == lcDet) {
+				txtId_troca.setVlrInteger( txtId.getVlrInteger() );
+				txtId_it.setVlrInteger( lcDet.gerarSeqId() );
+			}
+		} catch ( SQLException e ) {
+			try {
+				con.rollback();
+			} catch ( SQLException e1 ) {
+				e1.printStackTrace();
+			}
+			Funcoes.mensagemErro( this, "Erro buscando sequencia para ID.\n" + e.getMessage() );
+		}
+
+	}
+
+	public void beforePost(PostEvent pevt) {
+		super.beforePost( pevt );
+		if (pevt.getListaCampos()==lcCampos) {
+			txtCodemp.setVlrInteger( Aplicativo.iCodEmp );
+			txtCodfilial.setVlrInteger( ListaCampos.getMasterFilial( "EQTROCAREFPROD" ) );
+		}
+	}
+
+	public void beforeCarrega( CarregaEvent cevt ) {
+
+	}
+
+	public void afterCarrega( CarregaEvent cevt ) {
+		if (cevt.getListaCampos()==lcCampos) {
+			if ( ! txtId_troca.getVlrString().equals( txtId.getVlrString() )) {
+				txtId_troca.setVlrInteger( txtId.getVlrInteger() );
+				lcDet.carregaDados();
+			}
+		}
+	}
 }
