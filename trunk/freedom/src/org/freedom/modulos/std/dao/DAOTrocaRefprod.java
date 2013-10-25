3664cd7f1ea96f4a3c7503f1e9da9c96006e894b
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2013-10-25 12:01:48 +0000

    Continuação das implementações de rotinas para troca de referências de produtos.

--- trunk/freedom/src/org/freedom/modulos/std/dao/DAOTrocaRefprod.java
+++ trunk/freedom/src/org/freedom/modulos/std/dao/DAOTrocaRefprod.java
@@ -25,6 +25,7 @@ package org.freedom.modulos.std.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 import org.freedom.infra.dao.AbstractDAO;
 import org.freedom.infra.model.jdbc.DbConnection;
@@ -32,6 +33,8 @@ import org.freedom.infra.model.jdbc.DbConnection;
 
 public class DAOTrocaRefprod extends AbstractDAO {
 
+	public enum DATABASE {FireBird}
+	
 	public DAOTrocaRefprod( DbConnection connection, Integer codemp, Integer codfilial ) {
 		super( connection, codemp, codfilial );
 	}
@@ -59,5 +62,84 @@ public class DAOTrocaRefprod extends AbstractDAO {
 			throw new Exception(e.getMessage());
 		}
 		return found;
+	}
+	
+	public Integer gerarSeqId (String tabela) throws SQLException {
+		PreparedStatement ps = null;
+		ResultSet rs = null; 
+		Integer id = 0;
+		ps = getConn().prepareStatement( "select biseq from sgsequence_idsp(?)" );
+		int param = 1;
+		ps.setString( param, tabela.toLowerCase() );
+		rs = ps.executeQuery();
+		if (rs.next()) {
+			id = rs.getInt( "biseq" ); 
+		}
+		rs.close();
+		ps.close();
+		getConn().commit();
+		return id;
+	}
+
+	public Vector<Table> selectTableChange() throws Exception {
+		Vector<Table> result = new Vector<Table>();
+		StringBuilder sql = getSqlTables(DATABASE.FireBird);
+		try {
+			PreparedStatement ps = getConn().prepareStatement( sql.toString() );
+			ResultSet rs = ps.executeQuery();
+			while ( rs.next() ) {
+				result.addElement( new Table(rs.getString("table_name"), rs.getString("field_name")) );
+			}
+			rs.close();
+			ps.close();
+			getConn().commit();
+		} catch (SQLException e) {
+			getConn().rollback();
+			e.printStackTrace();
+			throw new Exception("Erro carregando tabelas !\n"+e.getMessage());
+		}
+		return result;
+	}
+	
+	private StringBuilder getSqlTables(DATABASE db) {
+		StringBuilder result = new StringBuilder();
+		if (db==DATABASE.FireBird) {
+			result.append("select rdb$relation_name table_name, rdb$filed_name field_name ");
+			result.append("from rdb$relation_fields ");
+			result.append("where rdb$field_name like 'REFPROD%' ");
+			result.append("and rdb$relation_name not in ('EQTROCAREFPROD','EQITTROCAREFPROD','EQITTROCARPLOG')");
+			result.append("order by rdb$relation_name, rdb$field_name");
+		}
+		return result;
+	}
+	
+	public class Table {
+
+		String table_name = null;
+		String field_name = null;
+		public Table(String table_name, String field_name) {
+			setTable_name( table_name );
+			setField_name( field_name );
+		}
+		
+		public String getTable_name() {
+		
+			return table_name;
+		}
+		
+		public String getField_name() {
+		
+			return field_name;
+		}
+		
+		public void setTable_name( String table_name ) {
+		
+			this.table_name = table_name;
+		}
+		
+		public void setField_name( String field_name ) {
+		
+			this.field_name = field_name;
+		}
 	}
 }
