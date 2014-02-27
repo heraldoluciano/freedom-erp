23ccddc8734c96d599c1a80a11bd257374c90ca4
robson <robson@stpinf.com>
2014-02-27 14:43:20 +0000

    Criação de gride para armazenar cestas e itens de cestas na tela de consulta de clientes.

--- freedom-fw1/src/org/freedom/modulos/std/orcamento/bean/Cesta.java
+++ freedom-fw1/src/org/freedom/modulos/std/orcamento/bean/Cesta.java
@@ -1,13 +1,20 @@
 package org.freedom.modulos.std.orcamento.bean;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 public class Cesta {
 	private Integer codemp;
 	private Integer codfilial;
 	private Integer codcli;
+	private String razcli;
+	private Date datacesta;
+	private BigDecimal qtdcesta;
+	private BigDecimal vlrdesccesta;
+	private BigDecimal vlrliqcesta;
+	public Boolean sel;
 	private List<Item> itens;
-	
 	public Integer getCodemp() {
 		return codemp;
 	}
@@ -26,12 +33,47 @@ public class Cesta {
 	public void setCodcli(Integer codcli) {
 		this.codcli = codcli;
 	}
+	public String getRazcli() {
+		return razcli;
+	}
+	public void setRazcli(String razcli) {
+		this.razcli = razcli;
+	}
+	public Date getDatacesta() {
+		return datacesta;
+	}
+	public void setDatacesta(Date datacesta) {
+		this.datacesta = datacesta;
+	}
+	public BigDecimal getQtdcesta() {
+		return qtdcesta;
+	}
+	public void setQtdcesta(BigDecimal qtdcesta) {
+		this.qtdcesta = qtdcesta;
+	}
+	public BigDecimal getVlrdesccesta() {
+		return vlrdesccesta;
+	}
+	public void setVlrdesccesta(BigDecimal vlrdesccesta) {
+		this.vlrdesccesta = vlrdesccesta;
+	}
+	public BigDecimal getVlrliqcesta() {
+		return vlrliqcesta;
+	}
+	public void setVlrliqcesta(BigDecimal vlrliqcesta) {
+		this.vlrliqcesta = vlrliqcesta;
+	}
 	public List<Item> getItens() {
 		return itens;
+	}
+	public Boolean getSel() {
+		return sel;
+	}
+	public void setSel(Boolean sel) {
+		this.sel = sel;
 	}
 	public void setItens(List<Item> itens) {
 		this.itens = itens;
 	}
-	
 
 }
--- freedom-fw1/src/org/freedom/modulos/std/orcamento/bussiness/CestaFactory.java
+++ freedom-fw1/src/org/freedom/modulos/std/orcamento/bussiness/CestaFactory.java
@@ -1,5 +1,22 @@
 package org.freedom.modulos.std.orcamento.bussiness;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.freedom.modulos.std.orcamento.bean.Cesta;
+
 public class CestaFactory {
-	
+	private List<Cesta> cestas = null; 
+	public CestaFactory() {
+		setCestas(new ArrayList<Cesta>());
+	}
+	public List<Cesta> getCestas() {
+		if (this.cestas==null) {
+			this.cestas = new ArrayList<Cesta>();
+		}
+		return cestas;
+	}
+	public void setCestas(List<Cesta> cestas) {
+		this.cestas = cestas;
+	}
 }
