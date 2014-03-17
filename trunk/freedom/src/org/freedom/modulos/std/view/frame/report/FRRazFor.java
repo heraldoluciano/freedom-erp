95e2d41255513c283aaa184a43ca6be4a4e0942d
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2014-03-17 13:42:36 +0000

    Criação de view para o relatório razão de fornecedores.

--- trunk/freedom/src/org/freedom/modulos/std/view/frame/report/FRRazFor.java
+++ trunk/freedom/src/org/freedom/modulos/std/view/frame/report/FRRazFor.java
@@ -123,89 +123,97 @@ public class FRRazFor extends FRelatorio {
 
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		StringBuffer sSQL = new StringBuffer();
-		StringBuffer sCab = new StringBuffer();
+		StringBuffer sql = new StringBuffer();
+		StringBuffer cab = new StringBuffer();
 
 		try {
 
 			codfor = txtCodFor.getVlrInteger().intValue();
 			if ( codfor != 0 ) {
-				sCab.append( "FORNECEDOR - " + txtDescFor.getVlrString() );
+				cab.append( "Forn.: " );
+				cab.append( codfor );
+				cab.append( " - " );
+				cab.append( txtDescFor.getVlrString().trim());
+				cab.append( " / ");
 			}
+			cab.append( "Período de: " );
+			cab.append( txtDataini.getVlrString() );
+			cab.append( " até: " );
+			cab.append( txtDatafim.getVlrString() );
 
-			sSQL.append( "SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
-			sSQL.append( "CAST( '" );
-			sSQL.append( Funcoes.dateToStrDB( txtDataini.getVlrDate() ) );
+			sql.append( "SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
+			sql.append( "CAST( '" );
+			sql.append( Funcoes.dateToStrDB( txtDataini.getVlrDate() ) );
 			/**
 			 * Tipo A = Saldo anterior Busca na FNPAGAR todas as compras com valor financeiro a pagar (VLRPAG)
 			 */
-			sSQL.append( "' AS DATE) DATA, 'A' TIPO, ");
-			sSQL.append( "'A' TIPOSUBLANCA, ");
-			sSQL.append( "0 DOC, 0.00 VLRDEB, " );
-			sSQL.append( "(COALESCE( ( SELECT SUM(P.VLRPARCPAG) " );
-			sSQL.append( "FROM FNPAGAR P " );
-			sSQL.append( "WHERE P.CODEMP=? AND P.CODFILIAL=? AND " );
-			sSQL.append( "P.CODEMPFR=F.CODEMP AND P.CODFILIALFR=F.CODFILIAL AND " );
-			sSQL.append( "P.CODFOR=F.CODFOR AND " );
-			sSQL.append( "P.DATAPAG < ? ),0) + " );
+			sql.append( "' AS DATE) DATA, 'A' TIPO, ");
+			sql.append( "'A' TIPOSUBLANCA, ");
+			sql.append( "0 DOC, 0.00 VLRDEB, " );
+			sql.append( "(COALESCE( ( SELECT SUM(P.VLRPARCPAG) " );
+			sql.append( "FROM FNPAGAR P " );
+			sql.append( "WHERE P.CODEMP=? AND P.CODFILIAL=? AND " );
+			sql.append( "P.CODEMPFR=F.CODEMP AND P.CODFILIALFR=F.CODFILIAL AND " );
+			sql.append( "P.CODFOR=F.CODFOR AND " );
+			sql.append( "P.DATAPAG < ? ),0) + " );
 			/**
 			 * Subtrai o valor pago do saldo anterior O sinal do sublanca é invertido, então o sinal - está subtraindo
 			 */
-			sSQL.append( "COALESCE( ( SELECT SUM(SL.VLRSUBLANCA*-1) " );
-			sSQL.append( "FROM FNSUBLANCA SL " );
-			sSQL.append( "WHERE SL.CODEMPFR=F.CODEMP AND SL.CODFILIALFR=F.CODFILIAL AND " );
-			sSQL.append( "SL.CODFOR=F.CODFOR AND SL.CODSUBLANCA<>0 AND " );
-			sSQL.append( "SL.CODEMP=? AND SL.CODFILIAL=? AND " );
-			sSQL.append( "SL.DATASUBLANCA < ? ), 0) +  " );
+			sql.append( "COALESCE( ( SELECT SUM(SL.VLRSUBLANCA*-1) " );
+			sql.append( "FROM FNSUBLANCA SL " );
+			sql.append( "WHERE SL.CODEMPFR=F.CODEMP AND SL.CODFILIALFR=F.CODFILIAL AND " );
+			sql.append( "SL.CODFOR=F.CODFOR AND SL.CODSUBLANCA<>0 AND " );
+			sql.append( "SL.CODEMP=? AND SL.CODFILIAL=? AND " );
+			sql.append( "SL.DATASUBLANCA < ? ), 0) +  " );
 
 			/**
 			 * Subtrai o valor do desconto na data do lançamento financeiro
 			 */
 
-			/*sSQL.append( "COALESCE( ( SELECT SUM(SL.VLRSUBLANCA) FROM FNSUBLANCA SL, SGPREFERE1 PF WHERE  " );
-			sSQL.append( " SL.CODEMPFR=F.CODEMP AND SL.CODFILIALFR=F.CODFILIAL AND SL.CODFOR=F.CODFOR AND " );
-			//sSQL.append( " SL.CODEMPPN=PF.CODEMPDR AND SL.CODFILIALPN=PF.CODFILIALDR AND SL.CODPLAN=PF.CODPLANDR AND " );
-			sSQL.append( " SL.TIPOSUBLANCA=? AND " );
-			sSQL.append( " SL.CODEMP=? AND SL.CODFILIAL=? AND SL.DATASUBLANCA < ? ), 0) - " );
+			/*sql.append( "COALESCE( ( SELECT SUM(SL.VLRSUBLANCA) FROM FNSUBLANCA SL, SGPREFERE1 PF WHERE  " );
+			sql.append( " SL.CODEMPFR=F.CODEMP AND SL.CODFILIALFR=F.CODFILIAL AND SL.CODFOR=F.CODFOR AND " );
+			//sql.append( " SL.CODEMPPN=PF.CODEMPDR AND SL.CODFILIALPN=PF.CODFILIALDR AND SL.CODPLAN=PF.CODPLANDR AND " );
+			sql.append( " SL.TIPOSUBLANCA=? AND " );
+			sql.append( " SL.CODEMP=? AND SL.CODFILIAL=? AND SL.DATASUBLANCA < ? ), 0) - " );
 */
 			/**
 			 * Subtrai o valor das devoluções do saldo anterior
 			 */
-			sSQL.append( "( COALESCE( ( SELECT SUM(VD.VLRLIQVENDA) " );
-			sSQL.append( "FROM VDVENDA VD, EQTIPOMOV TM, EQCLIFOR CF " );
-			sSQL.append( "WHERE VD.CODEMP=? AND VD.CODFILIAL=? AND  TM.CODEMP=VD.CODEMPTM AND " );
-			sSQL.append( "TM.CODFILIAL=VD.CODFILIALTM AND TM.ESTIPOMOV='S' AND TM.TIPOMOV='DV' AND " );
-			sSQL.append( "TM.CODTIPOMOV=VD.CODTIPOMOV AND CF.CODEMPFR=F.CODEMP AND " );
-			sSQL.append( "CF.CODFILIALFR=F.CODFILIAL AND CF.CODFOR=F.CODFOR AND VD.CODEMPCL=CF.CODEMP AND " );
-			sSQL.append( "VD.CODFILIALCL=CF.CODFILIAL AND VD.CODCLI=CF.CODCLI AND VD.DTEMITVENDA < ? ),0) ) ) " );
-			sSQL.append( " VLRCRED " );
+			sql.append( "( COALESCE( ( SELECT SUM(VD.VLRLIQVENDA) " );
+			sql.append( "FROM VDVENDA VD, EQTIPOMOV TM, EQCLIFOR CF " );
+			sql.append( "WHERE VD.CODEMP=? AND VD.CODFILIAL=? AND  TM.CODEMP=VD.CODEMPTM AND " );
+			sql.append( "TM.CODFILIAL=VD.CODFILIALTM AND TM.ESTIPOMOV='S' AND TM.TIPOMOV='DV' AND " );
+			sql.append( "TM.CODTIPOMOV=VD.CODTIPOMOV AND CF.CODEMPFR=F.CODEMP AND " );
+			sql.append( "CF.CODFILIALFR=F.CODFILIAL AND CF.CODFOR=F.CODFOR AND VD.CODEMPCL=CF.CODEMP AND " );
+			sql.append( "VD.CODFILIALCL=CF.CODFILIAL AND VD.CODCLI=CF.CODCLI AND VD.DTEMITVENDA < ? ),0) ) ) " );
+			sql.append( " VLRCRED " );
 			/**
 			 * Filtro do fornecedor
 			 */
-			sSQL.append( "FROM CPFORNECED F " );
-			sSQL.append( "WHERE F.CODEMP=? AND F.CODFILIAL=? AND " );
+			sql.append( "FROM CPFORNECED F " );
+			sql.append( "WHERE F.CODEMP=? AND F.CODFILIAL=? AND " );
 			if ( codfor != 0 ) {
-				sSQL.append( "F.CODFOR=? AND " );
+				sql.append( "F.CODFOR=? AND " );
 			}
 			/**
 			 * Verifica a existência dos valores
 			 */
-			sSQL.append( "( EXISTS (SELECT * FROM FNPAGAR P2 " );
-			sSQL.append( "WHERE P2.CODEMP=? AND P2.CODFILIAL=? AND " );
-			sSQL.append( "P2.CODEMPFR=F.CODEMP AND P2.CODFILIALFR=F.CODFILIAL AND " );
-			sSQL.append( "P2.CODFOR=F.CODFOR AND DATAPAG BETWEEN ? AND ? )" );
-			sSQL.append( "OR EXISTS (SELECT * FROM FNSUBLANCA SL2 " );
-			sSQL.append( "WHERE F.CODEMP=SL2.CODEMPFR AND " );
-			sSQL.append( "F.CODFILIAL=SL2.CODFILIALFR AND F.CODFOR=SL2.CODFOR AND " );
-			sSQL.append( "SL2.CODEMP=? AND SL2.CODFILIAL=? AND " );
-			sSQL.append( "SL2.DATASUBLANCA BETWEEN ? AND ? ) OR " );
-			sSQL.append( " EXISTS (SELECT * FROM VDVENDA VD, EQTIPOMOV TM, EQCLIFOR CF " );
-			sSQL.append( "WHERE VD.CODEMP=? AND VD.CODFILIAL=? AND TM.CODEMP=VD.CODEMPTM AND " );
-			sSQL.append( "TM.CODFILIAL=VD.CODFILIALTM AND TM.CODTIPOMOV=VD.CODTIPOMOV AND " );
-			sSQL.append( "TM.ESTIPOMOV='S' AND TM.TIPOMOV='DV' AND " );
-			sSQL.append( "CF.CODEMPFR=F.CODEMP AND CF.CODFILIALFR=F.CODFILIAL AND CF.CODFOR=F.CODFOR AND " );
-			sSQL.append( "VD.CODEMPCL=CF.CODEMP AND VD.CODFILIALCL=CF.CODFILIAL AND " );
-			sSQL.append( "VD.CODCLI=CF.CODCLI AND VD.DTEMITVENDA BETWEEN ? AND ? ) ) " );
+			sql.append( "( EXISTS (SELECT * FROM FNPAGAR P2 " );
+			sql.append( "WHERE P2.CODEMP=? AND P2.CODFILIAL=? AND " );
+			sql.append( "P2.CODEMPFR=F.CODEMP AND P2.CODFILIALFR=F.CODFILIAL AND " );
+			sql.append( "P2.CODFOR=F.CODFOR AND DATAPAG BETWEEN ? AND ? )" );
+			sql.append( "OR EXISTS (SELECT * FROM FNSUBLANCA SL2 " );
+			sql.append( "WHERE F.CODEMP=SL2.CODEMPFR AND " );
+			sql.append( "F.CODFILIAL=SL2.CODFILIALFR AND F.CODFOR=SL2.CODFOR AND " );
+			sql.append( "SL2.CODEMP=? AND SL2.CODFILIAL=? AND " );
+			sql.append( "SL2.DATASUBLANCA BETWEEN ? AND ? ) OR " );
+			sql.append( " EXISTS (SELECT * FROM VDVENDA VD, EQTIPOMOV TM, EQCLIFOR CF " );
+			sql.append( "WHERE VD.CODEMP=? AND VD.CODFILIAL=? AND TM.CODEMP=VD.CODEMPTM AND " );
+			sql.append( "TM.CODFILIAL=VD.CODFILIALTM AND TM.CODTIPOMOV=VD.CODTIPOMOV AND " );
+			sql.append( "TM.ESTIPOMOV='S' AND TM.TIPOMOV='DV' AND " );
+			sql.append( "CF.CODEMPFR=F.CODEMP AND CF.CODFILIALFR=F.CODFILIAL AND CF.CODFOR=F.CODFOR AND " );
+			sql.append( "VD.CODEMPCL=CF.CODEMP AND VD.CODFILIALCL=CF.CODFILIAL AND " );
+			sql.append( "VD.CODCLI=CF.CODCLI AND VD.DTEMITVENDA BETWEEN ? AND ? ) ) " );
 			/**
 			 * Fim da query do saldo anterior
 			 */
@@ -213,99 +221,99 @@ public class FRRazFor extends FRelatorio {
 			/**
 			 * Query das compras
 			 */
-			sSQL.append( "UNION ALL " );
-			sSQL.append( "SELECT P.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
-			sSQL.append( "P.DATAPAG DATA, 'C' TIPO, ");
-			sSQL.append( "'C' TIPOSUBLANCA, ");
+			sql.append( "UNION ALL " );
+			sql.append( "SELECT P.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
+			sql.append( "P.DATAPAG DATA, 'C' TIPO, ");
+			sql.append( "'C' TIPOSUBLANCA, ");
 
-			sSQL.append( "P.DOCPAG DOC, " );
-			sSQL.append( "0.00 VLRDEB, P.VLRPARCPAG VLRCRED " );
-			sSQL.append( "FROM FNPAGAR P, CPFORNECED F " );
-			sSQL.append( "WHERE F.CODEMP=P.CODEMPFR AND F.CODFILIAL=P.CODFILIALFR AND " );
-			sSQL.append( "F.CODFOR=P.CODFOR AND " );
-			sSQL.append( "P.CODEMP=? AND P.CODFILIAL=? AND " );
+			sql.append( "P.DOCPAG DOC, " );
+			sql.append( "0.00 VLRDEB, P.VLRPARCPAG VLRCRED " );
+			sql.append( "FROM FNPAGAR P, CPFORNECED F " );
+			sql.append( "WHERE F.CODEMP=P.CODEMPFR AND F.CODFILIAL=P.CODFILIALFR AND " );
+			sql.append( "F.CODFOR=P.CODFOR AND " );
+			sql.append( "P.CODEMP=? AND P.CODFILIAL=? AND " );
 			if ( codfor != 0 ) {
-				sSQL.append( "F.CODFOR=? AND " );
+				sql.append( "F.CODFOR=? AND " );
 			}
-			sSQL.append( "P.DATAPAG BETWEEN ? AND ? " );
+			sql.append( "P.DATAPAG BETWEEN ? AND ? " );
 
 			/**
 			 * Query dos pagamentos
 			 */
-			sSQL.append( "UNION ALL " );
-			sSQL.append( "SELECT SL.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
-			sSQL.append( "SL.DATASUBLANCA DATA, ");
-			sSQL.append( " (CASE WHEN SL.TIPOSUBLANCA='P' THEN 'P' ELSE 'X' END) TIPO, ");
-			sSQL.append( "SL.TIPOSUBLANCA, ");
-			sSQL.append( "P.DOCPAG DOC, SL.VLRSUBLANCA VLRDEB, ");
-			sSQL.append( "(CASE WHEN SL.TIPOSUBLANCA IN ('J','D','M') THEN SL.VLRSUBLANCA ELSE 0.00 END) VLRCRED " );
-			sSQL.append( "FROM FNSUBLANCA SL, CPFORNECED F, FNPAGAR P " );
-			sSQL.append( "WHERE F.CODEMP=SL.CODEMPFR AND F.CODFILIAL=SL.CODFILIALFR AND " );
-			sSQL.append( "P.CODEMP=SL.CODEMPPG AND P.CODFILIAL=SL.CODFILIALPG AND P.CODPAG=SL.CODPAG AND " );
+			sql.append( "UNION ALL " );
+			sql.append( "SELECT SL.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
+			sql.append( "SL.DATASUBLANCA DATA, ");
+			sql.append( " (CASE WHEN SL.TIPOSUBLANCA='P' THEN 'P' ELSE 'X' END) TIPO, ");
+			sql.append( "SL.TIPOSUBLANCA, ");
+			sql.append( "P.DOCPAG DOC, SL.VLRSUBLANCA VLRDEB, ");
+			sql.append( "(CASE WHEN SL.TIPOSUBLANCA IN ('J','D','M') THEN SL.VLRSUBLANCA ELSE 0.00 END) VLRCRED " );
+			sql.append( "FROM FNSUBLANCA SL, CPFORNECED F, FNPAGAR P " );
+			sql.append( "WHERE F.CODEMP=SL.CODEMPFR AND F.CODFILIAL=SL.CODFILIALFR AND " );
+			sql.append( "P.CODEMP=SL.CODEMPPG AND P.CODFILIAL=SL.CODFILIALPG AND P.CODPAG=SL.CODPAG AND " );
 			// Incluido no tiposublanca P - Padrão, M - Multa e J-Juros
-			//sSQL.append( "SL.TIPOSUBLANCA IN ('P','M','J') AND ");
-			sSQL.append( "F.CODFOR=SL.CODFOR AND SL.CODSUBLANCA<>0 AND " );
+			//sql.append( "SL.TIPOSUBLANCA IN ('P','M','J') AND ");
+			sql.append( "F.CODFOR=SL.CODFOR AND SL.CODSUBLANCA<>0 AND " );
 			if ( codfor != 0 ) {
-				sSQL.append( "F.CODFOR=? AND " );
+				sql.append( "F.CODFOR=? AND " );
 			}
-			sSQL.append( "SL.CODEMP=? AND SL.CODFILIAL=? AND " );
-			sSQL.append( "SL.DATASUBLANCA BETWEEN ? AND ? " );
+			sql.append( "SL.CODEMP=? AND SL.CODFILIAL=? AND " );
+			sql.append( "SL.DATASUBLANCA BETWEEN ? AND ? " );
 			/**
 			 * Query dos cancelamentos
 			 */
-			/*sSQL.append( "UNION ALL ");
-			sSQL.append( " SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
-			sSQL.append( " IP.DTVENCITPAG DATA, 'X' TIPO, " );
-			sSQL.append( "'X' TIPOSUBLANCA, ");
-			sSQL.append( "P.DOCPAG DOC, COALESCE(SUM(IP.VLRCANCITPAG),0) VLRCRED, 0.00 VLRDEB " );
-			sSQL.append( "FROM FNPAGAR P, FNITPAGAR IP, CPFORNECED F WHERE P.CODEMP=? AND P.CODFILIAL=? AND P.CODEMPFR=F.CODEMP AND " );
-			sSQL.append( "P.CODFILIALFR=F.CODFILIAL AND P.CODFOR=F.CODFOR ");
-			sSQL.append( "AND IP.CODEMP=P.CODEMP AND IP.CODFILIAL=P.CODFILIAL AND IP.CODPAG=P.CODPAG AND IP.STATUSITPAG IN ('CP') ");
+			/*sql.append( "UNION ALL ");
+			sql.append( " SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, " );
+			sql.append( " IP.DTVENCITPAG DATA, 'X' TIPO, " );
+			sql.append( "'X' TIPOSUBLANCA, ");
+			sql.append( "P.DOCPAG DOC, COALESCE(SUM(IP.VLRCANCITPAG),0) VLRCRED, 0.00 VLRDEB " );
+			sql.append( "FROM FNPAGAR P, FNITPAGAR IP, CPFORNECED F WHERE P.CODEMP=? AND P.CODFILIAL=? AND P.CODEMPFR=F.CODEMP AND " );
+			sql.append( "P.CODFILIALFR=F.CODFILIAL AND P.CODFOR=F.CODFOR ");
+			sql.append( "AND IP.CODEMP=P.CODEMP AND IP.CODFILIAL=P.CODFILIAL AND IP.CODPAG=P.CODPAG AND IP.STATUSITPAG IN ('CP') ");
 			if ( codfor != 0 ) {
-				sSQL.append( "AND F.CODFOR=? " );
+				sql.append( "AND F.CODFOR=? " );
 			}
-			//sSQL.append( "AND R.CODEMP=? AND R.CODFILIAL=? AND " );
-			sSQL.append( "AND P.DATAPAG BETWEEN ? AND ? " );
-			sSQL.append( "GROUP BY 1, 2, 3, 4, 5, 6 ");
+			//sql.append( "AND R.CODEMP=? AND R.CODFILIAL=? AND " );
+			sql.append( "AND P.DATAPAG BETWEEN ? AND ? " );
+			sql.append( "GROUP BY 1, 2, 3, 4, 5, 6 ");
 				*/		
 			/**
 			 * Query das devoluções
 			 */
-			sSQL.append( "UNION ALL SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, VD.DTEMITVENDA DATA, " );
-			sSQL.append( " 'Z' TIPO, ");
-			sSQL.append( "'Z' TIPOSUBLANCA, " );
-			sSQL.append( "VD.DOCVENDA DOC, VD.VLRLIQVENDA VLRCRED, 0.00 VLRDEB " );
-			sSQL.append( "FROM VDVENDA VD, EQTIPOMOV TM, EQCLIFOR CF, CPFORNECED F " );
-			sSQL.append( "WHERE TM.CODEMP=VD.CODEMPTM AND TM.CODFILIAL=VD.CODFILIALTM AND " );
-			sSQL.append( "TM.CODTIPOMOV=VD.CODTIPOMOV AND TM.ESTIPOMOV='S' AND TM.TIPOMOV='DV' AND " );
-			sSQL.append( "CF.CODEMPFR=F.CODEMP AND CF.CODFILIALFR=F.CODFILIAL AND CF.CODFOR=F.CODFOR AND " );
-			sSQL.append( "VD.CODEMPCL=CF.CODEMP AND VD.CODFILIALCL=CF.CODFILIAL AND " );
-			sSQL.append( "VD.CODCLI=CF.CODCLI AND " );
+			sql.append( "UNION ALL SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, VD.DTEMITVENDA DATA, " );
+			sql.append( " 'Z' TIPO, ");
+			sql.append( "'Z' TIPOSUBLANCA, " );
+			sql.append( "VD.DOCVENDA DOC, VD.VLRLIQVENDA VLRCRED, 0.00 VLRDEB " );
+			sql.append( "FROM VDVENDA VD, EQTIPOMOV TM, EQCLIFOR CF, CPFORNECED F " );
+			sql.append( "WHERE TM.CODEMP=VD.CODEMPTM AND TM.CODFILIAL=VD.CODFILIALTM AND " );
+			sql.append( "TM.CODTIPOMOV=VD.CODTIPOMOV AND TM.ESTIPOMOV='S' AND TM.TIPOMOV='DV' AND " );
+			sql.append( "CF.CODEMPFR=F.CODEMP AND CF.CODFILIALFR=F.CODFILIAL AND CF.CODFOR=F.CODFOR AND " );
+			sql.append( "VD.CODEMPCL=CF.CODEMP AND VD.CODFILIALCL=CF.CODFILIAL AND " );
+			sql.append( "VD.CODCLI=CF.CODCLI AND " );
 			if ( codfor != 0 ) {
-				sSQL.append( "F.CODFOR=? AND " );
+				sql.append( "F.CODFOR=? AND " );
 			}
-			sSQL.append( "VD.CODEMP=? AND VD.CODFILIAL=? AND " );
-			sSQL.append( "VD.DTEMITVENDA BETWEEN ? AND ? " );
+			sql.append( "VD.CODEMP=? AND VD.CODFILIAL=? AND " );
+			sql.append( "VD.DTEMITVENDA BETWEEN ? AND ? " );
 
 			/**
 			 * Query dos descontos
 			 */
-/*			sSQL.append( "UNION ALL SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, SL.DATASUBLANCA DATA, " );
-			sSQL.append( " 'X' TIPO, P.DOCPAG DOC, (SL.VLRSUBLANCA * -1) VLRDEB,  0.00 VLRCRED " );
-			sSQL.append( "FROM FNSUBLANCA SL, FNPAGAR P, CPFORNECED F " );
-			sSQL.append( "WHERE SL.CODEMPPG=P.CODEMP AND SL.CODFILIALPG=P.CODFILIAL AND " );
-			sSQL.append( " SL.TIPOSUBLANCA=? AND " );
-			sSQL.append( " SL.CODPAG=P.CODPAG AND F.CODEMP=P.CODEMPFR AND F.CODFILIAL=P.CODFILIALFR AND " );
-			sSQL.append( "F.CODFOR=P.CODFOR AND " );
+/*			sql.append( "UNION ALL SELECT F.CODFOR CODEMIT, F.RAZFOR RAZEMIT, SL.DATASUBLANCA DATA, " );
+			sql.append( " 'X' TIPO, P.DOCPAG DOC, (SL.VLRSUBLANCA * -1) VLRDEB,  0.00 VLRCRED " );
+			sql.append( "FROM FNSUBLANCA SL, FNPAGAR P, CPFORNECED F " );
+			sql.append( "WHERE SL.CODEMPPG=P.CODEMP AND SL.CODFILIALPG=P.CODFILIAL AND " );
+			sql.append( " SL.TIPOSUBLANCA=? AND " );
+			sql.append( " SL.CODPAG=P.CODPAG AND F.CODEMP=P.CODEMPFR AND F.CODFILIAL=P.CODFILIALFR AND " );
+			sql.append( "F.CODFOR=P.CODFOR AND " );
 			if ( codfor != 0 ) {
-				sSQL.append( "F.CODFOR=? AND " );
+				sql.append( "F.CODFOR=? AND " );
 			}
-			sSQL.append( "P.CODEMP=? AND P.CODFILIAL=? AND " );
-			sSQL.append( "SL.DATASUBLANCA BETWEEN ? AND ?  " );
+			sql.append( "P.CODEMP=? AND P.CODFILIAL=? AND " );
+			sql.append( "SL.DATASUBLANCA BETWEEN ? AND ?  " );
 */
-			sSQL.append( "ORDER BY 1, 2, 3, 4, 6, 5" );
+			sql.append( "ORDER BY 1, 2, 3, 4, 6, 5" );
 
-			ps = con.prepareStatement( sSQL.toString() );
+			ps = con.prepareStatement( sql.toString() );
 			ps.setInt( param++, Aplicativo.iCodEmp ); // 1
 			ps.setInt( param++, ListaCampos.getMasterFilial( "FNPAGAR" ) ); // 2
 			ps.setDate( param++, Funcoes.strDateToSqlDate( txtDataini.getVlrString() ) ); // 3
@@ -391,11 +399,11 @@ public class FRRazFor extends FRelatorio {
 			ps.setDate( param++, Funcoes.strDateToSqlDate( txtDataini.getVlrString() ) ); // 44
 			ps.setDate( param++, Funcoes.strDateToSqlDate( txtDatafim.getVlrString() ) ); // 45
 */
-			System.out.println( "QUERY" + sSQL.toString() );
+			System.out.println( "QUERY" + sql.toString() );
 
 			rs = ps.executeQuery();
 
-			imprimiGrafico( bVisualizar, rs, sCab.toString() );
+			imprimiGrafico( bVisualizar, rs, cab.toString() );
 
 			rs.close();
 			ps.close();
@@ -409,7 +417,7 @@ public class FRRazFor extends FRelatorio {
 		}
 	}
 
-	private void imprimiGrafico( final TYPE_PRINT bVisualizar, final ResultSet rs, final String sCab ) {
+	private void imprimiGrafico( final TYPE_PRINT bVisualizar, final ResultSet rs, final String cab ) {
 
 		FPrinterJob dlGr = null;
 		HashMap<String, Object> hParam = new HashMap<String, Object>();
@@ -417,9 +425,9 @@ public class FRRazFor extends FRelatorio {
 		hParam.put( "CODEMP", Aplicativo.iCodEmp );
 		hParam.put( "CODFILIAL", ListaCampos.getMasterFilial( "FNPAGAR" ) );
 		hParam.put( "RAZAOEMP", Aplicativo.empresa.toString() );
-		hParam.put( "FILTROS", sCab );
+		hParam.put( "FILTROS", cab );
 
-		dlGr = new FPrinterJob( "relatorios/FRRazFor.jasper", "Relatório de Razão por fornecedor", sCab, rs, hParam, this );
+		dlGr = new FPrinterJob( "relatorios/FRRazFor.jasper", "Relatório de Razão por fornecedor", cab, rs, hParam, this );
 
 		if ( bVisualizar==TYPE_PRINT.VIEW ) {
 			dlGr.setVisible( true );
