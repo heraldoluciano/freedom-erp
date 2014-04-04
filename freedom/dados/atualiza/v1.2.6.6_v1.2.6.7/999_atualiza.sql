7bb3ca0890faab8ad54503ae46fd41bbcee9c0ec
robson <robson@stpinf.com>
2014-04-04 13:05:46 +0000

    Ajuste de dados do contas a pagar e receber.

--- freedom/dados/atualiza/v1.2.6.6_v1.2.6.7/999_atualiza.sql
+++ freedom/dados/atualiza/v1.2.6.6_v1.2.6.7/999_atualiza.sql
@@ -3,3 +3,11 @@ update lffrete set emmanut='S', dtmovfrete=dtemitfrete where dtmovfrete is null;
 update lffrete set emmanut='N' where emmanut='S';
 
 commit work;
+
+update fnitreceber r set r.emmanut='S', r.vlrparcitrec=r.vlrcancitrec where r.statusitrec='CR';
+
+commit work;
+
+update fnitreceber r set r.emmanut='N' where emmanut='S';
+
+commit work;
