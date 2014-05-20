a6192fb1c9b0e9d0f4620af16eb4084705fb7aba
heraldoluciano <heraldoluciano@gmail.com>
2014-05-20 18:12:59 +0000

    Retornando a palavra Orcamento.

---
+++ freedom/src/org/freedom/modulos/std/business/object/Orcamento.java
@@ -0,0 +1,672 @@
+package org.freedom.modulos.std.business.object;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+public class Orcamento {
+	
+	// ------------------------------------------------------------
+	private Integer codemp;
+	private Short codfilial;
+	private String tipoorc;
+	private Integer codorc;
+	private Integer codempae;
+	private Short codfilialae;
+	private Integer codatend;
+	private Integer codempcv;
+	private Short codfilialcv;
+	private Integer codconv;
+	private Date dtorc;
+	private Date dtvencorc;
+	private BigDecimal vlrprodorc;
+	private BigDecimal percdescorc;
+	private BigDecimal vlrdescorc;
+	private BigDecimal percadicorc;
+	private BigDecimal vlradicorc;
+	private BigDecimal vlrdescitorc;
+	private BigDecimal vlrliqorc;
+	private String statusorc;
+	private String obsorc;
+	private Integer codplanopag;
+	private Integer codfilialpg;
+	private Integer codemppg;
+	private String txt01;
+	private Integer codempvd;
+	private Short codfilialvd;
+	private Integer codvend;
+	private Integer codempcl;
+	private Short codfilialcl;
+	private Integer codcli;
+	private Integer codtpconv;
+	private Integer codfilialtc;
+	private Integer codemptc;
+	private Short prazoentorc;
+	private String emmanut;
+	private Integer codclcomis;
+	private Short codempcm;
+	private Integer codfilialcm;
+	private Integer codemptn;
+	private Short codfilialtn;
+	private Integer codtran;
+	private String tipofrete;
+	private String adicfrete;
+	private BigDecimal vlrfreteorc;
+	private BigDecimal vlrcomisorc;
+	private Integer codemptm;
+	private Short codfilialtm;
+	private Integer codtipomov;
+	private Date dtaprovorc;
+	private Date dtpreventorc;
+	private Date hpreventorc;
+	private String justificcancorc;
+	private String acorc;
+	private Date dtins;
+	private Date hins;
+	private String idusuins;
+	private Date dtalt;
+	private Date halt;
+	private String idusualt;
+	// ------------------------------------------------------------
+	
+	public Integer getCodemp() {
+	
+		return codemp;
+	}
+	
+	public void setCodemp( Integer codemp ) {
+	
+		this.codemp = codemp;
+	}
+	
+	public Short getCodfilial() {
+	
+		return codfilial;
+	}
+	
+	public void setCodfilial( Short codfilial ) {
+	
+		this.codfilial = codfilial;
+	}
+	
+	public String getTipoorc() {
+	
+		return tipoorc;
+	}
+	
+	public void setTipoorc( String tipoorc ) {
+	
+		this.tipoorc = tipoorc;
+	}
+	
+	public Integer getCodorc() {
+	
+		return codorc;
+	}
+	
+	public void setCodorc( Integer codorc ) {
+	
+		this.codorc = codorc;
+	}
+	
+	public Integer getCodempae() {
+	
+		return codempae;
+	}
+	
+	public void setCodempae( Integer codempae ) {
+	
+		this.codempae = codempae;
+	}
+	
+	public Short getCodfilialae() {
+	
+		return codfilialae;
+	}
+	
+	public void setCodfilialae( Short codfilialae ) {
+	
+		this.codfilialae = codfilialae;
+	}
+	
+	public Integer getCodatend() {
+	
+		return codatend;
+	}
+	
+	public void setCodatend( Integer codatend ) {
+	
+		this.codatend = codatend;
+	}
+	
+	public Integer getCodempcv() {
+	
+		return codempcv;
+	}
+	
+	public void setCodempcv( Integer codempcv ) {
+	
+		this.codempcv = codempcv;
+	}
+	
+	public Short getCodfilialcv() {
+	
+		return codfilialcv;
+	}
+	
+	public void setCodfilialcv( Short codfilialcv ) {
+	
+		this.codfilialcv = codfilialcv;
+	}
+	
+	public Integer getCodconv() {
+	
+		return codconv;
+	}
+	
+	public void setCodconv( Integer codconv ) {
+	
+		this.codconv = codconv;
+	}
+	
+	public Date getDtorc() {
+	
+		return dtorc;
+	}
+	
+	public void setDtorc( Date dtorc ) {
+	
+		this.dtorc = dtorc;
+	}
+	
+	public Date getDtvencorc() {
+	
+		return dtvencorc;
+	}
+	
+	public void setDtvencorc( Date dtvencorc ) {
+	
+		this.dtvencorc = dtvencorc;
+	}
+	
+	public BigDecimal getVlrprodorc() {
+	
+		return vlrprodorc;
+	}
+	
+	public void setVlrprodorc( BigDecimal vlrprodorc ) {
+	
+		this.vlrprodorc = vlrprodorc;
+	}
+	
+	public BigDecimal getPercdescorc() {
+	
+		return percdescorc;
+	}
+	
+	public void setPercdescorc( BigDecimal percdescorc ) {
+	
+		this.percdescorc = percdescorc;
+	}
+	
+	public BigDecimal getVlrdescorc() {
+	
+		return vlrdescorc;
+	}
+	
+	public void setVlrdescorc( BigDecimal vlrdescorc ) {
+	
+		this.vlrdescorc = vlrdescorc;
+	}
+	
+	public BigDecimal getPercadicorc() {
+	
+		return percadicorc;
+	}
+	
+	public void setPercadicorc( BigDecimal percadicorc ) {
+	
+		this.percadicorc = percadicorc;
+	}
+	
+	public BigDecimal getVlradicorc() {
+	
+		return vlradicorc;
+	}
+	
+	public void setVlradicorc( BigDecimal vlradicorc ) {
+	
+		this.vlradicorc = vlradicorc;
+	}
+	
+	public BigDecimal getVlrdescitorc() {
+	
+		return vlrdescitorc;
+	}
+	
+	public void setVlrdescitorc( BigDecimal vlrdescitorc ) {
+	
+		this.vlrdescitorc = vlrdescitorc;
+	}
+	
+	public BigDecimal getVlrliqorc() {
+	
+		return vlrliqorc;
+	}
+	
+	public void setVlrliqorc( BigDecimal vlrliqorc ) {
+	
+		this.vlrliqorc = vlrliqorc;
+	}
+	
+	public String getStatusorc() {
+	
+		return statusorc;
+	}
+	
+	public void setStatusorc( String statusorc ) {
+	
+		this.statusorc = statusorc;
+	}
+	
+	public String getObsorc() {
+	
+		return obsorc;
+	}
+	
+	public void setObsorc( String obsorc ) {
+	
+		this.obsorc = obsorc;
+	}
+	
+	public Integer getCodplanopag() {
+	
+		return codplanopag;
+	}
+	
+	public void setCodplanopag( Integer codplanopag ) {
+	
+		this.codplanopag = codplanopag;
+	}
+	
+	public Integer getCodfilialpg() {
+	
+		return codfilialpg;
+	}
+	
+	public void setCodfilialpg( Integer codfilialpg ) {
+	
+		this.codfilialpg = codfilialpg;
+	}
+	
+	public Integer getCodemppg() {
+	
+		return codemppg;
+	}
+	
+	public void setCodemppg( Integer codemppg ) {
+	
+		this.codemppg = codemppg;
+	}
+	
+	public String getTxt01() {
+	
+		return txt01;
+	}
+	
+	public void setTxt01( String txt01 ) {
+	
+		this.txt01 = txt01;
+	}
+	
+	public Integer getCodempvd() {
+	
+		return codempvd;
+	}
+	
+	public void setCodempvd( Integer codempvd ) {
+	
+		this.codempvd = codempvd;
+	}
+	
+	public Short getCodfilialvd() {
+	
+		return codfilialvd;
+	}
+	
+	public void setCodfilialvd( Short codfilialvd ) {
+	
+		this.codfilialvd = codfilialvd;
+	}
+	
+	public Integer getCodvend() {
+	
+		return codvend;
+	}
+	
+	public void setCodvend( Integer codvend ) {
+	
+		this.codvend = codvend;
+	}
+	
+	public Integer getCodempcl() {
+	
+		return codempcl;
+	}
+	
+	public void setCodempcl( Integer codempcl ) {
+	
+		this.codempcl = codempcl;
+	}
+	
+	public Short getCodfilialcl() {
+	
+		return codfilialcl;
+	}
+	
+	public void setCodfilialcl( Short codfilialcl ) {
+	
+		this.codfilialcl = codfilialcl;
+	}
+	
+	public Integer getCodcli() {
+	
+		return codcli;
+	}
+	
+	public void setCodcli( Integer codcli ) {
+	
+		this.codcli = codcli;
+	}
+	
+	public Integer getCodtpconv() {
+	
+		return codtpconv;
+	}
+	
+	public void setCodtpconv( Integer codtpconv ) {
+	
+		this.codtpconv = codtpconv;
+	}
+	
+	public Integer getCodfilialtc() {
+	
+		return codfilialtc;
+	}
+	
+	public void setCodfilialtc( Integer codfilialtc ) {
+	
+		this.codfilialtc = codfilialtc;
+	}
+	
+	public Integer getCodemptc() {
+	
+		return codemptc;
+	}
+	
+	public void setCodemptc( Integer codemptc ) {
+	
+		this.codemptc = codemptc;
+	}
+	
+	public Short getPrazoentorc() {
+	
+		return prazoentorc;
+	}
+	
+	public void setPrazoentorc( Short prazoentorc ) {
+	
+		this.prazoentorc = prazoentorc;
+	}
+	
+	public String getEmmanut() {
+	
+		return emmanut;
+	}
+	
+	public void setEmmanut( String emmanut ) {
+	
+		this.emmanut = emmanut;
+	}
+	
+	public Integer getCodclcomis() {
+	
+		return codclcomis;
+	}
+	
+	public void setCodclcomis( Integer codclcomis ) {
+	
+		this.codclcomis = codclcomis;
+	}
+	
+	public Short getCodempcm() {
+	
+		return codempcm;
+	}
+	
+	public void setCodempcm( Short codempcm ) {
+	
+		this.codempcm = codempcm;
+	}
+	
+	public Integer getCodfilialcm() {
+	
+		return codfilialcm;
+	}
+	
+	public void setCodfilialcm( Integer codfilialcm ) {
+	
+		this.codfilialcm = codfilialcm;
+	}
+	
+	public Integer getCodemptn() {
+	
+		return codemptn;
+	}
+	
+	public void setCodemptn( Integer codemptn ) {
+	
+		this.codemptn = codemptn;
+	}
+	
+	public Short getCodfilialtn() {
+	
+		return codfilialtn;
+	}
+	
+	public void setCodfilialtn( Short codfilialtn ) {
+	
+		this.codfilialtn = codfilialtn;
+	}
+	
+	public Integer getCodtran() {
+	
+		return codtran;
+	}
+	
+	public void setCodtran( Integer codtran ) {
+	
+		this.codtran = codtran;
+	}
+	
+	public String getTipofrete() {
+	
+		return tipofrete;
+	}
+	
+	public void setTipofrete( String tipofrete ) {
+	
+		this.tipofrete = tipofrete;
+	}
+	
+	public String getAdicfrete() {
+	
+		return adicfrete;
+	}
+	
+	public void setAdicfrete( String adicfrete ) {
+	
+		this.adicfrete = adicfrete;
+	}
+	
+	public BigDecimal getVlrfreteorc() {
+	
+		return vlrfreteorc;
+	}
+	
+	public void setVlrfreteorc( BigDecimal vlrfreteorc ) {
+	
+		this.vlrfreteorc = vlrfreteorc;
+	}
+	
+	public BigDecimal getVlrcomisorc() {
+	
+		return vlrcomisorc;
+	}
+	
+	public void setVlrcomisorc( BigDecimal vlrcomisorc ) {
+	
+		this.vlrcomisorc = vlrcomisorc;
+	}
+	
+	public Integer getCodemptm() {
+	
+		return codemptm;
+	}
+	
+	public void setCodemptm( Integer codemptm ) {
+	
+		this.codemptm = codemptm;
+	}
+	
+	public Short getCodfilialtm() {
+	
+		return codfilialtm;
+	}
+	
+	public void setCodfilialtm( Short codfilialtm ) {
+	
+		this.codfilialtm = codfilialtm;
+	}
+	
+	public Integer getCodtipomov() {
+	
+		return codtipomov;
+	}
+	
+	public void setCodtipomov( Integer codtipomov ) {
+	
+		this.codtipomov = codtipomov;
+	}
+	
+	public Date getDtaprovorc() {
+	
+		return dtaprovorc;
+	}
+	
+	public void setDtaprovorc( Date dtaprovorc ) {
+	
+		this.dtaprovorc = dtaprovorc;
+	}
+	
+	public Date getDtpreventorc() {
+	
+		return dtpreventorc;
+	}
+	
+	public void setDtpreventorc( Date dtpreventorc ) {
+	
+		this.dtpreventorc = dtpreventorc;
+	}
+	
+	public Date getHpreventorc() {
+	
+		return hpreventorc;
+	}
+	
+	public void setHpreventorc( Date hpreventorc ) {
+	
+		this.hpreventorc = hpreventorc;
+	}
+	
+	public String getJustificcancorc() {
+	
+		return justificcancorc;
+	}
+	
+	public void setJustificcancorc( String justificcancorc ) {
+	
+		this.justificcancorc = justificcancorc;
+	}
+	
+	public String getAcorc() {
+	
+		return acorc;
+	}
+	
+	public void setAcorc( String acorc ) {
+	
+		this.acorc = acorc;
+	}
+	
+	public Date getDtins() {
+	
+		return dtins;
+	}
+	
+	public void setDtins( Date dtins ) {
+	
+		this.dtins = dtins;
+	}
+	
+	public Date getHins() {
+	
+		return hins;
+	}
+	
+	public void setHins( Date hins ) {
+	
+		this.hins = hins;
+	}
+	
+	public String getIdusuins() {
+	
+		return idusuins;
+	}
+	
+	public void setIdusuins( String idusuins ) {
+	
+		this.idusuins = idusuins;
+	}
+	
+	public Date getDtalt() {
+	
+		return dtalt;
+	}
+	
+	public void setDtalt( Date dtalt ) {
+	
+		this.dtalt = dtalt;
+	}
+	
+	public Date getHalt() {
+	
+		return halt;
+	}
+	
+	public void setHalt( Date halt ) {
+	
+		this.halt = halt;
+	}
+	
+	public String getIdusualt() {
+	
+		return idusualt;
+	}
+	
+	public void setIdusualt( String idusualt ) {
+	
+		this.idusualt = idusualt;
+	}
+
+}
