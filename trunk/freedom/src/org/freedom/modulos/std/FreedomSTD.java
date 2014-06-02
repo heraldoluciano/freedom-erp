ad1a86cd5ce498875a196d15f2d09c8f1a779b7c
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2014-06-02 20:46:41 +0000

    Início de implementação de relatório de compras por fornecedor/produto.

--- trunk/freedom/src/org/freedom/modulos/std/FreedomSTD.java
+++ trunk/freedom/src/org/freedom/modulos/std/FreedomSTD.java
@@ -169,6 +169,7 @@ import org.freedom.modulos.std.view.frame.report.FRCodficProd;
 import org.freedom.modulos.std.view.frame.report.FRComissoes;
 import org.freedom.modulos.std.view.frame.report.FRCompras;
 import org.freedom.modulos.std.view.frame.report.FRComprasFor;
+import org.freedom.modulos.std.view.frame.report.FRComprasForProd;
 import org.freedom.modulos.std.view.frame.report.FRComprasMedia;
 import org.freedom.modulos.std.view.frame.report.FRConfEstoq;
 import org.freedom.modulos.std.view.frame.report.FRContaEstoque;
@@ -392,6 +393,7 @@ public class FreedomSTD extends AplicativoPD {
 		addSeparador( 200400000 );
 		addOpcao( 200400000, TP_OPCAO_ITEM, "Ordens de compra pendentes", "Ordens de compra pendentes", 's', 200407000, 2, true, FROCEntregaPrevista.class );
 		addOpcao( 200400000, TP_OPCAO_ITEM, "Média de compras por item", "Média de compras por item", 'i', 200408000, 2, true, FRComprasMedia.class );
+		addOpcao( 200400000, TP_OPCAO_ITEM, "Últimas Compras por forn./produto", "Últimas compras por fornecedor/produto", 'd', 200409000, 2, true, FRComprasForProd.class );
 
 		addOpcao( -1, TP_OPCAO_MENU, "Saída", "", 'S', 300000000, 0, false, null );
 		addOpcao( 300000000, TP_OPCAO_ITEM, "Venda", "Venda", 'V', 300100000, 1, true, FVenda.class );
