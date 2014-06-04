8e7e79064395c46a326d6b201bbf9e04fd01c7db
robson <robson@stpinf.com>
2014-06-04 17:14:02 +0000

    Remoção de comentários desnecessários.

--- freedom-ecf/src/org/freedom/ecf/test/ECFElginX5Test.java
+++ freedom-ecf/src/org/freedom/ecf/test/ECFElginX5Test.java
@@ -124,8 +124,7 @@ public class ECFElginX5Test extends TestCase {
 	 * 
 	 * ECFElginX5 ecf = new ECFElginX5( "COM1" );
 	 * 
-	 * System.out.print( "programaCaracterParaAutenticacao > " ); //int [] sesc = {143,137,137,249,0,255,137,137,137,0,143,137,137,249,0,255,129,129 }; int [] sesc = {1,2,4,8,16,32,64,128,64,32,16,8,4,2,1,129,129,129 }; assertTrue( trataresultFuncao(
-	 * ecf.programaCaracterParaAutenticacao( sesc ) ) );
+	 * System.out.print( "programaCaracterParaAutenticacao > " ); 
 	 * 
 	 * testComandosDeCupomFiscal(); System.out.print( "autenticacaoDeDocumento > " ); assertTrue( trataresultFuncao( ecf.autenticacaoDeDocumento() ) ); }
 	 */
