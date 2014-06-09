955cc5324f6f53d31b8a3e08268a19908203481c
heraldo.luciano <heraldo.luciano@gmail.com>
2014-06-09 11:34:27 +0000

    Correção ortográfica no Relatório de Vendas Físico. Colaboração do Sr. Luis Carlos.

--- freedom/src/org/freedom/modulos/std/view/frame/report/FRVendasFisico.java
+++ freedom/src/org/freedom/modulos/std/view/frame/report/FRVendasFisico.java
@@ -161,7 +161,7 @@ public class FRVendasFisico extends FRelatorio {
 		Vector<String> vVals3 = new Vector<String>();
 
 		vLabs3.addElement( "Financeiro" );
-		vLabs3.addElement( "Não Finaceiro" );
+		vLabs3.addElement( "Não Financeiro" );
 		vLabs3.addElement( "Ambos" );
 		vVals3.addElement( "S" );
 		vVals3.addElement( "N" );
