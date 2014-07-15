c5186736d39f728d69e7b87d258af1d10bd3c234
robson@stpinf.com <robson@stpinf.com@70d8741f-8852-0410-9fc6-e1294b66f7bf>
2014-07-15 17:32:23 +0000

    Implementação de visualização de relatórios via leitor de PDF externo.

--- trunk/freedom/src/org/freedom/modulos/grh/view/frame/crud/tabbed/FVaga.java
+++ trunk/freedom/src/org/freedom/modulos/grh/view/frame/crud/tabbed/FVaga.java
@@ -348,7 +348,7 @@ public class FVaga extends FTabDados {
 		}
 		else {
 			try {
-				JasperPrintManager.printReport( dlGr.getRelatorio(), true );
+				dlGr.print(true);
 			} catch ( Exception e ) {
 				e.printStackTrace();
 				Funcoes.mensagemErro( this, "Erro na geração do relátorio!" + e.getMessage(), true, con, e );
